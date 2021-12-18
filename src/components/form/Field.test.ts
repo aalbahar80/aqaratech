8f8dab791ed5e49b0c97075a3e12cb5da75cb0d6
ambiguous48 <33713262+ambiguous48@users.noreply.github.com>
2021-12-18 15:47:10 +0000

    add Field test

---
+++ src/components/form/Field.test.ts
@@ -0,0 +1,51 @@
+import { Field } from './Field';
+// @ponicode
+describe('Field.Field.addLinkField', () => {
+	test('0', () => {
+		const result = Field.addLinkField([
+			new Field({ fieldName: 'id', title: 'ID', editable: false }),
+			new Field({
+				fieldName: 'first_name',
+				title: 'First Name'
+			}),
+			new Field({
+				fieldName: 'last_name',
+				title: 'Last Name'
+			}),
+			new Field({
+				fieldName: 'email',
+				title: 'Email'
+			}),
+			new Field({
+				fieldName: 'phone',
+				title: 'Phone'
+			})
+		]);
+		expect(result).toStrictEqual([
+			new Field({
+				fieldName: 'actions',
+				title: 'Details',
+				editable: false,
+				sortable: false,
+				hideable: true
+			}),
+			new Field({ fieldName: 'id', title: 'ID', editable: false }),
+			new Field({
+				fieldName: 'first_name',
+				title: 'First Name'
+			}),
+			new Field({
+				fieldName: 'last_name',
+				title: 'Last Name'
+			}),
+			new Field({
+				fieldName: 'email',
+				title: 'Email'
+			}),
+			new Field({
+				fieldName: 'phone',
+				title: 'Phone'
+			})
+		]);
+	});
+});
