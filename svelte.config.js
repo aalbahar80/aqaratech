e07876c706ab623ff6e04777514307db0035858f
ambiguous48 <33713262+ambiguous48@users.noreply.github.com>
2021-12-20 04:55:12 +0000

    adapter auto

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,7 @@ import preprocess from 'svelte-preprocess';
 import path from 'path';
 // import vercel from '@sveltejs/adapter-vercel';
 // import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,7 +14,7 @@ const config = {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 		// adapter: vercel(),
-		// adapter: adapter(),
+		adapter: adapter(),
 		vite: {
 			resolve: {
 				alias: {
