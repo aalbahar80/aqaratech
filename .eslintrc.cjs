0e9624f08b57e434df7cc88fb915270fffb155c0
ambiguous48 <33713262+ambiguous48@users.noreply.github.com>
2021-12-24 17:06:47 +0000

    setup eslint presets

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,23 +1,35 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier", "plugin:storybook/recommended"],
-  plugins: ['svelte3', '@typescript-eslint'],
-  ignorePatterns: ['*.cjs'],
-  overrides: [{
-    files: ['*.svelte'],
-    processor: 'svelte3/svelte3'
-  }],
-  settings: {
-    'svelte3/typescript': () => require('typescript')
-  },
-  parserOptions: {
-    sourceType: 'module',
-    ecmaVersion: 2019
-  },
-  env: {
-    browser: true,
-    es2017: true,
-    node: true
-  }
+	root: true,
+	parser: '@typescript-eslint/parser',
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'prettier',
+		'plugin:storybook/recommended',
+    'airbnb-base',
+    'airbnb-typescript/base',
+	],
+	plugins: ['svelte3', '@typescript-eslint'],
+	ignorePatterns: ['*.cjs'],
+	overrides: [
+		{
+			files: ['*.svelte'],
+			processor: 'svelte3/svelte3',
+		},
+	],
+	settings: {
+		'svelte3/typescript': () => require('typescript'),
+	},
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 2019,
+    project: './tsconfig.json',
+    extraFileExtensions: ['.svelte'],
+	},
+	env: {
+		browser: true,
+		es2017: true,
+		node: true,
+	},
 };
