74595b458c9f3699d17a5c7e38dbcb33af8f62b2
ambiguous48 <33713262+ambiguous48@users.noreply.github.com>
2021-12-29 23:10:50 +0000

    pass around user.sub from auth0

--- src/routes/callback.ts
+++ src/routes/callback.ts
@@ -2,11 +2,9 @@
 //  Endpoint for redirection from GitHub after authorization...
 
 const tokenURL = 'https://dev-eehvhdp2.eu.auth0.com/oauth/token';
-const userURL = 'https://api.github.com/user';
+const userURL = 'https://dev-eehvhdp2.eu.auth0.com/userinfo';
 
-const ghAuthURL = 'https://dev-eehvhdp2.eu.auth0.com/authorize';
 const clientId = 'z6oqyOuPLao6XhJeCje9tZ8ZbiJa5zct';
-const scope = 'openid profile';
 const redirectUri = 'http://localhost:3000/callback';
 const secret =
 	'uSR4Gjf3XNN-1kfZGuppDqRdbz7XD6A4o2g8yY1GdZgqCXeYhWhdqfPUoIIJLBRf';
@@ -17,12 +15,17 @@ export async function get(req) {
 	//  ...to get an access_token for the authorized user from GitHub...
 	const tokens = await getTokens(code);
 	//  ...to get the user information from Github
-	// const user = await getUser(accessToken);
+	const usermaybe = await getUser(tokens.access_token);
+	// console.log(usermaybe)
+
 	// this mutates the locals object on the request
 	// and will be read by the hooks/handle function
 	// after the resolve
 	// req.locals.user =  { ...tokens } ;
-    req.locals.user = tokens.id_token 
+	// console.log(tokens);
+	// req.locals.user = tokens.id_token;
+	req.locals.user = usermaybe.sub;
+
 
 	return {
 		status: 302,
@@ -36,7 +39,6 @@ async function getTokens(code) {
 	const r = await fetch(tokenURL, {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
-        // headers: {'content-type': 'application/x-www-form-urlencoded'},
 		body: JSON.stringify({
 			grant_type: 'authorization_code',
 			client_id: clientId,
@@ -45,12 +47,21 @@ async function getTokens(code) {
 			redirect_uri: redirectUri,
 		}),
 	});
+	// console.log(r);
 	const r_1 = await r.json();
-    // console.log('whole reponse:', r_1)
+	// console.log(r_1);
+	// console.log('whole reponse:', r_1)
+	// console.log('access_token is:', r_1.access_token);
+	// console.log('id_token:', r_1.id_token);
 	return r_1;
 }
 
 async function getUser(accessToken) {
-    return accessToken;
-
+	const r = await fetch(userURL, {
+		headers: {
+			Accept: 'application/json',
+			Authorization: `Bearer ${accessToken}`,
+		},
+	});
+	return r.json();
 }
--- src/routes/login.ts
+++ src/routes/login.ts
@@ -3,7 +3,7 @@
 
 const ghAuthURL = 'https://dev-eehvhdp2.eu.auth0.com/authorize';
 const clientId = 'z6oqyOuPLao6XhJeCje9tZ8ZbiJa5zct';
-const scope = 'openid profile'
+const scope = 'openid name picture profile email https://hasura.io/jwt/claims'
 const redirectUri = 'http://localhost:3000/callback'
 
 // import auth from '$lib/services/auth';
@@ -14,7 +14,8 @@ export async function get(req) {
 	return {
 		status: 302,
 		headers: {
-			location: `${ghAuthURL}?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&state=${sessionId}`,
+			// location: `${ghAuthURL}?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&state=${sessionId}`,
+			location: `${ghAuthURL}?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}`,
 		},
 	};
 }
