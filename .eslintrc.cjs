24a98344951d09a202fcfe1373dfcf0f515ee176
ambiguous48 <33713262+ambiguous48@users.noreply.github.com>
2021-12-31 04:06:26 +0000

    turn off most linters

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,33 +2,37 @@ module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
 	extends: [
-		'airbnb-base',
-		'airbnb-typescript/base',
+		// 'airbnb-base',
+		// 'airbnb-typescript/base',
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
 		'plugin:@typescript-eslint/recommended-requiring-type-checking',
-		'plugin:import/recommended',
-		'plugin:import/typescript',
-		'plugin:storybook/recommended',
-		'plugin:jest/recommended',
-		'plugin:jest/style',
-		'prettier', // needs to be last
+		// 'plugin:import/recommended',
+		// 'plugin:import/typescript',
+		// 'plugin:storybook/recommended',
+		// 'plugin:jest/recommended',
+		// 'plugin:jest/style',
+		// 'prettier', // needs to be last
 	],
 	rules: {
 		// indent: 'off',
 		// 'no-tabs': 0,
 		// '@typescript-eslint/indent': [2, 'tab'],
-		'import/no-extraneous-dependencies': [
-			'error',
-			{
-				devDependencies: true,
-			},
-		],
-		'import/prefer-default-export': 'off',
-		'import/no-unresolved': 'error',
-		'no-console': 'off',
+		// 'import/no-extraneous-dependencies': [
+		// 	'error',
+		// 	{
+		// 		devDependencies: true,
+		// 	},
+		// ],
+		// 'import/prefer-default-export': 'off',
+		// 'import/no-unresolved': 'error',
+		// 'no-console': 'off',
 	},
-	plugins: ['svelte3', '@typescript-eslint', 'import'],
+	plugins: [
+		'svelte3',
+		'@typescript-eslint',
+		// 'import',
+	],
 	ignorePatterns: ['*.cjs'],
 	overrides: [
 		{
@@ -56,9 +60,9 @@ module.exports = {
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020,
-		// tsconfigRootDir: __dirname,
-		// project: 'tsconfig.json',
-		// extraFileExtensions: ['.svelte'],
+		tsconfigRootDir: __dirname,
+		project: ['./tsconfig.json'],
+		extraFileExtensions: ['.svelte'],
 	},
 	env: {
 		browser: true,
