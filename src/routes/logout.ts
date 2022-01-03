191fd7bf042f6f58985e7a15fb83999ddb519354
ambiguous48 <33713262+ambiguous48@users.noreply.github.com>
2022-01-03 17:32:55 +0000

    redirect to auth0 logout endpoint

--- src/routes/logout.ts
+++ src/routes/logout.ts
@@ -3,13 +3,26 @@
 import type { RequestHandler } from '@sveltejs/kit';
 
 export const get: RequestHandler = (req) => {
-	//  Sets the user equal to null...
+	//  Sets the user equal to null (local sign out)
 	req.locals.user = null;
-	//  ...and redirect the request back to the home page
+	req.locals.hasura = null;
+
+	// redirect to auth0 logout (global sign out)
+	const BASE_URL = 'https://dev-eehvhdp2.eu.auth0.com/v2/logout';
+	const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+	const redirectUrl = `${req.url.origin}`;
+	const query = new URLSearchParams({
+		client_id: clientId,
+		returnTo: redirectUrl,
+	});
+
+	const logoutUrl = `${BASE_URL}?${query.toString()}`;
+	console.log('🚀 ~ file: logout.ts ~ line 15 ~ logoutUrl', logoutUrl);
+
 	return {
 		status: 302,
 		headers: {
-			location: '/landing',
+			location: logoutUrl,
 		},
 	};
 };
