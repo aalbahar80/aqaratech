4782fd2ff94a1e49934db72916eb2def000304ad
nbe <aleisa.naser@gmail.com>
2022-01-11 22:37:05 +0000

    refactor filter in tablecs

--- src/components/table/TableCS.svelte
+++ src/components/table/TableCS.svelte
@@ -31,34 +31,35 @@
 	let searchTerm = '';
 	$: filter = !searchTerm
 		? {}
-		: fieldList
-				// disregard number fields if search term isn't a number
-				.filter((f) =>
-					parseInt(searchTerm, 10)
-						? f.searchable
-						: f.searchable && f.searchType === 'text',
-				)
-				.map((f) => {
-					// number scalars use _eq operator
-					if (f.searchType === 'number') {
-						return {
-							[f.fieldName]: { _eq: parseInt(searchTerm, 10) },
-						};
-					}
-					// text scalars use %_ilike% operator
-					if (f.searchType === 'text') {
-						return {
-							[f.fieldName]: { _ilike: `%${searchTerm}%` },
-						};
-					}
+		: {
+				_or: fieldList
+					// disregard number fields if search term isn't a number
+					.filter((f) =>
+						parseInt(searchTerm, 10)
+							? f.searchable
+							: f.searchable && f.searchType === 'text',
+					)
+					.map((f) => {
+						// number scalars use _eq operator
+						if (f.searchType === 'number') {
+							return {
+								[f.fieldName]: { _eq: parseInt(searchTerm, 10) },
+							};
+						}
+						// text scalars use %_ilike% operator
+						if (f.searchType === 'text') {
+							return {
+								[f.fieldName]: { _ilike: `%${searchTerm}%` },
+							};
+						}
 
-					console.warn(
-						'Unknown search type. Search type should be handled.',
-						f.searchType,
-					);
-					return {};
-				});
-
+						console.warn(
+							'Unknown search type. Search type should be handled.',
+							f.searchType,
+						);
+						return {};
+					}),
+		  };
 	// SORT
 	type SortInfo = {
 		[key: string]: Order_By;
@@ -70,7 +71,7 @@
 		limit: pageSize,
 		offset: (pageIndex - 1) * pageSize,
 		order_by: sortingInfo,
-		where: { _or: filter },
+		where: filter,
 	};
 	const pageQuery = operationStore(listDoc, queryVars);
 	$: $pageQuery.variables = queryVars;
