95baa0f9eb586128350b948b7673456ee4ac03c5
nbe <aleisa.naser@gmail.com>
2022-01-15 02:47:07 +0000

    remove old lib files

--- src/lib/definitions/ref.ts
+++
@@ -1,6 +0,0 @@
-import clients from './Clients';
-import properties from './Properties';
-import units from './Units';
-
-export { clients, properties, units};
-export default { clients, properties, units};
