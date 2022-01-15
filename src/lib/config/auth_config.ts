298a02ef24c660bd3567609ae6a96dadf91212e6
nbe <aleisa.naser@gmail.com>
2022-01-15 23:54:11 +0000

    hardcode vercel url

--- src/lib/config/auth_config.ts
+++ src/lib/config/auth_config.ts
@@ -9,18 +9,12 @@ const config = {
 export const getRedirectUri = (origin: string) => {
 	// const redirectUri = `${request.url.origin}/auth/callback/`;
 	const prefix = '/auth/callback/';
-	let base = '';
-	if (dev) {
-		base = origin;
-	} else if (process.env.VERCEL && process.env.VERCEL_URL) {
-		base = process.env.VERCEL_URL;
-	} else {
+	let base = origin;
+
+	if (process.env.VERCEL) {
 		base = 'https://svelte-14dec21.vercel.app';
 	}
-	console.log(
-		'🚀 ~ file: auth_config.ts ~ line 23 ~ getRedirectUri ~ `${base}${prefix}`',
-		`${base}${prefix}`,
-	);
+
 	return `${base}${prefix}`;
 };
 
