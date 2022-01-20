89bc79ba0aeb769cccdef83722945ad56106e802
nbe <aleisa.naser@gmail.com>
2022-01-20 13:19:08 +0000

    comment out logflarePinoVercel

--- src/lib/config/logger.ts
+++ src/lib/config/logger.ts
@@ -1,32 +1,33 @@
 import pino from 'pino';
-import { logflarePinoVercel } from 'pino-logflare';
+// import { logflarePinoVercel } from 'pino-logflare';
 
 // https://github.com/Logflare/pino-logflare/blob/master/docs/VERCEL.md
-const { stream, send } = logflarePinoVercel({
-	apiKey: import.meta.env.VITE_PINO_API_KEY,
-	sourceToken: import.meta.env.VITE_PINO_SOURCE_TOKEN,
-});
+// const { stream, send } = logflarePinoVercel({
+// 	apiKey: import.meta.env.VITE_PINO_API_KEY,
+// 	sourceToken: import.meta.env.VITE_PINO_SOURCE_TOKEN,
+// });
 
 export const logger = pino(
 	{
-		browser: {
-			transmit: {
-				level: 'info',
-				send: send,
-			},
-		},
-		level: 'debug',
-		base: {
-			env: process.env.VERCEL_ENV,
-			revision: process.env.VERCEL_GITHUB_COMMIT_SHA,
-		},
-		// transport: {
-		// 	target: 'pino-pretty',
-		// 	options: {
-		// 		ignore: 'pid,hostname',
-		// 		translateTime: true,
+		browser: { asObject: true },
+		// browser: {
+		// 	transmit: {
+		// 		level: 'info',
+		// 		send: send,
 		// 	},
 		// },
+		level: 'debug',
+		// base: {
+		// 	env: process.env.VERCEL_ENV,
+		// 	revision: process.env.VERCEL_GITHUB_COMMIT_SHA,
+		// },
+		transport: {
+			target: 'pino-pretty',
+			options: {
+				ignore: 'pid,hostname',
+				translateTime: true,
+			},
+		},
 	},
-	stream,
+	// stream,
 );
