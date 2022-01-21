26048a77e2801aca6f75391d7fb706f2d5368d77
nbe <aleisa.naser@gmail.com>
2022-01-21 19:37:34 +0000

    working in layout but no color

--- src/lib/config/colorLog.ts
+++ src/lib/config/colorLog.ts
@@ -1,16 +1,19 @@
 import { browser } from '$app/env';
 import { logger } from './logger';
-import colorize from '@pinojs/json-colorizer';
+// import colorize from '@pinojs/json-colorizer';
 
 /**
  * Custom log helper for use in development.
  * Enhances log with color.
  */
-export const f = (file: string, line: number, object: any) => {
+export const f = async (file: string, line: number, object: any) => {
 	const loc = `${file} ~ ${line}`;
 	logger.debug({ loc }, 'trying to colorize');
 	if (browser) {
 		return loc;
+	} else {
+		let colorize;
+		colorize = await import('@pinojs/json-colorizer');
+		return colorize.default({ loc, object }, { pretty: true });
 	}
-	return colorize({ loc, object }, { pretty: true });
 };
