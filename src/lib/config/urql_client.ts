80609479d3446f5bb458b9d33730668d02afd90e
nbe <aleisa.naser@gmail.com>
2022-01-21 20:48:05 +0000

    relocate urql_client to lib/config

---
+++ src/lib/config/urql_client.ts
@@ -0,0 +1,74 @@
+import type { GraphCacheConfig } from '$generated/graphql';
+import { cacheExchange } from '@urql/exchange-graphcache';
+import { createClient, dedupExchange, fetchExchange } from '@urql/svelte';
+import type { IntrospectionQuery } from 'graphql';
+import rawSchema from '../../urql-graphql.schema.json';
+
+const cacheConfig = cacheExchange<GraphCacheConfig>({
+	schema: rawSchema as unknown as IntrospectionQuery,
+	resolvers: {
+		query_root: {
+			clients_by_pk: (parent, args) => ({
+				__typename: 'clients',
+				id: args.id,
+			}),
+			properties_by_pk: (parent, args) => ({
+				__typename: 'properties',
+				id: args.id,
+			}),
+			units_by_pk: (parent, args) => ({ __typename: 'units', id: args.id }),
+			leases_by_pk: (parent, args) => ({
+				__typename: 'leases',
+				id: args.id,
+			}),
+			tenants_by_pk: (parent, args) => ({
+				__typename: 'tenants',
+				id: args.id,
+			}),
+			transactions_by_pk: (parent, args) => ({
+				__typename: 'transactions',
+				id: args.id,
+			}),
+			expenses_by_pk: (parent, args) => ({
+				__typename: 'expenses',
+				id: args.id,
+			}),
+			listings_by_pk: (parent, args) => ({
+				__typename: 'listings',
+				id: args.id,
+			}),
+			maintenance_orders_by_pk: (parent, args) => ({
+				__typename: 'maintenance_orders',
+				id: args.id,
+			}),
+		},
+	},
+	keys: {
+		tenants_aggregate: () => null,
+		tenants_aggregate_fields: () => null,
+		leases_aggregate: () => null,
+		leases_aggregate_fields: () => null,
+		units_aggregate: () => null,
+		units_aggregate_fields: () => null,
+		properties_aggregate: () => null,
+		properties_aggregate_fields: () => null,
+		clients_aggregate: () => null,
+		clients_aggregate_fields: () => null,
+		transactions_aggregate: () => null,
+		transactions_aggregate_fields: () => null,
+	},
+});
+
+export const client = createClient({
+	// Pass in the fetch from sveltekit to have access to serialized requests during hydration
+	fetch,
+	// dev: browser && dev
+	url: '/api/graphql',
+	exchanges: [
+		// devtoolsExchange,
+		dedupExchange,
+		cacheConfig,
+		// ssr,
+		fetchExchange,
+	],
+});
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,28 +1,20 @@
 <script lang="ts" context="module">
 	import Header from '$components/Header.svelte';
 	import ToastParent from '$components/toast/ToastParent.svelte';
-	import type { GraphCacheConfig } from '$generated/graphql';
 	import { logger } from '$lib/config/logger';
 	import type { Load } from '@sveltejs/kit';
-	import { cacheExchange } from '@urql/exchange-graphcache';
 	import type {
 		Client,
 		OperationContext,
 		TypedDocumentNode,
 	} from '@urql/svelte';
-	import {
-		createClient,
-		dedupExchange,
-		fetchExchange,
-		operationStore,
-		setClient,
-	} from '@urql/svelte';
+	import { operationStore, setClient } from '@urql/svelte';
 	import { Column, Content, Grid, Row } from 'carbon-components-svelte';
 	import 'carbon-components-svelte/css/all.css';
-	import type { DocumentNode, IntrospectionQuery } from 'graphql';
+	import type { DocumentNode } from 'graphql';
 	import isEmpty from 'just-is-empty';
+	import { client } from '$lib/config/urql_client';
 	import { get } from 'svelte/store';
-	import rawSchema from '../urql-graphql.schema.json';
 	import '../styles/tailwind.css';
 
 	export const load: Load<CLoad> = ({ fetch, stuff, session }) => {
@@ -35,75 +27,6 @@
 				// headers: { location: '/auth/login' },
 			};
 		}
-
-		const cacheConfig = cacheExchange<GraphCacheConfig>({
-			schema: rawSchema as unknown as IntrospectionQuery,
-			resolvers: {
-				query_root: {
-					clients_by_pk: (parent, args) => ({
-						__typename: 'clients',
-						id: args.id,
-					}),
-					properties_by_pk: (parent, args) => ({
-						__typename: 'properties',
-						id: args.id,
-					}),
-					units_by_pk: (parent, args) => ({ __typename: 'units', id: args.id }),
-					leases_by_pk: (parent, args) => ({
-						__typename: 'leases',
-						id: args.id,
-					}),
-					tenants_by_pk: (parent, args) => ({
-						__typename: 'tenants',
-						id: args.id,
-					}),
-					transactions_by_pk: (parent, args) => ({
-						__typename: 'transactions',
-						id: args.id,
-					}),
-					expenses_by_pk: (parent, args) => ({
-						__typename: 'expenses',
-						id: args.id,
-					}),
-					listings_by_pk: (parent, args) => ({
-						__typename: 'listings',
-						id: args.id,
-					}),
-					maintenance_orders_by_pk: (parent, args) => ({
-						__typename: 'maintenance_orders',
-						id: args.id,
-					}),
-				},
-			},
-			keys: {
-				tenants_aggregate: () => null,
-				tenants_aggregate_fields: () => null,
-				leases_aggregate: () => null,
-				leases_aggregate_fields: () => null,
-				units_aggregate: () => null,
-				units_aggregate_fields: () => null,
-				properties_aggregate: () => null,
-				properties_aggregate_fields: () => null,
-				clients_aggregate: () => null,
-				clients_aggregate_fields: () => null,
-				transactions_aggregate: () => null,
-				transactions_aggregate_fields: () => null,
-			},
-		});
-
-		const client = createClient({
-			// Pass in the fetch from sveltekit to have access to serialized requests during hydration
-			fetch,
-			// dev: browser && dev
-			url: '/api/graphql',
-			exchanges: [
-				// devtoolsExchange,
-				dedupExchange,
-				cacheConfig,
-				// ssr,
-				fetchExchange,
-			],
-		});
 
 		return {
 			stuff: {
