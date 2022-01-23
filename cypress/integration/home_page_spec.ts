1df062476fa8314663dc16c0d4bc3d64490f06e3
nbe <aleisa.naser@gmail.com>
2022-01-24 00:14:57 +0300

    create initial login test

--- cypress/integration/home_page_spec.ts
+++ cypress/integration/home_page_spec.ts
@@ -1,11 +1,29 @@
 describe('The Home Page', () => {
-	it('successfully loads', () => {
-		cy.visit('/');
+	it('Redirects to home page after', () => {
+		cy.visit('/auth/logout');
+		cy.visit('/landing');
 
 		cy.contains('Login').click();
 
 		cy.get('#username').type('dev.tester.3@mailthink.net');
 		cy.get('#password').type('HVuc1C8Ls9CN');
 		cy.contains('Continue').click();
+
+		cy.location('pathname').should('eq', '/');
+
+		cy.getCookie('user');
+
+		it('should not be an empty string', () => {
+			cy.getCookie('user').its('value').should('not.eq', '');
+		});
+
+		// cy.getCookie('user').should('not.be.empty');
+		// cy.getCookie('user').should('have.property', 'value');
+		// cy.getCookie('hasura').should('not.be.empty');
+	});
+	context('after getting signing in', () => {
+		it('have a value property', () => {
+			cy.getCookie('user').should('have.property', 'value');
+		});
 	});
 });
