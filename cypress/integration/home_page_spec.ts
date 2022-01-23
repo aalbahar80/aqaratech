e6e95d88dc85d7b2993489d4642ad69ff95f5cbb
nbe <aleisa.naser@gmail.com>
2022-01-23 21:40:51 +0300

    init cypress test to login

--- cypress/integration/home_page_spec.ts
+++ cypress/integration/home_page_spec.ts
@@ -1,5 +1,11 @@
 describe('The Home Page', () => {
 	it('successfully loads', () => {
 		cy.visit('/');
+
+		cy.contains('Login').click();
+
+		cy.get('#username').type('dev.tester.3@mailthink.net');
+		cy.get('#password').type('HVuc1C8Ls9CN');
+		cy.contains('Continue').click();
 	});
 });
