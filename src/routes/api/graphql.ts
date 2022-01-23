4acb22d8bf0c993b5737a9164f556c40da0ac2a0
nbe <aleisa.naser@gmail.com>
2022-01-24 00:18:29 +0300

    update graphql endpoint to event, remove request

--- src/routes/api/graphql.ts
+++ src/routes/api/graphql.ts
@@ -2,17 +2,18 @@ import { f } from '$lib/config/colorLog';
 import { logger } from '$lib/config/logger';
 import type { RequestHandler } from '@sveltejs/kit';
 
-export const post: RequestHandler = async (request) => {
+export const post: RequestHandler<Locals> = async (event) => {
 	// TODO use env vars for url. dev/stage/prod
 	const res = await fetch('https://hasura-xf70.onrender.com/v1/graphql', {
 		headers: {
 			// 'x-hasura-admin-secret': 'myadminsecret',
-			Authorization: `Bearer ${request.locals.hasura}`,
+			Authorization: `Bearer ${event.locals.hasura}`,
 		},
-		method: request.method,
-		body: request.rawBody,
+		method: event.request.method,
+		body: event.request.body,
 	});
 	logger.debug(f('graphql.ts', 13, { res }));
+
 	const data = await res.json();
 	return {
 		status: res.status,
