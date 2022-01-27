4e8f4bc65b2af01087e0ac58ec7a07b976030ae2
nbe <aleisa.naser@gmail.com>
2022-01-27 22:17:00 +0300

    update login test

--- cypress/integration/login.spec.ts
+++ cypress/integration/login.spec.ts
@@ -1,31 +1,42 @@
-// Cypress.Cookies.debug(true);
+Cypress.Cookies.debug(true);
+const logoutUrl =
+	'https://dev-eehvhdp2.eu.auth0.com/v2/logout?client_id=z6oqyOuPLao6XhJeCje9tZ8ZbiJa5zct&returnTo=http://localhost:3000';
 
 describe('A successful login', () => {
-	beforeEach(() => {
-		cy.viewport('macbook-16');
-		Cypress.Cookies.preserveOnce('user', 'hasura');
-	});
 	before(() => {
 		cy.viewport('macbook-16');
-		cy.visit('/auth/logout');
+		cy.request('/auth/logout');
+		cy.getCookies();
 		cy.visit('/landing');
 		cy.contains('Login').click();
 		cy.get('#username').type('dev.tester.3@mailthink.net');
 		cy.get('#password').type('HVuc1C8Ls9CN');
 		cy.contains('Continue').click();
+		cy.getCookies();
+	});
+	beforeEach(() => {
+		cy.viewport('macbook-16');
+		Cypress.Cookies.preserveOnce('user', 'hasura');
 	});
 
 	it('redirects to home page', () => {
+		cy.getCookies();
 		cy.location('pathname').should('eq', '/');
+		cy.getCookies();
+	});
+
+	it('has the user and hasura cookies', () => {
+		cy.getCookies();
 	});
 
 	describe('persists the user and hasura cookies', () => {
-		afterEach(() => {
-			cy.reload();
-		});
+		// afterEach(() => {
+		// 	cy.reload();
+		// });
 
 		it('upon redirection', () => {
 			['user', 'hasura'].forEach((cookie) => {
+				cy.getCookies();
 				cy.getCookie(cookie)
 					.should('exist')
 					.its('value')
