aa8bab75558c3af9c773c5210bead08857e462ee
nbe <aleisa.naser@gmail.com>
2022-01-27 22:48:23 +0300

    set router=false on landing page

--- src/routes/landing/index.svelte
+++ src/routes/landing/index.svelte
@@ -1,3 +1,7 @@
+<script lang="ts" context="module">
+	export const router = false;
+</script>
+
 <a href="/auth/login" rel="external">Login</a>
 <a href="/auth/logout" rel="external">Logout</a>
 <a href="/tenants">tenants</a>
