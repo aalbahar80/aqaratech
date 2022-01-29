9f953fbf4dce7b399650b35718c5f3220b744eb3
nbe <aleisa.naser@gmail.com>
2022-01-29 11:52:45 +0300

    refactor hero

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,9 +2,8 @@
 	<title>RE Co Admin</title>
 </svelte:head>
 
-<!-- Create Lorem Ipsum Hero using tailwind classes -->
 <div
-	class="grid grid-cols-1 grid-rows-2 h-1/2  w-full fixed  place-items-center p-6 gap-y-6"
+	class="grid grid-cols-1 grid-rows-2 h-full w-full place-items-center mt-12 gap-10"
 >
 	<h1 class="text-7xl foot-bold">Lorem Ipsum</h1>
 	<p class="text-2xl">
