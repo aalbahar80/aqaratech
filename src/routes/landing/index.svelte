0cfafed74c647c87066ea4a1d1c2ec13044f3fc0
nbe <aleisa.naser@gmail.com>
2022-01-29 09:07:38 +0300

    don't prerender landing page

--- src/routes/landing/index.svelte
+++ src/routes/landing/index.svelte
@@ -1,7 +1,3 @@
-<script lang="ts" context="module">
-	export const prerender = true;
-</script>
-
 <a href="/auth/login" rel="external">Login</a>
 <a href="/auth/logout" rel="external">Logout</a>
-<a href="/tenants">tenants</a>
+<a href="/tenants" rel="external">tenants</a>
