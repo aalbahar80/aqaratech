77f07ca2958b51207c27f6babd003bc8e06fbd8d
nbe <aleisa.naser@gmail.com>
2022-01-29 10:13:42 +0300

    center hero

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,14 +2,13 @@
 	<title>RE Co Admin</title>
 </svelte:head>
 
-<div class="hero bg-base-200">
-	<div class="text-center hero-content">
-		<div class="max-w-md">
-			<h1 class="mb-5 text-5xl font-bold">Lorem Ipsum</h1>
-			<p class="mb-5">
-				wvoluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-				quasi. In deleniti eaque aut repudiandae et a id nisi.
-			</p>
-		</div>
-	</div>
+<!-- Create Lorem Ipsum Hero using tailwind classes -->
+<div
+	class="grid grid-cols-1 grid-rows-2 h-1/2  w-full fixed  place-items-center p-6 gap-y-6"
+>
+	<h1 class="text-7xl foot-bold">Lorem Ipsum</h1>
+	<p class="text-2xl">
+		voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem.
+		voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem.
+	</p>
 </div>
