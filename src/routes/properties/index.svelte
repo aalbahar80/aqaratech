7bc3d08ee8bd754bea223c859ea1a2428cc6ca15
nbe <aleisa.naser@gmail.com>
2022-02-03 03:28:27 +0300

    don't prerender /properties

--- src/routes/properties/index.svelte
+++ src/routes/properties/index.svelte
@@ -1,8 +1,3 @@
-<script lang="ts" context="module">
-	// TODO remove prerender
-	export const prerender = true;
-</script>
-
 <script lang="ts">
 	import TableCS from '$components/table/TableCS.svelte';
 	import { fieldList } from '$lib/definitions/Properties';
