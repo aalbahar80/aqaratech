e20e3ea9307f707adc433e7638bfcefcced2dd50
nbe <aleisa.naser@gmail.com>
2022-02-05 07:26:15 +0300

    temp sample prisma endpoints

---
+++ src/routes/sample/[id].svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+
+	export let myId: string;
+	$: console.log($page.params.id);
+	$: id = $page.params.id;
+</script>
+
+<a href={`/sample/${+id + 1}`} sveltekit:prefetch>go next</a>
+<a href={`/sample/111`} sveltekit:prefetch>go next</a>
+<a href={`/sample/222`} sveltekit:prefetch>go next</a>
+<a href={`/sample/333`} sveltekit:prefetch>go next</a>
+<p>{myId}</p>
+
+<button
+	on:click={() => {
+		goto('/sample/' + (+id + 1));
+	}}
+>
+	try but go next</button
+>
