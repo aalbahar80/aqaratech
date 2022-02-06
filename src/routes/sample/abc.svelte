fd60819a816073bef5e8deeaa2443efbc4fd36e5
nbe <aleisa.naser@gmail.com>
2022-02-06 22:26:09 +0300

    sample use:action example

---
+++ src/routes/sample/abc.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	let num = 3;
+	function counter(element: HTMLButtonElement, increment = 5) {
+		let count = 0;
+		updateText();
+
+		function updateText() {
+			element.innerHTML = count.toString();
+		}
+
+		element.addEventListener('click', () => {
+			count += increment;
+			updateText();
+		});
+
+		return {
+			update(newIncrement) {
+				console.log(newIncrement);
+				increment = newIncrement;
+			},
+			destroy() {
+				element.removeEventListener('click', () => {
+					count += increment;
+					updateText();
+				});
+			},
+		};
+	}
+</script>
+
+<h1>Some Heading</h1>
+<h2 class="prose-xl">Some Heading</h2>
+<p>{num}</p>
+
+<!-- <button use:counter={num} on:click={() => num++} /> -->
+<input type="number" bind:value={num} />
+<button use:counter={num} />
+<!-- <button use:counter /> -->
