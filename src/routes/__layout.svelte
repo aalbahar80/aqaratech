2f9ffd86333bf99409acdb47b7da83a294ed5eae
nbe <aleisa.naser@gmail.com>
2022-02-06 22:25:32 +0300

    replace carobn header with twind ui

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,21 +1,17 @@
 <script lang="ts" context="module">
 	import { navigating, session } from '$app/stores';
-	import Header from '$components/Header.svelte';
+	import NavBar from '$components/form/NavBar.svelte';
 	import PreloadingIndicator from '$components/PreloadingIndicator.svelte';
 	import ToastParent from '$components/toast/ToastParent.svelte';
 	import { logger } from '$lib/config/logger';
 	import type { Load } from '@sveltejs/kit';
-	import 'carbon-components-svelte/css/all.css';
 	import LogRocket from 'logrocket';
 	import posthog from 'posthog-js';
 	import { onMount } from 'svelte';
 	import '../styles/tailwind.css';
 	// import PrismaClient from '$lib/config/prisma';
 
-	logger.warn('layout module script tag', '__layout.svelte ~ 20');
-
 	export const load: Load = async ({ fetch, session }) => {
-		logger.warn('load function initial', '__layout.svelte ~ 36');
 		logger.debug(!session.user, '__layout.svelte ~ 27');
 		// if (!session.user) {
 		if (false) {
@@ -25,7 +21,6 @@
 				// maxage: 0,
 			};
 		}
-		logger.warn('load function after user check', '__layout.svelte ~ 45');
 		logger.debug(session.userId, '__layout.svelte ~ 38');
 
 		return {};
@@ -33,9 +28,7 @@
 </script>
 
 <script lang="ts">
-	logger.warn('layout normal script tag', '__layout.svelte ~ 78');
 	onMount(() => {
-		logger.warn('layout on mount', '__layout.svelte ~ 82');
 		if (!window.location.href.includes('localhost')) {
 			// TODO choose one of posthog or logrocket
 			posthog.init('phc_9yCZuf3iVjCaKEH8TDb4sLaN2tg3hnyoIpqRIjGjNiz', {
@@ -55,8 +48,8 @@
 {/if}
 
 <div class="max-h-screen">
-	<Header />
-	<div class="mt-8 mx-4 lg:mx-8">
+	<NavBar />
+	<div class="mx-4 mt-8 lg:mx-8">
 		<ToastParent />
 		<slot />
 	</div>
