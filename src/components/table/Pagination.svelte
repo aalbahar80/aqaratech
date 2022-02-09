6b03a23f8fe9f93394242d1d8c48618bcab20ed2
nbe <aleisa.naser@gmail.com>
2022-02-09 11:56:52 +0300

    init pagination component

---
+++ src/components/table/Pagination.svelte
@@ -0,0 +1,130 @@
+<script lang="ts">
+	import { Icon } from '@steeze-ui/svelte-icon';
+	import { ChevronLeft, ChevronRight } from '@steeze-ui/heroicons';
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+
+	export let totalItems: number;
+	export let pageIndex: number;
+	export let start: number = 1;
+	export let end: number = 12;
+
+	const handlePageChange = (pageNumber: number) => {
+		const params = new URLSearchParams($page.url.searchParams.toString());
+
+		if (pageNumber === 1) {
+			params.delete('page');
+		} else {
+			params.set('page', encodeURIComponent(pageNumber));
+		}
+
+		const url = `${$page.url.pathname}?${params.toString()}`;
+		console.log('manual page change', url);
+		goto(url, {
+			noscroll: true,
+			keepfocus: true,
+		}).catch((err) => {
+			console.error(err);
+		});
+	};
+
+	const nextPage = () => {
+		if (pageIndex < totalItems) {
+			handlePageChange(pageIndex + 1);
+		}
+	};
+	$: console.log('passed in page index is ', pageIndex);
+</script>
+
+<div
+	class="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6"
+>
+	<div class="flex flex-1 justify-between sm:hidden">
+		<a
+			href="#"
+			class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+		>
+			Previous
+		</a>
+		<a
+			href="#"
+			class="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+		>
+			Next
+		</a>
+	</div>
+	<div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+		<div>
+			<p class="text-sm text-gray-700">
+				Showing <span class="font-medium">{start}</span> to
+				<span class="font-medium">{end}</span>
+				of{' '}
+				<span class="font-medium">{totalItems}</span> results
+			</p>
+		</div>
+		<div>
+			<nav
+				class="relative z-0 inline-flex -space-x-px rounded-md shadow-sm"
+				aria-label="Pagination"
+			>
+				<a
+					href="#"
+					class="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50"
+				>
+					<span class="sr-only">Previous</span>
+					<Icon src={ChevronLeft} class="h-5 w-5" aria-hidden="true" />
+				</a>
+				<!-- {/* Current: "z-10 bg-indigo-50 border-indigo-500 text-indigo-600", Default: "bg-white border-gray-300 text-gray-500 hover:bg-gray-50" */} -->
+				<a
+					href="#"
+					aria-current="page"
+					class="relative z-10 inline-flex items-center border border-indigo-500 bg-indigo-50 px-4 py-2 text-sm font-medium text-indigo-600"
+				>
+					1
+				</a>
+				<a
+					href="#"
+					class="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50"
+				>
+					2
+				</a>
+				<a
+					href="#"
+					class="relative hidden items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 md:inline-flex"
+				>
+					3
+				</a>
+				<span
+					class="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700"
+				>
+					...
+				</span>
+				<a
+					href="#"
+					class="relative hidden items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 md:inline-flex"
+				>
+					8
+				</a>
+				<a
+					href="#"
+					class="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50"
+				>
+					9
+				</a>
+				<a
+					href="#"
+					class="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50"
+				>
+					10
+				</a>
+				<button
+					class="relative inline-flex items-center rounded-r-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50"
+					on:click={() => handlePageChange(2)}
+				>
+					<span class="sr-only">Next</span>
+					<Icon src={ChevronRight} class="h-5 w-5" aria-hidden="true" />
+				</button>
+			</nav>
+		</div>
+	</div>
+</div>
