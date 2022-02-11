1d18395954fd7ac79ec5bd4330ad4ed2a0b00af2
nbe <aleisa.naser@gmail.com>
2022-02-11 10:56:35 +0300

    add client endpoint

--- src/lib/definitions/client.ts
+++ src/lib/definitions/client.ts
@@ -1,11 +1,28 @@
 import { z } from 'zod';
+import { Prisma } from '@prisma/client';
 
-const entityData = {};
-const defaultForm = {};
+export type ClientData = Prisma.ClientGetPayload<typeof entityData>;
+export const entityData = Prisma.validator<Prisma.ClientArgs>()({
+	select: {
+		id: true,
+		firstName: true,
+		lastName: true,
+		email: true,
+		phone: true,
+		civilid: true,
+	},
+});
 
+export const defaultForm: Omit<ClientData, 'id'> = {
+	firstName: null,
+	lastName: null,
+	phone: null,
+	email: null,
+	civilid: null,
+};
 export const formSchema = z.object({
-	first_name: z.string().min(1, { message: 'Required' }),
-	last_name: z.string().min(1, { message: 'Required' }),
+	firstName: z.string().min(1, { message: 'Required' }),
+	lastName: z.string().min(1, { message: 'Required' }),
 	email: z.string().email(),
 	phone: z.string().min(8).and(z.string().max(8)).or(z.literal('')),
 });
---
+++ src/routes/client/index.json.ts
@@ -0,0 +1,66 @@
+import prisma from '$lib/config/prisma';
+import { formSchema, entityData } from '$lib/definitions/client';
+import { parseParams } from '$lib/utils/table-utils';
+import type { Prisma } from '@prisma/client';
+import type { RequestHandler } from '@sveltejs/kit/types/endpoint';
+
+export const get: RequestHandler<{ rows: any[] }> = async ({ url }) => {
+	const {
+		options: { search, skip, sortDir, sortKey },
+	} = parseParams(url);
+
+	const data = await prisma.client.findMany({
+		take: 10,
+		skip,
+		orderBy: { [sortKey]: sortDir as Prisma.SortOrder },
+		where: {
+			OR: [
+				{ firstName: { contains: search } },
+				{ secondName: { contains: search } },
+				{ thirdName: { contains: search } },
+				{ lastName: { contains: search } },
+				{ email: { contains: search } },
+				{ phone: { contains: search } },
+				{ civilid: { contains: search } },
+			],
+		},
+		select: entityData.select,
+	});
+
+	// This is a hack to avoid the date to be reformatted during hydration
+	// which causes fluttering
+	const rows = JSON.parse(JSON.stringify(data));
+	return {
+		body: {
+			rows,
+		},
+	};
+};
+
+export const post: RequestHandler = async (event) => {
+	type Created = Omit<Prisma.ClientCreateArgs['data'], 'id'>;
+	const data: Created = await event.request.json();
+
+	try {
+		formSchema.parse(data);
+		if (data.dob) {
+			data.dob = new Date(data.dob);
+		}
+		const created = await prisma.client.create({
+			data,
+			select: entityData.select,
+		});
+		return {
+			status: 200,
+			body: created,
+		};
+	} catch (error: any) {
+		console.error(error);
+		return {
+			status: 400,
+			body: {
+				error: error.message,
+			},
+		};
+	}
+};
