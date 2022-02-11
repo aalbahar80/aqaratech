1c690e47f022c4ce03cd70d0f04326c412eb8a1f
nbe <aleisa.naser@gmail.com>
2022-02-11 05:16:42 +0300

    remove examples

--- src/routes/sample/noaction.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	let num = 3;
-	let increment = 5;
-</script>
-
-<p class="mt-20" />
-
-<input type="number" bind:value={increment} />
-<button on:click={() => (num += increment)}>{num}</button>
