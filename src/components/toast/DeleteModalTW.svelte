7b4f7b0704b5ed976be685f2d680aa57daba9286
nbe <aleisa.naser@gmail.com>
2022-02-13 17:10:40 +0300

    init tailwind Delete Modal

---
+++ src/components/toast/DeleteModalTW.svelte
@@ -0,0 +1,113 @@
+<script lang="ts">
+	import {
+		Dialog,
+		DialogOverlay,
+		DialogTitle,
+		Transition,
+		TransitionChild,
+	} from '@rgossiaux/svelte-headlessui';
+	import { Exclamation } from '@steeze-ui/heroicons';
+	import { Icon } from '@steeze-ui/svelte-icon';
+
+	export let isOpen: boolean;
+
+	const close = () => {
+		isOpen = false;
+	};
+
+	let cancelButton: HTMLButtonElement;
+</script>
+
+<Transition show={isOpen} as="div">
+	<Dialog
+		as="div"
+		class="fixed inset-0 z-10 overflow-y-auto"
+		initialFocus={cancelButton}
+		on:close={close}
+	>
+		<div
+			class="flex min-h-screen items-end justify-center px-4 pt-4 pb-20 text-center sm:block sm:p-0"
+		>
+			<TransitionChild
+				as="div"
+				enter="ease-out duration-300"
+				enterFrom="opacity-0"
+				enterTo="opacity-100"
+				leave="ease-in duration-200"
+				leaveFrom="opacity-100"
+				leaveTo="opacity-0"
+			>
+				<DialogOverlay
+					class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+				/>
+			</TransitionChild>
+
+			<!-- {/* This element is to trick the browser into centering the modal contents. */} -->
+			<TransitionChild
+				as="div"
+				enter="ease-out duration-300"
+				enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+				enterTo="opacity-100 translate-y-0 sm:scale-100"
+				leave="ease-in duration-200"
+				leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+				leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+			>
+				<span
+					class="hidden sm:inline-block sm:h-screen sm:align-middle"
+					aria-hidden="true"
+				>
+					&#8203;
+				</span>
+				<div
+					class="inline-block transform overflow-hidden rounded-lg bg-white text-left align-bottom shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:align-middle"
+				>
+					<div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+						<div class="sm:flex sm:items-start">
+							<div
+								class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10"
+							>
+								<Icon
+									src={Exclamation}
+									class="h-6 w-6 text-red-600"
+									aria-hidden="true"
+								/>
+							</div>
+							<div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+								<DialogTitle
+									as="h3"
+									class="text-lg font-medium leading-6 text-gray-900"
+								>
+									Deactivate account
+								</DialogTitle>
+								<div class="mt-2">
+									<p class="text-sm text-gray-500">
+										Are you sure you want to deactivate your account? All of
+										your data will be permanently removed. This action cannot be
+										undone.
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+						<button
+							type="button"
+							class="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+							on:click={close}
+						>
+							Deactivate
+						</button>
+						<button
+							type="button"
+							class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+							on:click={close}
+							bind:this={cancelButton}
+						>
+							Cancel
+						</button>
+					</div>
+				</div>
+			</TransitionChild>
+		</div>
+	</Dialog>
+</Transition>
