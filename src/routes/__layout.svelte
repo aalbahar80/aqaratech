0879b1feeccdcde90e10330ec29897cf33e64173
nbe <aleisa.naser@gmail.com>
2022-02-13 23:30:51 +0300

    tidy up layout.svelte

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,19 +1,16 @@
 <script lang="ts" context="module">
-	import { navigating, session } from '$app/stores';
 	import NavBar from '$components/form/NavBar.svelte';
 	import PreloadingIndicator from '$components/PreloadingIndicator.svelte';
 	import ToastParent from '$components/toast/ToastParent.svelte';
-	import { logger } from '$lib/config/logger';
 	import type { Load } from '@sveltejs/kit';
 	import LogRocket from 'logrocket';
 	import posthog from 'posthog-js';
 	import { onMount } from 'svelte';
 	import '../styles/tailwind.css';
-	// import PrismaClient from '$lib/config/prisma';
 
-	export const load: Load = async ({ fetch, session }) => {
-		logger.debug(!session.user, '__layout.svelte ~ 27');
+	export const load: Load = () => {
 		// if (!session.user) {
+		// eslint-disable-next-line no-constant-condition
 		if (false) {
 			return {
 				status: 302,
@@ -21,13 +18,14 @@
 				// maxage: 0,
 			};
 		}
-		logger.debug(session.userId, '__layout.svelte ~ 38');
 
 		return {};
 	};
 </script>
 
 <script lang="ts">
+	import { navigating, session } from '$app/stores';
+
 	onMount(() => {
 		if (!window.location.href.includes('localhost')) {
 			// TODO choose one of posthog or logrocket
