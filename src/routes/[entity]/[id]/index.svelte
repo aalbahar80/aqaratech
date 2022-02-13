df1ad3c0edceb1d950252aa930b35ce059b10321
nbe <aleisa.naser@gmail.com>
2022-02-13 16:29:29 +0300

    init basic detail page

---
+++ src/routes/[entity]/[id]/index.svelte
@@ -0,0 +1,20 @@
+<script context="module" lang="ts">
+	import type { Load } from '@sveltejs/kit';
+
+	export const load: Load = async ({ fetch, params }) => {
+		const { entity, id } = params;
+		const res = await fetch(`/${entity}/${id}.json`);
+		const data = await res.json();
+		return {
+			props: { data },
+		};
+	};
+</script>
+
+<script lang="ts">
+	export let data: any;
+</script>
+
+<pre>
+  {JSON.stringify(data)}
+</pre>
