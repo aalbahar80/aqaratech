3ee3cd891ebfa03e21bd5db9cb3ef4102ed216bd
nbe <aleisa.naser@gmail.com>
2022-02-15 21:32:43 +0300

    add z-index to dropdown menu

--- src/components/DropDown.svelte
+++ src/components/DropDown.svelte
@@ -30,7 +30,7 @@
 		leaveTo="transform opacity-0 scale-95"
 	>
 		<MenuItems
-			class="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+			class="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
 			><div class="py-1">
 				<MenuItem let:active>
 					<button
