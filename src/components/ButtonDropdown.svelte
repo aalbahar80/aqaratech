3c4c9ec386e47d091bce0a0dd5bb9c23140403ca
nbe <aleisa.naser@gmail.com>
2022-02-16 12:20:37 +0300

    bottom-10 was the only thing needed??

--- src/components/ButtonDropdown.svelte
+++ src/components/ButtonDropdown.svelte
@@ -16,14 +16,14 @@
 	];
 </script>
 
-<span class="relative inline-flex rounded-md shadow-sm">
+<span class="relative z-0 inline-flex rounded-md shadow-sm">
 	<button
 		type="button"
 		class="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
 	>
 		Save changes
 	</button>
-	<Menu as="div" class="relative -ml-px block">
+	<Menu as="span" class="relative -ml-px block">
 		<MenuButton
 			class="relative inline-flex items-center rounded-r-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
 		>
@@ -40,11 +40,11 @@
 			leaveTo="transform opacity-0 scale-95"
 		>
 			<MenuItems
-				class="absolute right-0 bottom-10 z-10 mt-2 -mr-1 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+				class="absolute right-0 bottom-10 mt-2 -mr-1 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
 			>
 				<div class="py-1">
 					{#each items as item (item.name)}
-						<MenuItem let:active as="button" class="w-full">
+						<MenuItem let:active as="button" class="w-full text-left">
 							<div
 								class={`${
 									active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'
