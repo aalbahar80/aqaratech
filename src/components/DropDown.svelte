b8165846783ef1a7326643735c76c3f447851961
nbe <aleisa.naser@gmail.com>
2022-02-16 03:06:51 +0300

    finalize style of dropdown button w icons

--- src/components/DropDown.svelte
+++ src/components/DropDown.svelte
@@ -6,8 +6,33 @@
 		MenuItems,
 		Transition,
 	} from '@rgossiaux/svelte-headlessui';
-	import { DotsVertical, PencilAlt } from '@steeze-ui/heroicons';
+	import {
+		Check,
+		ClipboardCopy,
+		DotsVertical,
+		PencilAlt,
+		Speakerphone,
+	} from '@steeze-ui/heroicons';
 	import { Icon } from '@steeze-ui/svelte-icon';
+
+	const items = [
+		{
+			icon: PencilAlt,
+			label: 'Edit',
+		},
+		{
+			icon: ClipboardCopy,
+			label: 'Copy URL',
+		},
+		{
+			icon: Speakerphone,
+			label: 'Send reminder',
+		},
+		{
+			icon: Check,
+			label: 'Mark as paid',
+		},
+	];
 </script>
 
 <Menu as="div" class="relative inline-block text-left">
@@ -32,76 +57,32 @@
 		<MenuItems
 			class="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
 			><div class="py-1">
-				<MenuItem let:active>
-					<a
-						class="group flex items-center px-4 py-2 text-sm"
-						class:bg-gray-100={active}
-						class:text-gray-900={active}
-						class:text-gray-700={!active}
+				{#each items as item}
+					<MenuItem
+						let:active
+						as="button"
+						class="w-full"
+						on:click={(e) => {
+							console.log(e);
+							console.log(`clicked ${item.label}`);
+						}}
 					>
-						<Icon
-							src={PencilAlt}
-							class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
-							aria-hidden="true"
-						/>
-						Edit
-					</a>
-				</MenuItem>
-				<MenuItem let:active as="div">
-					<button
-						class="group flex w-full items-center px-4 py-2 text-sm"
-						class:bg-gray-100={active}
-						class:text-gray-900={active}
-						class:text-gray-700={!active}
-					>
-						<Icon
-							src={PencilAlt}
-							class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
-							aria-hidden="true"
-						/>
-						Edit
-					</button>
-				</MenuItem>
-				<!-- The 2 menu items below is how it should be done. This prevents an extra unnecessary tag. -->
-				<MenuItem
-					let:active
-					as="button"
-					class="w-full"
-					on:click={() => {
-						console.log('clicked');
-					}}
-				>
-					<div
-						class="flex w-full items-center px-4 py-2 text-sm"
-						class:bg-gray-100={active}
-						class:text-gray-900={active}
-						class:text-gray-700={!active}
-					>
-						Edit
-					</div>
-				</MenuItem>
-				<MenuItem
-					let:active
-					as="button"
-					class="w-full"
-					on:click={() => {
-						console.log('clicked');
-					}}
-				>
-					<div
-						class="group flex items-center px-4 py-2 text-sm"
-						class:bg-gray-100={active}
-						class:text-gray-900={active}
-						class:text-gray-700={!active}
-					>
-						<Icon
-							src={PencilAlt}
-							class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
-							aria-hidden="true"
-						/>
-						Edit
-					</div>
-				</MenuItem>
+						<div
+							class="group flex items-center px-4 py-2 text-sm"
+							class:bg-gray-100={active}
+							class:text-gray-900={active}
+							class:text-gray-700={!active}
+						>
+							<Icon
+								src={item.icon}
+								class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+								aria-hidden="true"
+								theme="solid"
+							/>
+							{item.label}
+						</div>
+					</MenuItem>
+				{/each}
 			</div>
 		</MenuItems>
 	</Transition>
