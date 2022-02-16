ab34e63813e48aa15d558b7eae1a7e531b572b6c
nbe <aleisa.naser@gmail.com>
2022-02-16 05:21:41 +0300

    init leases card in tenant detail page

---
+++ src/components/LeasesCard.svelte
@@ -0,0 +1,81 @@
+<script lang="ts">
+	import { formatDateDiff } from '$lib/utils/date-utils';
+	import { Calendar, Home } from '@steeze-ui/heroicons';
+	import { Icon } from '@steeze-ui/svelte-icon';
+
+	export let leases: any[];
+</script>
+
+<!-- <pre>{JSON.stringify(leases[0], null, 2)}</pre> -->
+
+<div class="mt-4 overflow-hidden bg-white shadow sm:rounded-md">
+	<div class="border-b border-gray-200 bg-white px-4 py-5 sm:px-6">
+		<div
+			class="-ml-4 -mt-2 flex flex-wrap items-center justify-between sm:flex-nowrap"
+		>
+			<div class="ml-4 mt-2">
+				<h3 class="text-lg font-medium leading-6 text-gray-900">Leases</h3>
+			</div>
+			<div class="ml-4 mt-2 flex-shrink-0">
+				<button
+					type="button"
+					class="relative inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+				>
+					Create new lease
+				</button>
+			</div>
+		</div>
+	</div>
+
+	<ul class="divide-y divide-gray-200">
+		{#each leases as lease, idx (lease.id)}
+			<!-- {@const expired = daysLeftFromISO(lease.endDate) < 0} -->
+			{@const expired = idx % 2 === 0}
+			{@const expiredClass = expired
+				? 'text-red-800 bg-red-100'
+				: 'text-green-800 bg-green-100'}
+			<li>
+				<a href={`/lease/${lease.id}`} class="block hover:bg-gray-50">
+					<div class="px-4 py-4 sm:px-6">
+						<div class="flex items-center justify-between">
+							<p class="truncate text-sm font-medium text-indigo-600">
+								# {idx}
+							</p>
+							<div class="ml-2 flex flex-shrink-0">
+								<p
+									class={`${expiredClass} inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5`}
+								>
+									{expired ? 'Expired' : 'Active'}
+								</p>
+							</div>
+						</div>
+						<div class="mt-2 sm:flex sm:justify-between">
+							<div class="sm:flex">
+								<p class="flex items-center text-sm text-gray-500">
+									<Icon
+										src={Home}
+										class="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
+										aria-hidden="true"
+									/>
+									{lease.unit?.unitNumber}
+								</p>
+							</div>
+							<div class="mt-2 flex items-center text-sm text-gray-500 sm:mt-0">
+								<Icon
+									src={Calendar}
+									class="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
+									aria-hidden="true"
+								/>
+								<p>
+									Expiry: <time dateTime={lease.endDate}
+										>{formatDateDiff(lease.endDate).fullText}</time
+									>
+								</p>
+							</div>
+						</div>
+					</div>
+				</a>
+			</li>
+		{/each}
+	</ul>
+</div>
--- src/routes/tenant/[id].svelte
+++ src/routes/tenant/[id].svelte
@@ -10,146 +10,147 @@
 
 	export const load: Load = async ({ fetch }) => {
 		const res = await fetch('/transaction.json');
+		const leases = await fetch('/lease.json')
+			.then((res) => res.json())
+			.then((data) => data.rows);
 		const data = await res.json();
 
 		return {
 			props: {
 				transactions: data.rows,
+				leases,
 			},
 		};
 	};
 </script>
 
 <script lang="ts">
+	import LeasesCard from '$components/LeasesCard.svelte';
+
 	export let transactions: Jsonify<TransactionData[]>;
+	export let leases;
 </script>
 
-<div class="sm:block">
-	<div class="mx-auto max-w-6xl px-4 sm:px-6 lg:px-8">
-		<div class="mt-2 flex flex-col">
-			<div
-				class="min-w-full overflow-hidden overflow-x-auto align-middle shadow sm:rounded-lg"
-			>
-				<table class="min-w-full divide-y divide-gray-200">
-					<thead>
-						<tr>
-							<th
-								class="bg-gray-50 px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
+<div class="mx-auto max-w-6xl px-4 sm:px-6 lg:px-8">
+	<LeasesCard {leases} />
+	<div class="mt-2 flex flex-col">
+		<div
+			class="min-w-full overflow-hidden overflow-x-auto align-middle shadow sm:rounded-lg"
+		>
+			<table class="min-w-full divide-y divide-gray-200">
+				<thead>
+					<tr>
+						<th
+							class="bg-gray-50 px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
+						>
+							Transaction
+						</th>
+						<th
+							class="bg-gray-50 px-6 py-3 text-right text-xs font-medium uppercase tracking-wider text-gray-500"
+						>
+							Amount
+						</th>
+						<th
+							class="bg-gray-50 px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500 md:block"
+						>
+							Status
+						</th>
+						<th
+							class="bg-gray-50 px-6 py-3 text-right text-xs font-medium uppercase tracking-wider text-gray-500"
+						>
+							Date
+						</th>
+						<th
+							class="bg-gray-50 px-6 py-3 text-right text-xs font-medium uppercase tracking-wider text-gray-500 "
+						>
+							Action
+						</th>
+					</tr>
+				</thead>
+				<tbody class="divide-y divide-gray-200 bg-white">
+					{#each transactions as transaction (transaction.id)}
+						<tr class="bg-white">
+							<td
+								class="w-full max-w-0 whitespace-nowrap px-6 py-4 text-sm text-gray-900"
 							>
-								Transaction
-							</th>
-							<th
-								class="bg-gray-50 px-6 py-3 text-right text-xs font-medium uppercase tracking-wider text-gray-500"
+								<div class="flex">
+									<a
+										href={`/transaction/${transaction.id}`}
+										class="group inline-flex space-x-2 truncate text-sm"
+									>
+										<Icon
+											src={Cash}
+											theme="solid"
+											class="h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+											aria-hidden="true"
+										/>
+										<p class="truncate text-gray-500 group-hover:text-gray-900">
+											{transaction.memo}
+										</p>
+									</a>
+								</div>
+							</td>
+							<td
+								class="whitespace-nowrap px-6 py-4 text-right text-sm text-gray-500"
 							>
-								Amount
-							</th>
-							<th
-								class="bg-gray-50 px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500 md:block"
+								<span class="font-medium tabular-nums text-gray-900"
+									>{transaction.amount}
+								</span>
+								{'KWD'}
+							</td>
+							<td
+								class="whitespace-nowrap px-6 py-4 text-sm text-gray-500 md:block"
 							>
-								Status
-							</th>
-							<th
-								class="bg-gray-50 px-6 py-3 text-right text-xs font-medium uppercase tracking-wider text-gray-500"
+								<span
+									class="inline-flex items-center rounded-full px-2.5 py-0.5 text-xs font-medium capitalize"
+									class:badge__isPaid--true={transaction?.isPaid ?? false}
+									class:badge__isPaid--false={!transaction?.isPaid ?? false}
+								>
+									{transaction.isPaid}
+								</span>
+							</td>
+							<td
+								class="whitespace-nowrap px-6 py-4 text-right text-sm text-gray-500"
 							>
-								Date
-							</th>
-							<th
-								class="bg-gray-50 px-6 py-3 text-right text-xs font-medium uppercase tracking-wider text-gray-500 "
-							>
-								Action
-							</th>
+								<time dateTime={transaction.dueDate}
+									>{format(parseISO(transaction.createdAt), 'MMM dd, yy')}</time
+								>
+							</td>
+							<td class="text-center">
+								<DropDown />
+							</td>
 						</tr>
-					</thead>
-					<tbody class="divide-y divide-gray-200 bg-white">
-						{#each transactions as transaction (transaction.id)}
-							<tr class="bg-white">
-								<td
-									class="w-full max-w-0 whitespace-nowrap px-6 py-4 text-sm text-gray-900"
-								>
-									<div class="flex">
-										<a
-											href={`/transaction/${transaction.id}`}
-											class="group inline-flex space-x-2 truncate text-sm"
-										>
-											<Icon
-												src={Cash}
-												theme="solid"
-												class="h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
-												aria-hidden="true"
-											/>
-											<p
-												class="truncate text-gray-500 group-hover:text-gray-900"
-											>
-												{transaction.memo}
-											</p>
-										</a>
-									</div>
-								</td>
-								<td
-									class="whitespace-nowrap px-6 py-4 text-right text-sm text-gray-500"
-								>
-									<span class="font-medium tabular-nums text-gray-900"
-										>{transaction.amount}
-									</span>
-									{'KWD'}
-								</td>
-								<td
-									class="whitespace-nowrap px-6 py-4 text-sm text-gray-500 md:block"
-								>
-									<span
-										class="inline-flex items-center rounded-full px-2.5 py-0.5 text-xs font-medium capitalize"
-										class:badge__isPaid--true={transaction?.isPaid ?? false}
-										class:badge__isPaid--false={!transaction?.isPaid ?? false}
-									>
-										{transaction.isPaid}
-									</span>
-								</td>
-								<td
-									class="whitespace-nowrap px-6 py-4 text-right text-sm text-gray-500"
-								>
-									<time dateTime={transaction.dueDate}
-										>{format(
-											parseISO(transaction.createdAt),
-											'MMM dd, yy',
-										)}</time
-									>
-								</td>
-								<td class="text-center">
-									<DropDown />
-								</td>
-							</tr>
-						{/each}
-					</tbody>
-				</table>
-				<!-- {/* Pagination */} -->
-				<nav
-					class="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6"
-					aria-label="Pagination"
-				>
-					<div class="hidden sm:block">
-						<p class="text-sm text-gray-700">
-							Showing <span class="font-medium">1</span> to
-							<span class="font-medium">10</span>
-							of{' '}
-							<span class="font-medium">20</span> results
-						</p>
-					</div>
-					<div class="flex flex-1 justify-between sm:justify-end">
-						<button
-							href="#"
-							class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-						>
-							Previous
-						</button>
-						<button
-							href="#"
-							class="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-						>
-							Next
-						</button>
-					</div>
-				</nav>
-			</div>
+					{/each}
+				</tbody>
+			</table>
+			<!-- {/* Pagination */} -->
+			<nav
+				class="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6"
+				aria-label="Pagination"
+			>
+				<div class="hidden sm:block">
+					<p class="text-sm text-gray-700">
+						Showing <span class="font-medium">1</span> to
+						<span class="font-medium">10</span>
+						of{' '}
+						<span class="font-medium">20</span> results
+					</p>
+				</div>
+				<div class="flex flex-1 justify-between sm:justify-end">
+					<button
+						href="#"
+						class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+					>
+						Previous
+					</button>
+					<button
+						href="#"
+						class="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+					>
+						Next
+					</button>
+				</div>
+			</nav>
 		</div>
 	</div>
 </div>
