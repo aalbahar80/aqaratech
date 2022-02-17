38b760c0ec93cf20a6895148ef8c8a599c956e53
nbe <aleisa.naser@gmail.com>
2022-02-18 00:21:34 +0300

    increase button padding

--- src/components/ButtonDropdown.svelte
+++ src/components/ButtonDropdown.svelte
@@ -18,7 +18,7 @@
 <span class="relative z-0 inline-flex rounded-md shadow-sm">
 	<button
 		type="button"
-		class="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
+		class="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-8 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
 	>
 		View
 	</button>
