aa0439f083926b4c7e35059ae7e4a5ff86061934
nbe <aleisa.naser@gmail.com>
2022-02-18 04:01:46 +0300

    extract dropdown menu into component

---
+++ src/components/DropdownMenu.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import {
+		MenuItem,
+		MenuItems,
+		Transition,
+	} from '@rgossiaux/svelte-headlessui';
+	import { Icon } from '@steeze-ui/svelte-icon';
+	import type { IconSource } from '@steeze-ui/svelte-icon/types';
+
+	type MenuOption = {
+		icon: IconSource;
+		label: string;
+		onClick?: () => void;
+	};
+	export let items: MenuOption[];
+</script>
+
+<Transition
+	as="div"
+	enter="transition ease-out duration-100"
+	enterFrom="transform opacity-0 scale-95"
+	enterTo="transform opacity-100 scale-100"
+	leave="transition ease-in duration-75"
+	leaveFrom="transform opacity-100 scale-100"
+	leaveTo="transform opacity-0 scale-95"
+>
+	<MenuItems
+		class={`${
+			$$props.class ?? 'bottom-10'
+		} absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`}
+		><div class="py-1">
+			{#each items as item (item.label)}
+				<MenuItem
+					let:active
+					as="button"
+					class="w-full"
+					on:click={(e) => {
+						console.log(e);
+						console.log(`clicked ${item.label}`);
+					}}
+				>
+					<div
+						class="group flex items-center px-4 py-2 text-sm"
+						class:bg-gray-100={active}
+						class:text-gray-900={active}
+						class:text-gray-700={!active}
+					>
+						<Icon
+							src={item.icon}
+							class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+							aria-hidden="true"
+							theme="solid"
+						/>
+						{item.label}
+					</div>
+				</MenuItem>
+			{/each}
+		</div>
+	</MenuItems>
+</Transition>
