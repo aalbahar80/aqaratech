25d9429cf1367a1c3a189a95da3ebf9cd59c5fb5
nbe <aleisa.naser@gmail.com>
2022-02-20 15:04:03 +0300

    delete unused utils

--- src/lib/utils/table-utils.ts
+++ src/lib/utils/table-utils.ts
@@ -4,58 +4,7 @@ export default function assertNever(value: any, message?: string): never {
 	// eslint-disable-next-line @typescript-eslint/restrict-template-expressions
 	throw new Error(message || `Unexpected value: ${value}`);
 }
-export const parseParams = (url: URL) => {
-	const pageSize = Number(url.searchParams.get('pageSize')) || 11;
-	const pageIndex = Number(url.searchParams.get('p')) || 1;
-	const search = url.searchParams.get('q') || '';
-	const skip = (pageIndex - 1) * pageSize;
-	const sortDir = url.searchParams.get('sortDir') || 'desc';
-	const sortKey = url.searchParams.get('sortKey') || 'updatedAt';
 
-	return {
-		options: { pageSize, pageIndex, search, skip, sortDir, sortKey },
-		queryString: `?p=${pageIndex}&pageSize=${pageSize}&q=${search}&sortDir=${sortDir}&sortKey=${sortKey}`,
-	};
-};
-
-/**
- * Removes a search param if default.
- */
-// const removeDefault = (
-// 	params: URLSearchParams,
-// 	key: string,
-// 	defaultValue: string,
-// ) => {
-// 	if (params.get(key) === defaultValue) {
-// 		params.delete(key);
-// 	}
-// 	return params;
-// };
-
-/**
- * Construct a URL's searchParams.
- */
-export const handleParams = ({
-	pageIndex,
-	search,
-	sortKey,
-	sortDir,
-	pageSize,
-}: {
-	pageIndex: number;
-	search: string;
-	sortKey: string;
-	sortDir: string;
-	pageSize: number;
-}): URLSearchParams => {
-	const params = new URLSearchParams();
-	params.set('page', pageIndex.toString());
-	params.set('search', search);
-	params.set('sortKey', sortKey);
-	params.set('sortDir', sortDir);
-	params.set('pageSize', pageSize.toString());
-	return params;
-};
 interface TableOptions {
 	p?: string;
 	pageSize?: string;
@@ -71,11 +20,6 @@ export const getTableUrl = (url: URL, options: TableOptions): string => {
 			params.set(option[0], option[1]);
 		}
 	});
-	// loop through params and remove default values
-	// params.forEach((value, key) => {
-	// 	removeDefault(params, key, value);
-	// });
-
 	return `${url.pathname}?${params.toString()}`;
 };
 
