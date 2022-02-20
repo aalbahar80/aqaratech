e0b5a6bdc9104e5a3659d3b381536de905b83411
nbe <aleisa.naser@gmail.com>
2022-02-20 08:25:17 +0300

    remove extra imports for now

--- src/lib/definitions/index.ts
+++ src/lib/definitions/index.ts
@@ -1,10 +1,6 @@
 import type { InferMutationInput } from '$lib/client/trpc';
-import client from './client';
 import lease from './lease';
-import property from './property';
 import tenant from './tenant';
-import transaction from './transaction';
-import unit from './unit';
 
 const entities = ['tenants', 'leases'] as const;
 export type Entity = typeof entities[number];
@@ -22,12 +18,8 @@ type EntityDefinitions = {
 };
 
 const entityDefinitions: EntityDefinitions = {
-	// clients: client,
-	// properties: property,
-	// units: unit,
 	leases: lease,
 	tenants: tenant,
-	// transactions: transaction,
 };
 
 export default entityDefinitions;
