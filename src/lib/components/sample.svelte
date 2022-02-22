e0d109eb43f3b2dd51af332c6a6ca1d9518160c4
nbe <aleisa.naser@gmail.com>
2022-02-22 18:24:12 +0300

    example class as callback function twind headless

---
+++ src/lib/components/sample.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { Listbox, ListboxOption } from '@rgossiaux/svelte-headlessui';
+	import type { ListboxOptionSlots } from '@rgossiaux/svelte-headlessui/components/listbox/ListboxOption.svelte';
+
+	function classNames(...classes: (string | false | null | undefined)[]) {
+		return classes.filter(Boolean).join(' ');
+	}
+
+	const myClass = (info: ListboxOptionSlots['default']) => {
+		console.log(info.active);
+		console.log(info.selected);
+		console.log(info.disabled);
+		return classNames(
+			'bg-blue-300',
+			info.active ? 'text-lg text-green-200' : 'text-sm text-red-200',
+		);
+	};
+</script>
+
+<Listbox value="">
+	<ListboxOption value="" class={myClass}>
+		<div>My stuff here</div>
+	</ListboxOption>
+</Listbox>
