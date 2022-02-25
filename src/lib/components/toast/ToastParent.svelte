592f8a961667e91b20120324fd72e35ea35055b3
nbe <aleisa.naser@gmail.com>
2022-02-25 04:32:07 +0300

    use fly transition on toasts. make faster

--- src/lib/components/toast/ToastParent.svelte
+++ src/lib/components/toast/ToastParent.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
 	import Toast from '$components/toast/Toast.svelte';
-	import { toasts, dismissToast } from '$lib/stores/toast';
+	import { dismissToast, toasts } from '$lib/stores/toast';
 	import { flip } from 'svelte/animate';
-	import { fade } from 'svelte/transition';
+	import { fly } from 'svelte/transition';
 </script>
 
 {#if $toasts}
 	<ul class="top-18 fixed right-4 z-50 flex flex-col items-end gap-y-4">
 		{#each $toasts as { id, props } (id)}
-			<li transition:fade animate:flip={{ duration: 500 }}>
+			<li
+				in:fly={{ duration: 300, y: -200 }}
+				out:fly={{ duration: 100, x: 100 }}
+				animate:flip={{ duration: 200 }}
+			>
 				<Toast {...props} close={() => dismissToast(id)} />
 			</li>
 		{/each}
