afcaf1d76925ea9e3ff2099a334c0596bb33db51
nbe <aleisa.naser@gmail.com>
2022-02-26 09:06:42 +0300

    modal works perfect, NO TRANSITION

--- src/lib/components/lease/LeaseDetail.svelte
+++ src/lib/components/lease/LeaseDetail.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import type { Entity } from '$lib/definitions';
 	import IModalDelete from '../toast/IModalDelete.svelte';
+	import Modal3 from '../toast/Modal3.svelte';
+	import { fade } from 'svelte/transition';
 	import { onDestroy, onMount } from 'svelte';
 
 	onDestroy(() => {
@@ -20,18 +22,31 @@
 <pre>{JSON.stringify(lease, null, 2)}</pre>
 
 {#if isOpen}
-	<!-- <IModalDelete {isOpen} entity="leases" id={lease.id} /> -->
+	<!-- Applying a svelte transition here interferes with the unmounting. Causes duplicates. Especially upon navigation by pressing delete. -->
+	<!-- <div transition:fade|local> -->
+	<!-- <div transition:fade> -->
+	<!-- <Modal3 {isOpen} /> -->
+	<Modal3 bind:isOpen />
+	<!-- </div> -->
 {/if}
+<p>some text</p>
+<p>some text below</p>
 <!-- <div class="static">
 	<IModalDelete bind:isOpen entity="leases" id={lease.id} />
 </div> -->
 
-<IModalDelete bind:isOpen entity="leases" id={lease.id} />
+<!-- <IModalDelete bind:isOpen entity="leases" id={lease.id} /> -->
 <!-- <svelte:component this={ModalDelete} {isOpen} loading /> -->
 <!-- <svelte:component this={IModalDelete} bind:isOpen {id} {entity} intro /> -->
 
 <button
 	on:click={() => {
 		isOpen = true;
-	}}>Delete</button
+	}}>Open</button
+>
+
+<button
+	on:click={() => {
+		isOpen = false;
+	}}>Hide it</button
 >
--- src/routes/leases/[id].svelte
+++ src/routes/leases/[id].svelte
@@ -1,13 +1,7 @@
 <script lang="ts" context="module">
-	import LeasesCard from '$components/tenant/LeasesCard.svelte';
-	import TenantDetail from '$components/tenant/TenantDetail.svelte';
-	import TrxColumn from '$components/tenant/TrxColumn.svelte';
 	import trpc, { type InferQueryOutput } from '$lib/client/trpc';
 	import LeaseDetail from '$lib/components/lease/LeaseDetail.svelte';
-	import FModalDelete from '$lib/components/toast/FModalDelete.svelte';
 	import type { Load } from '@sveltejs/kit';
-	import flatten from 'lodash-es/flatten.js';
-	import map from 'lodash-es/map.js';
 
 	export const load: Load = async ({ params }) => {
 		if (params.id === 'add') return { fallthrough: true };
@@ -22,14 +16,6 @@
 <script lang="ts">
 	type Lease = NonNullable<InferQueryOutput<'leases:read'>>;
 	export let lease: Lease;
-	let isOpen = false;
-
-	const handleOpenModal = () => {
-		isOpen = true;
-	};
 </script>
 
-<!-- <FModalDelete id={lease.id} entity="leases" bind:isOpen /> -->
-<div class="mx-auto flex max-w-6xl flex-col space-y-6 p-4 sm:p-6 lg:p-8">
-	<LeaseDetail {lease} on:openDeleteModal={handleOpenModal} />
-</div>
+<LeaseDetail {lease} />
