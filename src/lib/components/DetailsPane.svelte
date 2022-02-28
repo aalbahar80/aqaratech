1dda099c73ae8ec497540d85e2c86e09a1839279
nbe <aleisa.naser@gmail.com>
2022-02-28 08:08:10 +0300

    refactor into DetailsPane

---
+++ src/lib/components/DetailsPane.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+	import ButtonDropdown from '$components/ButtonDropdown.svelte';
+	import type { Option } from '$lib/types';
+	import { PaperClip, Pencil, Trash } from '@steeze-ui/heroicons';
+	import { Icon } from '@steeze-ui/svelte-icon';
+
+	// type Data = NonNullable<InferQueryOutput<`${'tenants' | 'leases'}:read`>>;
+	export let details: [string, string][];
+	export let files: [string, string][];
+
+	const options: Option[] = [
+		{ label: 'Update', href: '#', icon: Pencil, type: 'link' },
+		{ label: 'Remove', href: '#', icon: Trash, type: 'link' },
+	];
+</script>
+
+<div class="border-t border-gray-200">
+	<dl>
+		{#each details as [key, value]}
+			<div class="row">
+				<dt class="label">{key}</dt>
+				<dd class="definition">{value}</dd>
+			</div>
+		{/each}
+		<div class="row">
+			<dt class="label">Files</dt>
+			<dd class="definition">
+				<ul class="divide-y divide-gray-200 rounded-md border border-gray-200">
+					{#each files as [key, value]}
+						<li
+							class="flex items-center justify-between py-3 pl-3 pr-4 text-sm"
+						>
+							<div class="flex w-0 flex-1 items-center">
+								<Icon
+									src={PaperClip}
+									class="h-5 w-5 flex-shrink-0 text-gray-400"
+									aria-hidden="true"
+								/>
+								<span class="ml-2 w-0 flex-1 truncate">{key}</span>
+							</div>
+							<ButtonDropdown
+								class="bottom-10"
+								defaultOption={{ label: 'View', href: '#', type: 'link' }}
+								{options}
+							/>
+						</li>
+					{/each}
+				</ul>
+			</dd>
+		</div>
+	</dl>
+</div>
+
+<style lang="postcss">
+	.row {
+		@apply px-4 py-5 last:rounded-b-md odd:bg-gray-50 even:bg-white sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6;
+	}
+	.label {
+		@apply text-sm font-medium text-gray-500;
+	}
+	.definition {
+		@apply mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0;
+	}
+</style>
--- src/lib/components/tenant/TenantDetail.svelte
+++ src/lib/components/tenant/TenantDetail.svelte
@@ -4,14 +4,28 @@
 	import ModalDelete from '$lib/components/toast/ModalDelete.svelte';
 	import type { Option } from '$lib/types';
 	import { concatIfExists } from '$lib/utils/table-utils';
-	import { PaperClip, Pencil, Trash } from '@steeze-ui/heroicons';
-	import { Icon } from '@steeze-ui/svelte-icon';
+	import { Pencil, Trash } from '@steeze-ui/heroicons';
+	import DetailsPane from '../DetailsPane.svelte';
 
 	type Tenant = NonNullable<InferQueryOutput<'tenants:read'>>;
 	export let tenant: Tenant;
 
 	let isOpen = false;
 
+	const details: [string, string][] = [
+		[
+			'Full Name',
+			concatIfExists([tenant.firstName, tenant.secondName, tenant.lastName]),
+		],
+		['Phone', tenant.phone],
+		['Email', tenant.email],
+		['Civil id', tenant.civilid],
+	];
+
+	const files: [string, string][] = [
+		['Civil Id', ''],
+		['Passport', ''],
+	];
 	const options: Option[] = [
 		{ label: 'Update', href: '#', icon: Pencil, type: 'link' },
 		{ label: 'Remove', href: '#', icon: Trash, type: 'link' },
@@ -46,97 +60,5 @@
 			/>
 		</div>
 	</div>
-	<div class="border-t border-gray-200">
-		<dl>
-			<div class="row">
-				<dt class="label">Full name</dt>
-				<dd class="definition">
-					{concatIfExists([
-						tenant.firstName,
-						tenant.secondName,
-						tenant.lastName,
-					])}
-				</dd>
-			</div>
-			<div class="row">
-				<dt class="label">Phone</dt>
-				<dd class="definition">
-					{tenant.phone}
-				</dd>
-			</div>
-			<div class="row">
-				<dt class="label">Email</dt>
-				<dd class="definition">
-					{tenant.email}
-				</dd>
-			</div>
-			<div class="row">
-				<dt class="label">Civil Id</dt>
-				<dd class="definition">
-					{tenant.civilid}
-				</dd>
-			</div>
-			<div class="row">
-				<dt class="label">Files</dt>
-				<dd class="definition">
-					<ul
-						class="divide-y divide-gray-200 rounded-md border border-gray-200"
-					>
-						<li
-							class="flex items-center justify-between py-3 pl-3 pr-4 text-sm"
-						>
-							<div class="flex w-0 flex-1 items-center">
-								<Icon
-									src={PaperClip}
-									class="h-5 w-5 flex-shrink-0 text-gray-400"
-									aria-hidden="true"
-								/>
-								<span class="ml-2 w-0 flex-1 truncate">civil_id.pdf</span>
-							</div>
-
-							<ButtonDropdown
-								class="bottom-10"
-								defaultOption={{ label: 'View', href: '#', type: 'link' }}
-								{options}
-							/>
-						</li>
-						<li
-							class="flex items-center justify-between py-3 pl-3 pr-4 text-sm"
-						>
-							<div class="flex w-0 flex-1 items-center">
-								<Icon
-									src={PaperClip}
-									class="h-5 w-5 flex-shrink-0 text-gray-400"
-									aria-hidden="true"
-								/>
-								<span class="ml-2 w-0 flex-1 truncate">passport.pdf</span>
-							</div>
-
-							<ButtonDropdown
-								class="bottom-10"
-								defaultOption={{
-									label: 'View',
-									onClick: () => {},
-									type: 'button',
-								}}
-								{options}
-							/>
-						</li>
-					</ul>
-				</dd>
-			</div>
-		</dl>
-	</div>
+	<DetailsPane {details} {files} />
 </section>
-
-<style lang="postcss">
-	.row {
-		@apply px-4 py-5 last:rounded-b-md odd:bg-gray-50 even:bg-white sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6;
-	}
-	.label {
-		@apply text-sm font-medium text-gray-500;
-	}
-	.definition {
-		@apply mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0;
-	}
-</style>
