f7ffd419071c063b0846e272cced7fd690f2216e
nbe <aleisa.naser@gmail.com>
2022-03-02 04:58:27 +0300

    init contenteditable contract

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -36,7 +36,7 @@
 	}
 </script>
 
-<Disclosure as="nav" let:open class="bg-gray-800">
+<Disclosure as="nav" let:open class="bg-gray-800 print:hidden">
 	<div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
 		<div class="relative flex h-16 items-center justify-between">
 			<div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
