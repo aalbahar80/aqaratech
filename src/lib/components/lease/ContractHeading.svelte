b236c2a4223da7411a1b12252df1d7a2917c3961
nbe <aleisa.naser@gmail.com>
2022-03-02 10:41:10 +0300

    add printer icon

--- src/lib/components/lease/ContractHeading.svelte
+++ src/lib/components/lease/ContractHeading.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { ChevronLeft } from '@steeze-ui/heroicons';
+	import { ChevronLeft, Printer } from '@steeze-ui/heroicons';
 	import { Icon } from '@steeze-ui/svelte-icon';
 
 	export let id: string;
@@ -38,6 +38,7 @@
 				}}
 				class="ml-3 inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
 			>
+				<Icon src={Printer} class="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
 				Print
 			</button>
 		</div>
