a52a2c3ac7be5406e48d6f25cf0ac6161d5205ef
nbe <aleisa.naser@gmail.com>
2022-03-05 07:26:01 +0300

    init trx page

---
+++ src/routes/transactions/[id].svelte
@@ -0,0 +1,110 @@
+<script lang="ts" context="module">
+	import trpc from '$lib/client/trpc';
+	import type { Load } from '@sveltejs/kit';
+
+	export const load: Load = async ({ params }) => {
+		if (params.id === 'add') return { fallthrough: true };
+		const trx = await trpc.query('transactions:read', params.id);
+		return { props: { trx } };
+	};
+</script>
+
+<script lang="ts">
+	import type { InferQueryOutput } from '$lib/client/trpc';
+	import ModalDelete from '$lib/components/toast/ModalDelete.svelte';
+	import { Trash } from '@steeze-ui/heroicons';
+	import BreadCrumb from '$components/breadcrumbs/BreadCrumb.svelte';
+	import ButtonDropdown from '$components/ButtonDropdown.svelte';
+
+	type Transaction = InferQueryOutput<'transactions:read'>;
+	export let trx: Transaction;
+
+	let isOpen = false;
+	const openModal = () => {
+		isOpen = true;
+	};
+</script>
+
+<div class="mx-auto flex max-w-6xl flex-col space-y-6 p-4 sm:p-6 lg:p-8">
+	<div class="lg:flex lg:items-center lg:justify-between">
+		<ModalDelete bind:isOpen id={trx.id} entity="transactions" />
+		<div class="min-w-0 flex-1">
+			<BreadCrumb
+				crumbs={[
+					['tenants', trx.lease.tenantId],
+					['leases', trx.leaseId],
+				]}
+			/>
+			<div class="mt-2 flex items-center space-x-8">
+				<h2
+					class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl"
+				>
+					Transaction
+				</h2>
+				<span
+					class="not-paid inline-flex h-8 items-center rounded-md px-2.5 py-0.5 text-lg font-medium"
+					class:paid={trx.isPaid}
+					class:not-paid={!trx.isPaid}
+				>
+					{trx.isPaid ? 'Paid' : 'Not paid'}
+				</span>
+			</div>
+		</div>
+		<div class="mt-5 flex lg:mt-0 lg:ml-4">
+			<!-- <span class="sm:ml-3">
+        <a
+          class="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          href={`/leases/add?unitId=${lease.unit.id}&tenantId=${lease.tenant.id}&monthlyRent=${lease.monthlyRent}`}
+        >
+          <Icon
+            src={Refresh}
+            class="-ml-1 mr-2 hidden h-5 w-5 sm:block"
+            aria-hidden="true"
+          />
+          Renew
+        </a>
+      </span>
+   -->
+			<!-- <span class="ml-3">
+        <a
+          class="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          href={`/leases/${lease.id}/contract`}
+        >
+          <Icon
+            src={DocumentText}
+            class="-ml-1 mr-2 hidden h-5 w-5 sm:block"
+            aria-hidden="true"
+          />
+          Contract
+        </a>
+      </span> -->
+
+			<span class="ml-3">
+				<ButtonDropdown
+					defaultOption={{
+						label: 'Edit',
+						href: `/transactions/${trx.id}/edit`,
+						type: 'link',
+					}}
+					options={[
+						{
+							label: 'Delete',
+							icon: Trash,
+							onClick: openModal,
+							type: 'button',
+						},
+					]}
+				/>
+			</span>
+		</div>
+	</div>
+</div>
+
+<style lang="postcss">
+	.paid {
+		@apply bg-green-100 text-green-800;
+	}
+	.not-paid {
+		@apply bg-pink-100 text-pink-800;
+	}
+</style>
