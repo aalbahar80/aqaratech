2f37a39b4248512f9b5cea625fb9af2309fec1da
nbe <aleisa.naser@gmail.com>
2022-03-05 08:11:24 +0300

    add as prop to Button

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -8,32 +8,52 @@
 	export let text: string = '';
 	export let icon: IconSource | undefined = undefined;
 	export let solid: boolean = false;
+	export let as: 'button' | 'a' = 'button';
+	export let href: string = '';
 </script>
 
-<button type="submit" {disabled} class={$$props.class} on:click>
-	<Spinner {loading} />
-	{#if icon}
-		<Icon
-			src={icon}
-			theme={solid ? 'solid' : 'default'}
-			class="-ml-1 mr-2 hidden h-5 w-5 sm:block"
-			aria-hidden="true"
-		/>
-	{/if}
-	{text}
-</button>
+{#if as === 'button' || disabled}
+	<button type="submit" {disabled} class={$$props.class} on:click>
+		<Spinner {loading} />
+		{#if icon}
+			<Icon
+				src={icon}
+				theme={solid ? 'solid' : 'default'}
+				class="-ml-1 mr-2 hidden h-5 w-5 sm:block"
+				aria-hidden="true"
+			/>
+		{/if}
+		{text}
+	</button>
+{:else}
+	<!-- disabled <a> is rendered as button for convenience -->
+	<a {href} class={$$props.class}>
+		{#if icon}
+			<Icon
+				src={icon}
+				theme={solid ? 'solid' : 'default'}
+				class="-ml-1 mr-2 hidden h-5 w-5 sm:block"
+				aria-hidden="true"
+			/>
+		{/if}
+		{text}
+	</a>
+{/if}
 
 <style lang="postcss">
-	button {
+	button,
+	a {
 		@apply inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm;
 	}
 	button:disabled {
 		@apply cursor-not-allowed opacity-50;
 	}
-	button:hover {
+	button:hover,
+	a:hover {
 		@apply bg-indigo-700;
 	}
-	button:focus {
+	button:focus,
+	a:focus {
 		@apply outline-none ring-2 ring-indigo-500 ring-offset-2;
 	}
 </style>
