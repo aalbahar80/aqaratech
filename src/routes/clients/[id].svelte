15420808fbbd9e11e7850f9e1e0513c3395c1fa0
nbe <aleisa.naser@gmail.com>
2022-03-11 13:23:06 +0300

    add Heading to client/property id pages

--- src/routes/clients/[id].svelte
+++ src/routes/clients/[id].svelte
@@ -1,13 +1,11 @@
 <script lang="ts" context="module">
-	import ButtonDropdown from '$components/ButtonDropdown.svelte';
 	import type { InferQueryOutput } from '$lib/client/trpc';
 	import trpc from '$lib/client/trpc';
 	import DetailsPane from '$lib/components/DetailsPane.svelte';
+	import Heading from '$lib/components/Heading.svelte';
 	import PropertyList from '$lib/components/PropertyList.svelte';
-	import ModalDelete from '$lib/components/toast/ModalDelete.svelte';
 	import { dateFormat } from '$lib/utils/common';
 	import { concatIfExists } from '$lib/utils/table-utils';
-	import { Trash } from '@steeze-ui/heroicons';
 	import type { Load } from '@sveltejs/kit';
 
 	export const load: Load = async ({ params }) => {
@@ -29,41 +27,10 @@
 		['Created on', dateFormat(client.createdAt)],
 		['Last updated', client.updatedAt.toLocaleString()],
 	];
-
-	let isOpen = false;
-	const openModal = () => {
-		isOpen = true;
-	};
 </script>
 
 <div class="mx-auto flex max-w-6xl flex-col space-y-6 p-4 sm:p-6 lg:p-8">
-	<div class="flex items-center justify-between">
-		<ModalDelete bind:isOpen id={client.id} entity="clients" />
-		<div class="min-w-0 flex-1">
-			<div class="mt-2 flex items-center space-x-8">
-				<h2
-					class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl"
-				>
-					Client
-				</h2>
-			</div>
-		</div>
-		<div class="mt-5 flex space-x-3 lg:mt-0 lg:ml-4">
-			<ButtonDropdown
-				defaultOption={{
-					label: 'Edit',
-					href: `/clients/${client.id}/edit`,
-				}}
-				options={[
-					{
-						label: 'Delete',
-						icon: Trash,
-						onClick: openModal,
-					},
-				]}
-			/>
-		</div>
-	</div>
+	<Heading title="Client" id={client.id} entity="clients" />
 	<DetailsPane {details} />
 	<PropertyList properties={client.properties} clientId={client.id} />
 </div>
--- src/routes/properties/[id].svelte
+++ src/routes/properties/[id].svelte
@@ -1,14 +1,11 @@
 <script lang="ts" context="module">
-	import BreadCrumb from '$components/breadcrumbs/BreadCrumb.svelte';
-	import ButtonDropdown from '$components/ButtonDropdown.svelte';
 	import type { InferQueryOutput } from '$lib/client/trpc';
 	import trpc from '$lib/client/trpc';
 	import DetailsPane from '$lib/components/DetailsPane.svelte';
-	import ModalDelete from '$lib/components/toast/ModalDelete.svelte';
+	import Heading from '$lib/components/Heading.svelte';
 	import UnitsList from '$lib/components/UnitsList.svelte';
 	import { label } from '$lib/definitions/property';
 	import { dateFormat } from '$lib/utils/common';
-	import { Trash } from '@steeze-ui/heroicons';
 	import type { Load } from '@sveltejs/kit';
 
 	export const load: Load = async ({ params }) => {
@@ -28,42 +25,10 @@
 		['Created on', dateFormat(property.createdAt)],
 		['Last updated', property.updatedAt.toLocaleString()],
 	];
-
-	let isOpen = false;
-	const openModal = () => {
-		isOpen = true;
-	};
 </script>
 
 <div class="mx-auto flex max-w-6xl flex-col space-y-6 p-4 sm:p-6 lg:p-8">
-	<div class="flex items-center justify-between">
-		<ModalDelete bind:isOpen id={property.id} entity="properties" />
-		<div class="min-w-0 flex-1">
-			<BreadCrumb crumbs={[['clients', property.clientId]]} />
-			<div class="mt-2 flex items-center space-x-8">
-				<h2
-					class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl"
-				>
-					Property
-				</h2>
-			</div>
-		</div>
-		<div class="mt-5 flex space-x-3 lg:mt-0 lg:ml-4">
-			<ButtonDropdown
-				defaultOption={{
-					label: 'Edit',
-					href: `/properties/${property.id}/edit`,
-				}}
-				options={[
-					{
-						label: 'Delete',
-						icon: Trash,
-						onClick: openModal,
-					},
-				]}
-			/>
-		</div>
-	</div>
+	<Heading title="Property" id={property.id} entity="properties" />
 	<DetailsPane {details} />
 	<UnitsList units={property.units} propertyId={property.id} />
 </div>
