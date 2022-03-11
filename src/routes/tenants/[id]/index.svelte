8525f653c888767c8d98c26ca810731292efb9d9
nbe <aleisa.naser@gmail.com>
2022-03-11 18:14:08 +0300

    use generic Detail component in tenant id page

--- src/routes/tenants/[id]/index.svelte
+++ src/routes/tenants/[id]/index.svelte
@@ -1,8 +1,10 @@
 <script lang="ts" context="module">
 	import LeasesCard from '$components/tenant/LeasesCard.svelte';
-	import TenantDetail from '$components/tenant/TenantDetail.svelte';
 	import TrxColumn from '$components/tenant/TrxColumn.svelte';
 	import trpc, { type InferQueryOutput } from '$lib/client/trpc';
+	import DetailsPane from '$lib/components/DetailsPane.svelte';
+	import Heading from '$lib/components/Heading.svelte';
+	import { concatIfExists } from '$lib/utils/table-utils';
 	import type { Load } from '@sveltejs/kit';
 	import flatten from 'lodash-es/flatten.js';
 	import map from 'lodash-es/map.js';
@@ -20,10 +22,30 @@
 <script lang="ts">
 	type Tenant = NonNullable<InferQueryOutput<'tenants:read'>>;
 	export let tenant: Tenant;
+
+	const details: [string, string | null][] = [
+		[
+			'Full Name',
+			concatIfExists([tenant.firstName, tenant.secondName, tenant.lastName]),
+		],
+		['Phone', tenant.phone],
+		['Email', tenant.email],
+		['Civil id', tenant.civilid],
+		['Passport #', tenant.passportNum],
+		['Residency #', tenant.residencyNum],
+		['Residency Expiration', tenant.residencyEnd?.toLocaleDateString() ?? ''],
+		['Nationality', tenant.nationality],
+	];
+
+	const files: [string, string][] = [
+		['Civil Id', ''],
+		['Passport', ''],
+	];
 </script>
 
 <div class="mx-auto flex max-w-4xl flex-col space-y-6 p-4 sm:p-6 lg:p-8">
-	<TenantDetail {tenant} />
+	<Heading title="Tenant" id={tenant.id} entity="tenants" />
+	<DetailsPane {details} {files} />
 	<LeasesCard leases={tenant.leases} tenantId={tenant.id} />
 	<TrxColumn transactions={flatten(map(tenant.leases, 'transactions'))} />
 </div>
