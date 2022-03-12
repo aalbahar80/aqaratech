8e2592efa01a6db7f4e6333a50ff6602f7662419
nbe <aleisa.naser@gmail.com>
2022-03-12 21:39:45 +0300

    set max width on contract heading

--- src/lib/components/lease/ContractHeading.svelte
+++ src/lib/components/lease/ContractHeading.svelte
@@ -40,7 +40,7 @@
 				on:click={() => {
 					window.print();
 				}}
-				class="ml-3 inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+				class="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
 			>
 				<Icon src={Printer} class="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
 				Print
