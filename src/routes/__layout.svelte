4e158f2ce6d73fbf05142387971e4359ef31d952
nbe <aleisa.naser@gmail.com>
2022-03-19 20:24:00 +0300

    add back posthog and logrocket

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,6 +5,23 @@
 	import ToastParent from '$components/toast/ToastParent.svelte';
 	import '../styles/tailwind.css';
 	import 'tippy.js/dist/tippy.css';
+	import { onMount } from 'svelte';
+	import LogRocket from 'logrocket';
+	import posthog from 'posthog-js';
+
+	onMount(() => {
+		if (!window.location.href.includes('localhost')) {
+			// TODO choose one of posthog or logrocket
+			posthog.init('phc_9yCZuf3iVjCaKEH8TDb4sLaN2tg3hnyoIpqRIjGjNiz', {
+				api_host: 'https://app.posthog.com',
+			});
+			// posthog.identify($session.userId);
+			// posthog.capture('my event', { property: 'value' });
+
+			LogRocket.init('n4p0hb/svelte14dec');
+			// LogRocket.identify($session.userId);
+		}
+	});
 </script>
 
 {#if $navigating}
