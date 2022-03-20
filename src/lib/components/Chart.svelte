5d978777cc8e7872de681eeb97c050f925aaa5e6
nbe <aleisa.naser@gmail.com>
2022-03-20 16:46:59 +0300

    don't load in homepage

---
+++ src/lib/components/Chart.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let src: string;
+	export let title: string = src;
+	export let titled: boolean = true;
+	export let bordered: boolean = true;
+	export let dark: boolean = false;
+
+	const resultUrl = `${src}#bordered=false&titled=true`;
+</script>
+
+<iframe
+	src={resultUrl}
+	title="dashboard"
+	frameborder="0"
+	class="h-[36rem] w-full p-4"
+	allowtransparency
+/>
---
+++ src/routes/dashboard.svelte
@@ -0,0 +1,37 @@
+<script context="module" lang="ts">
+	import Chart from '$lib/components/Chart.svelte';
+	import type { Load } from '@sveltejs/kit';
+
+	export const load: Load = async ({ fetch }) => {
+		const res = await fetch('/api/metabase');
+		const { iframeUrl } = await res.json();
+		return {
+			props: { iframeUrl },
+		};
+	};
+</script>
+
+<script lang="ts">
+	export let iframeUrl: string;
+	console.log({ iframeUrl }, 'index.svelte ~ 16');
+</script>
+
+<svelte:head>
+	<title>Dashboard</title>
+</svelte:head>
+
+<!-- <div class="grid grid-cols-2 items-center justify-between gap-y-4"> -->
+<!-- <div
+	class="grid auto-rows-fr grid-cols-1 items-center justify-between gap-4 lg:auto-cols-fr lg:grid-cols-2"
+> -->
+<div class="grid auto-rows-fr grid-cols-1 gap-4 bg-white lg:grid-cols-2">
+	<Chart
+		src="https://metabase.letand.be/public/question/0bd400fa-803b-4697-aa84-ae1b4d9b6228"
+	/>
+	<Chart
+		src="https://metabase.letand.be/public/question/36b7faf8-8943-4204-a1f9-1c70f869ffd8"
+	/>
+	<Chart
+		src="http://metabase.letand.be/public/question/16a8fe1b-7ef5-4fda-ac80-431845f6795b"
+	/>
+</div>
