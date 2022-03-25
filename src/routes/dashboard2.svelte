29178c4ef51ef946d0f5c1387e4336dd14e52367
nbe <aleisa.naser@gmail.com>
2022-03-25 12:21:29 +0300

    remove height bindings

--- src/routes/dashboard2.svelte
+++ src/routes/dashboard2.svelte
@@ -11,22 +11,10 @@
 
 <script lang="ts">
 	export let iframeUrl: string;
-	let oh: number;
-	let oh2: number;
-	let ch: number;
-	let ch2: number;
-	let ih: number;
-	let ohh: number;
-	let y: number;
 </script>
 
-<svelte:window bind:scrollY={y} bind:outerHeight={ohh} bind:innerHeight={ih} />
-
-<!-- <div class="h-screen" bind:clientHeight={ch} bind:offsetHeight={oh}> -->
-<div class="h-[95vh] bg-white" bind:clientHeight={ch} bind:offsetHeight={oh}>
+<div class="h-[95vh] bg-white">
 	<iframe
-		bind:clientHeight={ch2}
-		bind:offsetHeight={oh2}
 		title="dashboard2"
 		src={iframeUrl}
 		frameborder="0"
@@ -34,4 +22,3 @@
 		class="flex min-h-full w-full flex-col"
 	/>
 </div>
-<!-- class="h-[300rem] w-full p-4" -->
