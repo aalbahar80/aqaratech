169c346780278fff5ae7ffd6e29c5c83b259726a
nbe <aleisa.naser@gmail.com>
2022-03-26 14:50:25 +0300

    tryprerender

---
+++ src/routes/tryprerender.svelte
@@ -0,0 +1,52 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+	import Chart from 'chart.js/auto';
+	import { onMount } from 'svelte';
+
+	let ctx: HTMLCanvasElement;
+	onMount(() => {
+		new Chart(ctx, {
+			type: 'bar',
+			data: {
+				labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+				datasets: [
+					{
+						label: '# of Votes',
+						data: [12, 19, 3, 5, 2, 3],
+						backgroundColor: [
+							'rgba(255, 99, 132, 0.2)',
+							'rgba(54, 162, 235, 0.2)',
+							'rgba(255, 206, 86, 0.2)',
+							'rgba(75, 192, 192, 0.2)',
+							'rgba(153, 102, 255, 0.2)',
+							'rgba(255, 159, 64, 0.2)',
+						],
+						borderColor: [
+							'rgba(255, 99, 132, 1)',
+							'rgba(54, 162, 235, 1)',
+							'rgba(255, 206, 86, 1)',
+							'rgba(75, 192, 192, 1)',
+							'rgba(153, 102, 255, 1)',
+							'rgba(255, 159, 64, 1)',
+						],
+						borderWidth: 1,
+					},
+				],
+			},
+			options: {
+				scales: {
+					y: {
+						beginAtZero: true,
+					},
+				},
+			},
+		});
+	});
+</script>
+
+<div class="mx-auto flex max-w-screen-lg flex-col space-y-6 p-4 sm:p-6 lg:p-8">
+	<canvas bind:this={ctx} width="400" height="400" />
+</div>
