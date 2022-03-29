7ad7b0c02bd3c1f323873709148074142e208683
nbe <aleisa.naser@gmail.com>
2022-03-30 02:17:42 +0300

    remove old test

--- src/lib/utils/group.test.ts
+++ src/lib/utils/group.test.ts
@@ -1,4 +1,4 @@
-import { groupIncome } from './group';
+import { getMonths } from './group';
 
 describe('Grouping a list of transactions', () => {
 	test('the sum of the output equals the sum of the inputs', () => {
@@ -12,9 +12,9 @@ describe('Grouping a list of transactions', () => {
 			{ date: new Date(Date.UTC(2020, 0, 3)), amount: 500, isPaid: true },
 			{ date: new Date(Date.UTC(2020, 0, 3)), amount: 600, isPaid: true },
 		];
-		const inputSum = data.reduce((acc, item) => acc + item.amount, 0);
-		const output = groupIncome(data);
-		const outputSum = output.reduce((acc, item) => acc + item.amount, 0);
-		expect(outputSum).toBe(inputSum);
+		// const inputSum = data.reduce((acc, item) => acc + item.amount, 0);
+		// const output = groupIncome(data);
+		// const outputSum = output.reduce((acc, item) => acc + item.amount, 0);
+		// expect(outputSum).toBe(inputSum);
 	});
 });
