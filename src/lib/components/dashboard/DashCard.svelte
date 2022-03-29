c11a416bc493dae2ee2a04826378103626b586ca
nbe <aleisa.naser@gmail.com>
2022-03-29 17:04:19 +0300

    init empty chart state

--- src/lib/components/dashboard/DashCard.svelte
+++ src/lib/components/dashboard/DashCard.svelte
@@ -8,7 +8,16 @@
 		<h3>{title}</h3>
 		<p>{subtitle}</p>
 	</div>
-	<div class="py-2">
+	<!-- <div class="py-2">
 		<slot />
+	</div> -->
+	<div
+		class="flex h-52 flex-col place-content-center rounded-md bg-gray-100 text-center"
+	>
+		<div class="prose place-self-center">
+			<small>No Data</small>
+			<br />
+			<small>There's no data available for your selection.</small>
+		</div>
 	</div>
 </div>
