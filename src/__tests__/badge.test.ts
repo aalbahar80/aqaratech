6c890aa2ad4a573a6255a8a3269f47868e1cc120
nbe <aleisa.naser@gmail.com>
2022-03-31 15:22:36 +0300

    trim vitest sample component test

--- src/__tests__/badge.test.ts
+++ src/__tests__/badge.test.ts
@@ -1,30 +1,8 @@
 import '@testing-library/jest-dom';
-import { cleanup, fireEvent, render, screen } from '@testing-library/svelte';
-// import Hello from '../routes/index.svelte';
+import { cleanup, render, screen } from '@testing-library/svelte';
 import Badge from '../lib/components/Badge.svelte';
 
 afterEach(() => cleanup());
-
-// async function renderHello(overrideProps = {}) {
-// // async function renderHello() {
-// 	const svl = render(Hello, {
-// 		// props: {
-// 		// 	data: [[1, 2, 3]],
-// 		// 	...overrideProps,
-// 		// },
-// 	});
-
-// 	await new Promise(setImmediate);
-
-// 	return svl;
-// }
-
-// test('should render hello', async () => {
-// 	await renderHello();
-
-// 	expect(screen.getByRole('heading')).toBeInTheDocument();
-// 	expect(screen.getByRole('heading')).toHaveTextContent('Lorem');
-// });
 
 describe('Badge.svelte', () => {
 	// TODO: @testing-library/svelte claims to add this automatically but it doesn't work without explicit afterEach
@@ -39,16 +17,6 @@ describe('Badge.svelte', () => {
 		expect(container.innerHTML).toContain('MyLabel');
 		expect(container.innerHTML).toMatchSnapshot();
 	});
-
-	// it('updates on button click', async () => {
-	// 	render(Badge, { count: 4 });
-	// 	const btn = screen.getByRole('button');
-	// 	const div = screen.getByText('4 x 2 = 8');
-	// 	await fireEvent.click(btn);
-	// 	expect(div.innerHTML).toBe('4 x 3 = 12');
-	// 	await fireEvent.click(btn);
-	// 	expect(div.innerHTML).toBe('4 x 4 = 16');
-	// });
 
 	it('has the correct color', async () => {
 		render(Badge, { label: 'MyLabel', badgeColor: 'red' });
