43fada79d59ef61aedccfb3444faaa0097a3f247
nbe <aleisa.naser@gmail.com>
2022-04-05 21:04:42 +0300

    move Auth0 provider into seperate file

--- src/lib/services/auth/auth0-auth.ts
+++ src/lib/services/auth/auth0-auth.ts
@@ -1,49 +1,5 @@
 import { SvelteKitAuth } from 'sk-auth';
-import type { OAuth2ProviderConfig } from 'sk-auth/dist/providers/oauth2';
-import { OAuth2Provider } from 'sk-auth/providers';
-
-interface Auth0Profile {
-	id: number;
-	login: string;
-	avatar_url: string;
-	url: string;
-	name: string;
-}
-interface Auth0Tokens {
-	access_token: string;
-	token_type: string;
-	expires_in: number;
-	scope?: string | undefined;
-	id_token?: string | undefined;
-	refresh_token?: string | undefined;
-}
-type Auth0OAuth2ProviderConfig = OAuth2ProviderConfig<
-	Auth0Profile,
-	Auth0Tokens
->;
-
-class Auth0OAuth2Provider extends OAuth2Provider<
-	Auth0Profile,
-	Auth0Tokens,
-	Auth0OAuth2ProviderConfig
-> {
-	constructor(config: Auth0OAuth2ProviderConfig) {
-		super({
-			...defaultConfig,
-			...config,
-		});
-	}
-}
-
-const defaultConfig: Auth0OAuth2ProviderConfig = {
-	// TODO don't hardcode tenant name
-	id: 'auth0',
-	scope: 'openid',
-	headers: {
-		Accept: 'application/json',
-		// 'Content-Type': 'application/json',
-	},
-};
+import { Auth0OAuth2Provider } from './auth0-provider';
 
 // TODO see vhscom starter
 const developmentOptions = {
---
+++ src/lib/services/auth/auth0-provider.ts
@@ -0,0 +1,44 @@
+import type { OAuth2ProviderConfig } from 'sk-auth/dist/providers/oauth2';
+import { OAuth2Provider } from 'sk-auth/providers';
+
+const defaultConfig: Auth0OAuth2ProviderConfig = {
+	id: 'auth0',
+	scope: 'openid',
+	headers: {
+		Accept: 'application/json',
+		// 'Content-Type': 'application/json',
+	},
+};
+
+interface Auth0Profile {
+	id: number;
+	login: string;
+	avatar_url: string;
+	url: string;
+	name: string;
+}
+interface Auth0Tokens {
+	access_token: string;
+	token_type: string;
+	expires_in: number;
+	scope?: string | undefined;
+	id_token?: string | undefined;
+	refresh_token?: string | undefined;
+}
+type Auth0OAuth2ProviderConfig = OAuth2ProviderConfig<
+	Auth0Profile,
+	Auth0Tokens
+>;
+
+export class Auth0OAuth2Provider extends OAuth2Provider<
+	Auth0Profile,
+	Auth0Tokens,
+	Auth0OAuth2ProviderConfig
+> {
+	constructor(config: Auth0OAuth2ProviderConfig) {
+		super({
+			...defaultConfig,
+			...config,
+		});
+	}
+}
