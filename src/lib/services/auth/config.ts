40866a074a75411796814549a07c5c680378336c
nbe <aleisa.naser@gmail.com>
2022-04-08 15:46:09 +0300

    tidy up dev config

--- src/lib/services/auth/config.ts
+++ src/lib/services/auth/config.ts
@@ -1,14 +1,13 @@
-import { dev } from '$app/env';
-
-const developmentOptions = dev
-	? {
-			host: 'localhost:3000',
-			protocol: 'http',
-			basePath: '/api/auth',
-	  }
-	: {};
-
 const domain = 'dev-eehvhdp2.eu.auth0.com';
+const callbackPath = '/api/auth/callback';
+
+let redirectUri = '';
+if (process.env.VERCEL && process.env.VERCEL_URL) {
+	redirectUri = `https://${process.env.VERCEL_URL}${callbackPath}`;
+} else {
+	redirectUri = `http://localhost:3000${callbackPath}`;
+}
+
 export const config = {
 	domain,
 	accessTokenUrl: `https://${domain}/oauth/token`,
@@ -19,7 +18,7 @@ export const config = {
 	clientSecret: import.meta.env.VITE_AUTH0_CLIENT_SECRET,
 	scope: 'openid profile email',
 	audience: 'letand.be/api',
-	redirectUri: `${developmentOptions.protocol}://${developmentOptions.host}${developmentOptions.basePath}/callback`,
+	redirectUri,
 };
 
 export const parseUser = (
