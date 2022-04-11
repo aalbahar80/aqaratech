5057976f178938371b00da0756afa0d262868b35
nbe <aleisa.naser@gmail.com>
2022-04-11 22:45:19 +0300

    add function overloads to getModel

--- src/lib/models/interfaces/utils/get-model.ts
+++ src/lib/models/interfaces/utils/get-model.ts
@@ -1,4 +1,3 @@
-import type { Entity, Model } from '$models/interfaces';
 import {
 	ClientModel,
 	LeaseModel,
@@ -7,22 +6,46 @@ import {
 	TenantModel,
 	TransactionModel,
 	UnitModel,
+	type Entity,
+	type Model,
 } from '$models/interfaces';
 
-export const getModel = (entity: Entity): Model => {
-	if (entity === 'clients') {
+const relationalNames = [
+	'clientId',
+	'propertyId',
+	'unitId',
+	'tenantId',
+] as const;
+
+export function getModel(entity: 'clients' | 'clientId'): typeof ClientModel;
+export function getModel(
+	entity: 'properties' | 'propertyId',
+): typeof PropertyModel;
+export function getModel(entity: 'units' | 'unitId'): typeof UnitModel;
+export function getModel(entity: 'tenants' | 'tenantId'): typeof TenantModel;
+export function getModel(entity: 'leases'): typeof LeaseModel;
+export function getModel(
+	entity: 'maintenanceOrders',
+): typeof MaintenanceOrderModel;
+export function getModel(entity: 'transactions'): typeof TransactionModel;
+export function getModel(
+	entity: Entity | typeof relationalNames[number],
+): Model;
+
+export function getModel(entity: Entity | typeof relationalNames[number]) {
+	if (entity === 'clients' || entity === 'clientId') {
 		return ClientModel;
 	}
-	if (entity === 'properties') {
+	if (entity === 'properties' || entity === 'propertyId') {
 		return PropertyModel;
 	}
 	if (entity === 'leases') {
 		return LeaseModel;
 	}
-	if (entity === 'units') {
+	if (entity === 'units' || entity === 'unitId') {
 		return UnitModel;
 	}
-	if (entity === 'tenants') {
+	if (entity === 'tenants' || entity === 'tenantId') {
 		return TenantModel;
 	}
 	if (entity === 'maintenanceOrders') {
@@ -32,4 +55,4 @@ export const getModel = (entity: Entity): Model => {
 		return TransactionModel;
 	}
 	throw new Error(`Unknown entity: ${entity}`);
-};
+}
