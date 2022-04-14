109f4ea06ac4ed824c8d3ddd3ef1ca840b383031
nbe <aleisa.naser@gmail.com>
2022-04-14 09:42:40 +0300

    uninstall cerbos/sdk

--- packages/site/src/lib/server/cerbos.ts
+++
@@ -1,9 +0,0 @@
-import { dev } from '$app/env';
-import * as pkg from '@cerbos/sdk';
-const { Cerbos } = pkg;
-
-const hostname = dev ? 'http://localhost:3592' : 'https://cerbos.letand.be';
-export const cerbos = new Cerbos({
-	hostname,
-	// logLevel: 'debug',
-});
