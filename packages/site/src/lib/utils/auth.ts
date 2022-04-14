6a014986748763fbbd5d7a387952e53c9bbb8ed3
nbe <aleisa.naser@gmail.com>
2022-04-14 09:07:06 +0300

    don't protect home page

--- packages/site/src/lib/utils/auth.ts
+++ packages/site/src/lib/utils/auth.ts
@@ -1,6 +1,6 @@
 export const protectRoute = (session: App.Session, pathname: string) => {
 	const signinUrl = '/login';
-	return pathname !== signinUrl && !session.user
+	return pathname !== signinUrl && pathname !== '/' && !session.user
 		? {
 				redirect: signinUrl,
 				status: 302,
