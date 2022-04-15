a1131e9174aaffc13655e941bd75c9c8e244daf3
nbe <aleisa.naser@gmail.com>
2022-04-15 20:15:46 +0300

    don't protect routes in dev mode

--- packages/site/src/lib/utils/auth.ts
+++ packages/site/src/lib/utils/auth.ts
@@ -1,6 +1,9 @@
+import { dev } from '$app/env';
+
 export const protectRoute = (session: App.Session, pathname: string) => {
 	const signinUrl = '/login';
-	return pathname !== signinUrl && pathname !== '/' && !session.user
+	// TODO create nonprotected 404 page
+	return !dev && pathname !== signinUrl && pathname !== '/' && !session.user
 		? {
 				redirect: signinUrl,
 				status: 302,
