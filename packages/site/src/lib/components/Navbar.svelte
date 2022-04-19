9653cac1c645c9be8e5e15403056606412c20582
nbe <aleisa.naser@gmail.com>
2022-04-19 05:00:21 +0300

    actually use dashboard href

--- packages/site/src/lib/components/Navbar.svelte
+++ packages/site/src/lib/components/Navbar.svelte
@@ -46,7 +46,7 @@
 				{:else if $session.authz}
 					<!-- TODO change href -->
 					<a
-						href="/clients"
+						href={dashboard}
 						class="inline-flex items-center rounded-md border border-transparent bg-gray-600 px-4 py-2 text-base font-medium text-white hover:bg-gray-700 md:hidden"
 					>
 						Dashboard
@@ -79,7 +79,7 @@
 			{#if $session.authz}
 				<!-- TODO change href -->
 				<a
-					href="/clients"
+					href={dashboard}
 					class="inline-flex items-center rounded-md border border-transparent bg-gray-600 px-4 py-2 text-base font-medium text-white hover:bg-gray-700"
 				>
 					Dashboard
