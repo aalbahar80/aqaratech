2948b94b29952ee4cbe4b71f60ad701f1bdbf75f
nbe <aleisa.naser@gmail.com>
2022-04-21 18:55:51 +0300

    init modular StackedList component

---
+++ packages/site/src/lib/components/StackedList.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+	import { getModel } from '$lib/models/interfaces/utils/get-model';
+	import type { Entity } from '$models/interfaces';
+	import { FolderAdd, Plus } from '@steeze-ui/heroicons';
+	import { Icon } from '@steeze-ui/svelte-icon';
+
+	export let hideActions = false;
+	export let entity: Entity;
+	export let count: number;
+
+	const model = getModel(entity);
+	const newHref = `/new/${model.plural}`;
+</script>
+
+<section class="overflow-hidden rounded-md bg-white shadow">
+	{#if count}
+		<!-- Section Heading -->
+		<div class="border-b border-gray-200 bg-white px-4 py-5 sm:px-6">
+			<div
+				class="-ml-4 -mt-2 flex flex-wrap items-center justify-between sm:flex-nowrap"
+			>
+				<div class="ml-4 mt-2">
+					<h3 class="text-lg font-medium leading-6 text-gray-900">
+						{model.pluralCap}
+					</h3>
+				</div>
+
+				{#if !hideActions}
+					<div class="ml-4 mt-2 flex-shrink-0">
+						<a
+							href={newHref}
+							class="relative inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+						>
+							Create new lease
+						</a>
+					</div>
+				{/if}
+			</div>
+		</div>
+
+		<ul class="divide-y divide-gray-200">
+			<slot />
+		</ul>
+	{:else}
+		<!-- Empty State -->
+		<div class="text-center py-8 sm:py-16">
+			<Icon
+				src={FolderAdd}
+				class="mx-auto h-12 w-12 text-gray-400"
+				aria-hidden="true"
+			/>
+			<h3 class="mt-2 text-sm font-medium text-gray-900">No {model.plural}</h3>
+			{#if hideActions}
+				<p class="mt-1 text-sm text-gray-500">Nothing here, yet.</p>
+			{:else}
+				<p class="mt-1 text-sm text-gray-500">
+					Get started by creating a new lease.
+				</p>
+				<div class="mt-6">
+					<a
+						href={newHref}
+						class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+					>
+						<Icon src={Plus} class="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+						New {model.singular}
+					</a>
+				</div>
+			{/if}
+		</div>
+	{/if}
+</section>
+
+<style>
+	section {
+		border-bottom-right-radius: var(--border-radius-b, 0.375rem);
+		border-bottom-left-radius: var(--border-radius-b, 0.375rem);
+	}
+</style>
--- packages/site/src/lib/components/lease/LeaseList.svelte
+++ packages/site/src/lib/components/lease/LeaseList.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import LeaseCard from '$lib/components/lease/LeaseCard.svelte';
-	import { FolderAdd, Plus } from '@steeze-ui/heroicons';
-	import { Icon } from '@steeze-ui/svelte-icon';
+	import StackedList from '$lib/components/StackedList.svelte';
 	import { flip } from 'svelte/animate';
 	import { fade } from 'svelte/transition';
 
@@ -27,80 +26,17 @@
 		end: Date;
 	}
 	export let leases: Lease[];
-	export let newHref = '/new/leases';
 	export let showIndex = false;
-	export let hideActions = false;
 </script>
 
-<section class="overflow-hidden rounded-md bg-white shadow">
-	{#if leases.length}
-		<!-- Section Heading -->
-		<div class="border-b border-gray-200 bg-white px-4 py-5 sm:px-6">
-			<div
-				class="-ml-4 -mt-2 flex flex-wrap items-center justify-between sm:flex-nowrap"
-			>
-				<div class="ml-4 mt-2">
-					<h3 class="text-lg font-medium leading-6 text-gray-900">Leases</h3>
-				</div>
-
-				{#if !hideActions}
-					<div class="ml-4 mt-2 flex-shrink-0">
-						<a
-							href={newHref}
-							class="relative inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-						>
-							Create new lease
-						</a>
-					</div>
-				{/if}
-			</div>
-		</div>
-
-		<ul class="divide-y divide-gray-200">
-			{#each leases as lease, index (lease.id)}
-				<li in:fade|local={{ duration: 200 }} animate:flip={{ duration: 200 }}>
-					<LeaseCard
-						{lease}
-						index={showIndex ? leases.length - index : undefined}
-						hrefBase={$page.stuff.hrefBase ?? ''}
-					/>
-				</li>
-			{/each}
-		</ul>
-	{:else}
-		<!-- Empty State -->
-		<div class="text-center py-8 sm:py-16">
-			<Icon
-				src={FolderAdd}
-				class="mx-auto h-12 w-12 text-gray-400"
-				aria-hidden="true"
+<StackedList entity="leases" count={leases.length}>
+	{#each leases as lease, index (lease.id)}
+		<li in:fade|local={{ duration: 200 }} animate:flip={{ duration: 200 }}>
+			<LeaseCard
+				{lease}
+				index={showIndex ? leases.length - index : undefined}
+				hrefBase={$page.stuff.hrefBase ?? ''}
 			/>
-			<h3 class="mt-2 text-sm font-medium text-gray-900">No leases</h3>
-			{#if hideActions}
-				<p class="mt-1 text-sm text-gray-500">
-					Nothing found, check back later.
-				</p>
-			{:else}
-				<p class="mt-1 text-sm text-gray-500">
-					Get started by creating a new lease.
-				</p>
-				<div class="mt-6">
-					<a
-						href={newHref}
-						class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-					>
-						<Icon src={Plus} class="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
-						New Lease
-					</a>
-				</div>
-			{/if}
-		</div>
-	{/if}
-</section>
-
-<style>
-	section {
-		border-bottom-right-radius: var(--border-radius-b, 0.375rem);
-		border-bottom-left-radius: var(--border-radius-b, 0.375rem);
-	}
-</style>
+		</li>
+	{/each}
+</StackedList>
