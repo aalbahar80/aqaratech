2d4824bd8a336c7aaf40d7a7f0ee96ba3f4082a2
nbe <aleisa.naser@gmail.com>
2022-04-21 13:23:19 +0300

    init temporal

--- packages/temporal/src/worker.ts
+++ packages/temporal/src/worker.ts
@@ -1,9 +1,14 @@
-import { Worker, Core } from '@temporalio/worker';
+import pkg from '@prisma/client';
+import {
+	DefaultLogger,
+	NativeConnection,
+	Runtime,
+	Worker,
+} from '@temporalio/worker';
+import path from 'path';
+import { URL } from 'url';
 // TODO remove .js extension?
 import { createActivities } from './activities.js';
-import { URL } from 'url';
-import path from 'path';
-import pkg from '@prisma/client';
 
 const { PrismaClient } = pkg;
 const prismaClient = new PrismaClient({});
@@ -11,18 +16,23 @@ const prismaClient = new PrismaClient({});
 run().catch((err) => console.log(err));
 
 async function run() {
-	await Core.install({
-		serverOptions: {
-			address: 'temporal.letand.be',
-		},
+	const logger = new DefaultLogger('DEBUG');
+	Runtime.install({
+		logger,
+		telemetryOptions: { logForwardingLevel: 'INFO' },
 	});
+
 	// Support running both complied code and ts-node/esm loader
 	const workflowsPath = new URL(
 		`./workflows${path.extname(import.meta.url)}`,
 		import.meta.url,
 	).pathname;
 
+	const connection = await NativeConnection.create({
+		// address: 'temporal.letand.be',
+	});
 	const worker = await Worker.create({
+		connection,
 		workflowsPath, // passed to Webpack for bundling
 		taskQueue: 'hello-world',
 		activities: createActivities(prismaClient),
