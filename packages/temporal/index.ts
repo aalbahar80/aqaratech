927e98b0164620abe98d022e4432d2724f09f248
nbe <aleisa.naser@gmail.com>
2022-04-22 20:51:31 +0300

    import workflows from ./src

--- packages/temporal/index.ts
+++ packages/temporal/index.ts
@@ -1,1 +1,3 @@
-export { trxNotificationWF, getNextReminder } from "./lib/workflows";
+// Vercel build will fail, need to build workflows or avoid ./lib
+// export { trxNotificationWF, getNextReminder } from "./lib/workflows";
+export { trxNotificationWF, getNextReminder } from "./src/workflows";
