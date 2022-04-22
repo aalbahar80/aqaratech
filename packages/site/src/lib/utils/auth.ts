eb367161a8e4e170c0f1c1cfa2bbde6d9f691110
nbe <aleisa.naser@gmail.com>
2022-04-23 01:25:29 +0300

    don't redirect owners for now

--- packages/site/src/lib/utils/auth.ts
+++ packages/site/src/lib/utils/auth.ts
@@ -2,10 +2,6 @@ export const protectRoute = (session: App.Session, pathname: string) => {
 	const profileUrl = '/account/profile';
 	const publicUrl = ['/', profileUrl].includes(pathname);
 
-	const contractUrl = /\/leases\/[a-f0-9-]+\/contract/;
-	const newLeaseUrl = /^\/new\/leases/;
-	const adminUrls = [contractUrl, newLeaseUrl];
-
 	let shouldRedirect = true;
 	let redirectUrl = '/';
 
@@ -20,8 +16,7 @@ export const protectRoute = (session: App.Session, pathname: string) => {
 		shouldRedirect = !pathname.startsWith('/portal/tenant/');
 		redirectUrl = `/portal/tenant/${session.authz.id}`;
 	} else if (session.authz?.isOwner) {
-		// TODO is needed if backend already checks for authz?
-		shouldRedirect = adminUrls.some((url) => pathname.match(url));
+		shouldRedirect = false;
 	} else if (session.authz?.isAdmin) {
 		shouldRedirect = false;
 	}
