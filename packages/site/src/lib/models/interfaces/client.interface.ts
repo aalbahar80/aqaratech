ae97acb2a3ee42443880363386b115b557313fa3
nbe <aleisa.naser@gmail.com>
2022-04-23 05:30:09 +0300

    simplify client form schema

--- packages/site/src/lib/models/interfaces/client.interface.ts
+++ packages/site/src/lib/models/interfaces/client.interface.ts
@@ -1,36 +1,49 @@
 import trpc from '$lib/client/trpc';
 import { getName } from '$lib/utils/common';
-import { falsyToNull, strToDate, trim } from '$lib/zodTransformers';
+import { digitsOnly, falsyToNull, strToDate, trim } from '$lib/zodTransformers';
 import type { IEntity } from '$models/interfaces/entity.interface';
 import { z } from 'zod';
 
 export const schema = z.object({
 	id: z.string().uuid().optional(),
-	firstName: z.string().min(1, { message: 'Required' }).transform(trim),
-	lastName: z.string().min(1, { message: 'Required' }).transform(trim),
-	email: z.string().email().or(z.literal('')).transform(falsyToNull),
+	firstName: z.string().min(2, { message: 'Required' }).transform(trim),
+	lastName: z.string().min(2, { message: 'Required' }).transform(trim),
+	email: z
+		.union([z.null(), z.literal(''), z.string().email()])
+		.transform(falsyToNull),
 	phone: z
-		.string()
-		.min(8)
-		.and(z.string().max(8))
-		.transform(trim)
-		.refine((val) => val.length === 0 || val.match(/^[0-9]+$/) !== null, {
-			message: 'Phone must contain only numbers',
-		}),
-	civilid: z
-		.string()
-		.min(12)
-		.and(z.string().max(12))
-		.or(z.literal(''))
-		.refine((val) => val.length === 0 || val.match(/^[0-9]+$/) !== null, {
-			message: 'Civil ID must contain only numbers',
-		})
+		.union([
+			z.null(),
+			z.literal(''),
+			z
+				.string()
+				.refine((val) => val.trim().length === 8, {
+					message: 'Phone number must be 8 digits',
+				})
+				.refine(digitsOnly, {
+					message: 'Phone must contain only numbers',
+				}),
+		])
 		.transform(trim)
 		.transform(falsyToNull),
-	dob: z.union([
-		z.preprocess(strToDate, z.date()).transform(falsyToNull),
-		z.literal('').transform(() => null),
-	]),
+	civilid: z
+		.union([
+			z.null(),
+			z.literal(''),
+			z
+				.string()
+				.refine((val) => val.trim().length === 12, {
+					message: 'Must be 12 digits',
+				})
+				.refine(digitsOnly, {
+					message: 'Must contain numbers only',
+				}),
+		])
+		.transform(trim)
+		.transform(falsyToNull),
+	dob: z
+		.union([z.null(), z.literal(''), z.preprocess(strToDate, z.date())])
+		.transform(falsyToNull),
 });
 
 const getOptions = async () => {
@@ -53,10 +66,10 @@ const ClientModelBase: IEntity<'clients'> = {
 	defaultForm: () => ({
 		firstName: '',
 		lastName: '',
-		phone: '',
-		email: '',
-		civilid: '',
-		dob: '',
+		phone: null,
+		email: null,
+		civilid: null,
+		dob: null,
 	}),
 };
 
