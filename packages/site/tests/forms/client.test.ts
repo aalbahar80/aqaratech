7235efd03eff7e3da73b32b6969ac57dd888709e
nbe <aleisa.naser@gmail.com>
2022-04-24 13:20:54 +0300

    assert response of 200

--- packages/site/tests/forms/client.test.ts
+++ packages/site/tests/forms/client.test.ts
@@ -1,23 +1,80 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
+import { fakeClient } from '../../../seed/generators.js';
+import { dateToInput } from '../../src/lib/utils/common.js';
 
 test.use({ storageState: './tests/config/adminStorageState.json' });
 
-test('Admin can create a new client', async ({ page }) => {
-	await page.goto('http://localhost:3000');
+const formTypes = [
+	{
+		name: 'new',
+		path: '/clients/new',
+	},
+	{
+		name: 'edit',
+		path: '/clients/:id/edit',
+	},
+];
 
-	await page.click('a:has-text("Clients")');
-	await page.click('a:has-text("New")');
+formTypes.forEach((formType) => {
+	test.describe(`${formType.name} client form`, async () => {
+		let clientId: string;
 
-	await page.fill('input[name="firstName"]', 'Test Client');
-	await page.fill('input[name="lastName"]', 'Test Client');
-	await page.fill('input[name="email"]', 'a@example.com');
-	await page.fill('input[name="phone"]', '12345678');
-	await page.fill('input[name="civilid"]', '123456789012');
-	await page.fill('input[name="dob"]', '1990-05-28');
+		test.beforeEach(async ({ page }) => {
+			const client = fakeClient();
+			await page.goto('http://localhost:3000');
 
-	await page.click('text="Create new"');
+			await page.click('a:has-text("Clients")');
+			await page.click('a:has-text("New")');
 
-	await expect(page).toHaveURL(
-		/^http:\/\/localhost:3000\/clients\/([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}){1}\/?$/,
-	);
+			await page.fill('input[name="firstName"]', client.firstName);
+			await page.fill('input[name="lastName"]', client.lastName);
+			await page.fill('input[name="email"]', client.email);
+			await page.fill('input[name="phone"]', client.phone);
+			await page.fill('input[name="civilid"]', client.civilid);
+			await page.fill('input[name="dob"]', dateToInput(client.dob));
+		});
+
+		test('returns a status of 200', async ({ page }) => {
+			const [request] = await Promise.all([
+				page.waitForRequest(/^http:\/\/localhost:3000\/trpc/),
+				await page.click('button[type="submit"]'),
+			]);
+
+			const response = await request.response();
+			expect(response.status()).toBe(200);
+
+			clientId = page.url().split('/').pop();
+		});
+
+		test('redirects to new client page', async ({ page }) => {
+			await page.click('button[type="submit"]');
+
+			await expect(page).toHaveURL(
+				/^http:\/\/localhost:3000\/clients\/([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}){1}\/?$/,
+			);
+
+			clientId = page.url().split('/').pop();
+		});
+
+		test('Client can be edited', async ({ page }) => {
+			const client = fakeClient();
+
+			await page.goto('http://localhost:3000/clients/' + clientId);
+
+			await page.click('a:has-text("Edit")');
+
+			await page.fill('input[name="firstName"]', client.firstName);
+			await page.fill('input[name="lastName"]', client.lastName);
+			await page.fill('input[name="email"]', client.email);
+			await page.fill('input[name="phone"]', client.phone);
+			await page.fill('input[name="civilid"]', client.civilid);
+			await page.fill('input[name="dob"]', dateToInput(client.dob));
+
+			await page.click('button[type="submit"]');
+
+			await expect(page).toHaveURL(
+				/^http:\/\/localhost:3000\/clients\/([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}){1}\/?$/,
+			);
+		});
+	});
 });
