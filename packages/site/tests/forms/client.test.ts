7e5e093888ad2949a297f8449288fef8fb65d988
nbe <aleisa.naser@gmail.com>
2022-04-26 05:31:16 +0300

    use clientForm.submit()

--- packages/site/tests/forms/client.test.ts
+++ packages/site/tests/forms/client.test.ts
@@ -1,7 +1,7 @@
 import type { Page } from '@playwright/test';
 import { fakeClient } from '../../../seed/generators.js';
 import { dateToInput } from '../../src/lib/utils/common.js';
-import { expect, test } from '../config/trpc-test.js';
+import { expect, test as base } from '../config/trpc-test.js';
 
 class Form {
 	constructor(public page: Page) {}
@@ -22,42 +22,9 @@ class ClientForm extends Form {
 	}
 }
 
-test.use({ storageState: './tests/config/adminStorageState.json' });
-test.describe(`New client form`, async () => {
-	test.beforeEach(async ({ page }) => {
-		const client = fakeClient();
-		await page.goto('/new/clients', {
-			waitUntil: 'networkidle',
-		});
-		await page.fill('input[name="firstName"]', client.firstName);
-		await page.fill('input[name="lastName"]', client.lastName);
-		await page.fill('input[name="email"]', client.email);
-		await page.fill('input[name="phone"]', client.phone);
-		await page.fill('input[name="civilid"]', client.civilid);
-		await page.fill('input[name="dob"]', dateToInput(client.dob));
-	});
+base.use({ storageState: './tests/config/adminStorageState.json' });
 
-	test('returns a status of 200', async ({ page }) => {
-		const [request] = await Promise.all([
-			page.waitForRequest(/^http:\/\/localhost:3000\/trpc/),
-			await page.click('button[type="submit"]'),
-		]);
-
-		const response = await request.response();
-		expect(response?.status()).toBe(200);
-	});
-
-	test('redirects to client detail page', async ({ page }) => {
-		await page.click('button[type="submit"]');
-
-		const re = new RegExp(
-			`/clients/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}`,
-		);
-		await expect(page).toHaveURL(re);
-	});
-});
-
-const editTest = test.extend<{ id: string; clientForm: ClientForm }>({
+const test = base.extend<{ id: string; clientForm: ClientForm }>({
 	id: async ({ trpcClient }, use) => {
 		// create a client
 		const { id } = await trpcClient.mutation('clients:create', fakeClient());
@@ -71,15 +38,43 @@ const editTest = test.extend<{ id: string; clientForm: ClientForm }>({
 	},
 });
 
-editTest.describe('Edit client form', async () => {
-	editTest.beforeEach(async ({ page, id }) => {
+test.describe(`New client form`, async () => {
+	test.beforeEach(async ({ page, clientForm }) => {
+		await page.goto('/new/clients', {
+			waitUntil: 'networkidle',
+		});
+		await clientForm.fill();
+	});
+
+	test('returns a status of 200', async ({ page, clientForm }) => {
+		const [request] = await Promise.all([
+			page.waitForRequest(/^http:\/\/localhost:3000\/trpc/),
+			await clientForm.submit(),
+		]);
+
+		const response = await request.response();
+		expect(response?.status()).toBe(200);
+	});
+
+	test('redirects to client detail page', async ({ page, clientForm }) => {
+		await clientForm.submit();
+
+		const re = new RegExp(
+			`/clients/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}`,
+		);
+		await expect(page).toHaveURL(re);
+	});
+});
+
+test.describe('Edit client form', async () => {
+	test.beforeEach(async ({ page, id }) => {
 		await page.goto(`/clients/${id}/edit`, {
 			waitUntil: 'networkidle',
 		});
 		// try context.on('sveltekit:start', ...)
 	});
 
-	editTest('returns a 200 response', async ({ page, clientForm }) => {
+	test('returns a 200 response', async ({ page, clientForm }) => {
 		await clientForm.fill();
 
 		const re = new RegExp('/trpc');
@@ -92,12 +87,9 @@ editTest.describe('Edit client form', async () => {
 		expect(response?.status()).toBe(200);
 	});
 
-	editTest(
-		'redirects to client detail page',
-		async ({ page, id, clientForm }) => {
-			await clientForm.submit();
-			await page.waitForLoadState('networkidle');
-			await expect(page).toHaveURL(`/clients/${id}`);
-		},
-	);
+	test('redirects to client detail page', async ({ page, id, clientForm }) => {
+		await clientForm.submit();
+		await page.waitForLoadState('networkidle');
+		await expect(page).toHaveURL(`/clients/${id}`);
+	});
 });
