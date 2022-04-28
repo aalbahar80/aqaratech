c7fe7b107bdb8f98b0a0c0bce8067f9c854e87f4
nbe <aleisa.naser@gmail.com>
2022-04-28 17:32:44 +0300

    refactor common edit form tests. All OK

--- packages/site/tests/forms/combined.test.ts
+++ packages/site/tests/forms/combined.test.ts
@@ -59,22 +59,47 @@ for (const Form of formEntities) {
 	});
 }
 
-test.describe('Form: edit ', async () => {
-	for (const entity of formEntities) {
-		test.use({
-			defaultForm: async ({}, use) => {
-				await use(entity);
-			},
-		});
-		test(`${entity.urlName}: returns 200`, async ({ page, formOption }) => {
-			const url = formOption.getUrl('edit');
+for (const Form of formEntities) {
+	test.describe(`Form: edit ${Form.urlName}`, async () => {
+		// TODO: consider deleting in an afterEach
+		test.beforeEach(async ({ page, trpcClient }) => {
+			const form = new Form(page);
+			await form.setup(trpcClient);
+			const url = form.getUrl('edit');
 			await page.goto(url);
 			await page.evaluate(() => window.started);
-			formOption.alter();
-			await formOption.fill();
-			const request = await formOption.getRequest();
+			form.alter();
+			await form.fill();
+		});
+
+		test(`${Form.urlName} returns 200`, async ({ page }) => {
+			const form = new Form(page);
+			const request = await form.getRequest();
 			const response = await request.response();
 			expect(response?.status()).toBe(200);
+			await page.waitForNavigation();
 		});
-	}
-});
+
+		test(`${Form.urlName} redirects to details page`, async ({ page }) => {
+			const form = new Form(page);
+			await form.submit();
+			await page.waitForNavigation();
+			const re = new RegExp(
+				`/${form.urlName}/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}`,
+			);
+			await expect(page).toHaveURL(re);
+		});
+
+		test(`${Form.urlName} basic details are correct`, async ({ page }) => {
+			const form = new Form(page);
+			await form.submit();
+			await page.waitForNavigation();
+
+			for (const b in form.basic()) {
+				const el = page.locator(`text=${b}`).first();
+				const re = new RegExp(`${b}`);
+				expect(await el.textContent()).toMatch(re);
+			}
+		});
+	});
+}
