25f785505d6a3e0daba4f5f678f66b19cdab7895
nbe <aleisa.naser@gmail.com>
2022-04-28 19:51:30 +0300

    combine login tests

---
+++ packages/site/tests/login.spec.ts
@@ -0,0 +1,72 @@
+import { test, expect } from '@playwright/test';
+
+const users = [
+	{
+		role: 'admin',
+		email: 'admin.dev@mailthink.net',
+		password: 'test12',
+		destination: '/',
+	},
+	{
+		role: 'owner',
+		email: 'client.dev@mailthink.net',
+		password: 'test12',
+		destination: /^http:\/\/localhost:3000\/clients\/.+\/dashboard$/,
+	},
+	{
+		role: 'tenant',
+		email: 'tenant.dev@mailthink.net',
+		password: 'test12',
+		destination: /^http:\/\/localhost:3000\/portal\/tenant\/.*$/,
+	},
+];
+
+for (const user of users) {
+	test.describe(`${user.role} login:`, async () => {
+		test.beforeEach(async ({ page }) => {
+			await page.goto('/');
+			await page.locator('text=Log In >> visible=true').click();
+
+			await page.fill('input[name="username"]', user.email);
+			await page.fill('input[name="password"]', user.password);
+			await page.locator('button[name="action"]').click();
+		});
+
+		test(`${user.role} redirected to correct url`, async ({ page }) => {
+			await page.waitForNavigation();
+			await expect(page).toHaveURL(user.destination);
+		});
+
+		test('accessToken exists', async ({ context }) => {
+			const cookies = await context.cookies();
+			const accessToken = cookies.find((c) => c.name === 'accessToken');
+			expect(accessToken).toMatchObject({
+				name: 'accessToken',
+				value: expect.stringMatching(
+					/^[a-zA-Z0-9-_]+\.[a-zA-Z0-9-_]+\.[a-zA-Z0-9-_]+$/,
+				),
+				domain: 'localhost',
+				path: '/',
+				expires: expect.any(Number),
+				httpOnly: true,
+				secure: true,
+			});
+		});
+
+		test('idToken exists', async ({ context }) => {
+			const cookies = await context.cookies();
+			const idToken = cookies.find((c) => c.name === 'idToken');
+			expect(idToken).toMatchObject({
+				name: 'idToken',
+				value: expect.stringMatching(
+					/^[a-zA-Z0-9-_]+\.[a-zA-Z0-9-_]+\.[a-zA-Z0-9-_]+$/,
+				),
+				domain: 'localhost',
+				path: '/',
+				expires: expect.any(Number),
+				httpOnly: true,
+				secure: true,
+			});
+		});
+	});
+}
--- packages/site/tests/test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test('home page has title', async ({ page }) => {
-	await page.goto('/');
-	expect(await page.textContent('h1')).toBe(
-		'A better way to manage your properties',
-	);
-});
