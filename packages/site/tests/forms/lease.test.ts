2607870211db7a98260563a34e4e71a23b36e93b
nbe <aleisa.naser@gmail.com>
2022-04-29 15:40:19 +0300

    split lease tests into own file

---
+++ packages/site/tests/forms/lease.test.ts
@@ -0,0 +1,70 @@
+import { getAddress, getName } from '../../src/lib/utils/common.js';
+import { test } from '../config/test-setup.js';
+import { preselected } from '../utils.js';
+
+test('new lease: preselected property from URL', async ({ page, unitForm }) => {
+	await page.goto(`/new/leases?unitId=${unitForm.id}`);
+	await page.evaluate(() => window.started);
+	await preselected(
+		page,
+		page.locator('#propertyId'),
+		getAddress(unitForm.property),
+	);
+});
+
+test('new lease: preselected unit from URL', async ({ page, unitForm }) => {
+	await page.goto(`/new/leases?unitId=${unitForm.id}`);
+	await page.evaluate(() => window.started);
+	await preselected(
+		page,
+		page.locator('#unitId'),
+		[unitForm.data.type, unitForm.data.unitNumber]
+			.filter((str) => str)
+			.join(' '),
+	);
+});
+
+test('new lease: preselected tenant from URL', async ({ page, unitForm }) => {
+	await page.goto(`/new/leases?unitId=${unitForm.id}`);
+	await page.evaluate(() => window.started);
+	await preselected(
+		page,
+		page.locator('#unitId'),
+		[unitForm.data.type, unitForm.data.unitNumber]
+			.filter((str) => str)
+			.join(' '),
+	);
+});
+
+test.describe('edit lease', async () => {
+	test.beforeEach(async ({ page, leaseForm }) => {
+		await page.goto(`/leases/${leaseForm.id}/edit`);
+		await page.evaluate(() => window.started);
+	});
+
+	// test('property is preselected', async ({ page, leaseForm }) => {
+	// 	await preselected(
+	// 		page,
+	// 		page.locator('#propertyId'),
+	// 		getAddress(leaseForm.property),
+	// 	);
+	// });
+
+	test('unit is preselected', async ({ page, leaseForm }) => {
+		await preselected(
+			page,
+			page.locator('#unitId'),
+			[leaseForm.unit.type, leaseForm.unit.unitNumber]
+				.filter((str) => str)
+				.join(' '),
+		);
+	});
+
+	test('tenant is preselected', async ({ page, leaseForm }) => {
+		await preselected(
+			page,
+			page.locator('#tenantId'),
+			getName(leaseForm.tenant),
+		);
+	});
+});
