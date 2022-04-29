21e2e03ec9b8325986d8dec22bc691852609b2b8
nbe <aleisa.naser@gmail.com>
2022-04-30 01:57:23 +0300

    add test "able to submit form"

--- packages/site/tests/forms/attribution.test.ts
+++ packages/site/tests/forms/attribution.test.ts
@@ -1,25 +1,26 @@
+import { ExpenseForm } from './form.js';
 import { getAddress, getName } from '../../src/lib/utils/common.js';
 import { expect, test } from '../config/test-setup.js';
 import { preselected } from '../utils.js';
 
 test.use({ storageState: './config/adminStorageState.json' });
-test.describe('Edit expense form', async () => {
-	test.beforeEach(async ({ page, expenseForm }) => {
-		await page.goto(`/${expenseForm.data.id}/edit`);
-		await page.evaluate(() => window.started);
-	});
+// test.describe('Edit expense form', async () => {
+// 	test.beforeEach(async ({ page, expenseForm }) => {
+// 		await page.goto(`/${expenseForm.data.id}/edit`);
+// 		await page.evaluate(() => window.started);
+// 	});
 
-	test('category is preselected', async ({ expenseForm, page }) => {
-		const el = page.locator('#category');
-		await preselected(page, el, expenseForm.data.category);
-	});
+// 	test('category is preselected', async ({ expenseForm, page }) => {
+// 		const el = page.locator('#category');
+// 		await preselected(page, el, expenseForm.data.category);
+// 	});
 
-	// test('radio is preselected', async ({ expenseForm, page }) => {
-	// 	const el = page.locator('.selection');
-	// 	const selected = await el.innerText();
-	// 	expect(selected).toMatch(expenseForm.data);
-	// });
-});
+// test('radio is preselected', async ({ expenseForm, page }) => {
+// 	const el = page.locator('.selection');
+// 	const selected = await el.innerText();
+// 	expect(selected).toMatch(expenseForm.data);
+// });
+// });
 
 // test('New expense: preselected clientId from URL', async ({
 // 	page,
@@ -52,3 +53,19 @@ test.describe('Edit expense form', async () => {
 // 		);
 // 	});
 // });
+
+test('able to submit', async ({ page }) => {
+	await page.goto(`/new/expenses`);
+	await page.evaluate(() => window.started);
+	const form = new ExpenseForm(page);
+	const button = page.locator('button[type="submit"]');
+	const el = page.locator('form');
+	// const isValid = await el.getAttribute('data-test');
+	expect.soft(await el.getAttribute('data-test')).toBe('ok');
+	await form.fill();
+	expect.soft(await el.getAttribute('data-test')).toBe('ok');
+	expect.soft(await button.isEnabled()).toBe(true);
+	await form.submit();
+	expect(await button.isEnabled()).toBe(false);
+	expect(await el.getAttribute('data-test')).toBe('ok');
+});
