c57573b60da4306de83adfcf6b7f2b2d38635852
nbe <aleisa.naser@gmail.com>
2022-05-01 01:47:12 +0300

    use fixtures in propertyForm tests

--- packages/site/tests/forms/property.test.ts
+++ packages/site/tests/forms/property.test.ts
@@ -1,54 +1,55 @@
-import { preselected } from '../utils.js';
 import { getAddress, getName } from '../../src/lib/utils/common.js';
-import { expect, test } from '../config/test-setup.js';
+import { expect, test as base } from '../config/test-setup.js';
+import { preselected } from '../utils.js';
+import { PropertyForm } from './form.js';
+
+const test = base.extend<{ form: PropertyForm }>({
+	form: async ({ page }, use) => {
+		const form = new PropertyForm();
+		form.page = page;
+		await form.setupEdit();
+		await use(form);
+		await form.clean();
+	},
+});
 
 test.use({ storageState: './config/adminStorageState.json' });
+
 test.describe('Edit property form', async () => {
-	test.beforeEach(async ({ page, propertyForm }) => {
-		await page.goto(`/properties/${propertyForm.data.id}/edit`);
+	test.beforeEach(async ({ page, form }) => {
+		await page.goto(`/properties/${form.data.id}/edit`);
 		await page.evaluate(() => window.started);
 	});
 
-	test('client is preselected', async ({ propertyForm, page }) => {
+	test('client is preselected', async ({ form, page }) => {
 		const el = page.locator('#clientId');
-		await preselected(page, el, getName(propertyForm.client));
+		await preselected(page, el, getName(form.client));
 	});
 
-	test('area is preselected', async ({ propertyForm, page }) => {
+	test('area is preselected', async ({ form, page }) => {
 		const el = page.locator('.selection');
 		const selected = await el.innerText();
-		expect(selected).toMatch(propertyForm.data.area);
+		expect(selected).toMatch(form.data.area);
 	});
 });
 
-test('New property: preselected clientId from URL', async ({
-	page,
-	clientForm,
-}) => {
-	await page.goto(`/new/properties?clientId=${clientForm.id}`);
+test('New property: preselected clientId from URL', async ({ page, form }) => {
+	await page.goto(`/new/properties?clientId=${form.client.id}`);
 	await page.evaluate(() => window.started);
-	await preselected(page, page.locator('#clientId'), getName(clientForm.data));
+	await preselected(page, page.locator('#clientId'), getName(form.client));
 });
 
 test.describe('new unit', async () => {
-	test.beforeEach(async ({ page, propertyForm }) => {
-		await page.goto(`/new/units?propertyId=${propertyForm.id}`);
+	test.beforeEach(async ({ page, form }) => {
+		await page.goto(`/new/units?propertyId=${form.id}`);
 		await page.evaluate(() => window.started);
 	});
 
-	test('preselected property from URL', async ({ page, propertyForm }) => {
-		await preselected(
-			page,
-			page.locator('#propertyId'),
-			getAddress(propertyForm.data),
-		);
+	test('preselected property from URL', async ({ page, form }) => {
+		await preselected(page, page.locator('#propertyId'), getAddress(form.data));
 	});
 
-	test('preselected client from URL', async ({ page, propertyForm }) => {
-		await preselected(
-			page,
-			page.locator('#clientId'),
-			getName(propertyForm.client),
-		);
+	test('preselected client from URL', async ({ page, form }) => {
+		await preselected(page, page.locator('#clientId'), getName(form.client));
 	});
 });
