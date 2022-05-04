cf3475351d4b66613d6fae3948b38e2b9360d2ed
nbe <aleisa.naser@gmail.com>
2022-05-04 15:15:16 +0300

    init tenantCreateUser

---
+++ packages/site/tests/login/tenant.spec.ts
@@ -0,0 +1,90 @@
+import { test, expect } from '@playwright/test';
+import { fakeTenant, testTenantId } from '../../../seed/generators.js';
+import prisma from '../config/prismaClient.js';
+
+// TODO: make compatible with global setup (which cleans db before running tests)
+const users = [
+	// {
+	// 	role: 'admin',
+	// 	email: 'admin.dev@mailthink.net',
+	// 	password: 'test12',
+	// 	destination: '/',
+	// },
+	// {
+	// 	role: 'owner',
+	// 	email: 'client.dev@mailthink.net',
+	// 	password: 'test12',
+	// 	destination: /^http:\/\/localhost:3000\/clients\/.+\/dashboard$/,
+	// },
+	{
+		role: 'tenant',
+		email: 'tenant.dev@mailthink.net',
+		password: 'test12',
+		destination: /^http:\/\/localhost:3000\/portal\/tenant\/.*$/,
+	},
+];
+
+// Explicitly set storageState to undefined to avoid reused auth state.
+test.use({ storageState: undefined });
+
+for (const user of users) {
+	test.describe(`${user.role} login:`, async () => {
+		test.beforeAll(async () => {
+			fakeTenant();
+			await prisma.tenant.create({
+				data: {
+					...fakeTenant(),
+					id: testTenantId,
+				},
+			});
+		});
+		test.beforeEach(async ({ page }) => {
+			await page.goto('/');
+			await page.locator('text=Log In >> visible=true').click();
+
+			await page.fill('input[name="username"]', user.email);
+			await page.fill('input[name="password"]', user.password);
+			await page.locator('button[name="action"]').click();
+		});
+
+		test(`${user.role} redirected to correct url`, async ({ page }) => {
+			await page.waitForNavigation({
+				waitUntil: 'networkidle',
+				timeout: 10000,
+			});
+			await expect(page).toHaveURL(user.destination);
+		});
+
+		test('accessToken exists', async ({ context }) => {
+			const cookies = await context.cookies();
+			const accessToken = cookies.find((c) => c.name === 'accessToken');
+			expect(accessToken).toMatchObject({
+				name: 'accessToken',
+				value: expect.stringMatching(
+					/^[a-zA-Z0-9-_]+\.[a-zA-Z0-9-_]+\.[a-zA-Z0-9-_]+$/,
+				),
+				domain: 'localhost',
+				path: '/',
+				expires: expect.any(Number),
+				httpOnly: true,
+				secure: true,
+			});
+		});
+
+		test('idToken exists', async ({ context }) => {
+			const cookies = await context.cookies();
+			const idToken = cookies.find((c) => c.name === 'idToken');
+			expect(idToken).toMatchObject({
+				name: 'idToken',
+				value: expect.stringMatching(
+					/^[a-zA-Z0-9-_]+\.[a-zA-Z0-9-_]+\.[a-zA-Z0-9-_]+$/,
+				),
+				domain: 'localhost',
+				path: '/',
+				expires: expect.any(Number),
+				httpOnly: true,
+				secure: true,
+			});
+		});
+	});
+}
