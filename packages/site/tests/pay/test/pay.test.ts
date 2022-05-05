d42114ba05a495f9af88252fac7631ed4e62481d
nbe <aleisa.naser@gmail.com>
2022-05-06 01:19:04 +0300

    remove tenant portal test

--- packages/site/tests/pay/test/pay.test.ts
+++ packages/site/tests/pay/test/pay.test.ts
@@ -1,9 +1,5 @@
 import { expect, test as base, type Page } from '@playwright/test';
-import {
-	testTenantEmail,
-	testTenantId,
-	testTenantPassword,
-} from '../../../../seed/generators.js';
+import { testTenantId } from '../../../../seed/generators.js';
 import { setupLease, setupTrx } from '../setup.js';
 
 const fill = async (page: Page, success = true) => {
@@ -73,54 +69,41 @@ test('indicate payment failure', async ({ page, trxId }) => {
 	await expect.soft(btn).toBeVisible();
 });
 
-// test('transaction can be paid without auth', async ({ page, trxId }) => {
-// 	await page.goto(`/p/transactions/${trxId}`);
+// test('portal shows correct payment status', async ({ page, trxId }) => {
+// 	// Login
+// 	await page.goto('/');
+// 	await page.locator('text=Log In >> visible=true').click();
+
+// 	await page.fill('input[name="username"]', testTenantEmail);
+// 	await page.fill('input[name="password"]', testTenantPassword);
+// 	await page.locator('button[name="action"]').click();
+
+// 	// TODO: replace with proper hydration check
+// 	await page.goto(`/portal/tenant/${testTenantId}`);
+// 	await page.click('text=Pay');
+
+// 	// Pay
 // 	await Promise.all([
 // 		page.waitForNavigation(),
 // 		page.locator('text=Pay').click(),
 // 	]);
-// 	await fill(page);
+// 	await page
+// 		.locator(
+// 			'text=Select Your Bank: Select Your Bank Ahli United Bank [AUB] Al Ahli Bank of Kuwait >> select',
+// 		)
+// 		.selectOption('201825717889145|Knet Test Card [KNET1]|0.000');
+// 	await page.locator('input[name="debitNumber"]').fill('0000000001');
+// 	await page.locator('select').nth(2).selectOption('9');
+// 	await page.locator('select').nth(3).selectOption('2025');
+// 	await page.locator('input[name="cardPin"]').fill('1111');
+// 	await page.locator('text=Submit').click();
+// 	await page.locator('input:has-text("Confirm")').click();
+// 	const trxList = page.locator('ul#trxList');
+// 	await expect.soft(trxList).toBeVisible();
 
-// 	const badge = page.locator('id=badge');
-// 	await expect.soft(badge).toHaveText('Paid');
-// 	await expect.soft(badge).toHaveClass(/green/);
+// 	// Confirm
+// 	const card = page.locator(`id=${trxId}`);
+// 	await expect.soft(card).toBeVisible();
+// 	await expect.soft(card).toHaveClass(/isPaid/);
+// 	await expect.soft(card).not.toContainText(/Pay/i);
 // });
-
-test('tenant can pay', async ({ page, trxId }) => {
-	// Login
-	await page.goto('/');
-	await page.locator('text=Log In >> visible=true').click();
-
-	await page.fill('input[name="username"]', testTenantEmail);
-	await page.fill('input[name="password"]', testTenantPassword);
-	await page.locator('button[name="action"]').click();
-
-	// TODO: replace with proper hydration check
-	await page.goto(`/portal/tenant/${testTenantId}`);
-	await page.click('text=Pay');
-
-	// Pay
-	await Promise.all([
-		page.waitForNavigation(),
-		page.locator('text=Pay').click(),
-	]);
-	await page
-		.locator(
-			'text=Select Your Bank: Select Your Bank Ahli United Bank [AUB] Al Ahli Bank of Kuwait >> select',
-		)
-		.selectOption('201825717889145|Knet Test Card [KNET1]|0.000');
-	await page.locator('input[name="debitNumber"]').fill('0000000001');
-	await page.locator('select').nth(2).selectOption('9');
-	await page.locator('select').nth(3).selectOption('2025');
-	await page.locator('input[name="cardPin"]').fill('1111');
-	await page.locator('text=Submit').click();
-	await page.locator('input:has-text("Confirm")').click();
-	const trxList = page.locator('ul#trxList');
-	await expect.soft(trxList).toBeVisible();
-
-	// Confirm
-	const card = page.locator(`id=${trxId}`);
-	await expect.soft(card).toBeVisible();
-	await expect.soft(card).toHaveClass(/isPaid/);
-	await expect.soft(card).not.toContainText(/Pay/i);
-});
