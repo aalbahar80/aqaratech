de8e71035619388d821044043ec5be46ce239a05
nbe <aleisa.naser@gmail.com>
2022-05-09 00:45:43 +0300

    output html report in CI env

--- packages/site/tests/config.ts
+++ packages/site/tests/config.ts
@@ -13,13 +13,13 @@ export const config: PlaywrightTestConfig = {
 	fullyParallel: true,
 	/* Opt out of parallel tests on CI. */
 	workers: process.env.CI ? 1 : undefined,
-	retries: process.env.CI ? 2 : 2,
+	retries: process.env.CI ? 5 : 2,
 	maxFailures: process.env.CI ? 30 : 30,
 	forbidOnly: !!process.env.CI,
 	timeout: process.env.CI ? 30000 : 30000,
 	expect: { timeout: 10000 },
 	globalSetup: path.resolve(__dirname, 'global-setup.ts'),
-	reporter: process.env.CI ? 'list' : [['list'], ['html']],
+	reporter: process.env.CI ? [['list'], ['html']] : [['list'], ['html']],
 	use: {
 		baseURL: 'http://localhost:3000/',
 		screenshot: 'only-on-failure',
