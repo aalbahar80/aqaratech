786cd4132317317b5ff1caa459675ed65e6acf30
nbe <aleisa.naser@gmail.com>
2022-05-14 02:09:56 +0300

    [WIP] init body rows

--- packages/site/src/lib/components/table/Condensed.svelte
+++ packages/site/src/lib/components/table/Condensed.svelte
@@ -148,6 +148,45 @@
 						<tbody class="divide-y divide-gray-200 bg-white">
 							{#each transactions as transaction (transaction.id)}
 								<tr>
+									{#each headers as header, idx (header)}
+										{#if header === 'edit'}
+											<!-- <td class="px-6 py-4 whitespace-nowrap">
+                      <button
+                        type="button"
+                        class="text-sm font-medium text-gray-900 hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-5"
+                      >
+                        <svg
+                          class="h-5 w-5"
+                          fill="currentColor"
+                          viewBox="0 0 20 20"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z"
+                            clip-rule="evenodd"
+                          />
+                        </svg>
+                      </button>
+                    </td> -->
+										{:else}
+											<td
+												class={classes(
+													idx === 0 ? 'sm:pl-6 pl-4 pr-3' : 'px-2',
+													'whitespace-nowrap py-2 text-sm text-gray-500',
+												)}
+												class:font-medium={bold1}
+												class:text-gray-900={bold1 || bold2}
+											>
+												{transaction[header]}
+											</td>
+											<!-- <td
+												class="px-6 py-4 whitespace-nowrap text-left text-sm leading-5 text-gray-500"
+											>
+												{transaction[header]}
+											</td> -->
+										{/if}
+									{/each}
+
 									<td
 										class="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-6"
 									>
