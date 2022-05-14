bf16d01cc1afff7b36ff13274fd121ba87a5c1a5
nbe <aleisa.naser@gmail.com>
2022-05-14 05:33:36 +0300

    reverse tabs

--- packages/site/src/lib/components/Tabs.svelte
+++ packages/site/src/lib/components/Tabs.svelte
@@ -1,30 +1,21 @@
 <script lang="ts">
 	import { classes } from '$lib/utils/classes';
-	import {
-		CreditCard,
-		OfficeBuilding,
-		User,
-		Users,
-	} from '@steeze-ui/heroicons';
+	import { ChartBar, Database } from '@steeze-ui/heroicons';
 	import { Icon } from '@steeze-ui/svelte-icon';
 
 	const tabs = [
-		{ name: 'My Account', href: '#', icon: User, current: false },
-		{ name: 'Company', href: '#', icon: OfficeBuilding, current: false },
-		{ name: 'Team Members', href: '#', icon: Users, current: true },
-		{ name: 'Billing', href: '#', icon: CreditCard, current: false },
+		{ name: 'Chart', href: '#', icon: ChartBar, current: true },
+		{ name: 'Data', href: '#', icon: Database, current: false },
 	];
 </script>
 
 <div>
 	<div class="sm:hidden">
 		<label for="tabs" class="sr-only"> Select a tab </label>
-		<!-- {/* Use an "onChange" listener to redirect the user to the selected tab URL. */} -->
 		<select
 			id="tabs"
 			name="tabs"
 			class="block w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500"
-			defaultValue={tabs.find((tab) => tab.current).name}
 		>
 			{#each tabs as tab (tab.name)}
 				<option>{tab.name}</option>
@@ -33,7 +24,7 @@
 	</div>
 	<div class="hidden sm:block">
 		<div class="border-b border-gray-200">
-			<nav class="-mb-px flex space-x-8" aria-label="Tabs">
+			<nav class="-mb-px flex flex-row-reverse gap-x-8" aria-label="Tabs">
 				{#each tabs as tab (tab.name)}
 					<a
 						href={tab.href}
