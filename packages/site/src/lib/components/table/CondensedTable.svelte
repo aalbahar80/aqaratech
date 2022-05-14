4c1ba3c54f00605d437f04e752d4f316ae2560df
nbe <aleisa.naser@gmail.com>
2022-05-14 04:40:12 +0300

    strip condensed title

--- packages/site/src/lib/components/table/CondensedTable.svelte
+++ packages/site/src/lib/components/table/CondensedTable.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import CondensedActionCell from '$lib/components/table/CondensedActionCell.svelte';
 	import CondensedCell from '$lib/components/table/CondensedCell.svelte';
+	import CondensedTitle from '$lib/components/table/CondensedTitle.svelte';
 	import { classes } from '$lib/utils';
 
 	interface RowHeader {
@@ -14,24 +15,8 @@
 </script>
 
 <div class="px-4 sm:px-6 lg:px-8">
-	<div class="sm:flex sm:items-center">
-		<div class="sm:flex-auto">
-			<h1 class="text-xl font-semibold text-gray-900">Transactions</h1>
-			<p class="mt-2 text-sm text-gray-700">
-				A table of placeholder stock market data that does not make any sense.
-			</p>
-		</div>
-		<div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-			<button
-				type="button"
-				class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
-			>
-				Export
-			</button>
-		</div>
-	</div>
 	<div class="mt-8 flex flex-col">
-		<div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+		<div class="-my-2 -mx-4 h-72 overflow-auto sm:-mx-6 lg:-mx-8">
 			<div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
 				<div
 					class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg"
