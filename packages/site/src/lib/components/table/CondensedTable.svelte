3d7bb8bdc8eb2e48d01bdee8a75ac41a2e790737
nbe <aleisa.naser@gmail.com>
2022-05-15 05:27:58 +0300

    add padding

--- packages/site/src/lib/components/table/CondensedTable.svelte
+++ packages/site/src/lib/components/table/CondensedTable.svelte
@@ -10,7 +10,7 @@
 	let pageIdx = 1;
 </script>
 
-<div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+<div class="inline-block min-w-full py-6 align-middle md:px-6 lg:px-8">
 	<div class="shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
 		<table class="min-w-full divide-y divide-gray-300">
 			<thead class="sticky bg-gray-50" style="inset-block-start: 0;">
