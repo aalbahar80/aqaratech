ffe3a8491cd8aa4e0f230af819c42e8cebca7079
nbe <aleisa.naser@gmail.com>
2022-05-16 21:55:49 +0300

    adjust second color

--- packages/seed/src/constants.ts
+++ packages/seed/src/constants.ts
@@ -309,7 +309,7 @@ export const coordinates: [number, number][] = [
 ];
 
 export const palette: { [key: string]: string[] } = {
-	2: ["#003f5c", "#ffa600"],
+	2: ["#003f5c", "#f95d6a"],
 	3: ["#003f5c", "#bc5090", "#ffa600"],
 	4: ["#003f5c", "#7a5195", "#ef5675", "#ffa600"],
 	5: ["#003f5c", "#58508d", "#bc5090", "#ff6361", "#ffa600"],
