5b688ddfe2da2e4991d386a62245bdb0668e6c71
nbe <aleisa.naser@gmail.com>
2022-05-18 02:06:34 +0300

    addToasts in AsyncButton

--- packages/site/src/lib/components/AsyncButton.svelte
+++ packages/site/src/lib/components/AsyncButton.svelte
@@ -1,15 +1,41 @@
 <script lang="ts">
-	export let func: () => Promise<any>;
+	import { addToast } from '$lib/stores/toast';
+
+	export let func: () => Promise<void> | Promise<Response>;
 	export let disabled = false;
+
 	let loading = false;
 
 	const handle = async () => {
 		disabled = true;
 		loading = true;
 		try {
-			await func();
+			const res = await func();
+			// check if function is a promise that resolves to a response
+			if (res instanceof Response) {
+				// dispatch success event
+				if (res.ok) {
+					addToast({
+						duration: 30000,
+						props: {
+							kind: 'success',
+							title: 'Success',
+						},
+					});
+				} else {
+					throw new Error(res.statusText);
+				}
+			}
 		} catch (e) {
+			// dispatch error event
 			console.error(e);
+			addToast({
+				duration: 30000,
+				props: {
+					kind: 'error',
+					title: 'Error',
+				},
+			});
 		} finally {
 			disabled = false;
 			loading = false;
