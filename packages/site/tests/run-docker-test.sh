4fdab8a06f675a77098a8d2adcded5fd7bc1e502
nbe <aleisa.naser@gmail.com>
2022-05-19 13:08:58 +0300

    remove dangling images after building new

--- packages/site/tests/run-docker-test.sh
+++ packages/site/tests/run-docker-test.sh
@@ -7,11 +7,12 @@ docker stop sitetest
 # remove exited containers
 docker rm $(docker ps -qa --no-trunc --filter "status=exited")
 
-# remove dangling images
-docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
 
 # build new image
 docker build --pull --rm -f "Dockerfile" -t aqtech:latest "."
+
+# remove dangling images
+docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
 
 # Run contaner from image
 # use port 3001 to avoid conflict with a local playwright instance
