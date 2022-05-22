d252101bfad756ce1a1743985bfd3d3d30e3f530
nbe <aleisa.naser@gmail.com>
2022-05-22 08:35:13 +0300

    rework Reminder class logic

--- packages/site/src/lib/models/classes/reminder.class.ts
+++ packages/site/src/lib/models/classes/reminder.class.ts
@@ -8,140 +8,146 @@ const {
 	twilioConfig: { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_FROM_NUMBER },
 } = environment;
 
+interface ReminderData {
+	email: string | null;
+	phone: string | null;
+	name: string;
+	amount: number;
+	date: Date;
+}
+
 interface EmailModel {
-	trxUrl: string;
+	email: string;
 	name: string;
 	amount: string;
 	date: string;
+	trxUrl: string;
 }
 
 export class Reminder {
-	constructor(
-		public trxId: string,
-		public phone?: string | null,
-		public email?: string | null,
-		public emailModel?: EmailModel | null,
-	) {}
-
-	async sendSms() {
-		if (!this.phone) {
-			await this.getContactInfo();
-		}
-		if (this.phone) {
-			const res = await fetch(
-				`https://api.twilio.com/2010-04-01/Accounts/${TWILIO_ACCOUNT_SID}/Messages.json`,
-				{
-					method: 'POST',
-					redirect: 'follow',
-					headers: {
-						'Content-Type': 'application/x-www-form-urlencoded',
-						Authorization: `Basic ${Buffer.from(
-							`${TWILIO_ACCOUNT_SID}:${TWILIO_AUTH_TOKEN}`,
-						).toString('base64')}`,
-					},
-					body: new URLSearchParams({
-						Body: this.trxUrl,
-						// MessagingServiceSid: TWILIO_MESSAGING_SERVICE_SID,
-						From: TWILIO_FROM_NUMBER,
-						To: this.phone,
-					}),
-				},
-			);
-			const sms = await res.json();
-			console.log(sms);
-			return sms;
-		} else {
-			throw new Error('No phone number');
-		}
-	}
-
-	async sendEmail() {
-		if (!this.email) {
-			await this.getContactInfo();
-		}
-		if (this.email) {
-			// handle res.ok/notok
-			const res = await fetch(
-				'https://api.postmarkapp.com/email/withTemplate',
-				{
-					method: 'POST',
-					headers: {
-						Accept: 'application/json',
-						'Content-Type': 'application/json',
-						'X-Postmark-Server-Token': 'cd6245ee-08c5-44ff-bfe0-5e4c4361ab70',
-					},
-					body: JSON.stringify({
-						From: 'Aqaratech <notifications@aqaratech.com>',
-						// To: this.email,
-						To: 'dev@aqaratech.com',
-						TemplateAlias: 'invoice',
-						TemplateModel: this.getEmailModel(),
-					}),
-				},
-			);
-			const data = await res.json();
-			console.log(data);
-			if (data.Message === 'OK') {
-				return true;
-			} else {
-				return false;
-			}
-		} else {
-			throw new Error('No email');
-		}
-	}
+	constructor(public trxId: string, public data?: ReminderData) {}
 
 	public get trxUrl(): string {
 		return `${urlOrigin}/p/transactions/${this.trxId}`;
 	}
 
-	get body(): string {
-		return `Please use this link to pay your monthly rent: \n ${this.trxUrl}`;
-	}
-
-	getEmailModel() {
-		if (!this.emailModel) {
-			const trx = {
-				id: 'a1145396-0e06-4e26-8891-e0ee0ec97902',
-				amount: 2826,
-				postAt: new Date(),
-				// address: '' // TODO: add
-			};
-			const body = {
-				name: 'John Doe',
-				amount: kwdFormat(trx.amount),
-				date: format(trx.postAt, 'MMM yyyy'),
-				trxUrl: this.trxUrl,
-			};
-			this.emailModel = body;
-		}
-		return this.emailModel;
+	async getSmsBody(): Promise<string> {
+		const info = await this.getContactInfo();
+		return `Use this link to pay your ${format(info.date, 'MMM')} : \n ${
+			this.trxUrl
+		}`;
 	}
 
 	private async getContactInfo() {
-		try {
-			if (!this.email || !this.phone) {
-				const trx = await prismaClient.transaction.findUnique({
-					where: { id: this.trxId },
-					select: {
-						lease: {
-							select: {
-								tenant: {
-									select: {
-										phone: true,
-										email: true,
-									},
+		if (!this.data) {
+			const trx = await prismaClient.transaction.findUnique({
+				where: { id: this.trxId },
+				select: {
+					amount: true,
+					postAt: true,
+					lease: {
+						select: {
+							tenant: {
+								select: {
+									phone: true,
+									email: true,
+									firstName: true,
+									lastName: true,
 								},
 							},
 						},
 					},
-					rejectOnNotFound: true,
-				});
-				this.email = trx.lease.tenant.email;
-				this.phone = trx.lease.tenant.phone;
-			}
-		} catch (e) {
-			console.error(e);
+				},
+				rejectOnNotFound: true, // TODO: reconsider
+			});
+			this.data = {
+				email: trx.lease.tenant.email,
+				phone: trx.lease.tenant.phone,
+				// amount: kwdFormat(trx.amount),
+				// date: format(trx.postAt, 'MMM yyyy'),
+				amount: trx.amount,
+				date: trx.postAt,
+				name: `${trx.lease.tenant.firstName} ${trx.lease.tenant.lastName}`,
+			};
+		}
+		return this.data;
+	}
+
+	async sendSms() {
+		const info = await this.getContactInfo();
+		const smsBody = await this.getSmsBody();
+		if (!info.phone) {
+			return {
+				success: false,
+				errorMsg: 'No phone number found',
+			};
+		}
+		const res = await fetch(
+			`https://api.twilio.com/2010-04-01/Accounts/${TWILIO_ACCOUNT_SID}/Messages.json`,
+			{
+				method: 'POST',
+				redirect: 'follow',
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded',
+					Authorization: `Basic ${Buffer.from(
+						`${TWILIO_ACCOUNT_SID}:${TWILIO_AUTH_TOKEN}`,
+					).toString('base64')}`,
+				},
+				body: new URLSearchParams({
+					Body: smsBody,
+					// MessagingServiceSid: TWILIO_MESSAGING_SERVICE_SID,
+					From: TWILIO_FROM_NUMBER,
+					To: info.phone,
+				}),
+			},
+		);
+		const sms = await res.json();
+		console.log(sms);
+		return {
+			success: true, // true here refers to fetch not throwing, still need to handle status >= 400
+			sms,
+			errorMsg: 'No phone number found',
+		};
+	}
+
+	async sendEmail() {
+		// TODO: handle res.ok/notok
+		const info = await this.getContactInfo();
+		const res = await fetch('https://api.postmarkapp.com/email/withTemplate', {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+				'X-Postmark-Server-Token': 'cd6245ee-08c5-44ff-bfe0-5e4c4361ab70',
+			},
+			body: JSON.stringify({
+				From: 'Aqaratech <notifications@aqaratech.com>',
+				To: info.email,
+				// To: 'dev@aqaratech.com',
+				TemplateAlias: 'invoice',
+				TemplateModel: {
+					email: info.email,
+					phone: info.phone,
+					amount: kwdFormat(info.amount),
+					date: format(info.date, 'MMM yyyy'),
+					name: info.name,
+					trxUrl: this.trxUrl,
+				} as EmailModel,
+			}),
+		});
+		const data = await res.json();
+		console.log(data);
+		if (data.Message === 'OK') {
+			return {
+				success: true,
+				data,
+			};
+		} else {
+			return {
+				success: false, // true here refers to fetch not throwing, still need to handle status >= 400
+				data,
+				errorMsg: 'Failed to send email',
+			};
 		}
 	}
 }
