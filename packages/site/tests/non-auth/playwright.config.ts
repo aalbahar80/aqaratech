00af616453dc8bcdb6e672556e156d52b7cf0e33
nbe <aleisa.naser@gmail.com>
2022-05-23 19:11:05 +0300

    re-enable payment tests

--- packages/site/tests/non-auth/playwright.config.ts
+++ packages/site/tests/non-auth/playwright.config.ts
@@ -4,12 +4,12 @@ import { config as baseConfig } from '../config.js';
 const config: PlaywrightTestConfig = {
 	...baseConfig,
 	projects: [
-		// {
-		// 	name: 'pay',
-		// 	testMatch: ['pay.test.ts'],
-		// 	timeout: 60 * 1000,
-		// 	retries: 0, // until mocking
-		// },
+		{
+			name: 'pay',
+			testMatch: ['pay.test.ts'],
+			timeout: 60 * 1000,
+			retries: 0, // until mocking
+		},
 		{
 			name: 'login',
 			testMatch: ['login.spec.ts'],
