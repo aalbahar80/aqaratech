a12b4786a020c4d4ac0a987208b12175a17054b2
nbe <aleisa.naser@gmail.com>
2022-05-25 10:00:40 +0300

    hide cancel button in Form

--- packages/site/src/lib/components/form/Form.svelte
+++ packages/site/src/lib/components/form/Form.svelte
@@ -202,12 +202,12 @@
 			</div>
 		</div>
 		<div class="flex flex-shrink-0 justify-end space-x-4 px-4 py-4">
-			<button
+			<!-- <button
 				type="button"
 				class="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
 			>
 				Cancel
-			</button>
+			</button> -->
 
 			<Button
 				text={FormType === 'edit' ? 'Save changes' : 'Create new'}
