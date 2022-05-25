e3d45dc50a50f9d7e4bef3882741ba5b54cbf073
nbe <aleisa.naser@gmail.com>
2022-05-25 05:48:42 +0300

    simplify grouping logic

--- packages/site/src/lib/components/dashboard/stores/charts/expense.ts
+++ packages/site/src/lib/components/dashboard/stores/charts/expense.ts
@@ -1,5 +1,6 @@
 import type { InferQueryOutput } from '$lib/client/trpc';
 import { getColor } from '$lib/config/constants';
+import { startCase } from '$lib/utils/common';
 import { getMonths } from '$lib/utils/group';
 import { closestTo, isSameDay } from 'date-fns';
 import { derived, type Writable } from 'svelte/store';
@@ -10,7 +11,6 @@ type ChartData = {
 	total: number;
 	date: Date;
 	group: string;
-	address: string;
 }[];
 
 const aggregate = (
@@ -25,15 +25,13 @@ const aggregate = (
 	data.forEach((trx) => {
 		const month = closestTo(trx.postAt, months);
 		const group =
-			expenseMeta.categories.find((c) => c.id === trx.expenseCategoryId)
-				?.expenseGroupId ?? 'other';
-		const address = trx.address;
+			groupBy === 'property'
+				? trx.address
+				: expenseMeta.categories.find((c) => c.id === trx.expenseCategoryId)
+						?.expenseGroupId ?? 'other';
 		if (month) {
 			const index = buckets.findIndex((bucket) => {
-				const condition =
-					groupBy === 'property'
-						? bucket.address === address
-						: bucket.group === group;
+				const condition = bucket.group === group;
 				return isSameDay(bucket.date, month) && condition;
 			});
 			if (index !== -1) {
@@ -43,7 +41,6 @@ const aggregate = (
 					total: trx.amount,
 					date: month,
 					group: group,
-					address,
 				});
 			}
 		}
@@ -60,21 +57,15 @@ const getDatasets = (
 	console.time('getDatasets');
 	const aggregated = aggregate(data, groupBy, expenseMeta);
 
-	const properties = aggregated.map((bucket) => bucket.address); // use id here?
-	const uniqueProperties = [...new Set(properties)];
-	uniqueProperties.sort((a, b) => a.localeCompare(b)); // alphabetical
+	const groups = aggregated.map((bucket) => bucket.group); // use id here?
+	const uniqueGroups = [...new Set(groups)];
+	uniqueGroups.sort((a, b) => a.localeCompare(b)); // alphabetical
 
-	const expenseGroups = expenseMeta.groups.map((c) => c.id);
-
-	const groups = groupBy === 'property' ? uniqueProperties : expenseGroups;
-	const datasets = groups.map((group, n) => {
-		const backgroundColor = getColor(n, groups.length);
+	const datasets = uniqueGroups.map((group, n) => {
+		const backgroundColor = getColor(n, uniqueGroups.length);
 		return {
-			label: group,
-			data:
-				groupBy === 'property'
-					? aggregated.filter((item) => item.address === group)
-					: aggregated.filter((item) => item.group === group),
+			label: startCase(group),
+			data: aggregated.filter((item) => item.group === group),
 			parsing: {
 				yAxisKey: 'total',
 				xAxisKey: 'date',
