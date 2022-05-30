46520a34694228335dc0ae167c568d35b9df4a4a
nbe <aleisa.naser@gmail.com>
2022-05-31 01:09:36 +0300

    don't cache chart data

--- packages/site/src/lib/server/trpc/config.ts
+++ packages/site/src/lib/server/trpc/config.ts
@@ -17,17 +17,8 @@ export const createContext = async (event: RequestEvent) => {
 export type Context = inferAsyncReturnType<typeof createContext>;
 
 export const responseMeta: ResponseMetaFn = ({ type, errors, paths }) => {
-	const charts = paths?.every((path) => path.startsWith('owner:charts'));
 	if (type === 'query' && errors.length === 0) {
-		if (charts) {
-			// TODO review caching, charts and others
-			const duration = 60 * 60 * 24 * 7;
-			return {
-				headers: {
-					'cache-control': `max-age=60, stale-while-revalidate=${duration}, private`,
-				},
-			};
-		} else if (paths?.includes('public:expenses:meta')) {
+		if (paths?.includes('public:expenses:meta')) {
 			const duration = 60 * 60 * 24 * 7;
 			return {
 				headers: {
