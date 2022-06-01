39458f6c5239eee25a99c17c247b5d440ad3e55b
nbe <aleisa.naser@gmail.com>
2022-06-01 07:16:36 +0300

    simplify save endpoint

--- packages/site/src/lib/server/trpc/routers/admin/expenseMeta.ts
+++ packages/site/src/lib/server/trpc/routers/admin/expenseMeta.ts
@@ -1,40 +1,36 @@
 import prismaClient from '$lib/server/prismaClient';
-import { Prisma } from '@prisma/client';
-import { TRPCError } from '@trpc/server';
 import { z } from 'zod';
 import { createRouter } from './createRouter';
 
 export const expenseMeta = createRouter()
 	.mutation('group:save', {
 		input: z.object({
-			id: z.string(),
+			id: z.number().optional(),
 			en: z.string(),
 			ar: z.string(),
 		}),
-		resolve: async ({ input }) => {
-			console.log({ input }, 'expenseMeta.ts ~ 17');
-			try {
-				await prismaClient.expenseGroup.upsert({
-					where: { id: input.id ?? undefined },
-					create: input,
-					update: input,
-				});
-			} catch (e) {
-				if (
-					e instanceof Prisma.PrismaClientKnownRequestError &&
-					e.code === 'P2002'
-				) {
-					console.error(e);
-					throw new TRPCError({
-						code: 'CONFLICT',
-						message: 'Duplicate key error',
-					});
-				}
-			}
-		},
+		resolve: ({ input: { id, ...data } }) =>
+			id
+				? prismaClient.expenseGroup.update({
+						data,
+						where: { id },
+						select: {
+							id: true,
+							en: true,
+							ar: true,
+						},
+				  })
+				: prismaClient.expenseGroup.create({
+						data,
+						select: {
+							id: true,
+							en: true,
+							ar: true,
+						},
+				  }),
 	})
 	.mutation('group:delete', {
-		input: z.string(),
+		input: z.number(),
 		resolve: async ({ input }) => {
 			await prismaClient.expenseGroup.delete({
 				where: { id: input },
