47b5c2788eaa3e5a102f1ff5f90ad042b72b5e32
nbe <aleisa.naser@gmail.com>
2022-06-02 16:32:25 +0300

    rename settings pages

--- packages/site/src/routes/settings/expense-categories.svelte
+++ packages/site/src/routes/settings/expense-categories.svelte
@@ -5,9 +5,12 @@
 	import { trpc, type InferQueryOutput } from '$lib/client/trpc';
 	import AsyncButton from '$lib/components/AsyncButton.svelte';
 	import { addToast } from '$lib/stores/toast';
+	import { classes } from '$lib/utils/classes';
 	import { Trash } from '@steeze-ui/heroicons';
 	import { Icon } from '@steeze-ui/svelte-icon';
 	import type { Load } from '@sveltejs/kit';
+	import UisLayerGroup from '~icons/uis/layer-group';
+	import HeroiconsSolidChartPie from '~icons/heroicons-solid/chart-pie';
 
 	export const load: Load = async ({ fetch }) => {
 		const { categories, groups } = await trpc(fetch).query(
@@ -30,8 +33,55 @@
 	const addCategory = async () => {
 		categories = [...categories, { en: '', ar: '' }];
 	};
+
+	const subNavigation = [
+		{
+			name: 'Expense Categories',
+			href: '/settings/expense-categories',
+			current: true,
+			icon: UisLayerGroup,
+		},
+		{
+			name: 'Expense Groups',
+			href: '/settings/expense-groups',
+			current: false,
+			icon: HeroiconsSolidChartPie,
+		},
+		// { name: 'Password', href: '#', current: false },
+		// { name: 'Notifications', href: '#', current: false },
+		// { name: 'Billing', href: '#', current: false },
+		// { name: 'Integrations', href: '#', current: false },
+	];
 </script>
 
+<aside class="py-6 lg:col-span-3">
+	<nav class="space-y-1">
+		{#each subNavigation as item (item.name)}
+			<a
+				href={item.href}
+				class={classes(
+					item.current
+						? 'bg-teal-50 border-teal-500 text-teal-700 hover:bg-teal-50 hover:text-teal-700'
+						: 'border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900',
+					'group border-l-4 px-3 py-2 flex items-center text-sm font-medium',
+				)}
+				aria-current={item.current ? 'page' : undefined}
+			>
+				<svelte:component
+					this={item.icon}
+					class={classes(
+						item.current
+							? 'text-teal-500 group-hover:text-teal-500'
+							: 'text-gray-400 group-hover:text-gray-500',
+						'flex-shrink-0 -ml-1 mr-3 h-6 w-6',
+					)}
+					aria-hidden="true"
+				/>
+				<span class="truncate">{item.name}</span>
+			</a>
+		{/each}
+	</nav>
+</aside>
 <h1 class="text-lg font-medium leading-6 text-gray-900">Expense Categories</h1>
 
 {#each categories as cat}
