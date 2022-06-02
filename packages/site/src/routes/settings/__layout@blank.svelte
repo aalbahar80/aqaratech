045e44598a7355fde0d95a1645c43896ae277d8e
nbe <aleisa.naser@gmail.com>
2022-06-02 16:56:22 +0300

    adjust layout spacing

---
+++ packages/site/src/routes/settings/__layout@blank.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { classes } from '$lib/utils/classes';
+	import HeroiconsSolidChartPie from '~icons/heroicons-solid/chart-pie';
+	import UisLayerGroup from '~icons/uis/layer-group';
+
+	const subNavigation = [
+		{
+			name: 'Expense Categories',
+			href: '/settings/expense-categories',
+			current: true,
+			icon: UisLayerGroup,
+		},
+		{
+			name: 'Expense Groups',
+			href: '/settings/expense-groups',
+			current: false,
+			icon: HeroiconsSolidChartPie,
+		},
+		// { name: 'Password', href: '#', current: false },
+		// { name: 'Notifications', href: '#', current: false },
+		// { name: 'Billing', href: '#', current: false },
+		// { name: 'Integrations', href: '#', current: false },
+	];
+</script>
+
+<div
+	class="mx-auto mt-4 flex max-w-7xl flex-col space-y-6 rounded-md  p-4 sm:p-6 lg:p-8"
+>
+	<div class="flex gap-4">
+		<aside class="py-6 lg:col-span-3">
+			<nav class="space-y-1">
+				{#each subNavigation as item (item.name)}
+					<a
+						href={item.href}
+						class={classes(
+							$page.url.pathname === item.href
+								? 'bg-teal-50 border-teal-500 text-teal-700 hover:bg-teal-50 hover:text-teal-700'
+								: 'border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900',
+							'group border-l-4 px-3 py-2 flex items-center text-sm font-medium',
+						)}
+						aria-current={$page.url.pathname === item.href ? 'page' : undefined}
+					>
+						<svelte:component
+							this={item.icon}
+							class={classes(
+								$page.url.pathname === item.href
+									? 'text-teal-500 group-hover:text-teal-500'
+									: 'text-gray-400 group-hover:text-gray-500',
+								'flex-shrink-0 -ml-1 mr-3 h-6 w-6',
+							)}
+							aria-hidden="true"
+						/>
+						<span class="truncate">{item.name}</span>
+					</a>
+				{/each}
+			</nav>
+		</aside>
+		<div>
+			<slot />
+		</div>
+	</div>
+</div>
--- packages/site/src/routes/settings/expense-categories.svelte
+++ packages/site/src/routes/settings/expense-categories.svelte
@@ -32,99 +32,106 @@
 	};
 </script>
 
-<h1 class="text-lg font-medium leading-6 text-gray-900">Expense Categories</h1>
+<div class="flex flex-col gap-4 pt-4">
+	<h1 class="text-lg font-medium leading-6 text-gray-900">
+		Expense Categories
+	</h1>
 
-{#each categories as cat}
-	<div class="flex flex-col gap-4 sm:h-10 sm:flex-row">
-		<input type="text" placeholder="English" bind:value={cat.en} />
-		<input type="text" placeholder="العربية" dir="rtl" bind:value={cat.ar} />
-		<Select
-			id="categoryId"
-			bind:current={cat.groupId}
-			options={groups
-				.filter((g) => g.id)
-				.map((i) => ({
-					label: `${i.en} - ${i.ar}`,
-					value: i.id,
-				}))}
-			on:select
-		/>
+	{#each categories as cat}
+		<div class="flex flex-col gap-4 sm:h-10 sm:flex-row">
+			<input type="text" placeholder="English" bind:value={cat.en} />
+			<input type="text" placeholder="العربية" dir="rtl" bind:value={cat.ar} />
+			<Select
+				id="categoryId"
+				bind:current={cat.groupId}
+				options={groups
+					.filter((g) => g.id)
+					.map((i) => ({
+						label: `${i.en} - ${i.ar}`,
+						value: i.id,
+					}))}
+				on:select
+			/>
 
-		<AsyncButton
-			func={async () => {
-				const { id } = await trpc().mutation('expenseMeta:category:save', cat);
-				cat.id = id;
-			}}
-			let:loading
-		>
-			<button
-				type="button"
-				class="inline-flex w-3/12 items-center justify-center gap-2 rounded-md border border-transparent bg-indigo-100 px-6 py-3 text-base font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+			<AsyncButton
+				func={async () => {
+					const { id } = await trpc().mutation(
+						'expenseMeta:category:save',
+						cat,
+					);
+					cat.id = id;
+				}}
+				let:loading
 			>
-				Save
-				<Spinner {loading} />
-			</button>
-		</AsyncButton>
-
-		<div class="w-1/12 text-red-300 sm:w-2/12 sm:self-center">
-			{#if cat.id}
-				<AsyncButton
-					func={async () => {
-						try {
-							await trpc().mutation('expenseMeta:category:delete', cat.id);
-							await fetchCategories();
-						} catch (e) {
-							if (isTRPCError(e) && e.data?.prismaError?.code === 'P2014') {
-								addToast({
-									props: {
-										kind: 'error',
-										title: 'Error',
-										subtitle:
-											'Cannot delete category because it is used by existing expenses.',
-									},
-								});
-							}
-						}
-					}}
-					let:loading
+				<button
+					type="button"
+					class="inline-flex w-3/12 items-center justify-center gap-2 rounded-md border border-transparent bg-indigo-100 px-6 py-3 text-base font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
 				>
-					{#if !loading}
-						<Icon src={Trash} aria-hidden="true" />
-					{:else}
-						<svg
-							class="animate-spin"
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-						>
-							<circle
-								class="opacity-25"
-								cx="12"
-								cy="12"
-								r="10"
-								stroke="currentColor"
-								stroke-width="4"
-							/>
-							<path
-								class="opacity-75"
-								fill="currentColor"
-								d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-							/>
-						</svg>
-					{/if}
-				</AsyncButton>
-			{/if}
-		</div>
-	</div>
-{/each}
+					Save
+					<Spinner {loading} />
+				</button>
+			</AsyncButton>
 
-<button
-	type="button"
-	class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-100 px-6 py-3 text-base font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-	on:click={() => addCategory()}
->
-	New
-</button>
+			<div class="w-1/12 text-red-300 sm:w-2/12 sm:self-center">
+				{#if cat.id}
+					<AsyncButton
+						func={async () => {
+							try {
+								await trpc().mutation('expenseMeta:category:delete', cat.id);
+								await fetchCategories();
+							} catch (e) {
+								if (isTRPCError(e) && e.data?.prismaError?.code === 'P2014') {
+									addToast({
+										props: {
+											kind: 'error',
+											title: 'Error',
+											subtitle:
+												'Cannot delete category because it is used by existing expenses.',
+										},
+									});
+								}
+							}
+						}}
+						let:loading
+					>
+						{#if !loading}
+							<Icon src={Trash} aria-hidden="true" />
+						{:else}
+							<svg
+								class="animate-spin"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+							>
+								<circle
+									class="opacity-25"
+									cx="12"
+									cy="12"
+									r="10"
+									stroke="currentColor"
+									stroke-width="4"
+								/>
+								<path
+									class="opacity-75"
+									fill="currentColor"
+									d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+								/>
+							</svg>
+						{/if}
+					</AsyncButton>
+				{/if}
+			</div>
+		</div>
+	{/each}
+
+	<button
+		type="button"
+		class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-100 px-6 py-3 text-base font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+		on:click={() => addCategory()}
+	>
+		New
+	</button>
+</div>
 
 <style lang="postcss">
 	input {
