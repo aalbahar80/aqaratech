96711a3a7efb7d30da01e53a8339dd752c1f939f
nbe <aleisa.naser@gmail.com>
2022-06-06 01:58:36 +0300

    fix waitlist button on homepage (safari)

--- packages/site/src/lib/components/LandingBanner.svelte
+++ packages/site/src/lib/components/LandingBanner.svelte
@@ -33,6 +33,7 @@
 													sveltekit:reload
 													type="submit"
 													class="block w-full rounded-md bg-gradient-to-r from-teal-500 to-cyan-600 py-3 px-4 text-center font-medium text-white shadow hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-cyan-400 focus:ring-offset-2 focus:ring-offset-gray-900"
+													style="-webkit-appearance: none;"
 												>
 													Join the waitlist
 												</a>
