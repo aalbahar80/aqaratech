c98360e728620240e5fab14f80da39e803f5411c
nbe <aleisa.naser@gmail.com>
2022-06-06 02:29:01 +0300

    add missing padding in leaseForm

--- packages/site/src/lib/components/lease/LeaseForm.svelte
+++ packages/site/src/lib/components/lease/LeaseForm.svelte
@@ -215,7 +215,7 @@
 	</div>
 
 	<!-- Property/Unit section -->
-	<div>
+	<div class="mt-10 sm:mt-0">
 		<div class="md:grid md:grid-cols-3 md:gap-6">
 			<div class="md:col-span-1">
 				<div class="px-4 sm:px-0">
