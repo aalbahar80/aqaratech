9ab9004a7a0e01f2362db4b9aebe1c3669f2c696
nbe <aleisa.naser@gmail.com>
2022-06-09 14:19:40 +0300

    open tableAction in new tab

--- packages/site/src/lib/components/table/CondensedActionCell.svelte
+++ packages/site/src/lib/components/table/CondensedActionCell.svelte
@@ -6,7 +6,12 @@
 <td
 	class="relative whitespace-nowrap py-2 pl-3 pr-4 text-right text-sm font-medium sm:pr-6"
 >
-	<a {href} sveltekit:prefetch class="text-indigo-600 hover:text-indigo-900">
+	<a
+		{href}
+		target="_blank"
+		sveltekit:prefetch
+		class="text-indigo-600 hover:text-indigo-900"
+	>
 		{label}
 	</a>
 </td>
