7e5dee935dcab0325ee8fd2695b3a80eb54c8cec
nbe <aleisa.naser@gmail.com>
2022-06-11 16:56:02 +0300

    generate swagger.json

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { writeFileSync } from 'fs';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Aqaratech API')
+    .setDescription('The Aqratech API description')
+    .setVersion('1.0')
+    .addTag('aqaratech')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  writeFileSync('./swagger.json', JSON.stringify(document));
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3002);
 }
+
 bootstrap();
