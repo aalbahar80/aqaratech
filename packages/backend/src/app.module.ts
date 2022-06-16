67b06e90bf0ac8c98c90735a844da152e5b576cb
nbe <aleisa.naser@gmail.com>
2022-06-17 00:18:11 +0300

    init prisma.service

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -10,8 +10,11 @@ import { TenantsController } from 'src/tenants/tenants.controller';
 import { TenantsService } from 'src/tenants/tenants.service';
 import { TenantsModule } from './tenants/tenants.module';
 
+import { PrismaModule } from './prisma/prisma.module';
+
+// should prisma be here or in local modules?
 @Module({
-  imports: [PortfoliosModule, TenantsModule],
+  imports: [PortfoliosModule, TenantsModule, PrismaModule],
   controllers: [AppController, TenantsController, PortfoliosController],
   providers: [AppService, TenantsService, PortfoliosService],
 })
