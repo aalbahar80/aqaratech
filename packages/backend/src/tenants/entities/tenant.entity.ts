2d28ee0000726ecef23ba3c6c9ebc742aecafb59
nbe <aleisa.naser@gmail.com>
2022-06-17 01:46:32 +0300

    add response type to /tenants

--- packages/backend/src/tenants/entities/tenant.entity.ts
+++ packages/backend/src/tenants/entities/tenant.entity.ts
@@ -1,1 +1,18 @@
-export class Tenant {}
+import { Tenant } from '@prisma/client';
+
+export class TenantEntity implements Tenant {
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+  fullName: string;
+  shortName: string | null;
+  civilid: string | null;
+  dob: Date | null;
+  phone: string | null;
+  email: string | null;
+  passportNum: string | null;
+  nationality: string | null;
+  residencyNum: string | null;
+  residencyEnd: Date | null;
+  contactMethod: string | null;
+}
--- packages/backend/src/tenants/tenants.controller.ts
+++ packages/backend/src/tenants/tenants.controller.ts
@@ -7,21 +7,26 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { TenantEntity } from 'src/tenants/entities/tenant.entity';
 import { CreateTenantDto } from './dto/create-tenant.dto';
 import { UpdateTenantDto } from './dto/update-tenant.dto';
 import { TenantsService } from './tenants.service';
 
 @Controller('tenants')
+@ApiTags('tenants')
 export class TenantsController {
   constructor(private readonly tenantsService: TenantsService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: TenantEntity })
   create(@Body() createTenantDto: CreateTenantDto) {
     console.log({ createTenantDto }, 'tenants.controller.ts ~ 23');
     return this.tenantsService.create(createTenantDto);
   }
 
   @Get()
+  @ApiOkResponse({ type: TenantEntity, isArray: true })
   findAll() {
     return this.tenantsService.findAll();
   }
@@ -32,16 +37,19 @@ export class TenantsController {
   // }
 
   @Get(':id')
+  @ApiOkResponse({ type: TenantEntity })
   findOne(@Param('id') id: string) {
     return this.tenantsService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOkResponse({ type: TenantEntity })
   update(@Param('id') id: string, @Body() updateTenantDto: UpdateTenantDto) {
     return this.tenantsService.update(id, updateTenantDto);
   }
 
   @Delete(':id')
+  @ApiOkResponse({ type: TenantEntity })
   remove(@Param('id') id: string) {
     return this.tenantsService.remove(id);
   }
