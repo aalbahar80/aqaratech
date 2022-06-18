97dac949aba5de461fce1fcba68e1c094ea884b5
nbe <aleisa.naser@gmail.com>
2022-06-18 08:12:38 +0300

    init jwks verify - ok

--- packages/backend/src/auth/auth.service.ts
+++ packages/backend/src/auth/auth.service.ts
@@ -12,6 +12,7 @@ export class AuthService {
   async validateUser(username: string, pass: string): Promise<any> {
     // const user = await this.usersService.findOne(username);
     const user = { username: 'admin', sub: '1' };
+    console.log({ user }, 'auth.service.ts ~ 15');
     return user;
     // if (user && user.password === pass) {
     //   const { password, ...result } = user;
@@ -20,10 +21,10 @@ export class AuthService {
     // return null;
   }
 
-  async login(user: any) {
-    const payload = { username: user.username, sub: user.userId };
-    return {
-      access_token: this.jwtService.sign(payload),
-    };
-  }
+  //   async login(user: any) {
+  //     const payload = { username: user.username, sub: user.userId };
+  //     return {
+  //       access_token: this.jwtService.sign(payload),
+  //     };
+  //   }
 }
--- packages/backend/src/auth/jwt.strategy.ts
+++ packages/backend/src/auth/jwt.strategy.ts
@@ -2,19 +2,28 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { jwtConstants } from './constants';
+import { passportJwtSecret } from 'jwks-rsa';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
+      secretOrKeyProvider: passportJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        // jwksUri: `${process.env.AUTH0_ISSUER_URL}.well-known/jwks.json`,
+        jwksUri: `https://dev-eehvhdp2.eu.auth0.com/.well-known/jwks.json`,
+      }),
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
-      secretOrKey: jwtConstants.secret,
-
+      // ignoreExpiration: false,
+      //   secretOrKey: jwtConstants.secret,
       // https://github.com/mikenicholson/passport-jwt#configure-strategy
-      //   audience: authConfig.AUTH0_API_AUDIENCE,
-      //   issuer: `${authConfig.AUTH0_DOMAIN}/`,
-      //   algorithms: ['RS256'],
+      // audience: authConfig.AUTH0_API_AUDIENCE,
+      // issuer: `${authConfig.AUTH0_DOMAIN}/`,
+      audience: 'letand.be/api',
+      issuer: `https://dev-eehvhdp2.eu.auth0.com/`, // TODO ensure backslash
+      algorithms: ['RS256'],
     });
   }
 
