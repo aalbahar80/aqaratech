86c11e38256e6e5fc47cebffa2c23ae3772ec879
nbe <aleisa.naser@gmail.com>
2022-06-18 10:50:30 +0300

    add role to openapi schema

--- packages/backend/src/users/dto/user.dto.ts
+++ packages/backend/src/users/dto/user.dto.ts
@@ -1,6 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { User } from '@prisma/client';
+import { IsUUID } from 'class-validator';
 
-// TODO add roles
+class Role {
+  @ApiProperty({ example: '73306383-93a8-4b61-a2f6-58866dcc2ae9' })
+  @IsUUID()
+  organizationId: string;
+}
+
+class Roles {
+  admins: Role[];
+  portfolios: Role[];
+  tenants: Role[];
+}
+
 export class UserDto implements User {
   id: string;
   createdAt: Date;
@@ -8,14 +21,4 @@ export class UserDto implements User {
   email: string;
   fullName: string | null;
   roles: Roles;
-}
-
-interface Roles {
-  admins: Role[];
-  portfolios: Role[];
-  tenants: Role[];
-}
-
-interface Role {
-  organizationId: string;
 }
