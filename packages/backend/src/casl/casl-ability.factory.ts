4ad8169d60d5397c61fd7800f8bbb4850b1e91b3
nbe <aleisa.naser@gmail.com>
2022-06-20 23:48:02 +0300

    fail fast in casl factory

--- packages/backend/src/casl/casl-ability.factory.ts
+++ packages/backend/src/casl/casl-ability.factory.ts
@@ -47,135 +47,150 @@ export class CaslAbilityFactory {
     console.log({ tenants });
 
     // ### Role: Organization###
+    if (orgs.length > 0) {
+      // fail fast if no orgs
 
-    // can manage tenants in their org
-    can(Action.Manage, ['Tenant'], {
-      organizationId: { in: orgs },
-    });
+      can(Action.Manage, ['Tenant'], {
+        organizationId: { in: orgs },
+      });
 
-    can(Action.Manage, ['Portfolio'], {
-      organizationId: { in: orgs },
-    });
+      can(Action.Manage, ['Portfolio'], {
+        organizationId: { in: orgs },
+      });
 
-    can(Action.Manage, ['Expense'], {
-      OR: [
-        { portfolio: { organizationId: { in: orgs } } },
-        { property: { portfolio: { organizationId: { in: orgs } } } },
-        { unit: { property: { portfolio: { organizationId: { in: orgs } } } } },
-      ],
-    });
+      can(Action.Manage, ['Expense'], {
+        OR: [
+          { portfolio: { organizationId: { in: orgs } } },
+          { property: { portfolio: { organizationId: { in: orgs } } } },
+          {
+            unit: { property: { portfolio: { organizationId: { in: orgs } } } },
+          },
+        ],
+      });
 
-    can(Action.Manage, ['Lease'], {
-      unit: { property: { portfolio: { organizationId: { in: orgs } } } },
-    });
-
-    can(Action.Manage, ['Property'], {
-      portfolio: { organizationId: { in: orgs } },
-    });
-
-    can(Action.Manage, ['MaintenanceOrder'], {
-      OR: [
-        { portfolio: { organizationId: { in: orgs } } },
-        { property: { portfolio: { organizationId: { in: orgs } } } },
-        { unit: { property: { portfolio: { organizationId: { in: orgs } } } } },
-        { tenant: { organizationId: { in: orgs } } },
-        // {expenses: {some: {}}}
-      ],
-    });
-
-    can(Action.Manage, ['Transaction'], {
-      lease: {
+      can(Action.Manage, ['Lease'], {
         unit: { property: { portfolio: { organizationId: { in: orgs } } } },
-      },
-    });
+      });
 
-    can(Action.Manage, ['Unit'], {
-      property: { portfolio: { organizationId: { in: orgs } } },
-    });
+      can(Action.Manage, ['Property'], {
+        portfolio: { organizationId: { in: orgs } },
+      });
+
+      can(Action.Manage, ['MaintenanceOrder'], {
+        OR: [
+          { portfolio: { organizationId: { in: orgs } } },
+          { property: { portfolio: { organizationId: { in: orgs } } } },
+          {
+            unit: { property: { portfolio: { organizationId: { in: orgs } } } },
+          },
+          { tenant: { organizationId: { in: orgs } } },
+          // {expenses: {some: {}}}
+        ],
+      });
+
+      can(Action.Manage, ['Transaction'], {
+        lease: {
+          unit: { property: { portfolio: { organizationId: { in: orgs } } } },
+        },
+      });
+
+      can(Action.Manage, ['Unit'], {
+        property: { portfolio: { organizationId: { in: orgs } } },
+      });
+    }
 
     // ### Role: Portfolio ###
+    if (portfolios.length > 0) {
+      // fail fast if no portfolios
 
-    // can view tenants who have leases in their properties
-    can(Action.Read, 'Tenant', {
-      leases: {
-        some: {
-          unit: {
-            property: {
-              portfolioId: { in: portfolios },
+      // can view tenants who have leases in their properties
+      can(Action.Read, 'Tenant', {
+        leases: {
+          some: {
+            unit: {
+              property: {
+                portfolioId: { in: portfolios },
+              },
             },
           },
         },
-      },
-    });
+      });
 
-    can(Action.Read, ['Expense'], {
-      OR: [
-        { portfolioId: { in: portfolios } },
-        { property: { portfolioId: { in: portfolios } } },
-        { unit: { property: { portfolioId: { in: portfolios } } } },
-      ],
-    });
+      can(Action.Read, ['Expense'], {
+        OR: [
+          { portfolioId: { in: portfolios } },
+          { property: { portfolioId: { in: portfolios } } },
+          { unit: { property: { portfolioId: { in: portfolios } } } },
+        ],
+      });
 
-    can(Action.Read, ['MaintenanceOrder'], {
-      OR: [
-        { portfolioId: { in: portfolios } },
-        { property: { portfolioId: { in: portfolios } } },
-        { unit: { property: { portfolioId: { in: portfolios } } } },
-      ],
-    });
+      can(Action.Read, ['MaintenanceOrder'], {
+        OR: [
+          { portfolioId: { in: portfolios } },
+          { property: { portfolioId: { in: portfolios } } },
+          { unit: { property: { portfolioId: { in: portfolios } } } },
+        ],
+      });
 
-    can(Action.Read, ['Lease'], {
-      unit: { property: { portfolioId: { in: portfolios } } },
-    });
-
-    can(Action.Read, ['Property'], {
-      portfolioId: { in: portfolios },
-    });
-
-    can(Action.Read, ['Transaction'], {
-      lease: {
+      can(Action.Read, ['Lease'], {
         unit: { property: { portfolioId: { in: portfolios } } },
-      },
-    });
+      });
 
-    can(Action.Read, ['Unit'], {
-      property: { portfolioId: { in: portfolios } },
-    });
+      can(Action.Read, ['Property'], {
+        portfolioId: { in: portfolios },
+      });
+
+      can(Action.Read, ['Transaction'], {
+        lease: {
+          unit: { property: { portfolioId: { in: portfolios } } },
+        },
+      });
+
+      can(Action.Read, ['Unit'], {
+        property: { portfolioId: { in: portfolios } },
+      });
+
+      can(Action.Read, ['Portfolio'], {
+        id: { in: portfolios },
+      });
+    }
 
     // ### Role: Tenant###
+    if (tenants.length > 0) {
+      // fail fast if no tenants
 
-    // can view all their tenant profiles
-    can(Action.Read, 'Tenant', {
-      id: { in: tenants },
-    });
+      // can view all their tenant profiles
+      can(Action.Read, 'Tenant', {
+        id: { in: tenants },
+      });
 
-    // can view all their leases
-    can(Action.Read, ['Lease'], {
-      tenantId: { in: tenants },
-    });
-
-    can(Action.Read, ['MaintenanceOrder'], {
-      tenantId: { in: tenants },
-    });
-
-    // TODO some fields should be public
-    can(Action.Read, ['Transaction'], {
-      lease: {
+      // can view all their leases
+      can(Action.Read, ['Lease'], {
         tenantId: { in: tenants },
-      },
-    });
+      });
 
-    // only some fields
-    // can(Action.Read, ['Property'], {
-    //   units: { some: { leases: { some: { tenantId: { in: tenants } } } } },
-    // });
+      can(Action.Read, ['MaintenanceOrder'], {
+        tenantId: { in: tenants },
+      });
 
-    // only some fields
-    // can(Action.Read, ['Unit'], {
-    //   leases: { some: { tenantId: { in: tenants } } },
-    // });
+      // TODO some fields should be public
+      can(Action.Read, ['Transaction'], {
+        lease: {
+          tenantId: { in: tenants },
+        },
+      });
 
-    // users can
+      // only some fields
+      // can(Action.Read, ['Property'], {
+      //   units: { some: { leases: { some: { tenantId: { in: tenants } } } } },
+      // });
+
+      // only some fields
+      // can(Action.Read, ['Unit'], {
+      //   leases: { some: { tenantId: { in: tenants } } },
+      // });
+    }
+
     return build();
   }
 }
