bd3012a4ac0159993baa87c056f3fb86434de602
nbe <aleisa.naser@gmail.com>
2022-06-22 18:42:34 +0300

    customize default tenantpageoptions

--- packages/backend/src/tenants/dto/tenant-page-options.dto.ts
+++ packages/backend/src/tenants/dto/tenant-page-options.dto.ts
@@ -1,3 +1,5 @@
 import { PageOptionsDto } from '../../common/dto/page-options.dto';
 
-export class TenantPageOptionsDto extends PageOptionsDto {}
+export class TenantPageOptionsDto extends PageOptionsDto {
+  override take: number = 20;
+}
