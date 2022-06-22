f2527476f9e0eae85d88f2fb91fa214a1f10789a
nbe <aleisa.naser@gmail.com>
2022-06-22 21:30:53 +0300

    update tenant dto

--- packages/backend/src/tenants/dto/tenant.dto.ts
+++ packages/backend/src/tenants/dto/tenant.dto.ts
@@ -1,19 +1,59 @@
+import { ApiHideProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { Tenant } from '@prisma/client';
+import {
+  IsEmail,
+  IsISO31661Alpha3,
+  IsISO8601,
+  IsPhoneNumber,
+  IsString,
+  Length,
+} from 'class-validator';
+import { AbstractDto } from 'src/common/dto/abstract.dto';
 
-export class TenantDto implements Tenant {
-  id: string;
+export class TenantDto extends AbstractDto implements Tenant {
+  @ApiHideProperty()
   organizationId: string;
-  createdAt: Date;
-  updatedAt: Date;
+
+  @Length(1, 255)
   fullName: string;
-  shortName: string | null;
-  civilid: string | null;
-  dob: Date | null;
-  phone: string | null;
-  email: string | null;
-  passportNum: string | null;
-  nationality: string | null;
-  residencyNum: string | null;
-  residencyEnd: Date | null;
-  contactMethod: string | null;
+
+  @ApiPropertyOptional()
+  @IsString()
+  shortName: string | null = null;
+
+  @ApiPropertyOptional()
+  @IsString()
+  civilid: string | null = null;
+
+  @ApiPropertyOptional()
+  @IsPhoneNumber()
+  phone: string | null = null;
+
+  @ApiPropertyOptional()
+  @IsEmail()
+  email: string | null = null;
+
+  @ApiPropertyOptional()
+  @IsISO8601()
+  dob: Date | null = null;
+
+  @ApiPropertyOptional()
+  @IsString()
+  passportNum: string | null = null;
+
+  @ApiPropertyOptional()
+  @IsISO31661Alpha3()
+  nationality: string | null = null;
+
+  @ApiPropertyOptional()
+  @IsString()
+  residencyNum: string | null = null;
+
+  @ApiPropertyOptional()
+  @IsISO8601()
+  residencyEnd: Date | null = null;
+
+  // TODO remove
+  @ApiHideProperty()
+  contactMethod: string | null = null;
 }
