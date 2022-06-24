40cec4bceddc83decb4e986d4ad34940d009b44b
nbe <aleisa.naser@gmail.com>
2022-06-24 21:31:31 +0300

    add initial prisma migration

---
+++ packages/backend/prisma/migrations/20220624183028_init/migration.sql
@@ -0,0 +1,273 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "email" TEXT NOT NULL,
+    "fullName" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Role" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "permissions" JSONB,
+    "userId" TEXT,
+    "organizationId" TEXT,
+    "portfolioId" TEXT,
+    "tenantId" TEXT,
+
+    CONSTRAINT "Role_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Tenant" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "fullName" TEXT NOT NULL DEFAULT E'',
+    "shortName" TEXT,
+    "civilid" TEXT,
+    "dob" TIMESTAMP(3),
+    "phone" TEXT,
+    "email" TEXT,
+    "passportNum" TEXT,
+    "nationality" TEXT,
+    "residencyNum" TEXT,
+    "residencyEnd" TIMESTAMP(3),
+    "organizationId" TEXT NOT NULL,
+
+    CONSTRAINT "Tenant_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Portfolio" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "fullName" TEXT NOT NULL DEFAULT E'',
+    "shortName" TEXT,
+    "civilid" TEXT,
+    "phone" TEXT,
+    "email" TEXT,
+    "dob" TIMESTAMP(3),
+    "organizationId" TEXT NOT NULL,
+
+    CONSTRAINT "Portfolio_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Organization" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "fullName" TEXT NOT NULL DEFAULT E'',
+    "shortName" TEXT,
+    "planId" TEXT,
+
+    CONSTRAINT "Organization_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Plan" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "label" TEXT NOT NULL DEFAULT E'',
+    "title" TEXT NOT NULL DEFAULT E'',
+    "description" TEXT,
+    "amount" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "Plan_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PlanInvoice" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "label" TEXT NOT NULL DEFAULT E'',
+    "title" TEXT NOT NULL DEFAULT E'',
+    "description" TEXT,
+    "amount" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "PlanInvoice_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ExpenseType" (
+    "id" SERIAL NOT NULL,
+    "labelEn" TEXT NOT NULL,
+    "labelAr" TEXT NOT NULL,
+    "description" TEXT,
+    "parentId" INTEGER,
+
+    CONSTRAINT "ExpenseType_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Expense" (
+    "id" TEXT NOT NULL,
+    "unitId" TEXT,
+    "propertyId" TEXT,
+    "portfolioId" TEXT,
+    "maintenanceOrderId" TEXT,
+    "categoryId" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "postAt" TIMESTAMP(3) NOT NULL,
+    "memo" TEXT,
+
+    CONSTRAINT "Expense_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Lease" (
+    "id" TEXT NOT NULL,
+    "tenantId" TEXT NOT NULL,
+    "unitId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "start" TIMESTAMP(3) NOT NULL,
+    "end" TIMESTAMP(3) NOT NULL,
+    "monthlyRent" DOUBLE PRECISION NOT NULL,
+    "deposit" DOUBLE PRECISION NOT NULL DEFAULT 0,
+    "deactivated" BOOLEAN NOT NULL DEFAULT false,
+    "notify" BOOLEAN NOT NULL DEFAULT true,
+    "license" TEXT,
+
+    CONSTRAINT "Lease_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "MaintenanceOrder" (
+    "id" TEXT NOT NULL,
+    "tenantId" TEXT,
+    "unitId" TEXT,
+    "propertyId" TEXT,
+    "portfolioId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "completedAt" TIMESTAMP(3),
+    "title" TEXT,
+    "description" TEXT,
+    "status" TEXT,
+
+    CONSTRAINT "MaintenanceOrder_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Property" (
+    "id" TEXT NOT NULL,
+    "portfolioId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "area" TEXT,
+    "block" TEXT,
+    "avenue" TEXT,
+    "street" TEXT,
+    "number" TEXT,
+    "parcel" TEXT,
+    "paci" TEXT,
+    "cost" DOUBLE PRECISION,
+    "long" DOUBLE PRECISION,
+    "lat" DOUBLE PRECISION,
+
+    CONSTRAINT "Property_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "LeaseInvoice" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "dueAt" TIMESTAMP(3),
+    "postAt" TIMESTAMP(3) NOT NULL,
+    "paidAt" TIMESTAMP(3),
+    "isPaid" BOOLEAN NOT NULL DEFAULT false,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "memo" TEXT,
+    "mfPaymentId" TEXT,
+    "leaseId" TEXT NOT NULL,
+
+    CONSTRAINT "LeaseInvoice_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Unit" (
+    "id" TEXT NOT NULL,
+    "propertyId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "unitNumber" TEXT NOT NULL,
+    "floor" DOUBLE PRECISION,
+    "size" DOUBLE PRECISION,
+    "bed" DOUBLE PRECISION,
+    "bath" DOUBLE PRECISION,
+    "marketRent" DOUBLE PRECISION,
+    "type" TEXT,
+    "usage" TEXT,
+
+    CONSTRAINT "Unit_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_OrganizationToPlanInvoice" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "Expense_unitId_idx" ON "Expense"("unitId");
+
+-- CreateIndex
+CREATE INDEX "Expense_propertyId_idx" ON "Expense"("propertyId");
+
+-- CreateIndex
+CREATE INDEX "Expense_portfolioId_idx" ON "Expense"("portfolioId");
+
+-- CreateIndex
+CREATE INDEX "Expense_maintenanceOrderId_idx" ON "Expense"("maintenanceOrderId");
+
+-- CreateIndex
+CREATE INDEX "Expense_categoryId_idx" ON "Expense"("categoryId");
+
+-- CreateIndex
+CREATE INDEX "Lease_tenantId_idx" ON "Lease"("tenantId");
+
+-- CreateIndex
+CREATE INDEX "Lease_unitId_idx" ON "Lease"("unitId");
+
+-- CreateIndex
+CREATE INDEX "MaintenanceOrder_tenantId_idx" ON "MaintenanceOrder"("tenantId");
+
+-- CreateIndex
+CREATE INDEX "MaintenanceOrder_unitId_idx" ON "MaintenanceOrder"("unitId");
+
+-- CreateIndex
+CREATE INDEX "MaintenanceOrder_propertyId_idx" ON "MaintenanceOrder"("propertyId");
+
+-- CreateIndex
+CREATE INDEX "MaintenanceOrder_portfolioId_idx" ON "MaintenanceOrder"("portfolioId");
+
+-- CreateIndex
+CREATE INDEX "Property_portfolioId_idx" ON "Property"("portfolioId");
+
+-- CreateIndex
+CREATE INDEX "LeaseInvoice_leaseId_idx" ON "LeaseInvoice"("leaseId");
+
+-- CreateIndex
+CREATE INDEX "Unit_propertyId_idx" ON "Unit"("propertyId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_OrganizationToPlanInvoice_AB_unique" ON "_OrganizationToPlanInvoice"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_OrganizationToPlanInvoice_B_index" ON "_OrganizationToPlanInvoice"("B");
