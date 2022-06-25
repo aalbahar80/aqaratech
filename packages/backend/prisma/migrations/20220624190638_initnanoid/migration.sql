525667b99ce5388d838ffac7dd347b101aae2077
nbe <aleisa.naser@gmail.com>
2022-06-25 03:41:49 +0300

    update postgres nanoid function

--- packages/backend/prisma/migrations/20220624190638_initnanoid/migration.sql
+++ packages/backend/prisma/migrations/20220624190638_initnanoid/migration.sql
@@ -2,20 +2,22 @@
 -- https://github.com/prisma/prisma/issues/2117#issuecomment-868728650
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
-CREATE OR REPLACE FUNCTION nanoid(size int DEFAULT 12)
-RETURNS text AS $$
+CREATE OR REPLACE FUNCTION nanoid(
+  size int DEFAULT 12,
+  alphabet varchar DEFAULT 'ModuleSymbhasOwnPr0123456789ABCDEFGHNRVfgctiUvzKqYTJkLxpZXIjQW'
+) RETURNS text AS $$
 DECLARE
   id text := '';
   i int := 0;
-  urlAlphabet char(64) := 'ModuleSymbhasOwnPr-0123456789abcdefghijklmnopqrstuvwxyz';
+  len int := length(alphabet);
   bytes bytea := gen_random_bytes(size);
   byte int;
   pos int;
 BEGIN
   WHILE i < size LOOP
     byte := get_byte(bytes, i);
-    pos := (byte & 63) + 1; -- + 1 because substr starts at 1 for some reason
-    id := id || substr(urlAlphabet, pos, 1);
+    pos := (byte & (len - 1)) + 1; -- + 1 because substr starts at 1 for some reason
+    id := id || substr(alphabet, pos, 1);
     i = i + 1;
   END LOOP;
   RETURN id;
