c52dc44df6c309c01c5a4f1b014c1387594b8b76
nbe <aleisa.naser@gmail.com>
2022-06-25 05:37:39 +0300

    define OptionalIfNullable

--- packages/backend/src/expenses/dto/expense.dto.ts
+++ packages/backend/src/expenses/dto/expense.dto.ts
@@ -1,26 +1,37 @@
 import { ApiHideProperty, PartialType } from '@nestjs/swagger';
 import { Expense } from '@prisma/client';
-import { IsISO8601, IsPositive, IsString } from 'class-validator';
+import { IsISO8601, IsOptional, IsPositive, IsString } from 'class-validator';
 import { AbstractDto } from 'src/common/dto/abstract.dto';
 import { Nanoid } from 'src/decorators/field.decorators';
 
-export class ExpenseDto extends AbstractDto implements Expense {
+type IsNullable<T> = null extends T ? T : never;
+type OptionalIfNullable<T> = {
+  [P in keyof T]?: T[P] extends IsNullable<T[P]> ? T[P] | undefined : T[P];
+};
+export class ExpenseDto
+  extends AbstractDto
+  implements OptionalIfNullable<Expense>
+{
   // TODO: constrain only one of the following fields to be set
   @Nanoid()
-  unitId: string | null = null;
+  @IsOptional()
+  unitId?: string | null;
 
   @Nanoid()
-  propertyId: string | null = null;
+  @IsOptional()
+  propertyId?: string | null;
 
   @Nanoid()
-  portfolioId: string | null = null;
+  @IsOptional()
+  portfolioId?: string | null;
 
   // TODO remove from schema
   @ApiHideProperty()
   maintenanceOrderId: string | null;
 
-  // use category name
-  categoryId: number | null = null;
+  // TODO use category name/prisma connect & set default here
+  @ApiHideProperty()
+  categoryId: number | null;
 
   @IsPositive()
   amount: number;
@@ -29,7 +40,7 @@ export class ExpenseDto extends AbstractDto implements Expense {
   postAt: Date;
 
   @IsString()
-  memo: string | null = null;
+  memo: string | null;
 }
 
 export class UpdateExpenseDto extends PartialType(ExpenseDto) {}
