3fc298b9a74d4cd32a8309652edfc232422e180e
nbe <aleisa.naser@gmail.com>
2022-06-29 06:36:32 +0300

    import start

--- packages/site/src/routes/units/index.svelte
+++ packages/site/src/routes/units/index.svelte
@@ -2,14 +2,11 @@
 	import { config } from '$lib/client/api-config';
 	import UnitsList from '$lib/components/unit/UnitsList.svelte';
 	import * as api from '@self/sdk';
-	import { UnitsApi } from '@self/sdk';
 	import type { LoadEvent } from '@sveltejs/kit';
 	import type { LP } from 'src/types/load-props';
 
 	export const load = async ({ fetch }: LoadEvent) => {
-		const data = await new UnitsApi(config(fetch)).findAllUnits();
-		// const a = new api.TenantsApi().tenantsControllerCreate;
-		const a = new api.TenantsApi();
+		const data = await new api.UnitsApi(config(fetch)).findAllUnits();
 
 		return {
 			props: { data },
