2e6f91553567dc2514f8d161a6f400c815b141e9
naleisa <a33713262+ambiguous48@users.noreply.github.com>
2022-06-30 16:26:02 +0300

    set api url to env var in vercel

--- packages/site/src/lib/client/api.ts
+++ packages/site/src/lib/client/api.ts
@@ -17,7 +17,7 @@ export const api = (loadFetch?: LoadEvent['fetch']) => {
 	let config: Configuration;
 
 	const basePath = import.meta.env.VITE_VERCEL_ENV
-		? 'https://nestjs-dev.onrender.com' // set to a vite env var in vercel
+		? import.meta.env.VITE_API_URL
 		: 'http://localhost:3002';
 
 	const headers = { Authorization: `Bearer ${token}` };
