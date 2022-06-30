6530546ac68b8782292bf7519921cd9b6b99d798
nbe <aleisa.naser@gmail.com>
2022-06-30 03:35:07 +0300

    include generated src in vsc

---
+++ packages/sdk/src/apis/index.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './DefaultApi';
+export * from './ExpensesApi';
+export * from './LeaseInvoicesApi';
+export * from './LeasesApi';
+export * from './PortfoliosApi';
+export * from './PropertiesApi';
+export * from './TenantsApi';
+export * from './UnitsApi';
+export * from './UsersApi';
