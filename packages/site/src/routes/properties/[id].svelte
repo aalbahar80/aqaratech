ef77afa23ff686cdd1c56e30e1c7c2fbfc1df40d
nbe <aleisa.naser@gmail.com>
2022-06-30 08:01:12 +0300

    refactor properties/:id.svelte

--- packages/site/src/routes/properties/[id].svelte
+++ packages/site/src/routes/properties/[id].svelte
@@ -1,48 +1,20 @@
 <script lang="ts" context="module">
-	import BreadCrumb from '$components/breadcrumbs/BreadCrumb.svelte';
-	import { trpc } from '$lib/client/trpc';
-	import DetailsPane from '$lib/components/DetailsPane.svelte';
-	import Heading from '$lib/components/Heading.svelte';
-	import UnitsList from '$lib/components/unit/UnitsList.svelte';
-	import { getAddress } from '$lib/utils/common';
-	import type { Props } from '$models/types/Props.type';
+	import { api } from '$lib/client/api';
+	import PropertyPage from '$lib/components/property/PropertyPage.svelte';
 	import type { LoadEvent } from '@sveltejs/kit';
+	import type { LP } from 'src/types/load-props';
 
-	export const load = async ({
-		params,
-		session,
-		fetch,
-	}: LoadEvent<{ id: string }>) => {
-		const property = session.authz?.isAdmin
-			? await trpc(fetch).query('properties:read', params.id)
-			: await trpc(fetch).query('owner:properties:read', params.id);
-
-		return { props: { property } };
+	export const load = async ({ fetch, params }: LoadEvent<{ id: string }>) => {
+		const data = await api(fetch).properties.findOneProperties({
+			id: params.id,
+		});
+		return { props: { data } };
 	};
 </script>
 
 <script lang="ts">
-	type Property = Props<typeof load>['property'];
-	export let property: Property;
-
-	let details: [string, string | null][];
-	$: details = [
-		['Address', getAddress(property)],
-		['Area', property.area],
-		['Block', property.block],
-		['Avenue', property.avenue],
-		['Street', property.street],
-		['Number', property.number],
-		['Parcel', property.parcel],
-		['Paci', property.paci],
-	];
+	type Prop = LP<typeof load>;
+	export let data: Prop['data'];
 </script>
 
-<Heading title="Property" id={property.id} entity="properties">
-	<svelte:fragment slot="breadcrumbs">
-		<BreadCrumb crumbs={{ portfolio: property.portfolioId }} />
-	</svelte:fragment>
-</Heading>
-
-<DetailsPane {details} />
-<UnitsList units={property.units} />
+<PropertyPage property={data} />
