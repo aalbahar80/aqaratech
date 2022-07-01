b8d8cc361c4ede1bff127f3401ffd4b946ab8c7a
nbe <aleisa.naser@gmail.com>
2022-07-01 07:40:13 +0300

    return prisma metrics

--- packages/backend/src/metrics/metrics.service.ts
+++ packages/backend/src/metrics/metrics.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { HealthService } from 'src/health/health.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { PrometheusService } from 'src/prometheus/prometheus.service';
 
 @Injectable()
 export class MetricsService {
-  public get metrics(): Promise<string> {
-    this.healthService.check();
-    return this.promClientService.metrics;
-  }
-
   constructor(
     private promClientService: PrometheusService,
     private healthService: HealthService,
+    private prisma: PrismaService,
   ) {}
+
+  public get metrics(): Promise<string> {
+    this.healthService.check();
+    return Promise.all([
+      this.promClientService.metrics,
+      this.prisma.$metrics.prometheus(),
+    ]).then(([prometheus, prisma]) => prometheus + prisma);
+  }
 }
