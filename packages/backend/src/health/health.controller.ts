ab968f00a9c0832b0d7e056b9f781855820d1dc9
nbe <aleisa.naser@gmail.com>
2022-07-01 07:10:41 +0300

    make public /metrics /health

--- packages/backend/src/health/health.controller.ts
+++ packages/backend/src/health/health.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { HealthCheckResult } from '@nestjs/terminus';
+import { Public } from 'src/auth/public.decorator';
 import { HealthService } from 'src/health/health.service';
 
 @Controller('health')
 export class HealthController {
   constructor(private healthService: HealthService) {}
 
+  @Public() // TODO prod remove
   @Get()
   public async check(): Promise<HealthCheckResult> {
     return await this.healthService.check();
--- packages/backend/src/metrics/metrics.controller.ts
+++ packages/backend/src/metrics/metrics.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from 'src/auth/public.decorator';
 import { MetricsService } from 'src/metrics/metrics.service';
 
 @Controller('metrics')
 export class MetricsController {
   constructor(private metricsService: MetricsService) {}
 
+  @Public() // TODO prod remove
   @Get()
   public metrics(): Promise<string> {
     return this.metricsService.metrics;
