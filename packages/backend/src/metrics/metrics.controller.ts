59925f4d042714dbc789f124d2deb515f1f95cae
nbe <aleisa.naser@gmail.com>
2022-07-01 05:19:00 +0300

    init metrics controller/srv

--- packages/backend/src/metrics/metrics.controller.ts
+++ packages/backend/src/metrics/metrics.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { MetricsService } from 'src/metrics/metrics.service';
 
 @Controller('metrics')
-export class MetricsController {}
+export class MetricsController {
+  constructor(private metricsService: MetricsService) {}
+
+  @Get()
+  public metrics(): Promise<string> {
+    return this.metricsService.metrics;
+  }
+}
