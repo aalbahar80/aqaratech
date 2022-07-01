98d41a4aaf9b15745ab02d564ee0495bd6202318
nbe <aleisa.naser@gmail.com>
2022-07-01 05:24:02 +0300

    resolve dependencies

--- packages/backend/src/health/health.module.ts
+++ packages/backend/src/health/health.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TerminusModule } from '@nestjs/terminus';
+import { PrometheusModule } from 'src/prometheus/prometheus.module';
 import { HealthController } from './health.controller';
 import { HealthService } from './health.service';
 
 @Module({
+  imports: [TerminusModule, PrometheusModule],
   controllers: [HealthController],
-  providers: [HealthService]
+  providers: [HealthService],
+  exports: [HealthService],
 })
 export class HealthModule {}
--- packages/backend/src/metrics/metrics.module.ts
+++ packages/backend/src/metrics/metrics.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
-import { MetricsService } from './metrics.service';
+import { HealthModule } from 'src/health/health.module';
+import { PrometheusModule } from 'src/prometheus/prometheus.module';
 import { MetricsController } from './metrics.controller';
+import { MetricsService } from './metrics.service';
 
 @Module({
   providers: [MetricsService],
-  controllers: [MetricsController]
+  controllers: [MetricsController],
+  imports: [PrometheusModule, HealthModule],
 })
 export class MetricsModule {}
