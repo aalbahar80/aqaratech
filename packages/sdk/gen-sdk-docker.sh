62813801f598befbd6965d33f338616e91762e07
nbe <aleisa.naser@gmail.com>
2022-07-03 08:19:17 +0300

    use java on render

--- packages/sdk/gen-sdk-docker.sh
+++ packages/sdk/gen-sdk-docker.sh
@@ -1,4 +1,4 @@
-if $CI; then
+if $CI || $RENDER; then
     echo "Generating SDK for LOCALENV"
     docker run --rm -v "$PWD/openapi.yaml:/local/input-spec" -v "$PWD/src:/local/output" openapitools/openapi-generator-cli:v6.0.0 generate --input-spec="/local/input-spec" --generator-name="typescript-fetch" --output="/local/output" --skip-validate-spec --additional-properties="supportsES6=true,typescriptThreePlus=true,nullSafeAdditionalProps=true,withInterfaces=true,prefixParameterInterfaces=true"
 else
