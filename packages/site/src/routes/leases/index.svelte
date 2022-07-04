fc27dc721ad20659c9ccb588a9fb5e905f8ca90f
nbe <aleisa.naser@gmail.com>
2022-07-04 05:03:52 +0300

    replace pagination

--- packages/site/src/routes/leases/index.svelte
+++ packages/site/src/routes/leases/index.svelte
@@ -1,12 +1,9 @@
 <script context="module" lang="ts">
-	import { session } from '$app/stores';
-	import { trpc } from '$lib/client/trpc';
 	import Filter from '$lib/components/Filter.svelte';
 	import LeaseList from '$lib/components/lease/LeaseList.svelte';
-	import type { Props } from '$lib/models/types/Props.type';
+	import Pagination from '$lib/components/table/Pagination.svelte';
 	import { parseParams } from '$lib/utils/parse-params';
 	import type { LoadEvent } from '@sveltejs/kit';
-	import compare from 'just-compare';
 	import type { LP } from 'src/types/load-props';
 
 	export const load = async ({ url, stuff }: LoadEvent) => {
@@ -24,10 +21,7 @@
 	export let leases: Prop['leases'];
 
 	// type Options = Props<typeof load>['options'];
-	// export let leases: Leases;
-	// export let pagination: Pagination;
 	// export let options: Options;
-
 	// let currentOptions = options;
 
 	const sortOptions = [
@@ -77,52 +71,5 @@
 <Filter bind:filters bind:currentSort {sortOptions} />
 <div class="">
 	<LeaseList {leases} --border-radius-b="0" />
-
-	<!-- <nav
-		class="flex items-center justify-between rounded-lg rounded-t-none border-t border-gray-200 bg-white px-4 py-3 shadow sm:px-6"
-		aria-label="Pagination"
-	>
-		<div class="hidden sm:block">
-			<p class="text-sm text-gray-700">
-				Showing <span class="font-medium">{pagination.start}</span> to
-				<span class="font-medium">{pagination.start + leases.length - 1}</span>
-				of{' '}
-				<span class="font-medium">{pagination.total}</span> results
-			</p>
-		</div>
-		<div class="flex flex-1 justify-between sm:justify-end">
-			<button
-				class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-				disabled={!pagination.hasPreviousPage}
-				on:click={() => {
-					options = {
-						...options,
-						pageIndex: Math.max(pagination.pageIndex - 1, 1),
-					};
-				}}
-				rel="prev"
-			>
-				Previous
-			</button>
-			<button
-				class="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-				disabled={!pagination.hasNextPage}
-				rel="next"
-				on:click={() => {
-					options = {
-						...options,
-						pageIndex: pagination.pageIndex + 1,
-					};
-				}}
-			>
-				Next
-			</button>
-		</div>
-	</nav> -->
+	<Pagination pagination={leases.pagination} />
 </div>
-
-<style lang="postcss">
-	nav button:disabled {
-		@apply cursor-not-allowed opacity-50;
-	}
-</style>
