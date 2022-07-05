0b15539e3af130a90b7f234f1f1cc9953f214895
nbe <aleisa.naser@gmail.com>
2022-07-06 01:17:59 +0300

    use openapi-generator directly after install java

--- packages/sdk/watcher.ts
+++ packages/sdk/watcher.ts
@@ -17,8 +17,8 @@ chokidar
 			if (changed || !oldStats) {
 				console.log("openapi.yaml changed detected, generating new sdk...");
 
-				// delete /dist directory
-				child_process.execSync("rm -rf ./dist");
+				// clean old directories
+				child_process.execSync("rm -rf ./src && rm -rf ./dist");
 
 				// generate sdk
 				const child = child_process.spawn("pnpm", ["run", "build"]);
