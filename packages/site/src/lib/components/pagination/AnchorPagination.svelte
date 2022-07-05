197ee9de60b7431291b47e347393ea44d8f11d92
nbe <aleisa.naser@gmail.com>
2022-07-06 02:46:38 +0300

    init anchor pagination

---
+++ packages/site/src/lib/components/pagination/AnchorPagination.svelte
@@ -0,0 +1,107 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { getButtons } from '$lib/components/table/pagination';
+	import { classes } from '$lib/utils';
+	import { getTableUrl } from '$lib/utils/table-utils';
+	import type { PaginatedDto } from '@self/sdk';
+	import { ChevronLeft, ChevronRight } from '@steeze-ui/heroicons';
+	import { Icon } from '@steeze-ui/svelte-icon';
+
+	export let pagination: PaginatedDto;
+
+	const newPageHref = (url: URL, newPageIndex: number) =>
+		getTableUrl(url, { p: newPageIndex.toString() });
+
+	$: nextPageHref = newPageHref($page.url, pagination.page + 1);
+	$: prevPageHref = newPageHref($page.url, pagination.page - 1);
+
+	$: buttons = getButtons(pagination.page, pagination.pageCount);
+</script>
+
+<div
+	class="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6"
+>
+	<div class="flex flex-1 sm:hidden">
+		<a
+			sveltekit:noscroll
+			class="relative inline-flex w-32 items-center justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+			href={pagination.hasPreviousPage ? prevPageHref : null}
+			class:disabled-anchor={!pagination.hasPreviousPage}
+		>
+			Previous
+		</a>
+		<a
+			sveltekit:noscroll
+			class="relative ml-3 inline-flex w-32 items-center justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+			href={pagination.hasNextPage ? nextPageHref : null}
+			class:disabled-anchor={!pagination.hasNextPage}
+		>
+			Next
+		</a>
+	</div>
+	<div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+		<div>
+			<p class="text-sm text-gray-700">
+				Showing <span class="font-medium">{pagination.start}</span> to
+				<span class="font-medium">{pagination.end}</span>
+				of{' '}
+				<span class="font-medium">{pagination.itemCount}</span> results
+			</p>
+		</div>
+		<div>
+			<nav
+				class="relative z-0 inline-flex -space-x-px rounded-md shadow-sm"
+				aria-label="Pagination"
+			>
+				<a
+					sveltekit:noscroll
+					class="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50"
+					href={pagination.hasPreviousPage ? prevPageHref : null}
+					class:disabled-anchor={!pagination.hasPreviousPage}
+				>
+					<span class="sr-only">Previous</span>
+					<Icon src={ChevronLeft} class="h-5 w-5" aria-hidden="true" />
+				</a>
+				{#each buttons as button}
+					{#if button}
+						{@const current = button === pagination.page}
+						<a
+							sveltekit:noscroll
+							aria-current={current ? 'page' : null}
+							href={newPageHref($page.url, button)}
+							class={classes(
+								current
+									? 'z-10 bg-indigo-50 border-indigo-500 text-indigo-600'
+									: 'bg-white border-gray-300 text-gray-500 hover:bg-gray-50',
+								'relative inline-flex items-center px-4 py-2 border text-sm font-medium',
+							)}
+						>
+							{button}
+						</a>
+					{:else}
+						<span
+							class="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700"
+						>
+							...
+						</span>
+					{/if}
+				{/each}
+				<a
+					sveltekit:noscroll
+					class="relative inline-flex items-center rounded-r-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50"
+					href={pagination.hasNextPage ? nextPageHref : null}
+					class:disabled-anchor={!pagination.hasNextPage}
+				>
+					<span class="sr-only">Next</span>
+					<Icon src={ChevronRight} class="h-5 w-5" aria-hidden="true" />
+				</a>
+			</nav>
+		</div>
+	</div>
+</div>
+
+<style lang="postcss">
+	.disabled-anchor {
+		@apply cursor-not-allowed opacity-50;
+	}
+</style>
