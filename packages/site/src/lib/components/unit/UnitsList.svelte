8b70491450d88c330b15db3decff0c9f51358f10
nbe <aleisa.naser@gmail.com>
2022-07-06 04:06:11 +0300

    add pagination to /units

--- packages/site/src/lib/components/unit/UnitsList.svelte
+++ packages/site/src/lib/components/unit/UnitsList.svelte
@@ -1,22 +1,23 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import UnitCard from '$components/unit/UnitCard.svelte';
+	import AnchorPagination from '$lib/components/pagination/AnchorPagination.svelte';
 	import StackedList from '$lib/components/StackedList.svelte';
-	import type { UnitDto } from '@self/sdk';
+	import type { PaginatedUnitDto } from '@self/sdk';
 	import FaSolidBath from '~icons/fa-solid/bath';
 	import Fa6SolidBed from '~icons/fa6-solid/bed';
 	import Fa6SolidElevator from '~icons/fa6-solid/elevator';
 	import GisMeasure from '~icons/gis/measure';
 
-	export let units: UnitDto[];
+	export let units: PaginatedUnitDto;
 
 	const createHref = $page.url.pathname.startsWith('/properties')
 		? `/new/units?propertyId=${$page.url.pathname.split('/').pop()}`
 		: '/new/units';
 </script>
 
-<StackedList entityTitle="units" count={units.length} {createHref}>
-	{#each units as unit (unit.id)}
+<StackedList entityTitle="units" count={units.results.length} {createHref}>
+	{#each units.results as unit (unit.id)}
 		{@const icons = [
 			{
 				label: unit.bed,
@@ -43,4 +44,5 @@
 			<UnitCard {unit} {icons} />
 		</li>
 	{/each}
+	<AnchorPagination pagination={units.pagination} />
 </StackedList>
