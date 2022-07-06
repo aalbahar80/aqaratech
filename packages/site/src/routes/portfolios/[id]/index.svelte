89911f8cbaf355ccf0bbf2c6884d5bde1c96bf2c
nbe <aleisa.naser@gmail.com>
2022-07-06 04:30:20 +0300

    add related pgn to /unit /portfolio

--- packages/site/src/routes/portfolios/[id]/index.svelte
+++ packages/site/src/routes/portfolios/[id]/index.svelte
@@ -1,12 +1,19 @@
 <script lang="ts" context="module">
 	import PortfolioPage from '$lib/components/portfolio/PortfolioPage.svelte';
+	import { parseParams } from '$lib/utils/parse-params';
 	import type { LoadEvent } from '@sveltejs/kit';
 	import type { LP } from 'src/types/load-props';
 
-	export const load = async ({ params, stuff }: LoadEvent<{ id: string }>) => {
+	export const load = async ({
+		params,
+		stuff,
+		url,
+	}: LoadEvent<{ id: string }>) => {
+		const sParams = parseParams(url);
+
 		const [portfolio, properties] = await Promise.all([
 			stuff.api!.portfolios.findOne({ id: params.id }),
-			stuff.api!.portfolios.findProperties({ id: params.id }),
+			stuff.api!.portfolios.findProperties({ id: params.id, ...sParams }),
 		]);
 
 		return { props: { portfolio, properties } };
--- packages/site/src/routes/units/[id].svelte
+++ packages/site/src/routes/units/[id].svelte
@@ -1,12 +1,19 @@
 <script lang="ts" context="module">
 	import UnitPage from '$lib/components/unit/UnitPage.svelte';
+	import { parseParams } from '$lib/utils/parse-params';
 	import type { LoadEvent } from '@sveltejs/kit';
 	import type { LP } from 'src/types/load-props';
 
-	export const load = async ({ params, stuff }: LoadEvent<{ id: string }>) => {
+	export const load = async ({
+		params,
+		stuff,
+		url,
+	}: LoadEvent<{ id: string }>) => {
+		const sParams = parseParams(url);
+
 		const [unit, leases] = await Promise.all([
 			stuff.api!.units.findOne({ id: params.id }),
-			stuff.api!.units.findLeases({ id: params.id }),
+			stuff.api!.units.findLeases({ id: params.id, ...sParams }),
 		]);
 
 		return { props: { unit, leases } };
