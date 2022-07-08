75af8ff44023ca552d3979c046ac747ee1c2630d
nbe <aleisa.naser@gmail.com>
2022-07-08 05:27:17 +0300

    add clean script to generate sdk

--- packages/sdk/watcher.ts
+++ packages/sdk/watcher.ts
@@ -17,9 +17,6 @@ chokidar
 			if (changed || !oldStats) {
 				console.log("openapi.yaml changed detected, generating new sdk...");
 
-				// clean old directories
-				child_process.execSync("rm -rf ./src && rm -rf ./dist");
-
 				child_process.execSync("pnpm run build");
 			} else {
 				console.log("skipping sdk generation...");
