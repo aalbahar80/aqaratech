ec9f5bee91eb3288fd884c836627f5573a75fb5f
nbe <aleisa.naser@gmail.com>
2022-07-08 03:27:03 +0300

    export by name

--- packages/seed/src/index.ts
+++ packages/seed/src/index.ts
@@ -1,3 +1,22 @@
-export * from "./generators.js";
+export {
+	fakeEmail,
+	fakeExpense,
+	fakeLease,
+	fakeLeaseInvoice,
+	fakeLeaseInvoiceBasic,
+	fakeMaintenanceOrder,
+	fakePortfolio,
+	fakeProperty,
+	fakeTenant,
+	fakeUnit,
+	testOrgEmail,
+	testOrgId,
+	testPassword,
+	testPortfolioEmail,
+	testPortfolioId,
+	testTenantEmail,
+	testTenantId,
+	timespan,
+} from "./generators.js";
 export { insertExpenseTypes } from "./prep-db.js";
 export { seed } from "./seed.js";
