379e7a3edbf534d5a54953cc023f4e8e5723bb6a
nbe <aleisa.naser@gmail.com>
2022-07-08 03:50:18 +0300

    [wip] lease predefined parse

--- packages/site/src/routes/new/leases.svelte
+++ packages/site/src/routes/new/leases.svelte
@@ -1,87 +1,90 @@
 <script context="module" lang="ts">
-	import { trpc } from '$lib/client/trpc';
 	import LeaseForm from '$lib/components/lease/LeaseForm.svelte';
 	import type { Predefined } from '$models/interfaces/lease.interface';
 	import type { Load } from '@sveltejs/kit';
 	import { Property } from '../../lib/models/classes/property.class';
 	import { Unit } from '../../lib/models/classes/unit.class';
 
-	export const load: Load = async ({ url, fetch }) => {
+	export const load: Load = async ({ url, stuff }) => {
 		let options = Object.fromEntries(url.searchParams.entries());
 		let predefined: Predefined | undefined;
 
-		if (options.leaseId) {
-			// renewing
-			const lease = await trpc(fetch).query('leases:read', options.leaseId);
-			predefined = {
-				initiator: 'lease',
-				tenantId: lease.tenantId,
-				fullName: lease.tenant.fullName ?? '',
-				unitId: lease.unitId,
-				unitType: lease.unit.type,
-				unitNumber: lease.unit.unitNumber,
-				propertyId: lease.unit.property.id,
-				address: Property.getLabel(lease.unit.property),
-				monthlyRent: lease.monthlyRent,
-				tenant: {
-					label: lease.tenant.shortName || lease.tenant.fullName,
-					value: lease.tenantId,
-				},
-				portfolio: {
-					label:
-						lease.unit.property.portfolio.shortName ||
-						lease.unit.property.portfolio.fullName,
-					value: lease.unit.property.portfolio.id,
-				},
-				property: {
-					label: Property.getLabel(lease.unit.property),
-					value: lease.unit.property.id,
-				},
-				unit: {
-					label: Unit.getLabel(lease.unit),
-					value: lease.unitId,
-				},
-			};
-		} else if (options.tenantId) {
-			const tenant = await trpc(fetch).query('tenants:read', options.tenantId);
-			predefined = {
-				initiator: 'tenant',
-				tenantId: tenant.id,
-				fullName: tenant.fullName,
-				tenant: {
-					label: tenant.shortName || tenant.fullName,
-					value: tenant.id,
-				},
-				portfolio: undefined,
-				property: undefined,
-				unit: undefined,
-			};
-		} else if (options.unitId) {
-			const unit = await trpc(fetch).query('units:read', options.unitId);
-			predefined = {
-				initiator: 'unit',
-				unitId: unit.id,
-				unitType: unit.type,
-				unitNumber: unit.unitNumber,
-				propertyId: unit.propertyId,
-				address: Property.getLabel(unit.property),
-				tenant: undefined,
-				portfolio: {
-					label:
-						unit.property.portfolio.shortName ||
-						unit.property.portfolio.fullName,
-					value: unit.property.portfolio.id,
-				},
-				property: {
-					label: Property.getLabel(unit.property),
-					value: unit.property.id,
-				},
-				unit: {
-					label: Unit.getLabel(unit),
-					value: unit.id,
-				},
-			};
-		}
+		// if (options.leaseId) {
+		// 	// renewing
+		// 	// const lease = await trpc(fetch).query('leases:read', options.leaseId);
+		// 	const lease = await stuff.api!.leases.findOne({ id: options.leaseId });
+		// 	predefined = {
+		// 		initiator: 'lease',
+		// 		tenantId: lease.tenantId,
+		// 		fullName: lease.ext.tenantName,
+		// 		unitId: lease.unitId,
+		// 		unitLabel: lease.ext.unitLabel,
+		// 		// unitType: lease.unit.type,
+		// 		// unitNumber: lease.unit.unitNumber,
+		// 		// propertyId: lease.unit.property.id,
+		// 		propertyId: lease.breadcrumbs.property.
+		// 		address: Property.getLabel(lease.unit.property),
+		// 		monthlyRent: lease.monthlyRent,
+		// 		tenant: {
+		// 			label: lease.tenant.shortName || lease.tenant.fullName,
+		// 			value: lease.tenantId,
+		// 		},
+		// 		portfolio: {
+		// 			label:
+		// 				lease.unit.property.portfolio.shortName ||
+		// 				lease.unit.property.portfolio.fullName,
+		// 			value: lease.unit.property.portfolio.id,
+		// 		},
+		// 		property: {
+		// 			label: Property.getLabel(lease.unit.property),
+		// 			value: lease.unit.property.id,
+		// 		},
+		// 		unit: {
+		// 			label: Unit.getLabel(lease.unit),
+		// 			value: lease.unitId,
+		// 		},
+		// 	};
+		// } else if (options.tenantId) {
+		// 	const tenant = await stuff.api!.tenants.findOne({ id: options.tenantId });
+		// 	predefined = {
+		// 		initiator: 'tenant',
+		// 		tenantId: tenant.id,
+		// 		fullName: tenant.fullName,
+		// 		tenant: {
+		// 			label: tenant.shortName || tenant.fullName,
+		// 			value: tenant.id,
+		// 		},
+		// 		portfolio: undefined,
+		// 		property: undefined,
+		// 		unit: undefined,
+		// 	};
+		// } else if (options.unitId) {
+		// 	const unit = await stuff.api!.units.findOne({ id: options.unitId });
+		// 	unit.hateoas.
+		// 	predefined = {
+		// 		initiator: 'unit',
+		// 		unitId: unit.id,
+		// 		unitType: unit.type,
+		// 		unitNumber: unit.unitNumber,
+		// 		propertyId: unit.propertyId,
+		// 		address: Property.getLabel(unit.property),
+		// 		tenant: undefined,
+		// 		portfolio: {
+		// 			label:
+		// 				unit.property.portfolio.shortName ||
+		// 				unit.property.portfolio.fullName,
+		// 			value: unit.property.portfolio.id,
+		// 		},
+		// 		property: {
+		// 			label: Property.getLabel(unit.property),
+		// 			value: unit.property.id,
+		// 		},
+		// 		unit: {
+		// 			label: Unit.getLabel(unit),
+		// 			value: unit.id,
+		// 		},
+		// 	};
+		// }
 		return {
 			props: { predefined },
 		};
