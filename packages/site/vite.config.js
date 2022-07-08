05c499b25bf3d50be4813e51cce7bdc2634e9338
nbe <aleisa.naser@gmail.com>
2022-07-08 03:00:35 +0300

    use vite config

---
+++ packages/site/vite.config.js
@@ -0,0 +1,73 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import path, { resolve } from 'path';
+import icons from 'unplugin-icons/vite';
+import { fileURLToPath } from 'url';
+// import { visualizer } from 'rollup-plugin-visualizer';
+
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [sveltekit(), icons({ compiler: 'svelte' })],
+	envDir: resolve(path.dirname(fileURLToPath(import.meta.url)), './env'),
+	server: {
+		watch: {
+			ignored: [
+				(path) => {
+					if (path.includes('layout')) {
+						return true;
+					}
+				},
+			],
+		},
+	},
+	// delete .svelte-kit folder b4 testing
+	test: {
+		exclude: ['**/tests/**', 'node_modules'],
+		deps: {
+			inline: ['date-fns'],
+		},
+	},
+	define:
+		process.env.NODE_ENV === 'production'
+			? {
+					__SENTRY_DEBUG__: false,
+					VITE_MY_VAR: 'SOME_VALUE',
+			  }
+			: undefined,
+	resolve: {
+		alias: {
+			$components: resolve('./src/lib/components'),
+			$lib: resolve('.', './src/lib'),
+			$models: resolve('./src/lib/models'),
+			$utils: resolve('.', './src/lib/utils'),
+			$routers: resolve('.', './src/lib/server/trpc/routers'),
+			$environment: resolve('.', './src/lib/environment'),
+			$user: resolve('.', './src/user'),
+			// '@self/site': resolve('./src/lib'),
+		},
+	},
+	ssr: {
+		noExternal:
+			process.env.NODE_ENV === 'production'
+				? [
+						'superjson',
+						// 'date-fns',
+				  ]
+				: undefined,
+	},
+	// plugins:
+	// 	process.env.NODE_ENV === 'development'
+	// 		? [
+	// 				visualizer((opts) => {
+	// 					return {
+	// 						filename: path.join(opts.dir, 'stats.html'),
+	// 						open: true,
+	// 						openOptions: {
+	// 							app: 'google-chrome'
+	// 						}
+	// 					};
+	// 				}),
+	// 		  ]
+	// 		: undefined,
+};
+
+export default config;
