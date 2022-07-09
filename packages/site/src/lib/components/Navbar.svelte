52e8692e4eb373e90e3c61888012e936008d6615
nbe <aleisa.naser@gmail.com>
2022-07-09 11:06:56 +0300

    simplify navbar structure

--- packages/site/src/lib/components/Navbar.svelte
+++ packages/site/src/lib/components/Navbar.svelte
@@ -20,53 +20,49 @@
 		class="relative mx-auto flex max-w-7xl items-center justify-between px-4 sm:px-6"
 		aria-label="Global"
 	>
-		<div class="flex flex-1 items-center">
-			<div class="flex w-full items-center justify-between lg:w-auto">
-				<a href="/">
-					<span class="sr-only">Workflow</span>
-					<img
-						class="h-8 w-auto sm:h-10"
-						src="https://tailwindui.com/img/logos/workflow-mark-teal-200-cyan-400.svg"
-						alt=""
-					/>
-				</a>
-				{#if $session.user}
-					<div class="-mr-2 flex items-center gap-6 lg:hidden">
-						{#if showDashboard}
-							<a
-								href={$session.user.role.home}
-								class="inline-flex items-center rounded-md border border-transparent bg-gray-600 px-4 py-2 text-base font-medium text-white hover:bg-gray-700 lg:hidden"
-							>
-								Dashboard
-							</a>
-						{/if}
-						<NavPopover {navigation} />
-					</div>
-				{:else}
+		<a href="/">
+			<span class="sr-only">Workflow</span>
+			<img
+				class="h-8 w-auto sm:h-10"
+				src="https://tailwindui.com/img/logos/workflow-mark-teal-200-cyan-400.svg"
+				alt=""
+			/>
+		</a>
+		{#if $session.user}
+			<div class="order-last -mr-2 flex items-center gap-6 lg:hidden">
+				{#if showDashboard}
 					<a
-						href="/api/auth/login"
-						sveltekit:reload
+						href={$session.user.role.home}
 						class="inline-flex items-center rounded-md border border-transparent bg-gray-600 px-4 py-2 text-base font-medium text-white hover:bg-gray-700 lg:hidden"
 					>
-						Log in
+						Dashboard
 					</a>
 				{/if}
+				<NavPopover {navigation} />
 			</div>
+		{:else}
+			<a
+				href="/api/auth/login"
+				sveltekit:reload
+				class="inline-flex items-center rounded-md border border-transparent bg-gray-600 px-4 py-2 text-base font-medium text-white hover:bg-gray-700 lg:hidden"
+			>
+				Log in
+			</a>
+		{/if}
 
-			<!-- Large screen: nav links -->
-			<div class="hidden space-x-8 lg:ml-10 lg:flex">
-				{#each navigation as item (item.name)}
-					<a
-						sveltekit:prefetch
-						href={item.href}
-						class="text-base font-medium text-white hover:text-gray-300"
-					>
-						{item.name}
-					</a>
-				{/each}
-			</div>
-			<SearchButton />
+		<!-- Large screen: nav links -->
+		<div class="hidden space-x-8 lg:ml-10 lg:flex">
+			{#each navigation as item (item.name)}
+				<a
+					sveltekit:prefetch
+					href={item.href}
+					class="text-base font-medium text-white hover:text-gray-300"
+				>
+					{item.name}
+				</a>
+			{/each}
 		</div>
+		<SearchButton />
 
 		<!-- Large screen: nav actions -->
 		<div class="hidden lg:flex lg:items-center lg:space-x-6">
