64e140a5a7531172ad42b8953496ec1d29aad826
nbe <aleisa.naser@gmail.com>
2022-07-09 04:52:55 +0300

    replace icons

--- packages/site/src/lib/components/MSearch.svelte
+++ packages/site/src/lib/components/MSearch.svelte
@@ -1,42 +1,44 @@
 <script lang="ts">
-import { classes } from '$lib/utils/classes';
-import { Fragment, useState } from 'react'
-import { SearchIcon } from '@heroicons/react/solid'
-import { EmojiSadIcon, GlobeIcon } from '@heroicons/react/outline'
-import { Combobox, Dialog, Transition } from '@headlessui/react'
+	import { classes } from '$lib/utils/classes';
+	// import { Fragment, useState } from 'react'
+	// import { SearchIcon } from '@heroicons/react/solid'
+	// import { EmojiSadIcon, GlobeIcon } from '@heroicons/react/outline'
+	// import { Combobox, Dialog, Transition } from '@headlessui/react'
+	import {
+		Dialog,
+		TransitionChild,
+		TransitionRoot,
+	} from '@rgossiaux/svelte-headlessui';
+	import { EmojiSad, Globe, Search } from '@steeze-ui/heroicons';
+	import { Icon } from '@steeze-ui/svelte-icon';
 
-const items = [
-  { id: 1, name: 'Workflow Inc.', category: 'Clients', url: '#' },
-  // More items...
-]
+	const items = [
+		{ id: 1, name: 'Workflow Inc.', category: 'Clients', url: '#' },
+		// More items...
+	];
 
+	let query = '';
+	let open = true;
 
-export default function Example() {
-  const [query, setQuery] = useState('')
+	const filteredItems =
+		query === ''
+			? []
+			: items.filter((item) => {
+					return item.name.toLowerCase().includes(query.toLowerCase());
+			  });
 
-  const [open, setOpen] = useState(true)
-
-  const filteredItems =
-    query === ''
-      ? []
-      : items.filter((item) => {
-          return item.name.toLowerCase().includes(query.toLowerCase())
-        })
-
-  const groups = filteredItems.reduce((groups, item) => {
-    return { ...groups, [item.category]: [...(groups[item.category] || []), item] }
-  }, {})
+	// make reactive
+	const groups = filteredItems.reduce((groups, item) => {
+		return {
+			...groups,
+			[item.category]: [...(groups[item.category] || []), item],
+		};
+	}, {});
 </script>
 
-<TransitionRoot
-	show={open}
-	as={Fragment}
-	afterLeave={() => setQuery('')}
-	appear
->
-	<Dialog as="div" className="relative z-10" onClose={setOpen}>
+<TransitionRoot show={open} afterLeave={() => setQuery('')} appear>
+	<Dialog as="div" class="relative z-10" onClose={setOpen}>
 		<TransitionChild
-			as={Fragment}
 			enter="ease-out duration-300"
 			enterFrom="opacity-0"
 			enterTo="opacity-100"
@@ -44,14 +46,11 @@ export default function Example() {
 			leaveFrom="opacity-100"
 			leaveTo="opacity-0"
 		>
-			<div
-				className="fixed inset-0 bg-gray-500 bg-opacity-25 transition-opacity"
-			/>
+			<div class="fixed inset-0 bg-gray-500 bg-opacity-25 transition-opacity" />
 		</TransitionChild>
 
-		<div className="fixed inset-0 z-10 overflow-y-auto p-4 sm:p-6 md:p-20">
+		<div class="fixed inset-0 z-10 overflow-y-auto p-4 sm:p-6 md:p-20">
 			<TransitionChild
-				as={Fragment}
 				enter="ease-out duration-300"
 				enterFrom="opacity-0 scale-95"
 				enterTo="opacity-100 scale-100"
@@ -60,16 +59,18 @@ export default function Example() {
 				leaveTo="opacity-0 scale-95"
 			>
 				<DialogPanel
-					className="mx-auto max-w-xl transform overflow-hidden rounded-xl bg-white shadow-2xl ring-1 ring-black ring-opacity-5 transition-all"
+					class="mx-auto max-w-xl transform overflow-hidden rounded-xl bg-white shadow-2xl ring-1 ring-black ring-opacity-5 transition-all"
 				>
 					<Combobox onChange={(item) => (window.location = item.url)}>
-						<div className="relative">
-							<SearchIcon
-								className="pointer-events-none absolute top-3.5 left-4 h-5 w-5 text-gray-400"
+						<div class="relative">
+							<Icon
+								src={Search}
+								class="pointer-events-none absolute top-3.5 left-4 h-5 w-5 text-gray-400"
 								aria-hidden="true"
+								theme="solid"
 							/>
 							<ComboboxInput
-								className="h-12 w-full border-0 bg-transparent pl-11 pr-4 text-gray-800 placeholder-gray-400 focus:ring-0 sm:text-sm"
+								class="h-12 w-full border-0 bg-transparent pl-11 pr-4 text-gray-800 placeholder-gray-400 focus:ring-0 sm:text-sm"
 								placeholder="Search..."
 								onChange={(event) => setQuery(event.target.value)}
 							/>
@@ -77,16 +78,18 @@ export default function Example() {
 
 						{#if query === ''}
 							<div
-								className="border-t border-gray-100 py-14 px-6 text-center text-sm sm:px-14"
+								class="border-t border-gray-100 py-14 px-6 text-center text-sm sm:px-14"
 							>
-								<GlobeIcon
-									className="mx-auto h-6 w-6 text-gray-400"
+								<Icon
+									src={Globe}
+									class="mx-auto h-6 w-6 text-gray-400"
 									aria-hidden="true"
+									theme="outline"
 								/>
-								<p className="mt-4 font-semibold text-gray-900">
+								<p class="mt-4 font-semibold text-gray-900">
 									Search for clients and projects
 								</p>
-								<p className="mt-2 text-gray-500">
+								<p class="mt-2 text-gray-500">
 									Quickly access clients and projects by running a global
 									search.
 								</p>
@@ -96,22 +99,22 @@ export default function Example() {
 						{#if filteredItems.length > 0}
 							<ComboboxOptions
 								static
-								className="max-h-80 scroll-pt-11 scroll-pb-2 space-y-2 overflow-y-auto pb-2"
+								class="max-h-80 scroll-pt-11 scroll-pb-2 space-y-2 overflow-y-auto pb-2"
 							>
 								{#each Object.entries(groups) as [category, items] (category)}
 									<li>
 										<h2
-											className="bg-gray-100 py-2.5 px-4 text-xs font-semibold text-gray-900"
+											class="bg-gray-100 py-2.5 px-4 text-xs font-semibold text-gray-900"
 										>
 											{category}
 										</h2>
-										<ul className="mt-2 text-sm text-gray-800">
+										<ul class="mt-2 text-sm text-gray-800">
 											{#each items as item}
 												<ComboboxOption
 													key={item.id}
 													value={item}
-													className={({ active }) =>
-														classNames(
+													class={({ active }) =>
+														classes(
 															'cursor-default select-none px-4 py-2',
 															active && 'bg-indigo-600 text-white',
 														)}
@@ -127,16 +130,16 @@ export default function Example() {
 
 						{#if query !== '' && filteredItems.length === 0}
 							<div
-								className="border-t border-gray-100 py-14 px-6 text-center text-sm sm:px-14"
+								class="border-t border-gray-100 py-14 px-6 text-center text-sm sm:px-14"
 							>
-								<EmojiSadIcon
-									className="mx-auto h-6 w-6 text-gray-400"
+								<Icon
+									src={EmojiSad}
+									class="mx-auto h-6 w-6 text-gray-400"
 									aria-hidden="true"
+									theme="outline"
 								/>
-								<p className="mt-4 font-semibold text-gray-900">
-									No results found
-								</p>
-								<p className="mt-2 text-gray-500">
+								<p class="mt-4 font-semibold text-gray-900">No results found</p>
+								<p class="mt-2 text-gray-500">
 									We couldn’t find anything with that term. Please try again.
 								</p>
 							</div>
