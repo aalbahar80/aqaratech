9807b39dc2da3577d89962d3abac2354695993c8
nbe <aleisa.naser@gmail.com>
2022-07-10 02:09:02 +0300

    set log level to debug

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -10,7 +10,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    logger: ['debug', 'log', 'warn', 'error'],
+    logger: ['log', 'warn', 'error'],
     cors: true, // TODO adjust in prod
   });
 
