46fef84de06a68a3cadd5124ad060e91440e29e2
nbe <aleisa.naser@gmail.com>
2022-07-09 10:25:51 +0300

    init searchbar

--- packages/site/src/lib/components/Navbar.svelte
+++ packages/site/src/lib/components/Navbar.svelte
@@ -5,6 +5,8 @@
 	import MSearch from '$lib/components/MSearch.svelte';
 	import { getDocs } from '$lib/components/navbar/docs-url';
 	import type { UserConfig } from '$lib/models/interfaces/user.interface';
+	import { Search } from '@steeze-ui/heroicons';
+	import { Icon } from '@steeze-ui/svelte-icon';
 
 	export let navigation: UserConfig['navLinks'] = [];
 
@@ -31,6 +33,33 @@
 					/>
 				</a>
 
+				<MSearch bind:open />
+				<div class="flex flex-1 justify-center px-2 lg:ml-6 lg:justify-end">
+					<div class="w-full max-w-lg lg:max-w-xs">
+						<button class="relative">
+							<div
+								class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3"
+							>
+								<Icon
+									src={Search}
+									class="h-5 w-5 text-gray-400"
+									aria-hidden="true"
+									theme="solid"
+								/>
+							</div>
+							<input
+								id="search"
+								name="search"
+								class="block w-full rounded-md border border-transparent bg-gray-700 py-2 pl-10 pr-3 leading-5 text-gray-300 placeholder-gray-400 focus:border-white focus:bg-white focus:text-gray-900 focus:outline-none focus:ring-white sm:text-sm"
+								placeholder="Search"
+								type="search"
+								on:click={() => {
+									open = true;
+								}}
+							/>
+						</button>
+					</div>
+				</div>
 				{#if $session.user}
 					<div class="-mr-2 flex items-center gap-6 lg:hidden">
 						{#if showDashboard}
@@ -79,14 +108,6 @@
 						Dashboard
 					</a>
 				{/if}
-				<MSearch bind:open />
-				<button
-					on:click={() => {
-						open = true;
-					}}
-				>
-					search
-				</button>
 				{#if dev}
 					<a
 						href="/account/profile"
