2c415269866232fb136f8e9c9eeddd4c70647a32
nbe <aleisa.naser@gmail.com>
2022-07-10 04:02:44 +0300

    rm viteconfig

--- packages/site/svelte.config.js
+++ packages/site/svelte.config.js
@@ -1,5 +1,7 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import icons from 'unplugin-icons/vite';
+// import { resolve } from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -22,6 +24,43 @@ const config = {
 			$environment: 'src/lib/environment',
 			$user: 'src/user',
 		},
+	},
+	vite: {
+		plugins: [icons({ compiler: 'svelte', autoInstall: false })],
+		// resolve: {
+		// alias: {
+		// 	$components: resolve('./src/lib/components'),
+		// 	$lib: resolve('.', './src/lib'),
+		// 	$models: resolve('./src/lib/models'),
+		// 	$utils: resolve('.', './src/lib/utils'),
+		// 	$routers: resolve('.', './src/lib/server/trpc/routers'),
+		// 	$environment: resolve('.', './src/lib/environment'),
+		// 	$user: resolve('.', './src/user'),
+		// 	// '@self/site': resolve('./src/lib'),
+		// },
+		// },
+		// test: {
+		// 	exclude: ['**/tests/**', 'node_modules'],
+		// 	deps: {
+		// 		inline: ['date-fns'],
+		// 	},
+		// },
+		// define:
+		// 	process.env.NODE_ENV === 'production'
+		// 		? {
+		// 				__SENTRY_DEBUG__: false,
+		// 				VITE_MY_VAR: 'SOME_VALUE',
+		// 		  }
+		// 		: undefined,
+		// ssr: {
+		// 	noExternal:
+		// 		process.env.NODE_ENV === 'production'
+		// 			? [
+		// 					'superjson',
+		// 					// 'date-fns',
+		// 			  ]
+		// 			: undefined,
+		// },
 	},
 };
 
