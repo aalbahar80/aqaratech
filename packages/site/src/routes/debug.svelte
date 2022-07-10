ff4305c1acdc174e115000e8ee0b6516a626eb2b
nbe <aleisa.naser@gmail.com>
2022-07-10 13:32:16 +0300

    init /debug

---
+++ packages/site/src/routes/debug.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { browser, dev, mode, prerendering, prod, server } from '$app/env';
+	import { assets, base } from '$app/paths';
+	import { navigating, page, session, updated } from '$app/stores';
+
+	export let processEnv: Record<string, any>;
+	export let environment: Record<string, any>;
+
+	const appEnv = { browser, dev, mode, prerendering, prod, server };
+	const appStores = {
+		updated: $updated,
+		navigating: $navigating,
+		page: $page,
+		session: $session,
+	};
+</script>
+
+<div class="prose">
+	<h3>import.meta.env</h3>
+	<pre>{JSON.stringify(import.meta.env, null, 2)}</pre>
+	<h3>import.meta.url</h3>
+	<pre>{JSON.stringify(import.meta.url, null, 2)}</pre>
+	<h3>import.meta</h3>
+	<pre>{JSON.stringify(import.meta, null, 2)}</pre>
+	<h3>$app/env</h3>
+	<pre>{JSON.stringify(appEnv, null, 2)}</pre>
+	<h3>environment config</h3>
+	<pre>{JSON.stringify(environment, null, 2)}</pre>
+	<h3>processEnv</h3>
+	<pre>{JSON.stringify(processEnv, null, 2)}</pre>
+	<h3>$app/stores</h3>
+	<pre>{JSON.stringify(appStores, null, 2)}</pre>
+	<h3>base</h3>
+	<pre>{JSON.stringify(base, null, 2)}</pre>
+	<h3>assets</h3>
+	<pre>{JSON.stringify(assets, null, 2)}</pre>
+</div>
---
+++ packages/site/src/routes/debug.ts
@@ -0,0 +1,16 @@
+import { environment } from '$environment';
+import type { RequestHandler } from '@sveltejs/kit';
+
+export const get: RequestHandler = async () => {
+	if (environment.envName === 'prod') {
+		return { status: 403 };
+	}
+
+	return {
+		status: 200,
+		body: {
+			environment,
+			processEnv: process.env,
+		},
+	};
+};
