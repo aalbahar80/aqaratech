eedf9c8460627b973b24d1386cc167d03edf38f7
nbe <aleisa.naser@gmail.com>
2022-07-12 21:19:55 +0300

    type cast url params

--- packages/site/src/lib/utils/parse-params.ts
+++ packages/site/src/lib/utils/parse-params.ts
@@ -1,4 +1,4 @@
-import type { SortOrderEnum } from '@self/sdk';
+import type { CombinedEnum, SortOrderEnum } from '@self/sdk';
 
 export const parseParams = (url: URL) => {
 	return {
@@ -6,7 +6,8 @@ export const parseParams = (url: URL) => {
 		take: +(url.searchParams.get('take') || 20),
 		q: url.searchParams.get('q') ?? undefined,
 		sortOrder: (url.searchParams.get('sortOrder') ?? 'desc') as SortOrderEnum,
-		orderBy: url.searchParams.get('orderBy') ?? undefined,
-		filter: url.searchParams.get('filter') ?? undefined,
+		// TODO fix types vs manual type-cast
+		orderBy: (url.searchParams.get('orderBy') as CombinedEnum) ?? undefined,
+		filter: (url.searchParams.get('filter') as unknown as object) ?? undefined,
 	};
 };
