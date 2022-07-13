2cdb6ea0201110a485221c00b4dcb8c177f8748d
nbe <aleisa.naser@gmail.com>
2022-07-13 08:38:30 +0300

    rm sample combobox

--- packages/site/src/lib/components/ComboboxSample.svelte
+++
@@ -1,7 +0,0 @@
-<!-- <script lang="ts">
-	import Combobox from '$lib/components/Combobox.svelte';
-	import Sample from '$lib/components/sample.svelte';
-</script> -->
-
-<!-- <Combobox /> -->
-<!-- <Sample /> -->
