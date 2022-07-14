b612a1ed728ba115337656df7ae88fdfbd650d4e
nbe <aleisa.naser@gmail.com>
2022-07-14 10:11:15 +0300

    rm entity classes

--- packages/site/src/lib/models/classes/index.ts
+++
@@ -1,9 +0,0 @@
-export { Portfolio } from './portfolio.class.js';
-// export { Entity } from './entity.class';
-export { Expense } from './expense.class.js';
-export { Lease } from './lease.class.js';
-export { MaintenanceOrder } from './maintenanceOrder.class.js';
-export { Property } from './property.class.js';
-export { Tenant } from './tenant.class.js';
-export { Transaction } from './transaction.class.js';
-export { Unit } from './unit.class.js';
