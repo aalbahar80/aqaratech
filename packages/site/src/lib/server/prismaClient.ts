41692dc2c57d02ab3ee6a10793b82bdf585652ec
nbe <aleisa.naser@gmail.com>
2022-07-14 12:28:21 +0300

    rm prisma dep from /site

--- packages/site/src/lib/server/prismaClient.ts
+++ packages/site/src/lib/server/prismaClient.ts
@@ -1,22 +1,23 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-import type { PrismaClient as PrismaClientType } from '@prisma/client';
-import pkg from '@prisma/client';
+// /* eslint-disable @typescript-eslint/ban-ts-comment */
+// import type { PrismaClient as PrismaClientType } from '@prisma/client';
+// import pkg from '@prisma/client';
 
-const { PrismaClient } = pkg;
+// const { PrismaClient } = pkg;
 
-const prismaClient =
-	// @ts-ignore
-	global.prismaClient ||
-	new PrismaClient({
-		// log: [{ level: 'query', emit: 'event' }, 'info', 'warn', 'error'],
-		// errorFormat: 'pretty',
-	});
+// const prismaClient =
+// 	// @ts-ignore
+// 	global.prismaClient ||
+// 	new PrismaClient({
+// 		// log: [{ level: 'query', emit: 'event' }, 'info', 'warn', 'error'],
+// 		// errorFormat: 'pretty',
+// 	});
 
-if (process.env.NODE_ENV === 'development') {
-	// @ts-ignore
-	global.prismaClient = prismaClient;
-}
-// prismaClient.$on('query', (e: any) => {
-// 	console.log(e);
-// });
-export default prismaClient as PrismaClientType;
+// if (process.env.NODE_ENV === 'development') {
+// 	// @ts-ignore
+// 	global.prismaClient = prismaClient;
+// }
+// // prismaClient.$on('query', (e: any) => {
+// // 	console.log(e);
+// // });
+// export default prismaClient as PrismaClientType;
+export default {};
