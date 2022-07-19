f71347ebf0a207006f189777b80f373990ac7738
nbe <aleisa.naser@gmail.com>
2022-07-19 03:46:41 +0300

    extract net income line chart

--- packages/site/src/lib/components/dashboard/cards/NetIncomeCard.svelte
+++ packages/site/src/lib/components/dashboard/cards/NetIncomeCard.svelte
@@ -1,39 +1,11 @@
 <script lang="ts">
-	import Chart from '$lib/components/Chart.svelte';
+	import NetIncomeLineChart from '$lib/components/dashboard/cards/NetIncomeLineChart.svelte';
 	import NetIncomeTable from '$lib/components/dashboard/cards/NetIncomeTable.svelte';
-	import { netChart } from '$lib/components/dashboard/charts/net';
 	import DashCard from '$lib/components/dashboard/DashCard.svelte';
-	import { getColor } from '$lib/config/constants';
 	import type { ByMonthDto } from '@self/sdk';
 
 	export let income: ByMonthDto[];
 	export let expenses: ByMonthDto[];
-
-	// CHART
-	$: datasets = [
-		{
-			label: 'Income',
-			borderColor: getColor(0, 2),
-			data: income,
-			parsing: {
-				yAxisKey: 'amount',
-				xAxisKey: 'date',
-			},
-			backgroundColor: getColor(0, 2),
-			// borderRadius: 10,
-		},
-		{
-			label: 'Expenses',
-			borderColor: getColor(1, 2),
-			data: expenses,
-			parsing: {
-				yAxisKey: 'amount',
-				xAxisKey: 'date',
-			},
-			backgroundColor: getColor(1, 2),
-			// borderRadius: 10,
-		},
-	];
 </script>
 
 <DashCard
@@ -42,9 +14,7 @@
 	empty={expenses.length < 1 && income.length < 1}
 >
 	<div slot="chart">
-		<Chart let:height let:width>
-			<canvas {height} {width} use:netChart={datasets} />
-		</Chart>
+		<NetIncomeLineChart {income} {expenses} />
 	</div>
 	<div slot="data">
 		<NetIncomeTable {income} {expenses} />
---
+++ packages/site/src/lib/components/dashboard/cards/NetIncomeLineChart.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import Chart from '$lib/components/Chart.svelte';
+	import { netChart } from '$lib/components/dashboard/charts/net';
+	import { getColor } from '$lib/config/constants';
+	import type { ByMonthDto } from '@self/sdk';
+
+	export let income: ByMonthDto[];
+	export let expenses: ByMonthDto[];
+
+	$: datasets = [
+		{
+			label: 'Income',
+			borderColor: getColor(0, 2),
+			data: income,
+			parsing: {
+				yAxisKey: 'amount',
+				xAxisKey: 'date',
+			},
+			backgroundColor: getColor(0, 2),
+			// borderRadius: 10,
+		},
+		{
+			label: 'Expenses',
+			borderColor: getColor(1, 2),
+			data: expenses,
+			parsing: {
+				yAxisKey: 'amount',
+				xAxisKey: 'date',
+			},
+			backgroundColor: getColor(1, 2),
+			// borderRadius: 10,
+		},
+	];
+</script>
+
+<Chart let:height let:width>
+	<canvas {height} {width} use:netChart={datasets} />
+</Chart>
