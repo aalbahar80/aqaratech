7a7d5607fc3ed93da6bf387c033c2ea94e29a859
nbe <aleisa.naser@gmail.com>
2022-07-19 18:15:25 +0300

    init revenue/expense cards on property/unit pages

--- packages/site/src/routes/properties/[id]/index.svelte
+++ packages/site/src/routes/properties/[id]/index.svelte
@@ -1,5 +1,7 @@
 <script lang="ts" context="module">
+	import ExpensesCard from '$lib/components/dashboard/cards/ExpensesCard.svelte';
 	import NetIncomeCard from '$lib/components/dashboard/cards/NetIncomeCard.svelte';
+	import RevenueCard from '$lib/components/dashboard/cards/RevenueCard.svelte';
 	import PropertyPage from '$lib/components/property/PropertyPage.svelte';
 	import UnitsList from '$lib/components/unit/UnitsList.svelte';
 	import { parseParams } from '$lib/utils/parse-params';
@@ -14,15 +16,20 @@
 		const sParams = parseParams(url);
 		const chartFilter = { propertyId: params.id };
 
-		const [property, units, income, expenses] = await Promise.all([
-			stuff.api!.properties.findOne({ id: params.id }),
-			stuff.api!.properties.findUnits({ id: params.id, ...sParams }),
+		const [property, units, income, expensesGrouped, invoices, expenses] =
+			await Promise.all([
+				stuff.api!.properties.findOne({ id: params.id }),
+				stuff.api!.properties.findUnits({ id: params.id, ...sParams }),
 
-			stuff.api!.analytics.getIncomeByMonth(chartFilter),
-			stuff.api!.analytics.getExpensesByMonth(chartFilter),
-		]);
+				stuff.api!.analytics.getIncomeByMonth(chartFilter),
+				stuff.api!.analytics.getExpensesByMonth(chartFilter),
+				stuff.api!.leaseInvoices.findAll(chartFilter),
+				stuff.api!.expenses.findAll(chartFilter), // TODO filter serverside
+			]);
 
-		return { props: { property, units, income, expenses } };
+		return {
+			props: { property, units, income, expensesGrouped, invoices, expenses },
+		};
 	};
 </script>
 
@@ -32,10 +39,14 @@
 	export let units: Prop['units'];
 
 	export let income: Prop['income'];
+	export let expensesGrouped: Prop['expensesGrouped'];
+	export let invoices: Prop['invoices'];
 	export let expenses: Prop['expenses'];
 </script>
 
 <PropertyPage {property}>
 	<UnitsList {units} />
-	<NetIncomeCard {income} {expenses} />
+	<NetIncomeCard {income} expenses={expensesGrouped} />
+	<RevenueCard {invoices} />
+	<ExpensesCard {expenses} />
 </PropertyPage>
--- packages/site/src/routes/units/[id]/index.svelte
+++ packages/site/src/routes/units/[id]/index.svelte
@@ -1,5 +1,7 @@
 <script lang="ts" context="module">
+	import ExpensesCard from '$lib/components/dashboard/cards/ExpensesCard.svelte';
 	import NetIncomeCard from '$lib/components/dashboard/cards/NetIncomeCard.svelte';
+	import RevenueCard from '$lib/components/dashboard/cards/RevenueCard.svelte';
 	import LeaseList from '$lib/components/lease/LeaseList.svelte';
 	import UnitPage from '$lib/components/unit/UnitPage.svelte';
 	import { parseParams } from '$lib/utils/parse-params';
@@ -14,15 +16,20 @@
 		const sParams = parseParams(url);
 		const chartFilter = { unitId: params.id };
 
-		const [unit, leases, income, expenses] = await Promise.all([
-			stuff.api!.units.findOne({ id: params.id }),
-			stuff.api!.units.findLeases({ id: params.id, ...sParams }),
+		const [unit, leases, income, expensesGrouped, invoices, expenses] =
+			await Promise.all([
+				stuff.api!.units.findOne({ id: params.id }),
+				stuff.api!.units.findLeases({ id: params.id, ...sParams }),
 
-			stuff.api!.analytics.getIncomeByMonth(chartFilter),
-			stuff.api!.analytics.getExpensesByMonth(chartFilter),
-		]);
+				stuff.api!.analytics.getIncomeByMonth(chartFilter),
+				stuff.api!.analytics.getExpensesByMonth(chartFilter),
+				stuff.api!.leaseInvoices.findAll(chartFilter),
+				stuff.api!.expenses.findAll(chartFilter), // TODO filter serverside
+			]);
 
-		return { props: { unit, leases, income, expenses } };
+		return {
+			props: { unit, leases, income, expensesGrouped, invoices, expenses },
+		};
 	};
 </script>
 
@@ -32,10 +39,14 @@
 	export let leases: Prop['leases'];
 
 	export let income: Prop['income'];
+	export let expensesGrouped: Prop['expensesGrouped'];
+	export let invoices: Prop['invoices'];
 	export let expenses: Prop['expenses'];
 </script>
 
 <UnitPage {unit}>
 	<LeaseList {leases} showIndex />
-	<NetIncomeCard {income} {expenses} />
+	<NetIncomeCard {income} expenses={expensesGrouped} />
+	<RevenueCard {invoices} />
+	<ExpensesCard {expenses} />
 </UnitPage>
