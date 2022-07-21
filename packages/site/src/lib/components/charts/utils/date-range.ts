467f0d1fef97cb34437b2fe68137c8c9f84e947c
nbe <aleisa.naser@gmail.com>
2022-07-21 15:33:55 +0300

    make default dateRange timezone agnostic

--- packages/site/src/lib/components/charts/utils/date-range.ts
+++ packages/site/src/lib/components/charts/utils/date-range.ts
@@ -2,5 +2,16 @@ import { subMonths } from 'date-fns';
 
 export const defaultRange = 6;
 
-export const rangeStart = (monthsAgo: number) =>
-	subMonths(new Date().setHours(0, 0, 0, 0), monthsAgo).toISOString();
+export const rangeStart = (monthsAgo: number): string => {
+	// Get the date/time N months ago
+	const date = subMonths(new Date(), monthsAgo);
+
+	// Get the equivalent UTC day/month/year
+	const day = date.getUTCDate();
+	const month = date.getUTCMonth();
+	const year = date.getUTCFullYear();
+
+	// Return a date object that represents midnight on that equivalent UTC day/month/year
+	const result = new Date(Date.UTC(year, month, day)).toISOString();
+	return result;
+};
