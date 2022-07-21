34f2f8911b1220e861ef2520259623588ac6bccd
nbe <aleisa.naser@gmail.com>
2022-07-21 19:15:05 +0300

    add icons

--- packages/site/src/lib/components/Navbar.svelte
+++ packages/site/src/lib/components/Navbar.svelte
@@ -4,13 +4,12 @@
 	import NavPopover from '$components/navbar/NavPopover.svelte';
 	import Dropdown from '$lib/components/buttons/Dropdown.svelte';
 	import DropdownMenu from '$lib/components/buttons/DropdownMenu.svelte';
-	import HybridButton from '$lib/components/buttons/HybridButton.svelte';
 	import { getDocs } from '$lib/components/navbar/docs-url';
 	import SearchButton from '$lib/components/search/SearchButton.svelte';
 	import { LOGIN, LOGOUT } from '$lib/constants/routes';
 	import type { MenuOption } from '$lib/models/interfaces/option.interface';
 	import type { UserConfig } from '$lib/models/interfaces/user.interface';
-	import { ChevronDown, Pencil, Trash } from '@steeze-ui/heroicons';
+	import { ChevronDown, Code, Cog, Logout } from '@steeze-ui/heroicons';
 	import { Icon } from '@steeze-ui/svelte-icon';
 
 	export let navigation: UserConfig['navLinks'] = [];
@@ -22,8 +21,9 @@
 	let open = false;
 
 	const options: MenuOption[] = [
-		{ label: 'Update', href: '#', icon: Pencil },
-		{ label: 'Remove', href: '#', icon: Trash },
+		...(dev ? [{ label: 'Debug', href: '/debug', icon: Code }] : []),
+		{ label: 'Settings', href: '#', icon: Cog },
+		{ label: 'Logout', href: LOGOUT, icon: Logout }, // sveltekit:reload?
 	];
 </script>
 
@@ -78,27 +78,6 @@
 
 		<!-- Large screen: nav actions -->
 		<div class="hidden lg:flex lg:items-center lg:space-x-6">
-			<Dropdown>
-				<div
-					slot="button"
-					class="inline-flex items-center rounded-md border border-transparent bg-gray-600 px-4 py-2  text-xs font-medium text-white hover:bg-gray-700"
-				>
-					<div>
-						<p>Org Name</p>
-						<span>sdfk</span>
-					</div>
-					<Icon
-						src={ChevronDown}
-						class="h-5 w-5"
-						aria-hidden="true"
-						theme="solid"
-					/>
-				</div>
-				<div slot="menu">
-					<DropdownMenu {options} />
-				</div>
-			</Dropdown>
-
 			{#if $session.user}
 				{#if showDashboard}
 					<a
@@ -109,16 +88,8 @@
 						Dashboard
 					</a>
 				{/if}
-				<!-- {#if dev}
-					<a
-						href="/debug"
-						class="text-base font-medium text-white hover:text-gray-300"
-					>
-						Debug
-					</a>
-				{/if} -->
 
-				<!-- {#if $session.user.role.isAdmin}
+				{#if $session.user.role.isAdmin}
 					<a
 						href={docs}
 						sveltekit:reload
@@ -127,21 +98,27 @@
 					>
 						Docs
 					</a>
-					<a
-						href="/settings/expense-categories"
-						class="text-base font-medium text-white hover:text-gray-300"
+				{/if}
+				<Dropdown>
+					<div
+						slot="button"
+						class="inline-flex items-center rounded-md border border-transparent bg-gray-600 px-4 py-2  text-xs font-medium text-white hover:bg-gray-700"
 					>
-						Settings
-					</a>
-				{/if} -->
-
-				<a
-					href={LOGOUT}
-					sveltekit:reload
-					class="text-base font-medium text-white hover:text-gray-300"
-				>
-					Log out
-				</a>
+						<div>
+							<p>{$session.user?.role.roleId}</p>
+							<span>{$session.user?.role.roleName}</span>
+						</div>
+						<Icon
+							src={ChevronDown}
+							class="h-5 w-5"
+							aria-hidden="true"
+							theme="solid"
+						/>
+					</div>
+					<div slot="menu">
+						<DropdownMenu {options} />
+					</div>
+				</Dropdown>
 			{:else}
 				<a
 					href={LOGIN}
