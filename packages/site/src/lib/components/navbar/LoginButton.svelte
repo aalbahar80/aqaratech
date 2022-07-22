3891f584d4020722068eae425d9f6e226d923fe9
nbe <aleisa.naser@gmail.com>
2022-07-22 22:28:46 +0300

    extract login button & navbar icon

---
+++ packages/site/src/lib/components/navbar/LoginButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { LOGIN } from '$lib/constants/routes';
+	import { session } from '$app/stores';
+</script>
+
+{#if !$session.user}
+	<a
+		href={LOGIN}
+		sveltekit:reload
+		class="inline-flex items-center rounded border border-transparent bg-gray-600 px-4 py-2 text-base font-medium text-white hover:bg-gray-700"
+	>
+		Log in
+	</a>
+{/if}
--- packages/site/src/lib/components/navbar/Navbar.svelte
+++ packages/site/src/lib/components/navbar/Navbar.svelte
@@ -2,9 +2,10 @@
 	import { session } from '$app/stores';
 	import NavPopover from '$components/navbar/NavPopover.svelte';
 	import DashboardButton from '$lib/components/navbar/DashboardButton.svelte';
+	import LoginButton from '$lib/components/navbar/LoginButton.svelte';
 	import NavbarDropdown from '$lib/components/navbar/NavbarDropdown.svelte';
+	import NavbarIcon from '$lib/components/navbar/NavbarIcon.svelte';
 	import SearchButton from '$lib/components/search/SearchButton.svelte';
-	import { LOGIN } from '$lib/constants/routes';
 
 	// Needs to be reactive?
 	const navigation = $session.user?.role.meta.navLinks || [];
@@ -15,31 +16,17 @@
 		class="relative mx-auto flex max-w-7xl items-center justify-between px-4 sm:px-6"
 		aria-label="Global"
 	>
-		<a href="/">
-			<span class="sr-only">Workflow</span>
-			<img
-				class="h-8 w-auto sm:h-10"
-				src="https://tailwindui.com/img/logos/workflow-mark-teal-200-cyan-400.svg"
-				alt=""
-			/>
-		</a>
+		<NavbarIcon />
 		{#if $session.user}
 			<div class="order-last -mr-2 flex items-center gap-6 lg:hidden">
 				<DashboardButton />
 				<NavPopover {navigation} />
 			</div>
-		{:else}
-			<a
-				href={LOGIN}
-				sveltekit:reload
-				class="inline-flex items-center rounded border border-transparent bg-gray-600 px-4 py-2 text-base font-medium text-white hover:bg-gray-700 lg:hidden"
-			>
-				Log in
-			</a>
 		{/if}
 
 		<!-- Large screen: nav links -->
 		<div class="hidden space-x-4 lg:ml-10 lg:flex">
+			<DashboardButton />
 			{#each navigation as item (item.name)}
 				<a
 					sveltekit:prefetch
@@ -55,17 +42,9 @@
 		<!-- Large screen: nav actions -->
 		<div class="hidden lg:flex lg:items-center lg:space-x-6">
 			{#if $session.user}
-				<DashboardButton />
 				<NavbarDropdown />
-			{:else}
-				<a
-					href={LOGIN}
-					sveltekit:reload
-					class="inline-flex items-center rounded border border-transparent bg-gray-600 px-4 py-2 text-base font-medium text-white hover:bg-gray-700"
-				>
-					Log in
-				</a>
 			{/if}
 		</div>
+		<LoginButton />
 	</nav>
 </div>
