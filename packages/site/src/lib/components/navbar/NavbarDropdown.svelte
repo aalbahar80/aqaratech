ba6fc2be4856df61b93c50abf17a9e5ba9323d50
nbe <aleisa.naser@gmail.com>
2022-07-22 21:56:42 +0300

    grant navbar dropdown a higher z-index

--- packages/site/src/lib/components/navbar/NavbarDropdown.svelte
+++ packages/site/src/lib/components/navbar/NavbarDropdown.svelte
@@ -52,7 +52,7 @@
 		/>
 	</div>
 	<div slot="menu">
-		<DropdownMenu>
+		<DropdownMenu class="z-10">
 			{#each options as option}
 				<MenuItem let:active>
 					<a href={option.href} sveltekit:reload>
