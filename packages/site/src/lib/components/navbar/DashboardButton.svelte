10f0ff280fcd2861d8cdd9ab00b6372594798a1a
nbe <aleisa.naser@gmail.com>
2022-07-22 22:15:09 +0300

    extract dashboard button

---
+++ packages/site/src/lib/components/navbar/DashboardButton.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { session } from '$app/stores';
+
+	$: showDashboard =
+		$session.user?.role.roleType === 'PORTFOLIO' ||
+		$session.user?.role.roleType === 'TENANT';
+</script>
+
+{#if showDashboard}
+	<a
+		href={$session.user?.role.meta.home}
+		class="inline-flex items-center rounded border border-transparent bg-gray-600 px-4 py-2 text-base font-medium text-white hover:bg-gray-700"
+		sveltekit:prefetch
+	>
+		Dashboard
+	</a>
+{/if}
--- packages/site/src/lib/components/navbar/Navbar.svelte
+++ packages/site/src/lib/components/navbar/Navbar.svelte
@@ -1,30 +1,13 @@
 <script lang="ts">
-	import { dev } from '$app/env';
 	import { session } from '$app/stores';
 	import NavPopover from '$components/navbar/NavPopover.svelte';
-	import Dropdown from '$lib/components/buttons/Dropdown.svelte';
-	import DropdownMenu from '$lib/components/buttons/DropdownMenu.svelte';
-	import MenuIconItem from '$lib/components/buttons/MenuIconItem.svelte';
-	import { getDocs } from '$lib/components/navbar/docs-url';
+	import DashboardButton from '$lib/components/navbar/DashboardButton.svelte';
 	import NavbarDropdown from '$lib/components/navbar/NavbarDropdown.svelte';
 	import SearchButton from '$lib/components/search/SearchButton.svelte';
-	import { LOGIN, LOGOUT } from '$lib/constants/routes';
-	import type { MenuOption } from '$lib/models/interfaces/option.interface';
-	import { MenuItem } from '@rgossiaux/svelte-headlessui';
-	import {
-		ChevronDown,
-		Code,
-		InformationCircle,
-		Logout,
-	} from '@steeze-ui/heroicons';
-	import { Icon } from '@steeze-ui/svelte-icon';
+	import { LOGIN } from '$lib/constants/routes';
 
 	// Needs to be reactive?
 	const navigation = $session.user?.role.meta.navLinks || [];
-
-	$: showDashboard =
-		$session.user?.role.roleType === 'PORTFOLIO' ||
-		$session.user?.role.roleType === 'TENANT';
 </script>
 
 <div class="bg-gray-900 py-1.5 print:hidden">
@@ -42,14 +25,7 @@
 		</a>
 		{#if $session.user}
 			<div class="order-last -mr-2 flex items-center gap-6 lg:hidden">
-				{#if showDashboard}
-					<a
-						href={$session.user.role.meta.home}
-						class="inline-flex items-center rounded border border-transparent bg-gray-600 px-4 py-2 text-base font-medium text-white hover:bg-gray-700 lg:hidden"
-					>
-						Dashboard
-					</a>
-				{/if}
+				<DashboardButton />
 				<NavPopover {navigation} />
 			</div>
 		{:else}
@@ -79,15 +55,7 @@
 		<!-- Large screen: nav actions -->
 		<div class="hidden lg:flex lg:items-center lg:space-x-6">
 			{#if $session.user}
-				{#if showDashboard}
-					<a
-						href={$session.user.role.meta.home}
-						class="inline-flex items-center rounded border border-transparent bg-gray-600 px-4 py-2 text-base font-medium text-white hover:bg-gray-700"
-						sveltekit:prefetch
-					>
-						Dashboard
-					</a>
-				{/if}
+				<DashboardButton />
 				<NavbarDropdown />
 			{:else}
 				<a
