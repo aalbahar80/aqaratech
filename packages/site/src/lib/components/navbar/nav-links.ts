00aee446bc287bd12a2f75cbade527dfc3261230
nbe <aleisa.naser@gmail.com>
2022-07-23 22:28:14 +0300

    add settings to navbar dropdown

--- packages/site/src/lib/components/navbar/nav-links.ts
+++ packages/site/src/lib/components/navbar/nav-links.ts
@@ -2,7 +2,7 @@ import { dev } from '$app/env';
 import { getDocs } from '$lib/components/navbar/docs-url';
 import { LOGOUT } from '$lib/constants/routes';
 import type { NavbarItem } from '$lib/models/types/auth.type';
-import { Code, InformationCircle, Logout } from '@steeze-ui/heroicons';
+import { Code, Cog, InformationCircle, Logout } from '@steeze-ui/heroicons';
 
 export const getRoleOptions = (user: App.Session['user']): NavbarItem[] =>
 	user?.roles.map((role) => ({
@@ -13,7 +13,7 @@ export const getRoleOptions = (user: App.Session['user']): NavbarItem[] =>
 export const getNavOptions = (user: App.Session['user']) => [
 	...getRoleOptions(user),
 	...(dev ? [{ label: 'Debug', href: '/debug', icon: Code }] : []),
-	// { label: 'Settings', href: '#', icon: Cog },
+	{ label: 'Settings', href: '/settings/expense-tree', icon: Cog },
 	{ label: 'Docs', href: getDocs(), icon: InformationCircle }, // TODO: open in new tab { target="_blank" } & sveltekit:reload & only admins?
 	{ label: 'Logout', href: LOGOUT, icon: Logout }, // sveltekit:reload?
 ];
