851ede579e654ce0f69ddd1de348431b463cd7d9
nbe <aleisa.naser@gmail.com>
2022-07-24 08:22:19 +0300

    add search keyboard shortcut

--- packages/site/src/lib/components/search/SearchButton.svelte
+++ packages/site/src/lib/components/search/SearchButton.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import SearchPalette from '$lib/components/search/SearchPalette.svelte';
+	import { shortcut } from '$lib/utils/shortcut';
 	import { Search } from '@steeze-ui/heroicons';
 	import { Icon } from '@steeze-ui/svelte-icon';
 
@@ -9,6 +10,7 @@
 <SearchPalette bind:open />
 <button
 	class="flex flex-1 justify-center px-8 lg:ml-6 lg:justify-end"
+	use:shortcut={{ control: true, code: 'KeyK' }}
 	on:click={() => {
 		open = true;
 	}}
@@ -39,6 +41,12 @@
 				class="block w-full rounded-md border border-transparent bg-gray-700 py-2 pl-10 pr-3 leading-5 text-gray-300 placeholder-gray-400 focus:border-white focus:bg-white focus:text-gray-900 focus:outline-none focus:ring-white sm:text-sm"
 			>
 				Quick search...
+				<kbd class="font-sans font-semibold dark:text-slate-500"
+					><abbr
+						title="Command"
+						class="text-slate-300 no-underline dark:text-slate-500">⌘</abbr
+					> K</kbd
+				>
 			</span>
 		</div>
 	</div>
