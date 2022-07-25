991f60e181e8943d5734956edef0df1771630d79
nbe <aleisa.naser@gmail.com>
2022-07-25 09:14:49 +0300

    init placehoder org card/list

---
+++ packages/site/src/lib/components/organization/OrganizationList.svelte
@@ -0,0 +1,32 @@
+<!-- <script lang="ts">
+	import { page } from '$app/stores';
+	import OrganizationCard from '$lib/components/organization/OrganizationCard.svelte';
+	import AnchorPagination from '$lib/components/pagination/AnchorPagination.svelte';
+	import StackedList from '$lib/components/StackedList.svelte';
+	import { create } from '$lib/utils/route-helpers';
+	import type { PaginatedOrganizationDto } from '@self/sdk';
+
+	export let organizations: PaginatedOrganizationDto;
+
+	const formUrl = create({
+		entity: 'organizations',
+		predefined:
+			$page.url.pathname.startsWith('/properties') &&
+			new Map<string, any>([
+				['propertyId', $page.url.pathname.split('/').pop()],
+			]),
+	});
+</script>
+
+<StackedList
+	entityTitle="organizations"
+	count={organizations.results.length}
+	{formUrl}
+>
+	{#each organizations.results as organization (organization.id)}
+		<li>
+			<OrganizationCard {organization} />
+		</li>
+	{/each}
+	<AnchorPagination pagination={organizations.pagination} />
+</StackedList> -->
