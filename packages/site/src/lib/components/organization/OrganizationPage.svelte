b1e352047c37dbd66b51faf877dd8a25201f7758
nbe <aleisa.naser@gmail.com>
2022-07-25 09:10:29 +0300

    init org page

---
+++ packages/site/src/lib/components/organization/OrganizationPage.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import DetailsPane from '$lib/components/DetailsPane.svelte';
+	import Heading from '$lib/components/Heading.svelte';
+	import RoleList from '$lib/components/role/RoleList.svelte';
+	import type { OrganizationDto, PaginatedRoleDto } from '@self/sdk';
+
+	export let organization: OrganizationDto;
+	export let roles: PaginatedRoleDto;
+
+	$: details = [
+		['Name', organization.fullName],
+		['Phone', organization.label],
+	] as [string, string | null][];
+</script>
+
+<Heading title="Organization" id={organization.id} entity="organizations" />
+<DetailsPane {details} />
+<RoleList {roles} />
--- packages/site/src/routes/organizations/[id]/index.svelte
+++ packages/site/src/routes/organizations/[id]/index.svelte
@@ -4,18 +4,20 @@
 	import type { LP } from 'src/types/load-props';
 
 	export const load = async ({ params, stuff }: LoadEvent<{ id: string }>) => {
-		const organization = await stuff.api!.organizations.findOne({
-			id: params.id,
-		});
+		const [organization, roles] = await Promise.all([
+			stuff.api!.organizations.findOne({ id: params.id }),
+			// TODO handle pagination & default limit
+			stuff.api!.organizations.findRoles({ id: params.id }),
+		]);
 
-		return { props: { organization } };
+		return { props: { organization, roles } };
 	};
 </script>
 
 <script lang="ts">
 	type Prop = LP<typeof load>;
 	export let organization: Prop['organization'];
+	export let roles: Prop['roles'];
 </script>
 
-<pre>{JSON.stringify(organization, null, 2)}</pre>
-<!-- <OrganizationPage {organization} /> -->
+<OrganizationPage {organization} {roles} />
