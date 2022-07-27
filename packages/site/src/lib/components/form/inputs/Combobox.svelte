9aeb2bf438295cf7079430af2e38deb7db66e052
nbe <aleisa.naser@gmail.com>
2022-07-27 22:51:30 +0300

    init

--- packages/site/src/lib/components/form/inputs/Combobox.svelte
+++ packages/site/src/lib/components/form/inputs/Combobox.svelte
@@ -23,6 +23,14 @@
 	export let disabled = false;
 	export let invalid = false;
 
+	let activeOption: Option | undefined;
+	$: console.log('activeOption: ', activeOption);
+	$: {
+		if (query) {
+			activeOption = filtered[0];
+		}
+	}
+
 	let selection: SelectedOption = options.find(
 		(option) => option.value === initialValue,
 	);
@@ -62,6 +70,88 @@
 		query = '';
 		selection = undefined;
 	};
+
+	export const select = async (option: Option) => {
+		// await tick();
+		dispatch('select', { value: option.value });
+		selection = option;
+		query = '';
+	};
+
+	async function handleKeydown(event: KeyboardEvent) {
+		// https://github.com/janosh/svelte-multiselect/blob/main/src/lib/MultiSelect.svelte#L192
+		// on escape or tab out of input: dismiss options dropdown and reset search text
+		https: console.log(event.key);
+		if (event.key === `Escape` || event.key === `Tab`) {
+			forceOpen = false;
+			query = '';
+		}
+		// on enter key: toggle active option and reset search text
+		else if (event.key === `Enter`) {
+			event.preventDefault(); // prevent enter key from triggering form submission
+
+			if (activeOption) {
+				select(activeOption);
+				forceOpen = false;
+				activeOption = undefined;
+				// query = '';
+			}
+			// else if (allowUserOptions && searchText.length > 0) {
+			// 	// user entered text but no options match, so if allowUserOptions is truthy, we create new option
+			// 	add(searchText);
+			// }
+			// no active option and no search text means the options dropdown is closed
+			// in which case enter means open it
+			else {
+				forceOpen = true;
+			}
+		}
+		// on up/down arrow keys: update active option
+		// else if ([`ArrowDown`, `ArrowUp`].includes(event.key)) {
+		// 	// if no option is active yet, but there are matching options, make first one active
+		// 	if (activeOption === null && matchingOptions.length > 0) {
+		// 		activeOption = matchingOptions[0];
+		// 		return;
+		// 	} else if (allowUserOptions && searchText.length > 0) {
+		// 		// if allowUserOptions is truthy and user entered text but no options match, we make
+		// 		// <li>{addUserMsg}</li> active on keydown (or toggle it if already active)
+		// 		activeMsg = !activeMsg;
+		// 		return;
+		// 	} else if (activeOption === null) {
+		// 		// if no option is active and no options are matching, do nothing
+		// 		return;
+		// 	}
+		// 	const increment = event.key === `ArrowUp` ? -1 : 1;
+		// 	const newActiveIdx = matchingOptions.indexOf(activeOption) + increment;
+
+		// 	if (newActiveIdx < 0) {
+		// 		// wrap around top
+		// 		activeOption = matchingOptions[matchingOptions.length - 1];
+		// 	} else if (newActiveIdx === matchingOptions.length) {
+		// 		// wrap around bottom
+		// 		activeOption = matchingOptions[0];
+		// 	} else {
+		// 		// default case: select next/previous in item list
+		// 		activeOption = matchingOptions[newActiveIdx];
+		// 	}
+		// 	if (autoScroll) {
+		// 		// TODO This ugly timeout hack is needed to properly scroll element into view when wrapping
+		// 		// around start/end of option list. Find a better solution than waiting 10 ms to.
+		// 		setTimeout(() => {
+		// 			const li = document.querySelector(`ul.options > li.active`);
+		// 			li?.scrollIntoView();
+		// 		}, 10);
+		// 	}
+		// }
+		// // on backspace key: remove last selected option
+		// else if (
+		// 	event.key === `Backspace` &&
+		// 	selectedLabels.length > 0 &&
+		// 	!searchText
+		// ) {
+		// 	remove(selectedLabels.at(-1) as string | number);
+		// }
+	}
 </script>
 
 <Listbox
@@ -88,12 +178,14 @@
 			class:form-invalid={invalid}
 			placeholder="Search..."
 			type="text"
-			value={selection?.label ?? ''}
+			value={selection?.label?.trim() ?? ''}
+			on:keydown={handleKeydown}
 			on:click={() => {
 				forceOpen = true;
 			}}
 			on:input={(event) => {
-				query = event.currentTarget?.value;
+				// expense categories use leading spaces to indicate hierarchy
+				query = event.currentTarget?.value.trim();
 				if (!query) {
 					dispatch('select', { value: null });
 				}
@@ -138,7 +230,9 @@
 							value={item}
 							class={classes(
 								'relative cursor-default select-none py-2 pl-3 pr-9',
-								hovering ? 'bg-indigo-600 text-white' : 'text-gray-900',
+								hovering || activeOption === item
+									? 'bg-indigo-600 text-white'
+									: 'text-gray-900',
 							)}
 							let:selected
 						>
