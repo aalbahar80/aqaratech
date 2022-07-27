08dc422975103f324d6be46f925ae5289d55b458
nbe <aleisa.naser@gmail.com>
2022-07-27 21:40:18 +0300

    transform expense.categoryId to null if falsey

--- packages/site/src/lib/models/schemas/expense.schema.ts
+++ packages/site/src/lib/models/schemas/expense.schema.ts
@@ -1,10 +1,18 @@
 import { zodnanoid } from '$lib/models/schemas/nano-id.schema';
-import { falsyToNull, strToDate, trim } from '$lib/zodTransformers.js';
+import {
+	falsyToNull,
+	falsyToNullExceptZero,
+	strToDate,
+	trim,
+} from '$lib/zodTransformers.js';
 import { z } from 'zod';
 
 export const updateSchema = z.object({
 	amount: z.number().gt(0),
-	categoryId: z.union([z.number(), z.string()]).nullable(),
+	categoryId: z
+		.union([z.number(), z.string()])
+		.nullable()
+		.transform(falsyToNullExceptZero),
 	postAt: z.preprocess(strToDate, z.date()),
 	memo: z.string().transform(trim).transform(falsyToNull).nullable(),
 });
