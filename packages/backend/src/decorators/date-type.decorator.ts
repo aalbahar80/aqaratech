9cfe68f5d8fa9c49c5d70641d8370cbafcdcb7e6
nbe <aleisa.naser@gmail.com>
2022-07-30 04:41:03 +0300

    warn if time not midnight utc

--- packages/backend/src/decorators/date-type.decorator.ts
+++ packages/backend/src/decorators/date-type.decorator.ts
@@ -1,7 +1,11 @@
 import { applyDecorators } from '@nestjs/common';
 import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
-import { IsDate, isISO8601 } from 'class-validator';
+import { IsDate } from 'class-validator';
+// Uninstall `validator` once this issue is resolved:
+// Instead use the same function from `class-validator`, also called `isISO8601`.
+// https://github.com/typestack/class-validator/issues/1061
+import isISO8601 from 'validator/lib/isISO8601';
 
 /**
  * Decorator to make openapi-generator to treat dates as strings.
@@ -26,6 +30,17 @@ export function DateType(required = true): PropertyDecorator {
   return applyDecorators(
     // 1. Transform
     Transform((p) => {
+      if (
+        isISO8601(p.value, { strict: true }) &&
+        typeof p.value === 'string' &&
+        !p.value.endsWith('00:00:00.000Z')
+      ) {
+        console.warn(
+          'DateType: ISO8601 string is not ending with "00:00:00.000Z"',
+        );
+        console.log(p);
+      }
+
       if (!isISO8601(p.value)) {
         return null;
       } else {
