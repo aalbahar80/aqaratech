0a02659caef185084568d8f9c38ed53e3e57316d
nbe <aleisa.naser@gmail.com>
2022-07-30 07:25:34 +0300

    run svelte-check on sdk generation

--- packages/sdk/watcher.ts
+++ packages/sdk/watcher.ts
@@ -1,8 +1,18 @@
 import child_process from "child_process";
 import chokidar from "chokidar";
 import * as fs from "fs";
-
 let oldStats: fs.Stats | undefined;
+
+interface SpawnSyncReturns<T> {
+	pid: number;
+	output: Array<T | null>;
+	stdout: T;
+	stderr: T;
+	status: number | null;
+	signal: NodeJS.Signals | null;
+	error?: Error | undefined;
+}
+
 chokidar
 	.watch("./openapi.yaml", {
 		alwaysStat: true,
@@ -19,7 +29,13 @@ chokidar
 			if (changed || !oldStats) {
 				console.log("openapi.yaml changed detected, generating new sdk...");
 
-				child_process.execSync("pnpm run build");
+				try {
+					child_process.execSync("pnpm run build");
+					child_process.execSync("pnpm --filter @self/site run check");
+				} catch (error) {
+					const e = error as SpawnSyncReturns<Buffer>;
+					console.log(e.stdout.toString("utf-8"));
+				}
 			} else {
 				console.log("skipping sdk generation...");
 			}
