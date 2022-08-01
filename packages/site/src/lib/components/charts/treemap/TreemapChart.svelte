5b61ccc733ace082ae3ef0307beb844b019a9c7d
nbe <aleisa.naser@gmail.com>
2022-08-01 11:34:01 +0300

    style treemap crumbs

--- packages/site/src/lib/components/charts/treemap/TreemapChart.svelte
+++ packages/site/src/lib/components/charts/treemap/TreemapChart.svelte
@@ -33,7 +33,7 @@
 			node = node.parent;
 		}
 
-		return crumbs.join('/');
+		return crumbs;
 	};
 
 	const extents = tweened(undefined, {
@@ -64,11 +64,28 @@ Create a treemap from a d3-hierarchy.
 -->
 
 <button
-	class="breadcrumbs"
+	class="flex items-center p-1.5"
 	disabled={!selected.parent}
 	on:click={() => (selected = selected.parent)}
 >
-	{breadcrumbs(selected)}
+	{#each breadcrumbs(selected) as crumb, idx}
+		{#if idx !== 0}
+			<svg
+				class="h-5 w-5 flex-shrink-0 text-gray-300"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="currentColor"
+				viewBox="0 0 20 20"
+				aria-hidden="true"
+			>
+				<path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z" />
+			</svg>
+		{/if}
+		<span
+			class="font-medium text-gray-500 last:font-semibold last:text-indigo-600 hover:text-gray-700 last:hover:text-indigo-900"
+		>
+			{crumb}
+		</span>
+	{/each}
 </button>
 
 <div class="chart">
