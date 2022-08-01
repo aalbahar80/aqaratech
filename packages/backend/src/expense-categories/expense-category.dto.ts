ab19aeda1435c51d6773f6b29531e3ce4575e6e5
nbe <aleisa.naser@gmail.com>
2022-08-01 21:29:46 +0300

    rework dto prevent sdk convert null to undefined

--- packages/backend/src/expense-categories/expense-category.dto.ts
+++ packages/backend/src/expense-categories/expense-category.dto.ts
@@ -1,3 +1,4 @@
+import { IntersectionType, PartialType, PickType } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import {
   IsArray,
@@ -8,17 +9,23 @@ import {
 } from 'class-validator';
 import { Nanoid } from 'src/decorators/field.decorators';
 
-export class CreateExpenseCategoryDto {
+export class ExpenseCategoryDto {
+  constructor(partial: Partial<ExpenseCategoryDto>) {
+    Object.assign(this, partial);
+  }
+  @Nanoid()
+  id: string;
+
   @IsString()
   labelEn: string;
 
   @IsString()
   @IsOptional()
-  parentId?: string | null;
+  parentId: string | null;
 
   @IsString()
   @IsOptional()
-  labelAr?: string | null;
+  labelAr: string | null;
 
   @IsString()
   @IsOptional()
@@ -28,21 +35,31 @@ export class CreateExpenseCategoryDto {
   isGroup: boolean;
 }
 
-export class UpdateExpenseCategoryDto extends CreateExpenseCategoryDto {
-  @Nanoid()
-  id: string;
-}
+// The significance of having all properties marked as NON-optional in the main class is that
+// this way, the generated sdk will not transfrom undefined to null when grabbing data.
+export class CreateExpenseCategoryDto extends IntersectionType(
+  PickType(ExpenseCategoryDto, ['labelEn', 'isGroup']),
+  PartialType(
+    PickType(ExpenseCategoryDto, ['parentId', 'labelAr', 'description']),
+  ),
+) {}
+
+export class UpdateExpenseCategoryDto extends IntersectionType(
+  PickType(ExpenseCategoryDto, ['id']),
+  PartialType(
+    PickType(ExpenseCategoryDto, [
+      'labelEn',
+      'parentId',
+      'labelAr',
+      'description',
+      'isGroup',
+    ]),
+  ),
+) {}
 
 export class UpdateAllExpenseCategoriesDto {
   @IsArray()
   @ValidateNested({ each: true })
   @Type(() => UpdateExpenseCategoryDto)
   items: UpdateExpenseCategoryDto[];
-}
-
-export class ExpenseCategoryDto extends UpdateExpenseCategoryDto {
-  constructor(partial: Partial<ExpenseCategoryDto>) {
-    super();
-    Object.assign(this, partial);
-  }
 }
