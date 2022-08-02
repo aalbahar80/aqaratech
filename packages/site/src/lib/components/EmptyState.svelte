7364fb8bfcab11ba36e683b623f574598f3a2de4
nbe <aleisa.naser@gmail.com>
2022-08-02 20:41:56 +0300

    use slot in empty state

--- packages/site/src/lib/components/EmptyState.svelte
+++ packages/site/src/lib/components/EmptyState.svelte
@@ -26,15 +26,17 @@
 		<p class="mt-1 text-sm text-gray-500">
 			{message}
 		</p>
-		<div class="mt-6">
-			<a
-				href={formUrl}
-				class="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-				sveltekit:prefetch
-			>
-				<Icon src={Plus} class="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
-				{buttonText}
-			</a>
-		</div>
+		<slot>
+			<div class="mt-6">
+				<a
+					href={formUrl}
+					class="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+					sveltekit:prefetch
+				>
+					<Icon src={Plus} class="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+					{buttonText}
+				</a>
+			</div>
+		</slot>
 	{/if}
 </div>
--- packages/site/src/routes/welcome.svelte
+++ packages/site/src/routes/welcome.svelte
@@ -17,16 +17,16 @@
 		buttonText="Create an organization"
 		formUrl="/organizations/new"
 		message=""
-	/>
-
-	<!-- <Button
-		text="Create an organization"
-		as="a"
-		href="/organizations/new"
-		class="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
 	>
-		<MaterialSymbolsAddBusinessRounded class="" />
-	</Button> -->
+		<Button
+			text="Create an organization"
+			as="a"
+			href="/organizations/new"
+			class="mt-6 inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+		>
+			<MaterialSymbolsAddBusinessRounded class="" />
+		</Button>
+	</EmptyState>
 
 	<!-- <small>
 		<a
