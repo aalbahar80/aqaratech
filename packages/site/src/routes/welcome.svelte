9fb9be671ccb87dca8c3465578170b2ad98cbf7c
nbe <aleisa.naser@gmail.com>
2022-08-02 20:28:25 +0300

    rm extra button on welcomepage

--- packages/site/src/routes/welcome.svelte
+++ packages/site/src/routes/welcome.svelte
@@ -19,21 +19,21 @@
 		message=""
 	/>
 
-	<Button
+	<!-- <Button
 		text="Create an organization"
 		as="a"
 		href="/organizations/new"
 		class="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
 	>
 		<MaterialSymbolsAddBusinessRounded class="" />
-	</Button>
+	</Button> -->
 
-	<small>
-		<!-- <a
+	<!-- <small>
+		<a
 			href="#"
 			class="text-sm font-medium text-indigo-600 hover:text-indigo-500"
 		>
 			Expecting an invitation?
-		</a> -->
-	</small>
+		</a>
+	</small> -->
 </div>
