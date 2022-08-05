2b3bca81649067292273406e32871a236b16f9b2
nbe <aleisa.naser@gmail.com>
2022-08-05 04:57:07 +0300

    move org test form

--- packages/test/forms/organization-new.spec.ts
+++ packages/test/forms/organization-new.spec.ts
@@ -1,5 +1,5 @@
 import { expect } from "@playwright/test";
-import { test } from "./config";
+import { test } from "../config";
 
 test.use({ storageState: "./storageState.json" });
 test("existing user can create new org", async ({ page }) => {
