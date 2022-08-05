d1e68d26c0de4078b9a929ba9da65bc43145d341
nbe <aleisa.naser@gmail.com>
2022-08-05 06:50:28 +0300

    match screenshot on first try

--- packages/test/forms/portfolio-new.spec.ts
+++ packages/test/forms/portfolio-new.spec.ts
@@ -2,7 +2,7 @@ import { expect } from "@playwright/test";
 import { test } from "../config";
 
 test.use({ storageState: "./storageState.json" });
-test("test", async ({ page }) => {
+test("test", async ({ page }, info) => {
 	await page.goto("/");
 	await page.locator("text=Portfolios").click();
 	await expect(page).toHaveURL("http://localhost:3000/portfolios");
@@ -30,17 +30,29 @@ test("test", async ({ page }) => {
 		`/portfolios/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}`
 	);
 	await expect(page).toHaveURL(id);
-	const before = await page.screenshot({
+	const url = page.url();
+
+	// take screenshot
+	info.snapshotDir;
+
+	expect(page.locator("text=Portfolio")).toBeTruthy();
+	await page.screenshot({
 		fullPage: true,
-		path: "./portfolio.png",
+		path: info.snapshotDir + "/portfolio-linux.png",
 	});
 
 	await page.locator("text=Edit").click();
-
 	const edit = new RegExp(
 		`/portfolios/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/edit`
 	);
 	await expect(page).toHaveURL(edit);
+	await page.locator("text=Save changes").click();
+
+	// ensure same entity
+	await expect(page).toHaveURL(url);
+
+	// match screenshot
+	expect(page.locator("text=Portfolio")).toBeTruthy();
 	expect(await page.screenshot({ fullPage: true })).toMatchSnapshot({
 		name: "portfolio.png",
 	});
