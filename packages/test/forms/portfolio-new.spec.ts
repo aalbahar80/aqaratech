995f968cc5a85e6ac33b3b741e79ed720404894d
nbe <aleisa.naser@gmail.com>
2022-08-05 05:08:26 +0300

    new portfolio form test

---
+++ packages/test/forms/portfolio-new.spec.ts
@@ -0,0 +1,40 @@
+import { expect } from "@playwright/test";
+import { test } from "../config";
+
+test.use({ storageState: "./storageState.json" });
+test("test", async ({ page }) => {
+	await page.goto("/");
+	await page.locator("text=Portfolios").click();
+	await expect(page).toHaveURL("http://localhost:3000/portfolios");
+
+	await page.locator("text=Create new portfolio").click();
+	await expect(page).toHaveURL("http://localhost:3000/portfolios/new");
+
+	await page.locator('input[name="fullName"]').click();
+	await page.locator('input[name="fullName"]').fill("John Doe");
+
+	await page.locator('input[name="label"]').click();
+	await page.locator('input[name="label"]').fill("JD");
+
+	await page.locator('input[name="phone"]').click();
+	await page.locator('input[name="phone"]').fill("91234567");
+
+	await page.locator('input[name="civilid"]').click();
+	await page.locator('input[name="civilid"]').fill("123456789012");
+
+	await page.locator('input[name="dob"]').fill("2022-08-05");
+
+	await page.locator("text=Create new").click();
+
+	const id = new RegExp(
+		`/portfolios/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}`
+	);
+	await expect(page).toHaveURL(id);
+
+	await page.locator("text=Edit").click();
+
+	const edit = new RegExp(
+		`/portfolios/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/edit`
+	);
+	await expect(page).toHaveURL(edit);
+});
