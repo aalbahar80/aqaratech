8b9673bcf722be19a0030ad6e3c025609914b972
nbe <aleisa.naser@gmail.com>
2022-08-07 00:46:32 +0300

    import arabic font

--- packages/site/src/lib/components/leaseInvoice/invoice-to-pdf.ts
+++ packages/site/src/lib/components/leaseInvoice/invoice-to-pdf.ts
@@ -1,5 +1,7 @@
 import type { LeaseInvoiceDto } from '@self/sdk';
 import type jsPDFInvoiceTemplate from 'jspdf-invoice-template';
+// import AmiriRegular from './Amiri-Regular.ttf?raw';
+import AmiriRegular from './Amiri-Regular.ttf';
 
 /**
  * Only works in browser. Do not call server-side.
@@ -7,6 +9,7 @@ import type jsPDFInvoiceTemplate from 'jspdf-invoice-template';
 export const createPDF = async (options: PDFOptions) => {
 	// use inline import because `jsPDFInvoiceTemplate` package only works in browser
 	const pdfPkg = await import('jspdf-invoice-template');
+	// pdfPkg.jsPDF.API.events.
 	// const parameters: PdfProps = {};
 	// const doc = new pdfPkg.jsPDF({
 	// 	filters: ['ASCIIHexEncode'],
@@ -19,7 +22,7 @@ export const createPDF = async (options: PDFOptions) => {
 	// set font
 	console.log({ doc }, 'invoice-to-pdf.ts ~ 21');
 	if (!doc) return;
-	// doc.addFileToVFS('Amiri-Regular.ttf', AmiriRegular);
+	doc.addFileToVFS('Amiri-Regular.ttf', AmiriRegular);
 	doc.addFont('/Amiri-Regular.ttf', 'Amiri', 'normal');
 	doc.setFont('Amiri'); // set font
 	doc.setFontSize(50);
