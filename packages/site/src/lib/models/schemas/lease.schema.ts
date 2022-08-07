973d1ba20a9e366802c1ecdf921255ccec7990e3
nbe <aleisa.naser@gmail.com>
2022-08-08 02:20:59 +0300

    dupe lease schema for correct type inference

--- packages/site/src/lib/models/schemas/lease.schema.ts
+++ packages/site/src/lib/models/schemas/lease.schema.ts
@@ -2,38 +2,56 @@ import { isID } from '$lib/models/schemas/id.schema';
 import { zodIsDateRequired } from '$lib/utils/zod-validators';
 import { z } from 'zod';
 
-export const base = z.object({
-	monthlyRent: z.number().min(1),
-	start: zodIsDateRequired(),
-	end: zodIsDateRequired(),
-	notify: z.boolean(),
-	canPay: z.boolean(),
-});
+export const updateSchema = z
+	.object({
+		monthlyRent: z.number().min(1),
+		start: zodIsDateRequired(),
+		end: zodIsDateRequired(),
+		notify: z.boolean(),
+		canPay: z.boolean(),
+	})
 
-export const createBase = base.extend({
-	tenantId: isID,
-	unitId: isID,
-});
+	.refine(
+		(val) =>
+			val.start && val.end && Date.parse(val.start) < Date.parse(val.end),
+		{
+			path: ['start'],
+			message: 'Start date must be before end date',
+		},
+	)
+	.refine(
+		(val) =>
+			val.start && val.end && Date.parse(val.start) < Date.parse(val.end),
+		{
+			path: ['end'],
+			message: 'End date must be after start date',
+		},
+	);
 
-export const refined = (s: typeof createBase) =>
-	s
-		.refine(
-			(val) =>
-				val.start && val.end && Date.parse(val.start) < Date.parse(val.end),
-			{
-				path: ['start'],
-				message: 'Start date must be before end date',
-			},
-		)
-		.refine(
-			(val) =>
-				val.start && val.end && Date.parse(val.start) < Date.parse(val.end),
-			{
-				path: ['end'],
-				message: 'End date must be after start date',
-			},
-		);
-
-// @ts-ignore
-export const updateSchema = refined(base);
-export const createSchema = refined(createBase);
+export const createSchema = z
+	.object({
+		tenantId: isID,
+		unitId: isID,
+		// dupes
+		monthlyRent: z.number().min(1),
+		start: zodIsDateRequired(),
+		end: zodIsDateRequired(),
+		notify: z.boolean(),
+		canPay: z.boolean(),
+	})
+	.refine(
+		(val) =>
+			val.start && val.end && Date.parse(val.start) < Date.parse(val.end),
+		{
+			path: ['start'],
+			message: 'Start date must be before end date',
+		},
+	)
+	.refine(
+		(val) =>
+			val.start && val.end && Date.parse(val.start) < Date.parse(val.end),
+		{
+			path: ['end'],
+			message: 'End date must be after start date',
+		},
+	);
