99ee91ab8d74b32cd2d40185d6c195cf4cac1a3d
nbe <aleisa.naser@gmail.com>
2022-08-09 13:59:06 +0300

    simplify fileform predefined relation

--- packages/site/src/lib/components/file/FileForm.svelte
+++ packages/site/src/lib/components/file/FileForm.svelte
@@ -2,49 +2,14 @@
 	import { page } from '$app/stores';
 	import Form from '$lib/components/form/Form.svelte';
 	import { Field } from '$lib/models/classes/Field.class';
-	import type { PredefinedFile } from '$lib/models/interfaces/predefined.interface';
 	import { schema } from '$lib/models/schemas/file-schema';
 
-	export let predefined: PredefinedFile;
+	export let relationKey: string;
+	export let relationValue: string;
 
 	const basicFields = [
-		new Field('tenantId', {
-			value: predefined?.tenantId,
-			disabled: true,
-			autoInit: true,
-		}),
-		new Field('portfolioId', {
-			value: predefined?.portfolioId,
-			disabled: true,
-			autoInit: true,
-		}),
-		new Field('propertyId', {
-			value: predefined?.propertyId,
-			disabled: true,
-			autoInit: true,
-		}),
-		new Field('unitId', {
-			value: predefined?.unitId,
-			disabled: true,
-			autoInit: true,
-		}),
-		new Field('expenseId', {
-			value: predefined?.expenseId,
-			disabled: true,
-			autoInit: true,
-		}),
-		new Field('leaseId', {
-			value: predefined?.leaseId,
-			disabled: true,
-			autoInit: true,
-		}),
-		new Field('leaseInvoiceId', {
-			value: predefined?.leaseInvoiceId,
-			disabled: true,
-			autoInit: true,
-		}),
-		new Field('maintenanceOrderId', {
-			value: predefined?.maintenanceOrderId,
+		new Field(relationKey, {
+			value: relationValue,
 			disabled: true,
 			autoInit: true,
 		}),
--- packages/site/src/routes/files/new.svelte
+++ packages/site/src/routes/files/new.svelte
@@ -16,13 +16,22 @@
 			maintenanceOrderId: url.searchParams.get('maintenanceOrderId'),
 		};
 
-		return { props: { predefined } };
+		const relation = Object.entries(predefined).find(([key, value]) => value);
+		const relationKey = relation?.[0];
+		const relationValue = relation?.[1];
+
+		if (!relationKey || !relationValue || typeof relationValue !== 'string') {
+			throw new Error('Insufficient URL parameters');
+		}
+
+		return { props: { relationKey, relationValue } };
 	};
 </script>
 
 <script lang="ts">
 	type Prop = LP<typeof load>;
-	export let predefined: Prop['predefined'];
+	export let relationKey: Prop['relationKey'];
+	export let relationValue: Prop['relationValue'];
 </script>
 
-<FileForm {predefined} />
+<FileForm {relationKey} {relationValue} />
