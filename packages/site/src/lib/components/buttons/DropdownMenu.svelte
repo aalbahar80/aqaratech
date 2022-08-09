b169ba7c644bcb136e1326bc1cc8a683c6d2134c
nbe <aleisa.naser@gmail.com>
2022-08-09 20:27:46 +0300

    remove unused $props.class from dropdown menu

--- packages/site/src/lib/components/buttons/DropdownMenu.svelte
+++ packages/site/src/lib/components/buttons/DropdownMenu.svelte
@@ -12,7 +12,7 @@
 	leaveTo="transform opacity-0 scale-95"
 >
 	<MenuItems
-		class={`${$$props.class} z-20 absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`}
+		class="absolute right-0 z-20 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
 	>
 		<slot />
 	</MenuItems>
