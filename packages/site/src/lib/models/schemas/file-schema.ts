6a252c7fc585215a76863a2e07f6834a53ccf1ae
nbe <aleisa.naser@gmail.com>
2022-08-09 14:06:42 +0300

    include relation in fileform

--- packages/site/src/lib/models/schemas/file-schema.ts
+++ packages/site/src/lib/models/schemas/file-schema.ts
@@ -1,7 +1,19 @@
+import { isID } from '$lib/models/schemas/id.schema';
 import { trim } from '$lib/zodTransformers.js';
 import { z } from 'zod';
 
 export const schema = z.object({
 	fileName: z.string().min(1, { message: 'Required' }).transform(trim),
 	file: z.any(),
+
+	// Do not change. Specifically, this is sent using the multipart/form-data content type,
+	// Which causes the api sdk to change null to "null". Therefore, falsey values should NOT be sent.
+	tenantId: isID.nullish(),
+	portfolioId: isID.nullish(),
+	propertyId: isID.nullish(),
+	unitId: isID.nullish(),
+	expenseId: isID.nullish(),
+	leaseId: isID.nullish(),
+	leaseInvoiceId: isID.nullish(),
+	maintenanceOrderId: isID.nullish(),
 });
