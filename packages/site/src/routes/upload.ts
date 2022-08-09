594c865f420ef0498809a28d06004654825c6a3e
nbe <aleisa.naser@gmail.com>
2022-08-09 14:46:38 +0300

    stale upload files

--- packages/site/src/routes/upload.ts
+++
@@ -1,8 +0,0 @@
-import { environment } from '$environment';
-import type { RequestHandler } from '@sveltejs/kit';
-
-const { authConfig } = environment;
-
-export const POST: RequestHandler = () => {
-	// TODO
-};
