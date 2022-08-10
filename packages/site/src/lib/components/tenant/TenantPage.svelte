9bdcf5d6b56cfabf19740e1e8284587a79828e10
nbe <aleisa.naser@gmail.com>
2022-08-10 15:44:53 +0300

    add files to tenant page

--- packages/site/src/lib/components/tenant/TenantPage.svelte
+++ packages/site/src/lib/components/tenant/TenantPage.svelte
@@ -1,50 +1,10 @@
 <script lang="ts">
-	import { session } from '$app/stores';
 	import Button from '$lib/components/buttons/Button.svelte';
-	import DetailsPane from '$lib/components/DetailsPane.svelte';
 	import Heading from '$lib/components/Heading.svelte';
-	import LeaseList from '$lib/components/lease/LeaseList.svelte';
-	import LeaseInvoiceList from '$lib/components/leaseInvoice/LeaseInvoiceList.svelte';
-	import MemberList from '$lib/components/member/MemberList.svelte';
-	import { countries } from '$lib/constants/countries';
-	import { toUTCFormat } from '$lib/utils/common';
-	import { create } from '$lib/utils/route-helpers';
-	import type {
-		PaginatedLeaseDto,
-		PaginatedLeaseInvoiceDto,
-		PaginatedRoleDto,
-		TenantDto,
-	} from '@self/sdk';
+	import type { TenantDto } from '@self/sdk';
 	import { Collection } from '@steeze-ui/heroicons';
 
 	export let tenant: TenantDto;
-	export let leases: PaginatedLeaseDto;
-	export let invoices: PaginatedLeaseInvoiceDto;
-	export let roles: PaginatedRoleDto;
-
-	$: details = [
-		['Name', tenant.fullName],
-		...(tenant.label ? [['Label', tenant.label]] : []),
-		['Label', tenant.label],
-		['Phone', tenant.phone],
-		['Date of Birth', tenant.dob ? toUTCFormat(tenant.dob) : ''],
-		['Civil id', tenant.civilid],
-		[
-			'Nationality',
-			countries.find((c) => c.alpha3Code === tenant.nationality)?.name,
-		],
-		['Passport #', tenant.passportNum],
-		['Residency #', tenant.residencyNum],
-		[
-			'Residency Expiration',
-			tenant.residencyEnd ? toUTCFormat(tenant.residencyEnd) : '',
-		],
-	] as [string, string | null][];
-
-	const files: [string, string][] = [
-		['Civil Id', ''],
-		['Passport', ''],
-	];
 </script>
 
 <Heading title="Tenant" id={tenant.id} entity="tenants">
@@ -59,19 +19,3 @@
 		/>
 	</svelte:fragment>
 </Heading>
-<DetailsPane {details} {files} />
-{#if $session.user?.role?.roleType === 'ORGADMIN'}
-	<MemberList {roles} />
-{/if}
-<LeaseList
-	{leases}
-	formUrl={(function () {
-		const base = create({ entity: 'leases' });
-		const searchParams = new URLSearchParams({
-			tenantId: tenant.id,
-		});
-		return `${base}?${searchParams.toString()}`;
-	})()}
-	showIndex
-/>
-<LeaseInvoiceList leaseInvoices={invoices} />
--- packages/site/src/routes/tenants/[id]/index.svelte
+++ packages/site/src/routes/tenants/[id]/index.svelte
@@ -1,6 +1,14 @@
 <script lang="ts" context="module">
+	import { session } from '$app/stores';
+	import DetailsPane from '$lib/components/DetailsPane.svelte';
+	import LeaseList from '$lib/components/lease/LeaseList.svelte';
+	import LeaseInvoiceList from '$lib/components/leaseInvoice/LeaseInvoiceList.svelte';
+	import MemberList from '$lib/components/member/MemberList.svelte';
 	import TenantPage from '$lib/components/tenant/TenantPage.svelte';
+	import { countries } from '$lib/constants/countries';
+	import { toUTCFormat } from '$lib/utils/common';
 	import { parseParams } from '$lib/utils/parse-params';
+	import { create } from '$lib/utils/route-helpers';
 	import type { LoadEvent } from '@sveltejs/kit';
 	import type { LP } from 'src/types/load-props';
 
@@ -10,15 +18,20 @@
 		url,
 	}: LoadEvent<{ id: string }>) => {
 		const sParams = parseParams(url);
+		const tenantId = params.id;
 
-		const [tenant, leases, invoices, roles] = await Promise.all([
-			stuff.api!.tenants.findOne({ id: params.id }),
-			stuff.api!.tenants.findLeases({ id: params.id }),
-			stuff.api!.tenants.findInvoices({ id: params.id, ...sParams }),
-			stuff.api!.tenants.findRoles({ id: params.id }),
+		const [tenant, files, leases, invoices, roles] = await Promise.all([
+			stuff.api!.tenants.findOne({ id: tenantId }),
+			stuff.api!.files.findAll({
+				relationKey: 'tenants',
+				relationValue: tenantId,
+			}),
+			stuff.api!.tenants.findLeases({ id: tenantId }),
+			stuff.api!.tenants.findInvoices({ id: tenantId, ...sParams }),
+			stuff.api!.tenants.findRoles({ id: tenantId }),
 		]);
 
-		return { props: { tenant, leases, invoices, roles } };
+		return { props: { tenant, files, leases, invoices, roles } };
 	};
 </script>
 
@@ -28,6 +41,46 @@
 	export let leases: Prop['leases'];
 	export let invoices: Prop['invoices'];
 	export let roles: Prop['roles'];
+	export let files: Prop['files'];
+
+	$: details = [
+		['Name', tenant.fullName],
+		...(tenant.label ? [['Label', tenant.label]] : []),
+		['Label', tenant.label],
+		['Phone', tenant.phone],
+		['Date of Birth', tenant.dob ? toUTCFormat(tenant.dob) : ''],
+		['Civil id', tenant.civilid],
+		[
+			'Nationality',
+			countries.find((c) => c.alpha3Code === tenant.nationality)?.name,
+		],
+		['Passport #', tenant.passportNum],
+		['Residency #', tenant.residencyNum],
+		[
+			'Residency Expiration',
+			tenant.residencyEnd ? toUTCFormat(tenant.residencyEnd) : '',
+		],
+	] as [string, string | null][];
 </script>
 
-<TenantPage {tenant} {leases} {invoices} {roles} />
+<TenantPage {tenant} />
+
+<DetailsPane {details} {files} />
+
+{#if $session.user?.role?.roleType === 'ORGADMIN'}
+	<MemberList {roles} />
+{/if}
+
+<LeaseList
+	{leases}
+	formUrl={(function () {
+		const base = create({ entity: 'leases' });
+		const searchParams = new URLSearchParams({
+			tenantId: tenant.id,
+		});
+		return `${base}?${searchParams.toString()}`;
+	})()}
+	showIndex
+/>
+
+<LeaseInvoiceList leaseInvoices={invoices} />
