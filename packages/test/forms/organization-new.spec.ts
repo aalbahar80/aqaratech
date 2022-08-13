457fcb288a1d7766d086cb5a957c89639247911a
nbe <aleisa.naser@gmail.com>
2022-08-14 00:28:47 +0300

    update new org test

--- packages/test/forms/organization-new.spec.ts
+++ packages/test/forms/organization-new.spec.ts
@@ -7,23 +7,16 @@ test("existing user can create new org", async ({ page }) => {
 	// wait a bit for dropdown to load
 	await new Promise((resolve) => setTimeout(resolve, 1000));
 
-	await page
-		.locator('button:has-text("Pfeffer - Herman Organization")')
-		.click();
+	await page.locator("data-testid=navbar-dropdown").click();
 	await page.locator("text=Switch Role").click();
-	await expect(page).toHaveURL(
-		"http://localhost:3000/users/3e70ffbc-9fdd-4191-9400-d937e7072b72/roles"
-	);
 	await page.locator("text=Create new Organization").click();
 	await expect(page).toHaveURL("http://localhost:3000/organizations/new");
-	await page.locator('input[name="fullName"]').click();
+	await new Promise((resolve) => setTimeout(resolve, 1000)); // hydration (used sveltekit:reload)
+
 	await page.locator('input[name="fullName"]').fill("My new organization");
-	await page.locator('input[name="label"]').click();
 	await page.locator('input[name="label"]').fill("newOrg");
 	await page.locator("text=Save").click();
 
-	const locator = page.locator(
-		'button:has-text("My new organization Organization")'
-	);
+	const locator = page.locator("text=My new organization");
 	await expect(locator).toBeVisible();
 });
