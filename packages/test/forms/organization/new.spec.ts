fa483d47b36219b81a5aa4397a5ab63a6e6c8ee4
nbe <aleisa.naser@gmail.com>
2022-08-17 01:31:12 +0300

    test org form

--- packages/test/forms/organization/new.spec.ts
+++ packages/test/forms/organization/new.spec.ts
@@ -2,7 +2,7 @@ import { expect } from "@playwright/test";
 import { test } from "../../config";
 
 test("existing user can create new org", async ({ page }) => {
-	page.goto("/");
+	await page.goto("/");
 
 	// wait a bit for dropdown to load
 	await new Promise((resolve) => setTimeout(resolve, 1000));
@@ -18,5 +18,16 @@ test("existing user can create new org", async ({ page }) => {
 	await page.locator("text=Save").click();
 
 	const locator = page.locator("text=My new organization");
+	await expect(locator).toBeVisible();
+});
+
+test("can be submitted", async ({ page }) => {
+	await page.goto("/organizations/new");
+
+	await page.locator('input[name="fullName"]').fill("My Second Org");
+	await page.locator('input[name="label"]').fill("Org 2");
+	await page.locator("text=Save").click();
+
+	const locator = page.locator("text=My Second Org");
 	await expect(locator).toBeVisible();
 });
