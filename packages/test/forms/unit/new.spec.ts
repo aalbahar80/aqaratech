73ce35c46a90262a7fabc012318b1752fb328936
nbe <aleisa.naser@gmail.com>
2022-08-17 01:50:01 +0300

    init unit form smoke test

---
+++ packages/test/forms/unit/new.spec.ts
@@ -0,0 +1,46 @@
+import { expect } from "@playwright/test";
+import { sample } from "@self/seed";
+import { test } from "../../config";
+
+test("smoke", async ({ page }, info) => {
+	await page.goto("/units/new");
+
+	await page.locator("#portfolioId").click();
+	await page.locator(`text=${sample.portfolios[0].fullName}`).click();
+
+	await page.locator("#propertyId").click();
+	await page.locator(`data-testid=${sample.properties[0].id}`).click();
+
+	await page.locator("#unitNumber").fill("5");
+	await page.selectOption("#type", { label: "مخزن" });
+
+	await page.locator("text=Save").click();
+
+	const id = new RegExp(
+		`/units/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}`
+	);
+	await expect(page).toHaveURL(id);
+	const url = page.url();
+
+	// take screenshot
+	expect(page.locator("text=unit")).toBeTruthy();
+	await page.locator("#detailsPane").screenshot({
+		path: info.snapshotDir + `/unit-${info.snapshotSuffix}.png`,
+	});
+
+	await page.locator("text=Edit").click();
+	const edit = new RegExp(
+		`/units/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/edit`
+	);
+	await expect(page).toHaveURL(edit);
+	await page.locator("text=Save").click();
+
+	// ensure same entity
+	await expect(page).toHaveURL(url);
+
+	// match screenshot
+	expect(page.locator("text=unit")).toBeTruthy();
+	expect(await page.locator("#detailsPane").screenshot()).toMatchSnapshot({
+		name: "unit.png",
+	});
+});
