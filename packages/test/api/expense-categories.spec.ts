eff79f706520408182477e88b78ab95923bbdc25
nbe <aleisa.naser@gmail.com>
2022-08-18 07:20:07 +0300

    add placeholder expense category tests

---
+++ packages/test/api/expense-categories.spec.ts
@@ -0,0 +1,18 @@
+import { testOrgRoleId } from "@self/seed";
+import { expect, test } from "../token";
+
+test.use({
+	extraHTTPHeaders: {
+		"x-role-id": testOrgRoleId,
+	},
+});
+
+test.skip("cannot assign expense to non-leaf node category", async ({
+	request,
+	token,
+}) => {});
+
+test.skip("can assign expense to leaf node category", async ({
+	request,
+	token,
+}) => {});
