aae6e63747cbc751ea169ecec611992bf3e41b50
nbe <aleisa.naser@gmail.com>
2022-08-18 14:47:16 +0300

    change docker registry name to aqtech

--- docker-push-api.sh
+++ docker-push-api.sh
@@ -2,7 +2,7 @@
 docker build -t aqaratech-api -f "Dockerfile.backend" .
 
 # tag
-docker tag aqaratech-api registry.digitalocean.com/aqaratech/aqaratech-api
+docker tag aqaratech-api registry.digitalocean.com/aqtech/aqaratech-api
 
 # push
-docker push registry.digitalocean.com/aqaratech/aqaratech-api
+docker push registry.digitalocean.com/aqtech/aqaratech-api
--- docker-push-site.sh
+++ docker-push-site.sh
@@ -2,7 +2,7 @@
 docker build -t aqaratech-site -f "Dockerfile.site" .
 
 # tag
-docker tag aqaratech-site registry.digitalocean.com/aqaratech/aqaratech-site
+docker tag aqaratech-site registry.digitalocean.com/aqtech/aqaratech-site
 
 # push
-docker push registry.digitalocean.com/aqaratech/aqaratech-site
+docker push registry.digitalocean.com/aqaratech/aqtech-site
