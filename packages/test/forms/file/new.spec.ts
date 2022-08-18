8c87c72faf4861215a48d579942588f6a94bb918
nbe <aleisa.naser@gmail.com>
2022-08-18 11:32:12 +0300

    test uploaded file against local

--- packages/test/forms/file/new.spec.ts
+++ packages/test/forms/file/new.spec.ts
@@ -1,6 +1,7 @@
 import { expect } from "@playwright/test";
 import { sample, testOrgRoleId } from "@self/seed";
 import { entitiesMap } from "@self/utils";
+import { promises } from "node:fs";
 import { test } from "../../config";
 
 const fileEntity = entitiesMap.file;
@@ -15,17 +16,17 @@ const params = new URLSearchParams({
 const url = `/${fileEntity.urlName}/new?${params.toString()}`;
 test("files can be uploaded", async ({ page, request, token }, info) => {
 	const fileName = "myfile";
+	const localFilePath = "./forms/file/upload-test.png";
 
 	// upload file
 	await page.goto(url);
 	await page.locator('input[name="fileName"]').fill(fileName);
-	await page
-		.locator('input[name="file"]')
-		.setInputFiles("./forms/file/upload-test.png");
+	await page.locator('input[name="file"]').setInputFiles(localFilePath);
 	await page.locator("text=Save").click();
 
 	await expect(page).toHaveURL(`/${portfolioEntity.urlName}/${portfolio.id}`);
 
+	// grab uploaded file
 	const key = `${portfolioEntity.title}/${portfolio.id}/${fileName}`;
 	const response = await request.get(
 		`http://localhost:3002/${
@@ -38,6 +39,16 @@ test("files can be uploaded", async ({ page, request, token }, info) => {
 			},
 		}
 	);
-
 	expect(response.status()).toBe(200);
+
+	const presignedUrl = await response.text();
+	const response2 = await request.get(presignedUrl);
+	const uploadedFile = await response2.body();
+
+	// file buffer matches
+	const localFile = await promises.readFile(localFilePath);
+	expect(uploadedFile.toString()).toEqual(localFile.toString());
+
+	// file size matches
+	expect(uploadedFile.length).toEqual(localFile.length);
 });
