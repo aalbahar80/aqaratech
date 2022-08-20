05498be0464a2e29a71df454b173d0d43914cd6b
nbe <aleisa.naser@gmail.com>
2022-08-20 20:07:27 +0300

    fix tailwind postcss syntax errors

--- packages/site/svelte.config.js
+++ packages/site/svelte.config.js
@@ -1,11 +1,21 @@
 import adapter from '@sveltejs/adapter-auto';
 import adapterNode from '@sveltejs/adapter-node';
-import preprocess from 'svelte-preprocess';
+import { dirname, join } from 'path';
+import sveltePreprocess from 'svelte-preprocess';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
-	preprocess: [preprocess({ postcss: true })],
+	preprocess: [
+		sveltePreprocess({
+			postcss: {
+				configFilePath: join(__dirname, 'postcss.config.cjs'),
+			},
+		}),
+	],
 	kit: {
 		adapter:
 			process.env.VERCEL || process.env.CF_PAGES ? adapter() : adapterNode(),
