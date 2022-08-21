ef19afe454033ee8a48d3962d165f4cc42ff9c9b
nbe <aleisa.naser@gmail.com>
2022-08-21 10:07:21 +0300

    init frappe chart

---
+++ packages/site/src/lib/components/charts/Heatmap.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Chart from 'svelte-frappe-charts';
+
+	const colors = ['#ebedf0', '#c0ddf9', '#73b3f3', '#3886e1', '#17459e'];
+	const date1 = new Date().getTime() / 1000;
+	console.log({ date1 }, 'Heatmap.svelte ~ 15');
+	let data = {
+		dataPoints: {
+			1661065253.095: 20,
+			'1463673055': 113,
+			'1476892421': 57,
+		},
+		start: new Date('2022-07-29'),
+		end: new Date('2023-07-29'),
+	};
+</script>
+
+<Chart type="heatmap" {data} {colors} />
