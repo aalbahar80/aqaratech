2536e7969d53136a052237bf7296df314ca5cf9e
nbe <aleisa.naser@gmail.com>
2022-08-21 08:29:58 +0300

    wider groupby selector

--- packages/site/src/lib/components/dashboard/cards/RevenueCard.svelte
+++ packages/site/src/lib/components/dashboard/cards/RevenueCard.svelte
@@ -20,7 +20,7 @@
 	subtitle="Breakdown of rent income by status & property."
 	empty={invoices.results.length < 1}
 >
-	<div slot="groupBy" class="flex h-14 w-1/3 pb-4">
+	<div slot="groupBy" class="flex h-14 pb-4 md:w-2/5">
 		<span
 			class="mt-1 inline-flex w-full items-center break-words rounded-none rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-gray-500 shadow-sm sm:text-sm"
 		>
