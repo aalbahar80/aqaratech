3498ccc73ce0e73ca726e50e0fe8258b54f64157
nbe <aleisa.naser@gmail.com>
2022-08-22 04:54:32 +0300

    clamp earliest heatmap date

--- packages/site/src/lib/components/charts/OccupancyHeatmap.svelte
+++ packages/site/src/lib/components/charts/OccupancyHeatmap.svelte
@@ -9,11 +9,17 @@
 	export let occupancy: Occupancy[];
 	const colors = ['#ebedf0', '#c0ddf9', '#73b3f3', '#3886e1', '#17459e'];
 
+	const oneYearAgo = new Date();
+	oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
+
+	const earliest = new Date(occupancy[0]!.date);
+	const start = earliest > oneYearAgo ? oneYearAgo : earliest;
+
 	$: data = {
 		dataPoints: R.fromPairs(
 			occupancy.map((o) => [o.date / 1000, o.occupiedPct]),
 		),
-		start: new Date(occupancy[0]!.date),
+		start,
 		end: new Date(occupancy[occupancy.length - 1]!.date),
 	};
 </script>
