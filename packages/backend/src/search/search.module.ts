1de962c4575fc93a2917d464588380b2ec384551
nbe <aleisa.naser@gmail.com>
2022-08-25 01:15:35 +0300

    don't make searchservice global

--- packages/backend/src/search/search.module.ts
+++ packages/backend/src/search/search.module.ts
@@ -1,8 +1,7 @@
-import { Global, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { SearchController } from './search.controller';
 import { SearchService } from './search.service';
 
-@Global()
 @Module({
   providers: [SearchService],
   controllers: [SearchController],
