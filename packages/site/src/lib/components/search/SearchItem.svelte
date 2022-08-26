d396354586059f37cea4832a4bebe817e514ac0b
nbe <aleisa.naser@gmail.com>
2022-08-26 03:53:31 +0300

    dedupe wrapper class

--- packages/site/src/lib/components/search/SearchItem.svelte
+++ packages/site/src/lib/components/search/SearchItem.svelte
@@ -38,19 +38,12 @@
 			)}
 		>
 			{@html item._formatted.title}
+			{#each Object.entries(highlightedFields) as [key, val]}
+				<p>
+					{key}:
+					{@html val}
+				</p>
+			{/each}
 		</div>
-		{#each Object.entries(highlightedFields) as [key, val]}
-			<div
-				class={classes(
-					'cursor-default select-none px-4 py-2 [&_mark]:underline [&_mark]:underline-offset-2',
-					hovering
-						? 'bg-indigo-600 text-white [&_mark]:text-white [&_mark]:bg-inherit'
-						: '[&_mark]:text-indigo-600 [&_mark]:bg-inherit [&_mark]:decoration-indigo-500 [&_mark]:decoration-2',
-				)}
-			>
-				{key}:
-				{@html val}
-			</div>
-		{/each}
 	</ListboxOption>
 </Hoverable>
