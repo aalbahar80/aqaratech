3540a02857666751ad9db7ea921597718b8e1eef
nbe <aleisa.naser@gmail.com>
2022-08-28 22:41:15 +0300

    rm problematic satisifies type helper

--- packages/backend/src/common/parse-location-filter.ts
+++ packages/backend/src/common/parse-location-filter.ts
@@ -1,8 +1,6 @@
-import { Prisma } from '@prisma/client';
-import { satisfies } from '@self/utils';
 import { DashboardFilterDto } from 'src/aggregate/dto/aggregate.dto';
 
-type LocationFilter = Prisma.ExpenseWhereInput | Prisma.UnitWhereInput;
+// type LocationFilter = Prisma.ExpenseWhereInput | Prisma.UnitWhereInput;
 
 export const parseLocationFilter = ({
   filter,
@@ -14,12 +12,12 @@ export const parseLocationFilter = ({
   let locationFilter;
   if (filter?.unitId) {
     const key = entity === 'Unit' ? 'id' : 'unitId';
-    locationFilter = { [key]: filter.unitId };
+    locationFilter = { [key]: filter.unitId } as const;
   } else if (filter?.propertyId) {
-    locationFilter = { propertyId: filter.propertyId };
+    locationFilter = { propertyId: filter.propertyId } as const;
   } else {
-    locationFilter = { portfolioId: filter?.portfolioId };
+    locationFilter = { portfolioId: filter?.portfolioId } as const;
   }
 
-  return satisfies<LocationFilter>()(locationFilter);
+  return locationFilter;
 };
