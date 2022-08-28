81d7cac321c348607869c559f0420058ef0d5cec
nbe <aleisa.naser@gmail.com>
2022-08-28 22:00:14 +0300

    set redirect href after deleting payout

--- packages/site/src/lib/components/payout/PayoutPage.svelte
+++ packages/site/src/lib/components/payout/PayoutPage.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import BreadCrumb from '$lib/components/breadcrumbs/BreadCrumb.svelte';
 	import Heading from '$lib/components/Heading.svelte';
 	import type { PayoutDto } from '@self/sdk';
+	import { entitiesMap } from '@self/utils';
 
 	export let payout: PayoutDto;
 </script>
 
-<Heading title="Payout" id={payout.id} entity="payout">
+<Heading
+	title="Payout"
+	id={payout.id}
+	entity="payout"
+	onDelete={async () => {
+		await goto(`/${entitiesMap.portfolio.urlName}/${payout.portfolioId}`);
+	}}
+>
 	<svelte:fragment slot="breadcrumbs">
 		<BreadCrumb crumbs={payout.breadcrumbs} />
 	</svelte:fragment>
