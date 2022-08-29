e04cdf457ca567e2943f85ccd7941724bdde4cea
nbe <aleisa.naser@gmail.com>
2022-08-29 23:41:05 +0300

    init sorting example

--- packages/site/src/routes/portfolios/[id]/expenses/+page.svelte
+++ packages/site/src/routes/portfolios/[id]/expenses/+page.svelte
@@ -3,95 +3,111 @@
 		createSvelteTable,
 		flexRender,
 		getCoreRowModel,
+		getSortedRowModel,
 		type ColumnDef,
 		type TableOptions,
 	} from '@tanstack/svelte-table';
 	import { writable } from 'svelte/store';
-	import type { PageData } from './$types';
+	import { makeData, type Person } from './makeData';
 
-	export let data: PageData;
+	// import type { PageData } from './$types';
+	// export let data: PageData;
 
-	type Person = {
-		firstName: string;
-		lastName: string;
-		age: number;
-		visits: number;
-		status: string;
-		progress: number;
+	const columns: ColumnDef<Person>[] = [
+		{
+			header: 'Name',
+			footer: (props) => props.column.id,
+			columns: [
+				{
+					accessorKey: 'firstName',
+					cell: (info) => info.getValue(),
+					footer: (props) => props.column.id,
+				},
+				{
+					accessorFn: (row) => row.lastName,
+					id: 'lastName',
+					cell: (info) => info.getValue(),
+					header: () => 'Last Name',
+					footer: (props) => props.column.id,
+				},
+			],
+		},
+		{
+			header: 'Info',
+			footer: (props) => props.column.id,
+			columns: [
+				{
+					accessorKey: 'age',
+					header: () => 'Age',
+					footer: (props) => props.column.id,
+				},
+				{
+					header: 'More Info',
+					columns: [
+						{
+							accessorKey: 'visits',
+							header: () => 'Visits',
+							footer: (props) => props.column.id,
+						},
+						{
+							accessorKey: 'status',
+							header: 'Status',
+							footer: (props) => props.column.id,
+						},
+						{
+							accessorKey: 'progress',
+							header: 'Profile Progress',
+							footer: (props) => props.column.id,
+						},
+					],
+				},
+			],
+		},
+	];
+
+	const data = makeData(100_000);
+
+	let sorting = [];
+
+	const setSorting = (updater) => {
+		if (updater instanceof Function) {
+			sorting = updater(sorting);
+		} else {
+			sorting = updater;
+		}
+		options.update((old) => ({
+			...old,
+			state: {
+				...old.state,
+				sorting,
+			},
+		}));
 	};
 
-	const defaultData: Person[] = [
-		{
-			firstName: 'tanner',
-			lastName: 'linsley',
-			age: 24,
-			visits: 100,
-			status: 'In Relationship',
-			progress: 50,
-		},
-		{
-			firstName: 'tandy',
-			lastName: 'miller',
-			age: 40,
-			visits: 40,
-			status: 'Single',
-			progress: 80,
-		},
-		{
-			firstName: 'joe',
-			lastName: 'dirte',
-			age: 45,
-			visits: 20,
-			status: 'Complicated',
-			progress: 10,
-		},
-	];
-
-	const defaultColumns: ColumnDef<Person>[] = [
-		{
-			accessorKey: 'firstName',
-			cell: (info) => info.getValue(),
-			footer: (info) => info.column.id,
-		},
-		{
-			accessorFn: (row) => row.lastName,
-			id: 'lastName',
-			cell: (info) => info.getValue(),
-			header: () => 'Last Name',
-			footer: (info) => info.column.id,
-		},
-		{
-			accessorKey: 'age',
-			header: () => 'Age',
-			footer: (info) => info.column.id,
-		},
-		{
-			accessorKey: 'visits',
-			header: () => 'Visits',
-			footer: (info) => info.column.id,
-		},
-		{
-			accessorKey: 'status',
-			header: 'Status',
-			footer: (info) => info.column.id,
-		},
-		{
-			accessorKey: 'progress',
-			header: 'Profile Progress',
-			footer: (info) => info.column.id,
-		},
-	];
-
 	const options = writable<TableOptions<Person>>({
-		data: defaultData,
-		columns: defaultColumns,
+		data,
+		columns,
+		state: {
+			sorting,
+		},
+		onSortingChange: setSorting,
 		getCoreRowModel: getCoreRowModel(),
+		getSortedRowModel: getSortedRowModel(),
+		debugTable: true,
 	});
+
+	const refreshData = () => {
+		console.info('refresh');
+		options.update((prev) => ({
+			...prev,
+			data: makeData(100_000),
+		}));
+	};
 
 	const rerender = () => {
 		options.update((options) => ({
 			...options,
-			data: defaultData,
+			data,
 		}));
 	};
 
@@ -99,19 +115,30 @@
 </script>
 
 <div class="p-2">
+	<div class="h-2" />
 	<table>
 		<thead>
 			{#each $table.getHeaderGroups() as headerGroup}
 				<tr>
 					{#each headerGroup.headers as header}
-						<th>
+						<th colSpan={header.colSpan}>
 							{#if !header.isPlaceholder}
-								<svelte:component
-									this={flexRender(
-										header.column.columnDef.header,
-										header.getContext(),
-									)}
-								/>
+								<div
+									class:cursor-pointer={header.column.getCanSort()}
+									class:select-none={header.column.getCanSort()}
+									on:click={header.column.getToggleSortingHandler()}
+								>
+									<svelte:component
+										this={flexRender(
+											header.column.columnDef.header,
+											header.getContext(),
+										)}
+									/>
+									{{
+										asc: ' 🔼',
+										desc: ' 🔽',
+									}[header.column.getIsSorted().toString()] ?? ''}
+								</div>
 							{/if}
 						</th>
 					{/each}
@@ -119,7 +146,7 @@
 			{/each}
 		</thead>
 		<tbody>
-			{#each $table.getRowModel().rows as row}
+			{#each $table.getRowModel().rows.slice(0, 10) as row}
 				<tr>
 					{#each row.getVisibleCells() as cell}
 						<td>
@@ -135,7 +162,7 @@
 			{#each $table.getFooterGroups() as footerGroup}
 				<tr>
 					{#each footerGroup.headers as header}
-						<th>
+						<th colSpan={header.colSpan}>
 							{#if !header.isPlaceholder}
 								<svelte:component
 									this={flexRender(
@@ -150,8 +177,14 @@
 			{/each}
 		</tfoot>
 	</table>
-	<div class="h-4" />
-	<button on:click={() => rerender()} class="border p-2"> Rerender </button>
+	<div>{$table.getRowModel().rows.length} Rows</div>
+	<div>
+		<button on:click={() => rerender()}>Force Rerender</button>
+	</div>
+	<div>
+		<button on:click={() => refreshData()}>Refresh Data</button>
+	</div>
+	<pre>{JSON.stringify($table.getState().sorting, null, 2)}</pre>
 </div>
 
 <style>
