057567f52eb9ec75ccb4876785cce09f4fda9cac
nbe <aleisa.naser@gmail.com>
2022-08-30 01:48:17 +0300

    use searchquery const

--- packages/site/src/lib/utils/parse-params.ts
+++ packages/site/src/lib/utils/parse-params.ts
@@ -2,6 +2,7 @@ import {
 	DEFAULT_PAGINATION_KEY,
 	ORDER_BY,
 	PAGE_SIZE,
+	SEARCH_QUERY,
 	SORT_ORDER,
 } from '$lib/constants/pagination-keys';
 import type { CombinedEnum, SortOrderEnum } from '@self/sdk';
@@ -10,7 +11,7 @@ export const parseParams = (searchParams: URLSearchParams) => {
 	return {
 		page: +(searchParams.get(DEFAULT_PAGINATION_KEY) || 1),
 		take: +(searchParams.get(PAGE_SIZE) || 20),
-		q: searchParams.get('q') ?? undefined,
+		q: searchParams.get(SEARCH_QUERY) ?? undefined,
 		sortOrder: (searchParams.get(SORT_ORDER) ?? 'desc') as SortOrderEnum,
 		// TODO fix types vs manual type-cast
 		orderBy: (searchParams.get(ORDER_BY) as CombinedEnum) ?? undefined,
