592565fff68cde4ee095309b854c75d8e3aa9896
nbe <aleisa.naser@gmail.com>
2022-08-29 23:22:19 +0300

    hover style if href

--- packages/site/src/lib/components/balance/BalanceLineItem.svelte
+++ packages/site/src/lib/components/balance/BalanceLineItem.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
+	import { classes } from '$lib/utils/classes';
+
 	export let href: string | undefined = undefined;
 </script>
 
 <li class="justify-between odd:bg-gray-50 even:bg-white">
 	<a
 		{href}
-		class="flex items-center justify-between p-4 text-gray-500 hover:bg-gray-100 sm:px-6"
+		class={classes(
+			'flex items-center justify-between p-4 text-gray-500 sm:px-6',
+			href ? 'hover:bg-gray-100' : '',
+		)}
 	>
 		<dt class="text-sm font-medium">
 			<slot name="label" />
