6fadc127ed53b8f11f754e99b18d4e4b16f5d7b6
nbe <aleisa.naser@gmail.com>
2022-08-31 20:36:07 +0300

    use minified papaparse

--- packages/site/src/lib/utils/respond-with-csv.ts
+++ packages/site/src/lib/utils/respond-with-csv.ts
@@ -1,12 +1,14 @@
 import { flatten } from '$lib/utils/flatten';
-import * as Papa from 'papaparse';
+// import * as Papa from 'papaparse';
+// @ts-expect-error minified version has no types
+import { unparse } from 'papaparse/papaparse.min.js';
 
 /**
  * Nested objects are flattened to dot-notation.
  */
 export const respondWithCsv = (data: any[], filename: string) => {
 	const flat = data.map((e) => flatten(e));
-	const csv = Papa.unparse(flat);
+	const csv = unparse(flat);
 
 	return new Response(csv, {
 		headers: {
