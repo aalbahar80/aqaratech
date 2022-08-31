ebdcbb4b331b4978bba0a80058f10cfe5ac1de77
nbe <aleisa.naser@gmail.com>
2022-08-31 19:08:37 +0300

    avoid lodash

--- packages/site/src/lib/utils/flatten.ts
+++ packages/site/src/lib/utils/flatten.ts
@@ -1,14 +1,12 @@
-import * as _ from 'lodash-es';
-
 export function flatten(obj: Record<string, any>, prefix = '') {
 	const propName = prefix ? prefix + '.' : '',
 		ret: Record<string, any> = {};
 
 	for (const attr in obj) {
-		if (_.isArray(obj[attr])) {
+		if (Array.isArray(obj[attr])) {
 			ret[attr] = obj[attr].join(',');
 		} else if (typeof obj[attr] === 'object') {
-			_.extend(ret, flatten(obj[attr], propName + attr));
+			Object.assign(ret, flatten(obj[attr], propName + attr));
 		} else {
 			ret[propName + attr] = obj[attr];
 		}
