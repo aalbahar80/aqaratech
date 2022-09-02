d20387de55eb32381e75ee61b831406859fd97ef
nbe <aleisa.naser@gmail.com>
2022-09-03 02:54:03 +0300

    track schema in sdk package instead of backend

--- packages/backend/src/swagger.ts
+++ packages/backend/src/swagger.ts
@@ -86,7 +86,7 @@ export const setupSwagger = async (app: INestApplication) => {
     const { dump } = await import('js-yaml');
     // For consumption of swagger-ui
     writeFileSync(
-      './openapi.yaml',
+      '../sdk/openapi.yaml',
       dump(document, {
         // schema: 'http://json-schema.org/draft-04/schema#',
       }),
