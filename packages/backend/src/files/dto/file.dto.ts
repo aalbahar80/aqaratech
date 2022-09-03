86fbd30221e2e1b569ca64eac239c71e47de8a17
nbe <aleisa.naser@gmail.com>
2022-09-03 10:41:01 +0300

    use lowercase file dto props

--- packages/backend/src/files/dto/file.dto.ts
+++ packages/backend/src/files/dto/file.dto.ts
@@ -1,4 +1,5 @@
 import { ListObjectsV2Output } from '@aws-sdk/client-s3';
+import { InternalServerErrorException } from '@nestjs/common';
 import { DBEntitiesMap, entitiesMap } from '@self/utils';
 import { Expose } from 'class-transformer';
 import { IsEnum, IsNumber, IsOptional, IsString } from 'class-validator';
@@ -9,13 +10,18 @@ import { IUser } from 'src/interfaces/user.interface';
 export class FileDto {
   constructor(obj: NonNullable<ListObjectsV2Output['Contents']>[0]) {
     Object.assign(this, obj);
+    if (!obj.Key) {
+      throw new InternalServerErrorException('Error in FileDto, invalid key');
+    }
+    this.key = obj.Key;
+    this.size = obj.Size || 0;
   }
 
   @IsString()
-  Key: string;
+  key: string;
 
   @IsNumber()
-  Size: number;
+  size: number;
 }
 
 export class DirectoryRequestDto {
