932000faee3cb9188b6024899ebc275b925ee8d0
nbe <aleisa.naser@gmail.com>
2022-09-03 10:12:18 +0300

    log when timeout

--- packages/backend/src/interceptors/timeout.interceptor.ts
+++ packages/backend/src/interceptors/timeout.interceptor.ts
@@ -2,6 +2,7 @@ import {
   CallHandler,
   ExecutionContext,
   Injectable,
+  Logger,
   NestInterceptor,
   RequestTimeoutException,
 } from '@nestjs/common';
@@ -10,11 +11,14 @@ import { catchError, timeout } from 'rxjs/operators';
 
 @Injectable()
 export class TimeoutInterceptor implements NestInterceptor {
+  private readonly logger = new Logger(TimeoutInterceptor.name);
+
   intercept(_context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       timeout(5000),
       catchError((err) => {
         if (err instanceof TimeoutError) {
+          this.logger.warn('Request timed out');
           return throwError(() => new RequestTimeoutException());
         }
         return throwError(() => err);
