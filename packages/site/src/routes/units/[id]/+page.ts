2b89eb5fb2b2c91216a7e7975b2a26428944e843
nbe <aleisa.naser@gmail.com>
2022-09-06 06:55:59 +0300

    don't use ho-load in units/id route

--- packages/site/src/routes/units/[id]/+page.ts
+++ packages/site/src/routes/units/[id]/+page.ts
@@ -1,46 +1,47 @@
-import { fancy } from '$lib/client/ho-load';
 import { getDashboardData } from '$lib/components/charts/get-dashboard-data';
 import { parseParams } from '$lib/utils/parse-params';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = fancy(
-	async ({ api, params, url: { searchParams } }) => {
-		const unitId = params.id;
-		// TODO handle pagination defaults
-		const sParams = parseParams(searchParams);
-		const [
-			unit,
-			leases,
-			invoicesGrouped,
-			expensesGrouped,
-			invoices,
-			expenses,
-			invoicesGroupedPaid,
-			invoicesGroupedUnpaid,
-			occupancy,
-			futureOccupancy,
-			categories,
-		] = await Promise.all([
-			api.units.findOne({ id: unitId }),
-			api.units.findLeases({ id: unitId, ...sParams }),
-			...(await getDashboardData({
-				api: api,
-				searchParams,
-				unitId,
-			})),
-		]);
-		return {
-			unit,
-			leases,
-			invoicesGrouped,
-			expensesGrouped,
-			invoices,
-			expenses,
-			invoicesGroupedPaid,
-			invoicesGroupedUnpaid,
-			occupancy,
-			futureOccupancy,
-			categories,
-		};
-	},
-);
+export const load: PageLoad = async ({
+	params,
+	url: { searchParams },
+	parent,
+}) => {
+	const unitId = params.id;
+	// TODO handle pagination defaults
+	const sParams = parseParams(searchParams);
+
+	const parentStuff = await parent();
+
+	const [
+		unit,
+		leases,
+		invoicesGrouped,
+		expensesGrouped,
+		invoices,
+		expenses,
+		invoicesGroupedPaid,
+		invoicesGroupedUnpaid,
+		occupancy,
+		futureOccupancy,
+		categories,
+	] = await Promise.all([
+		parentStuff.api.units.findOne({ id: unitId }),
+		parentStuff.api.units.findLeases({ id: unitId, ...sParams }),
+		...(await getDashboardData({ api: parentStuff.api, searchParams, unitId })),
+	]);
+
+	return {
+		unit,
+		leases,
+		invoicesGrouped,
+		expensesGrouped,
+		invoices,
+		expenses,
+		invoicesGroupedPaid,
+		invoicesGroupedUnpaid,
+		occupancy,
+		futureOccupancy,
+		categories,
+	};
+};
