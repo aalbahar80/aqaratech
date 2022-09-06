defcd159f9085ecd464a29c85a3833302d3beea5
nbe <aleisa.naser@gmail.com>
2022-09-06 06:46:38 +0300

    rm sample

--- packages/site/src/routes/units/sample/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import type { PageData } from './$types';
-
-	export let data: PageData;
-</script>
-
-<pre>{JSON.stringify(data.units.results, null, 2)}</pre>
-<!-- <pre>{JSON.stringify(data.abc, null, 2)}</pre> -->
--- packages/site/src/routes/units/sample/+page.ts
+++
@@ -1,7 +0,0 @@
-import { fancy } from '$lib/client/ho-load';
-import type { PageLoad } from './$types';
-
-export const load: PageLoad = fancy(async ({ api }) => {
-	const units = await api.units.findAll();
-	return { units };
-});
