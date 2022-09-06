714fe6ce7e2658722926366acbdf4e5d5d41435c
nbe <aleisa.naser@gmail.com>
2022-09-06 04:23:25 +0300

    fmt

--- packages/site/src/routes/units/+page.ts
+++ packages/site/src/routes/units/+page.ts
@@ -1,6 +1,6 @@
 import { parseParams } from '$lib/utils/parse-params';
-
 import type { PageLoad } from './$types';
+
 export const load: PageLoad = async ({ url: { searchParams }, parent }) => {
 	const { page, take, q } = parseParams(searchParams);
 
