62d41ca8647bfeeb7208cd4b3b81b6e98404d8c8
nbe <aleisa.naser@gmail.com>
2022-09-06 06:46:51 +0300

    restore api in layout

--- packages/site/src/routes/+layout.ts
+++ packages/site/src/routes/+layout.ts
@@ -1,7 +1,14 @@
+import { api } from '$lib/client/api';
 import type { LayoutLoad } from './$types';
 
-export const load: LayoutLoad = async ({ data }) => {
+export const load: LayoutLoad = async ({ fetch, data }) => {
+	const apiClient = api({
+		loadFetch: fetch,
+		token: data.accessToken,
+		roleId: data.user?.role?.id,
+	});
 	return {
+		api: apiClient,
 		...data,
 		apiConfig: {
 			token: data.accessToken,
