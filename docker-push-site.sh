9d770980e8085e9dd964369cac56db6c480d06a9
nbe <aleisa.naser@gmail.com>
2022-09-07 12:23:32 +0300

    let vite handle loading of static env vars at build time

--- docker-push-site.sh
+++ docker-push-site.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# build (grabs values from .env)
-source ./.env && docker build -t registry.digitalocean.com/aqtech/aqaratech-site -f "Dockerfile.site" --build-arg PUBLIC_AQARATECH_ENV=$PUBLIC_AQARATECH_ENV --build-arg PUBLIC_SITE_URL=$PUBLIC_SITE_URL --build-arg PUBLIC_API_URL=$PUBLIC_API_URL --build-arg PUBLIC_API_URL_LOCAL=$PUBLIC_API_URL_LOCAL --build-arg AUTH0_CLIENT_SECRET=$AUTH0_CLIENT_SECRET .
+# build
+docker build -t registry.digitalocean.com/aqtech/aqaratech-site -f "Dockerfile.site" .
 
 # push
 docker push registry.digitalocean.com/aqtech/aqaratech-site
