5a05c426172b9fb66007ca41119543beccfa1739
nbe <aleisa.naser@gmail.com>
2022-09-07 12:28:31 +0300

    inject only essential docker ARGs

--- docker-push-site.sh
+++ docker-push-site.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # build
-docker build -t registry.digitalocean.com/aqtech/aqaratech-site -f "Dockerfile.site" .
+## .env is loaded to inject Docker ARGs
+source ./.env && docker build -t registry.digitalocean.com/aqtech/aqaratech-site -f "Dockerfile.site" --build-arg PUBLIC_SITE_URL=$PUBLIC_SITE_URL .
 
 # push
 docker push registry.digitalocean.com/aqtech/aqaratech-site
