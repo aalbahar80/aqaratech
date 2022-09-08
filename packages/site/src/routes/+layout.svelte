1ec0e044643c8fc233935c206aaef9cac838de33
nbe <aleisa.naser@gmail.com>
2022-09-09 02:37:25 +0300

    init sentry/svelte

--- packages/site/src/routes/+layout.svelte
+++ packages/site/src/routes/+layout.svelte
@@ -1,16 +1,22 @@
 <script lang="ts">
+	import { dev } from '$app/environment';
 	import { navigating, page } from '$app/stores';
 	import ToastParent from '$components/toast/ToastParent.svelte';
 	import { PUBLIC_AQARATECH_ENV } from '$env/static/public';
 	import Alert from '$lib/components/navbar/Alert.svelte';
 	import Navbar from '$lib/components/navbar/Navbar.svelte';
 	import PreloadingIndicator from '$lib/components/PreloadingIndicator.svelte';
+	import type { Scope } from '@sentry/svelte';
+	import * as Sentry from '@sentry/svelte';
+	import { BrowserTracing } from '@sentry/tracing';
 	import LogRocket from 'logrocket';
 	import { onMount } from 'svelte';
 	import { MetaTags } from 'svelte-meta-tags';
 	import '../styles/tailwind.css';
 	import '../styles/theme.postcss';
-	// skeleton theme/styles
+	import type { PageData } from './$types';
+
+	export let data: PageData;
 
 	onMount(() => {
 		// communicate that the app is ready - used for testing
@@ -25,6 +31,31 @@
 					name: $page.data.user.fullName || '',
 				});
 			}
+
+			Sentry.init({
+				dsn: 'https://9b3cb0c95789401ea34643252fed4173@o1210217.ingest.sentry.io/6345874',
+				integrations: [new BrowserTracing()],
+				tracesSampleRate: 0.25,
+				debug: dev,
+				environment: PUBLIC_AQARATECH_ENV,
+				release: '0.0.0',
+			});
+
+			Sentry.configureScope((scope: Scope) => {
+				scope.setTag('roleType', data.user?.role?.roleType || '');
+				scope.setUser({
+					id: data.user?.id || '',
+					email: data.user?.email || '',
+					username: data.user?.fullName || '',
+					roleId: data.user?.role?.id || '',
+				});
+			});
+
+			LogRocket.getSessionURL((sessionURL) => {
+				Sentry.configureScope((scope) => {
+					scope.setExtra('sessionURL', sessionURL);
+				});
+			});
 		}
 	});
 </script>
