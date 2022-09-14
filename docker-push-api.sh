0e59368c4a0ca9e711a6d62624819006c270e409
nbe <aleisa.naser@gmail.com>
2022-09-14 23:43:42 +0300

    allow passing in version to docker push script

--- docker-push-api.sh
+++ docker-push-api.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+# allow passing a version as an argument, otherwise use the version from package.json suffixed with -dev
+
+if [ -z "$1" ]; then
+  VERSION=$(awk -F'"' '/"version": ".+"/{ print $4; exit; }' packages/site/package.json)-dev
+else
+  VERSION=$1
+fi
+
+# print the version
+echo "Version: $VERSION"
+
 # build
 docker build \
--t registry.digitalocean.com/aqtech/aqaratech-api:$(awk -F'"' '/"version": ".+"/{ print $4; exit; }' packages/backend/package.json) \
+-t registry.digitalocean.com/aqtech/aqaratech-api:$VERSION \
 -f "Dockerfile.backend" . && \
 docker push \
-registry.digitalocean.com/aqtech/aqaratech-api:$(awk -F'"' '/"version": ".+"/{ print $4; exit; }' packages/backend/package.json)
+registry.digitalocean.com/aqtech/aqaratech-api:$VERSION
