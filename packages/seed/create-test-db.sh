2097bf7c1c893b3b2f65f2bbd658dd1fe4dc347b
nbe <aleisa.naser@gmail.com>
2022-09-15 11:43:03 +0300

    pin postgres version

--- packages/seed/create-test-db.sh
+++ packages/seed/create-test-db.sh
@@ -9,13 +9,13 @@ echo "Preparing DB. DATABASE_URL is $DATABASE_URL"
 # check if the container is already running, if not, start it
 if ! docker ps | grep test-postgres; then
   echo "Starting test-postgres container"
-  docker run --rm --name test-postgres -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -d -p 5434:5432 postgres
+  docker run --rm --name test-postgres -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -d -p 5434:5432 postgres:14
 fi
 
 # poll until the server is ready
 until docker exec test-postgres pg_isready -U postgres; do sleep 1; done
 
-echo "Postgres is ready @ $DATABASE_URL. Note the port is $(echo $DATABASE_URL | awk -F ':' '{print $2}')"
+echo "Postgres is ready @ $DATABASE_URL. Note the port is $(echo $DATABASE_URL | awk -F ':' '{print $4}')"
 
 # create prisma dir if it doesn't exist
 rm -rf prisma
