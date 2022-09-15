ce3185f5faebf2fc87268af3d0fccf7628204567
nbe <aleisa.naser@gmail.com>
2022-09-15 11:31:53 +0300

    simpler test setup

--- packages/seed/create-test-db.sh
+++ packages/seed/create-test-db.sh
@@ -6,13 +6,23 @@ set -e
 
 echo "Preparing DB. DATABASE_URL is $DATABASE_URL"
 
-# create a docker container running postgresql
-docker run --rm --name test-postgres -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -d -p 5434:5432 postgres && \
+# check if the container is already running, if not, start it
+if ! docker ps | grep test-postgres; then
+  echo "Starting test-postgres container"
+  docker run --rm --name test-postgres -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -d -p 5434:5432 postgres
+fi
 
 # poll until the server is ready
-until docker exec test-postgres pg_isready -U postgres; do sleep 1; done && \
-echo "Postgres is ready @ $DATABASE_URL" && \
-mkdir prisma && \
-cp ../backend/prisma/schema.prisma ./prisma/schema.prisma && \
-cp -r ../backend/prisma/migrations ./prisma/migrations && \
+until docker exec test-postgres pg_isready -U postgres; do sleep 1; done
+
+echo "Postgres is ready @ $DATABASE_URL. Note the port is $(echo $DATABASE_URL | awk -F ':' '{print $2}')"
+
+# create prisma dir if it doesn't exist
+rm -rf prisma
+mkdir -p prisma
+
+# copy schema/migrations, overwriting any existing files
+cp ../backend/prisma/schema.prisma prisma/schema.prisma
+cp -r ../backend/prisma/migrations prisma/migrations
+
 DATABASE_URL=$DATABASE_URL npx prisma migrate reset --force
