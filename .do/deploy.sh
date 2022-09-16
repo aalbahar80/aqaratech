79c52527833d99ce6b775755ec6bcec17c7bfc4b
nbe <aleisa.naser@gmail.com>
2022-09-16 19:11:06 +0300

    get project id from env

--- .do/deploy.sh
+++ .do/deploy.sh
@@ -3,23 +3,20 @@
 # Script that creates a new deployment for a DigitalOcean App Platform app using doctl
 
 # Get app name from arguments
-# APP_ID=$1
-# DO_APP_NAME="master"
-
-DO_PROJECT_ID="95f2e61f-f483-4518-89ae-79b1d9200dd9"
+DO_APP_NAME=$1
 
 echo "Project ID: $DO_PROJECT_ID"
 
 # If the project ID is empty, exit
 if [ -z "$DO_PROJECT_ID" ]; then
-  echo "Project ID is empty"
+  echo "No project ID found"
   echo "Exiting..."
   exit 1
 fi
 
 # Check DO_APP_NAME is not empty
 if [ -z "$DO_APP_NAME" ]; then
-  echo "App name is empty"
+  echo "No app name found"
   echo "Exiting..."
   exit 1
 fi
