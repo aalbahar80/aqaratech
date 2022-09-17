0405e3fa8fd9eb25d5561274e5e82bd251e76486
nbe <aleisa.naser@gmail.com>
2022-09-18 02:19:52 +0300

    create script to export env variables for svelte

---
+++ scripts/export-env.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# A script to set & export environment variables from a .env file
+# Usage: ./export-env.sh .env
+
+# NOTE: To export into the current shell, run the following command:
+# source ./export-env.sh .env
+
+# Start
+
+ENV_FILE=$1
+
+# warn if running without source
+if [ "$BASH_SOURCE" = "$0" ]; then
+  echo "WARNING: This script should be run with 'source' to export the environment variables into the current shell."
+  echo "Example: source ./export-env.sh .env"
+fi
+
+# Check if the .env file exists
+if [ ! -f "$ENV_FILE" ]; then
+  echo "Environment file does not exist: $ENV_FILE"
+  echo "Exiting..."
+  exit 1
+fi
+
+# Set, log, and export the environment variables so they are available to subsequent commands
+echo "Setting environment variables..."
+while IFS= read -r LINE; do
+  if [[ $LINE =~ ^[A-Za-z0-9_]+=.+$ ]]; then
+    KEY=$(echo "$LINE" | cut -d '=' -f 1)
+    VALUE=$(echo "$LINE" | cut -d '=' -f 2-)
+    echo "$KEY=$VALUE"
+    export "$KEY=$VALUE"
+  fi
+done < "$ENV_FILE"
