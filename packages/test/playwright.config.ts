a7e8326de902810d3fd084772438c7b44ed12dce
nbe <aleisa.naser@gmail.com>
2022-09-18 15:18:45 +0300

    use plain node command to start site server

--- packages/test/playwright.config.ts
+++ packages/test/playwright.config.ts
@@ -41,7 +41,7 @@ const config: PlaywrightTestConfig = {
 		},
 		{
 			cwd: "../site",
-			command: "pnpm run -F @self/backend health-check && pnpm run preview",
+			command: "node build/index.js",
 			port: 3000,
 			reuseExistingServer: !process.env.CI,
 			ignoreHTTPSErrors: true,
