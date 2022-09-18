4e86def72aa864f415eea718ee1460983be16ab5
nbe <aleisa.naser@gmail.com>
2022-09-18 18:41:58 +0300

    add seed-db job

--- .do/deploy.sh
+++ .do/deploy.sh
@@ -22,7 +22,8 @@ fi
 yq -i '
   .name = strenv(DO_APP_NAME) |
   .services[0].image.tag = strenv(DO_APP_NAME) |
-  .services[1].image.tag = strenv(DO_APP_NAME)
+  .services[1].image.tag = strenv(DO_APP_NAME) |
+  .jobs[0].image.tag = strenv(DO_APP_NAME)
 ' ./.do/spec.yml
 
 # Pretty print spec.yml using yq
