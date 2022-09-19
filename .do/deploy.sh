34434dd66e3ac2d45b7f823620b915f2723f0142
nbe <aleisa.naser@gmail.com>
2022-09-20 02:06:00 +0300

    set GH_REF in spec.yml

--- .do/deploy.sh
+++ .do/deploy.sh
@@ -23,7 +23,8 @@ yq -i '
   .name = strenv(DO_APP_NAME) |
   .services[0].image.tag = strenv(DO_APP_NAME) |
   .services[1].image.tag = strenv(DO_APP_NAME) |
-  .jobs[0].image.tag = strenv(DO_APP_NAME)
+  .jobs[0].image.tag = strenv(DO_APP_NAME) |
+  .jobs[0].envs[2].value = strenv(DO_APP_NAME)
 ' ./.do/spec.yml
 
 # Pretty print spec.yml using yq
