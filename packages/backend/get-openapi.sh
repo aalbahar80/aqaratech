19ea8775a366b579073f6ee56aba7db1a129b120
nbe <aleisa.naser@gmail.com>
2022-09-20 05:53:12 +0300

    generate sdk into site/src/api

---
+++ packages/backend/get-openapi.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+JAR_FILE="openapi-generator-cli-6.1.0.jar"
+
+# check if openapi-generator-cli.jar exists in the current directory
+if [ ! -f "$JAR_FILE" ]; then
+  echo "Downloading $JAR_FILE..."
+  # download openapi-generator-cli.jar
+  # wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/6.1.0/openapi-generator-cli-6.1.0.jar -O ./openapi-generator-cli-6.1.0.jar
+  wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/6.1.0/$JAR_FILE -O ./$JAR_FILE
+else
+  echo "$JAR_FILE already exists. Skipping download..."
+fi
+
