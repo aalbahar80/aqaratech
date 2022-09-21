720f4d83e5dff950dd1aa24b0f81ef057f31b5e8
nbe <aleisa.naser@gmail.com>
2022-09-21 10:42:57 +0300

    add will_Create flag

--- .do/deploy.mjs
+++ .do/deploy.mjs
@@ -14,8 +14,10 @@ const allApps = await $`doctl apps list --format ID,Name --output json`;
 const app = JSON.parse(allApps).find((app) => app.spec.name === appName);
 
 let appId = "";
+let WILL_CREATE = false;
 let latestSpec;
 if (!app) {
+	WILL_CREATE = true;
 	console.log(chalk.red(`App ${appName} not found.`));
 	// await $`zx .do/deploy-new.mjs --app-name ${appName} --site-version ${siteVersion} --backend-version ${backendVersion}`;
 	// If app does not yet exist, use the spec.yml file as a base.
@@ -49,10 +51,14 @@ console.log(latestSpec);
 
 // deploy
 console.log(chalk.blue(`Deploying...`));
-await $`doctl apps create --upsert ${appId} --spec ${SPEC} --output json`;
 
-// If the app is being created for the first time, create-deployment will fail.
-if (appId) {
+if (WILL_CREATE) {
+	// Create new app
+	await $`doctl apps create --spec ${SPEC} --output json`;
+} else if (appId) {
+	// Update existing app
+	await $`doctl apps update ${appId} --spec ${SPEC} --output json`;
+
 	// Create a new deployment because it's the only way to "Force rebuild".
 	// Otherwise, sometimes the new image is not pulled. Could be because the image tag is the same (e.g. "latest").
 	await $`doctl apps create-deployment ${appId} --force-rebuild`;
