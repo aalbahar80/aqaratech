1aa1bb8d7b57ee9bf330daccf61ea1e188207050
nbe <aleisa.naser@gmail.com>
2022-09-21 05:47:10 +0300

    init script to deploy app

---
+++ .do/deploy.mjs
@@ -0,0 +1,52 @@
+#!/usr/bin/env zx
+
+import "zx/globals";
+
+console.log({ argv });
+
+const appName = argv["app-name"];
+const siteVersion = argv["site-version"];
+const backendVersion = argv["backend-version"];
+
+const SPEC = "app.yml";
+
+// get app id
+console.log(chalk.blue(`Getting app ${appName} info...`));
+
+const allApps = await $`doctl apps list --format ID,Name --output json`;
+const app = JSON.parse(allApps).find((app) => app.spec.name === appName);
+
+if (!app) {
+	console.log(chalk.red(`App ${appName} not found.`));
+	await $`exit 1`;
+}
+
+const appId = app.id;
+
+// console.log({ app });
+console.log(chalk.blue(`App ${appName} id: ${appId}`));
+
+// get latest spec
+console.log(chalk.blue(`Getting latest spec for app ${appName}...`));
+await $`doctl apps spec get ${appId} > ${SPEC}`;
+const latestSpec = YAML.parse(await fs.readFile(SPEC, "utf8"));
+console.log(latestSpec);
+
+// bump versions
+console.log(chalk.blue(`Bumping versions...`));
+
+latestSpec.services.forEach((service) => {
+	if (service.name === "site") {
+		service.image.tag = siteVersion;
+	} else if (service.name === "backend") {
+		service.image.tag = backendVersion;
+	}
+});
+
+// save spec
+console.log(chalk.blue(`Saving spec...`));
+await fs.writeFile(SPEC, YAML.stringify(latestSpec));
+
+// deploy
+console.log(chalk.blue(`Deploying...`));
+await $`doctl apps update ${appId} --spec ${SPEC} --output json`;
