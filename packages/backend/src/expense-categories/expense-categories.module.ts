dfc2a55b6597be73663766c4d47466d965b46240
nbe <aleisa.naser@gmail.com>
2022-09-23 22:31:01 +0300

    run format

--- packages/backend/src/expense-categories/expense-categories.module.ts
+++ packages/backend/src/expense-categories/expense-categories.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ExpenseCategoriesService } from './expense-categories.service';
 import { ExpenseCategoriesController } from './expense-categories.controller';
+import { ExpenseCategoriesService } from './expense-categories.service';
 
 @Module({
   controllers: [ExpenseCategoriesController],
-  providers: [ExpenseCategoriesService]
+  providers: [ExpenseCategoriesService],
 })
 export class ExpenseCategoriesModule {}
