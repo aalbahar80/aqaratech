9ca65d8b1fd0e07d1bf58745dfa958802a4c7ed4
nbe <aleisa.naser@gmail.com>
2022-09-27 01:02:36 +0300

    use barrel files

---
+++ packages/seed/src/factory/index.ts
@@ -0,0 +1,6 @@
+export * from './organization';
+export * from './portfolio';
+export * from './property';
+export * from './role';
+export * from './tenant';
+export * from './user';
--- packages/seed/src/factory/portfolio.ts
+++ packages/seed/src/factory/portfolio.ts
@@ -1,32 +1,21 @@
 import { faker } from '@faker-js/faker';
-import type { Property } from '@prisma/client';
+import type { Portfolio } from '@prisma/client';
 import * as Factory from 'factory.ts';
-import { areas } from '../constants';
 import { createdAt, updatedAt } from '../utils/dates';
 import { ID } from '../utils/uuid';
 
-export const propertyFactory = Factory.Sync.makeFactoryWithRequired<
-	Property,
-	'organizationId' | 'portfolioId'
+export const portfolioFactory = Factory.Sync.makeFactoryWithRequired<
+	Portfolio,
+	'organizationId'
 >({
-	area: areas[Math.floor(Math.random() * areas.length)]![1],
 	id: ID(),
 	createdAt: createdAt(),
 	updatedAt: updatedAt(),
-
+	fullName: faker.name.fullName(),
 	label: null,
-	cost: null, // rm?
-
-	// address
-	block: faker.datatype.number({ min: 1, max: 13 }).toString(),
-	street: `شارع ${faker.helpers.arrayElement([
-		faker.name.lastName(),
-		faker.datatype.number({ min: 1, max: 500 }).toString(),
-	])}`,
-	avenue: faker.address.streetName(),
-	number: faker.datatype.number({ min: 1, max: 100 }).toString(),
-	lat: +faker.address.latitude(),
-	long: +faker.address.longitude(),
-	paci: faker.datatype.number({ min: 10000000, max: 19999999 }).toString(),
-	parcel: faker.datatype.number({ min: 100, max: 999999 }).toString(),
+	civilid: faker.datatype
+		.number({ min: 200000000000, max: 399999999999 })
+		.toString(),
+	dob: faker.date.past(),
+	phone: faker.phone.number('9#######'),
 });
