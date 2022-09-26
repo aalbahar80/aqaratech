e546c2f92107038ec0da88a23dde18612b85b341
nbe <aleisa.naser@gmail.com>
2022-09-26 10:58:27 +0300

    init turbo env allowlist

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -23,8 +23,27 @@ module.exports = {
 	},
 	root: true,
 	rules: {
-		// '@next/next/no-html-link-for-pages': 'off',
-		// 'react/jsx-key': 'off',
+		// https://github.com/vercel/turborepo/blob/main/packages/eslint-plugin-turbo/docs/rules/no-undeclared-env-vars.md
+		'turbo/no-undeclared-env-vars': [
+			'error',
+			{
+				// An array of strings (or regular expressions) to exclude.
+				// NOTE: an env variable should only be excluded if it has no effect on build outputs.
+				allowList: [
+					'ORIGIN',
+					'PUBLIC_AQARATECH_ENV',
+					'PUBLIC_SITE_URL',
+					'PUBLIC_API_URL',
+					'PUBLIC_API_URL_LOCAL',
+					'PUBLIC_AQ_DEBUG_SITE',
+					'PUBLIC_AQ_DEBUG_NEST',
+					'PUBLIC_AQ_DEBUG_PRISMA',
+					'PUBLIC_AQ_DEBUG_SENTRY',
+					'PUBLIC_TRACE_RATE',
+					'MEILISEARCH_HOST',
+				],
+			},
+		],
 	},
 	ignorePatterns: ['.eslintrc.js', 'env.d.ts'],
 };
