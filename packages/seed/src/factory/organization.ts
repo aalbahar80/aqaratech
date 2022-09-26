a293141cd2e028638b5862fabd16115fe805b865
nbe <aleisa.naser@gmail.com>
2022-09-26 13:28:42 +0300

    add property factory

--- packages/seed/src/factory/organization.ts
+++ packages/seed/src/factory/organization.ts
@@ -1,6 +1,14 @@
 import { faker } from '@faker-js/faker';
-import { Organization, Role, User } from '@prisma/client';
+import type {
+	Organization,
+	Portfolio,
+	Property,
+	Role,
+	Tenant,
+	User,
+} from '@prisma/client';
 import * as Factory from 'factory.ts';
+import { areas } from '../constants';
 import { createdAt, updatedAt } from '../utils/dates';
 import { ID } from '../utils/uuid';
 
@@ -22,16 +30,83 @@ export const organizationFactory = Factory.Sync.makeFactory<Organization>({
 	planId: null,
 });
 
-export const roleFactory = Factory.Sync.makeFactory<Role>({
+export const roleFactory = Factory.Sync.makeFactoryWithRequired<
+	Role,
+	'userId' | 'organizationId'
+>({
 	id: ID(),
 	createdAt: createdAt(),
 	updatedAt: updatedAt(),
-	organizationId: ID(),
+	roleType: Factory.each(() => 'ORGADMIN'),
 	portfolioId: null,
 	tenantId: null,
-	permissions: null,
-	userId: ID(),
 	isAccepted: faker.datatype.boolean(),
 	isDefault: faker.datatype.boolean(),
-	roleType: Factory.each(() => 'ORGADMIN'),
+	permissions: null,
+});
+
+export const tenantFactory = Factory.Sync.makeFactoryWithRequired<
+	Tenant,
+	'organizationId'
+>({
+	id: ID(),
+	createdAt: createdAt(),
+	updatedAt: updatedAt(),
+	fullName: faker.name.fullName(),
+	label: null,
+	civilid: faker.datatype
+		.number({ min: 200000000000, max: 399999999999 })
+		.toString(),
+	dob: faker.date.past(),
+	phone: faker.phone.number('9#######'),
+	passportNum: faker.datatype
+		.number({ min: 100000000, max: 999999999 })
+		.toString(),
+	nationality: faker.address.countryCode('alpha-3'),
+	residencyNum: faker.datatype
+		.number({ min: 100000000, max: 999999999 })
+		.toString(),
+	residencyEnd: faker.date.future(2),
+});
+
+export const portfolioFactory = Factory.Sync.makeFactoryWithRequired<
+	Portfolio,
+	'organizationId'
+>({
+	id: ID(),
+	createdAt: createdAt(),
+	updatedAt: updatedAt(),
+	fullName: faker.name.fullName(),
+	label: null,
+	civilid: faker.datatype
+		.number({ min: 200000000000, max: 399999999999 })
+		.toString(),
+	dob: faker.date.past(),
+	phone: faker.phone.number('9#######'),
+});
+
+export const propertyFactory = Factory.Sync.makeFactoryWithRequired<
+	Property,
+	'organizationId' | 'portfolioId'
+>({
+	area: areas[Math.floor(Math.random() * areas.length)]![1],
+	id: ID(),
+	createdAt: createdAt(),
+	updatedAt: updatedAt(),
+
+	label: null,
+	cost: null, // rm?
+
+	// address
+	block: faker.datatype.number({ min: 1, max: 13 }).toString(),
+	street: `شارع ${faker.helpers.arrayElement([
+		faker.name.lastName(),
+		faker.datatype.number({ min: 1, max: 500 }).toString(),
+	])}`,
+	avenue: faker.address.streetName(),
+	number: faker.datatype.number({ min: 1, max: 100 }).toString(),
+	lat: +faker.address.latitude(),
+	long: +faker.address.longitude(),
+	paci: faker.datatype.number({ min: 10000000, max: 19999999 }).toString(),
+	parcel: faker.datatype.number({ min: 100, max: 999999 }).toString(),
 });
