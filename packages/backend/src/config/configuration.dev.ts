d92da387c65e1229f9f4c291951b773a3426561c
nbe <aleisa.naser@gmail.com>
2022-09-28 18:14:03 +0300

    get r2 and meili values from .env

--- packages/backend/src/config/configuration.dev.ts
+++ packages/backend/src/config/configuration.dev.ts
@@ -13,27 +13,22 @@ export const developmentEnvironment = (): EnvironmentConfig => ({
 		AUTH0_API_AUDIENCE: 'letand.be/api',
 	},
 	mailConfig: {
-		POSTMARK_TOKEN:
-			process.env.POSTMARK_TOKEN || 'aecd4fd3-1314-44e9-b1b5-d7dbb89fd0ca',
+		POSTMARK_TOKEN: process.env.POSTMARK_TOKEN,
 	},
 	debug: {
 		DEBUG_NEST: process.env.AQ_DEBUG_NEST == '1',
 		DEBUG_PRISMA: process.env.AQ_DEBUG_PRISMA == '1',
 	},
 	meiliSearchConfig: {
-		HOST: process.env.MEILISEARCH_HOST || 'http://localhost:7700',
-		API_KEY: process.env.MEILI_MASTER_KEY || 'MASTER_KEY',
+		HOST: process.env.MEILISEARCH_HOST,
+		API_KEY: process.env.MEILISEARCH_API_KEY,
 	},
 	siteConfig: {
-		PUBLIC_SITE_URL: process.env.PUBLIC_SITE_URL || 'http://localhost:3000',
+		PUBLIC_SITE_URL: process.env.PUBLIC_SITE_URL,
 	},
 	r2Config: {
-		R2_ACCOUNT_ID:
-			process.env.R2_ACCOUNT_ID || '8aa55983de332834393a9f4c4fbf1e35',
-		R2_ACCESS_KEY_ID:
-			process.env.R2_ACCESS_KEY_ID || '54ab96f25c44b09ab1f47d586f363ec1',
-		R2_SECRET_ACCESS_KEY:
-			process.env.R2_SECRET_ACCESS_KEY ||
-			'a37d8ba3101b0635f5aa119c9aafad6f0dfffc318948f91a54c24fcf6c4cd9bf',
+		R2_ACCOUNT_ID: process.env.R2_ACCOUNT_ID,
+		R2_ACCESS_KEY_ID: process.env.R2_ACCESS_KEY_ID,
+		R2_SECRET_ACCESS_KEY: process.env.R2_SECRET_ACCESS_KEY,
 	},
 });
