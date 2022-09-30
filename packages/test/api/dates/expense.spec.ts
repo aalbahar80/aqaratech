bacf3a750d1d8c04cbaf8a8d8d7e648764952e35
nbe <aleisa.naser@gmail.com>
2022-09-30 07:53:46 +0300

    test postAt datetime defaults

---
+++ packages/test/api/dates/expense.spec.ts
@@ -0,0 +1,27 @@
+import { expect } from '@playwright/test';
+import { expenseFactory } from '@self/seed';
+import * as R from 'remeda';
+import type { ExpenseDto } from '../../types/api';
+import { test } from '../api-config';
+
+test('postAt defaults to midnight UTC', async ({ request, portfolio }) => {
+	const expense = expenseFactory.build({
+		portfolioId: portfolio.id,
+		organizationId: portfolio.organizationId,
+		postAt: '2021-01-01',
+	});
+
+	const res = await request.post('/expenses', {
+		data: R.pick(expense, [
+			'organizationId',
+			'portfolioId',
+			'amount',
+			'postAt',
+		]),
+	});
+
+	const body = (await res.json()) as ExpenseDto;
+
+	// check postAt is midnight UTC
+	expect(body.postAt).toBe('2021-01-01T00:00:00.000Z');
+});
