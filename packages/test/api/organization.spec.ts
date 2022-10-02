3c841407b698ec19dff89503df57ad6189b8cbb1
nbe <aleisa.naser@gmail.com>
2022-10-03 01:32:27 +0300

    update flaky can get org test

--- packages/test/api/organization.spec.ts
+++ packages/test/api/organization.spec.ts
@@ -1,7 +1,6 @@
 import { expect } from '@playwright/test';
 import { organizationFactory } from '@self/seed';
 import * as R from 'remeda';
-import type { OrganizationCreatedDto } from '../types/api';
 import { test } from './api-config';
 
 test(`can create organization`, async ({ request }) => {
@@ -15,24 +14,13 @@ test(`can create organization`, async ({ request }) => {
 	expect.soft(res.status()).toBe(201);
 });
 
-test(`can get organization`, async ({ request }) => {
-	const org = organizationFactory.build();
-	const picked = R.pick(org, ['fullName', 'label']);
+test(`can get organization`, async ({ request, org }) => {
+	const res = await request.get(`/organizations/${org.organization.id}`);
 
-	const res = await request.post(`/organizations`, {
-		data: picked,
+	expect.soft(res.status()).toBe(200);
+	expect.soft(await res.json()).toMatchObject({
+		id: org.organization.id,
+		fullName: org.organization.fullName,
+		label: org.organization.label,
 	});
-
-	expect.soft(res.status()).toBe(201);
-
-	const created = (await res.json()) as OrganizationCreatedDto;
-
-	const res2 = await request.get(`/organizations/${created.organization.id}`, {
-		headers: {
-			'x-role-id': created.roleId,
-		},
-	});
-
-	expect.soft(res2.status()).toBe(200);
-	expect.soft(await res2.json()).toMatchObject(picked);
 });
