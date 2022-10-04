fc0b7cd22cf3be52027d9be7d1d42d71e2360e06
nbe <aleisa.naser@gmail.com>
2022-10-04 16:06:23 +0300

    rm todo

--- packages/site/src/routes/users/[id]/roles/+page.ts
+++ packages/site/src/routes/users/[id]/roles/+page.ts
@@ -2,7 +2,7 @@ import { api } from '$api';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch }) => {
-	// TODO: discard this load api call and use locals.user
+	// get fresh data from backend
 	const user = await api(fetch).users.findProfile();
 
 	return { user };
