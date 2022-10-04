f4c26d1e0c5d5af73241c61357e950d7c3cbc323
nbe <aleisa.naser@gmail.com>
2022-10-04 16:12:22 +0300

    use user in dropdown

--- packages/site/src/lib/components/navbar/NavbarDropdown.svelte
+++ packages/site/src/lib/components/navbar/NavbarDropdown.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { page } from '$app/stores';
 	import Dropdown from '$lib/components/buttons/Dropdown.svelte';
 	import DropdownMenu from '$lib/components/buttons/DropdownMenu.svelte';
 	import MenuItemChild from '$lib/components/buttons/MenuItemChild.svelte';
@@ -19,10 +18,10 @@
 		class="group inline-flex items-center rounded border border-transparent px-4 py-2  text-sm font-medium text-white hover:bg-gray-700"
 	>
 		<div class="pr-3">
-			{#if $page.data.user?.role}
-				<p>{$page.data.user?.role?.organization.title}</p>
+			{#if user.role}
+				<p>{user.role?.organization.title}</p>
 				<span class="font-medium text-slate-300 group-hover:text-white">
-					{$page.data.user?.role?.meta.roleLabel}
+					{user.role?.meta.roleLabel}
 				</span>
 			{:else}
 				<p>Welcome to Aqaratech</p>
