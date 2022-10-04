015ddeb2074de92a44f761cde1ad0b2c541bf046
nbe <aleisa.naser@gmail.com>
2022-10-04 08:40:09 +0300

    types

--- packages/test/api/balance.spec.ts
+++ packages/test/api/balance.spec.ts
@@ -1,5 +1,6 @@
 import { expect } from '@playwright/test';
 import { sample } from '@self/seed';
+import type { BalanceDto } from '../types/api';
 import { test } from './api-fixtures';
 
 const portfolio = sample.portfolios[0];
@@ -9,6 +10,10 @@ const payouts = sample.payouts;
 
 // TODO: other tests that add data will cause this test to fail
 test.skip(`balance`, async ({ request }) => {
+	if (!portfolio) {
+		throw new Error(`No portfolio found`);
+	}
+
 	const res = await request.get(`/portfolios/${portfolio.id}/balance`);
 
 	await expect(res).toBeOK();
@@ -20,10 +25,14 @@ test.skip(`balance`, async ({ request }) => {
 		payouts: payouts.reduce((acc, cur) => acc + cur.amount, 0),
 	};
 
-	const body = await res.json();
+	const body = (await res.json()) as BalanceDto;
+
 	expect.soft(body.leaseInvoices).toBe(sums.leaseInvoices);
+
 	expect.soft(body.expenses).toBe(sums.expenses);
+
 	expect.soft(body.payouts).toBe(sums.payouts);
+
 	expect
 		.soft(body.total)
 		.toBe(sums.leaseInvoices - sums.expenses - sums.payouts);
