2a06c76a4fda57d2aa0678d66ce4ea518c6e2310
nbe <aleisa.naser@gmail.com>
2022-10-06 01:31:05 +0300

    fix aqenv type import path

--- packages/utils/src/config/should-enable-sentry.ts
+++ packages/utils/src/config/should-enable-sentry.ts
@@ -1,4 +1,4 @@
-import type { AqaratechConfig } from './aqaratech-config';
+import type { AqaratechEnv } from '../../../../types/environment';
 
 /**
  * Only run Sentry in production/staging, or when debugging it.
@@ -8,18 +8,30 @@ export const shouldEnableSentry = ({
 	PUBLIC_AQ_DEBUG_SENTRY,
 }: Params) => {
 	if (PUBLIC_AQ_DEBUG_SENTRY == '1') {
+		console.log('Sentry is enabled because PUBLIC_AQ_DEBUG_SENTRY is set');
 		return true;
 	} else if (
 		PUBLIC_AQARATECH_ENV === 'production' ||
 		PUBLIC_AQARATECH_ENV === 'staging'
 	) {
+		console.log(
+			'Sentry is enabled because PUBLIC_AQARATECH_ENV is set to',
+			PUBLIC_AQARATECH_ENV,
+		);
 		return true;
 	} else {
+		console.log(
+			'Sentry has been disabled based on the environment variables.',
+			{
+				PUBLIC_AQARATECH_ENV,
+				PUBLIC_AQ_DEBUG_SENTRY,
+			},
+		);
 		return false;
 	}
 };
 
 type Params = Pick<
-	AqaratechConfig,
+	AqaratechEnv,
 	'PUBLIC_AQARATECH_ENV' | 'PUBLIC_AQ_DEBUG_SENTRY'
 >;
