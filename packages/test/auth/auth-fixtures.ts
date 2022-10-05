15a36dd4d84945f5294f17d827696f2eefaa2902
nbe <aleisa.naser@gmail.com>
2022-10-05 20:12:15 +0300

    use fixtures for token tests

---
+++ packages/test/auth/auth-fixtures.ts
@@ -0,0 +1,34 @@
+import { test as base } from '@playwright/test';
+
+interface Token {
+	name: string;
+	value: string;
+}
+
+export interface TokenTestOptions {
+	token: Token | undefined;
+}
+
+export const test = base.extend<TokenTestOptions>({
+	token: [undefined, { option: true }],
+	page: async ({ page, token, baseURL }, use) => {
+		const domain = baseURL ? new URL(baseURL).host : 'localhost';
+
+		// replace the idToken cookie with an expired one
+		// TODO: clear all cookies instead? If so, check access token doesn't interfere with result.
+		await page.context().addCookies([
+			{
+				name: token.name,
+				value: token.value,
+				domain,
+				path: '/',
+				expires: Date.now() / 1000 + 86400, // expires tomorrow
+				// TODO: differentiate between jwt expiry and cookie expiry. Test both.
+			},
+		]);
+
+		await use(page);
+	},
+});
+
+export { expect } from '@playwright/test';
---
+++ packages/test/auth/token/expired-jwt.spec.ts
@@ -0,0 +1,40 @@
+import { expect, test } from '../auth-fixtures';
+
+// Consider using test.use() to skip global setup login?
+// test.use({ token: { name: 'idToken', value: '123' } });
+
+test.describe('expired jwt', () => {
+	test.beforeEach(async ({ baseURL, page }) => {
+		await page.goto(baseURL!);
+	});
+
+	test('redirect to login form', async ({ page }) => {
+		// expect to be redirected to login page
+		const emailInput = page.locator('input[name="username"]');
+		const passwordInput = page.locator('input[name="password"]');
+		await expect(emailInput).toBeVisible();
+		await expect(passwordInput).toBeVisible();
+	});
+
+	test('cookies are cleared', async ({ page }) => {
+		// expect idToken and accessToken to be cleared
+		const cookies = await page.context().cookies();
+
+		const cookieNames = ['idToken', 'accessToken'];
+
+		for (const cookieName of cookieNames) {
+			const cookie = cookies.find((cookie) => cookie.name === cookieName);
+
+			expect(cookie).toBe(undefined);
+		}
+	});
+});
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+test.skip('expired cookies are handled', async () => {});
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+test.skip('expired accesstoken is refreshed', async () => {});
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+test.skip('jwt is refreshed', async () => {});
