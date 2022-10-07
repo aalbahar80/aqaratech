e097ffda81a7a0288ee20f659383782a99816824
nbe <aleisa.naser@gmail.com>
2022-10-07 18:00:19 +0300

    print names of modified files

--- packages/site/remove-slash.sh
+++ packages/site/remove-slash.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # A script that take a path as argument, then searches recursively for any file that contains the string string_decoder/ and replaces it with string_decoder:
+# More info: https://github.com/sveltejs/kit/pull/7031
 
 # Usage: ./remove-slash.sh /path/to/your/project
 
@@ -9,4 +10,9 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-find $1 -type f -exec sed -i 's/string_decoder\//string_decoder/g' {} \;
+# For each file:
+# 1. Check if it contains the string string_decoder/
+# 2. If it does, print the file name
+# 3. Replace string_decoder/ with string_decoder:
+echo "Replacing string_decoder/ with string_decoder in the following files:"
+find $1 -type f -exec grep -l 'string_decoder/' {} \; | xargs -I % /bin/bash -c 'echo %; sed -i "s/string_decoder\//string_decoder/g" %'
