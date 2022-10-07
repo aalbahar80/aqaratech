d6a81d063f5f341dbabbb062aa9cecfb142bde72
nbe <aleisa.naser@gmail.com>
2022-10-07 20:19:58 +0300

    rm security header definition

--- packages/backend/src/decorators/swagger-auth.decorator.ts
+++ packages/backend/src/decorators/swagger-auth.decorator.ts
@@ -1,11 +1,10 @@
 import { applyDecorators } from '@nestjs/common';
-import { ApiBearerAuth } from '@nestjs/swagger';
 
 /**
  * Decorator for adding auth to a swagger document.
  */
 export function SwaggerAuth() {
-	return applyDecorators(ApiBearerAuth());
+	return applyDecorators();
 	// ApiSecurity('oauth-swagger'),
 	// ApiOAuth2(['openid', 'profile', 'email'], 'oauth-swagger'),
 }
