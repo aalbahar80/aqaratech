e19976b3cc6876ec1ea69fe3a49a9f61cb17735f
nbe <aleisa.naser@gmail.com>
2022-10-07 21:54:16 +0300

    try to extract ports manually

--- packages/test/global-teardown.ts
+++ packages/test/global-teardown.ts
@@ -1,9 +1,27 @@
+import type { FullConfig } from '@playwright/test';
 import cp from 'node:child_process';
 import kill from 'tree-kill';
 
-function globalTeardown() {
-	// kill any hanging servers
-	const PORTS = [3000, 3002];
+function globalTeardown(config: FullConfig) {
+	// Kill any hanging servers
+
+	// const PORTS = [3000, 3002];
+
+	console.log('[GLOBAL-TEARDOWN] Attempting to extract ports from config');
+
+	// Grab ports from config
+	// @ts-expect-error webServers is a private property
+	// eslint-disable-next-line
+	const PORTS: number[] = config._webServers.map((ws) => ws.port);
+
+	if (!PORTS.length) {
+		console.warn(
+			'[GLOBAL-TEARDOWN] No ports found. This might be an error. Consider listing the ports manually in global-teardown.ts',
+		);
+		return;
+	}
+
+	console.log({ PORTS });
 
 	const pids: number[] = [];
 	for (const port of PORTS) {
