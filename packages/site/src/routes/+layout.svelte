22db3b036811504e2bc71599d45d0502bce58d3c
nbe <aleisa.naser@gmail.com>
2022-10-09 04:07:07 +0300

    rm manual release value from sentry.init

--- packages/site/src/routes/+layout.svelte
+++ packages/site/src/routes/+layout.svelte
@@ -38,7 +38,6 @@
 				// TODO: use environment variable to set the DSN
 				dsn: 'https://9b3cb0c95789401ea34643252fed4173@o1210217.ingest.sentry.io/6345874',
 				integrations: [new BrowserTracing()],
-				release: `site-${__AQARATECH_APP_VERSION__}`,
 			});
 
 			Sentry.configureScope((scope) => {
