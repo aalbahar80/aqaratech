43b074ee9fed9442bb456af8e334cac6a1b59e81
nbe <aleisa.naser@gmail.com>
2022-10-10 11:51:30 +0300

    kill process/server only if it was started by playwright

--- packages/test/global-teardown.ts
+++ packages/test/global-teardown.ts
@@ -2,7 +2,7 @@ import type { FullConfig } from '@playwright/test';
 import cp from 'node:child_process';
 import kill from 'tree-kill';
 
-function globalTeardown(config: FullConfig) {
+async function globalTeardown(config: FullConfig) {
 	// Kill any hanging servers
 
 	// const PORTS = [3000, 3002];
@@ -20,8 +20,6 @@ function globalTeardown(config: FullConfig) {
 		);
 		return;
 	}
-
-	console.log({ PORTS });
 
 	const pids: number[] = [];
 	for (const port of PORTS) {
@@ -41,9 +39,46 @@ function globalTeardown(config: FullConfig) {
 	console.log(`Preparing to kill ${pids.length} processes`);
 
 	for (const pid of pids) {
-		console.log(`Killing process ${pid}`);
-		kill(pid);
+		if (wasProcessStartedByPlaywright(pid)) {
+			console.log(`Killing process ${pid}`);
+			kill(pid);
+		}
+	}
+
+	// wait for the processes to die
+	await new Promise((resolve) => setTimeout(resolve, 100));
+
+	for (const port of PORTS) {
+		try {
+			const status = cp.execSync(`lsof -i :${port}`).toString().trim();
+			console.log(status);
+			console.warn(`Port ${port} is still in use`);
+		} catch (error) {
+			console.log(`Port ${port} is free`);
+		}
 	}
 }
 
 export default globalTeardown;
+
+const wasProcessStartedByPlaywright = (pid: number): boolean => {
+	// use `ps -axwo pid,stat | grep ${pid}` to get the process status
+	// if it is not running in the foreground, it was started by playwright
+
+	const processStatus = cp
+		.execSync(`ps -axwo pid,stat | grep ${pid}`)
+		.toString()
+		.trim();
+
+	console.log({ processStatus }, 'global-teardown.ts ~ 56');
+
+	const inBackground = processStatus.includes('<');
+
+	if (inBackground) {
+		console.log(`Process ${pid} was started by Playwright`);
+		return true;
+	} else {
+		console.log(`Process ${pid} was NOT started by Playwright`);
+		return false;
+	}
+};
