ac2d14389e8ac9271821adc117f14b8fde079132
nbe <aleisa.naser@gmail.com>
2022-10-11 20:51:55 +0300

    extract prisma exception handling

--- packages/backend/src/interceptors/error.interceptor.ts
+++ packages/backend/src/interceptors/error.interceptor.ts
@@ -1,21 +1,18 @@
 import { ForbiddenError } from '@casl/ability';
 import {
 	BadGatewayException,
-	BadRequestException,
 	CallHandler,
-	ConflictException,
 	ExecutionContext,
 	ForbiddenException,
-	HttpException,
 	Injectable,
 	Logger,
 	NestInterceptor,
-	NotFoundException,
 } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { Request } from 'express';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { mapPrismaException } from 'src/prisma/prisma-exception-mapper';
 import { UserDto } from 'src/users/dto/user.dto';
 
 /**
@@ -32,13 +29,6 @@ import { UserDto } from 'src/users/dto/user.dto';
 export class ErrorsInterceptor implements NestInterceptor {
 	private readonly logger = new Logger(ErrorsInterceptor.name);
 
-	private errorCodesStatusMapping: ErrorCodesStatusMapping = {
-		P2000: BadRequestException,
-		P2002: ConflictException,
-		P2014: BadRequestException,
-		P2025: NotFoundException,
-	};
-
 	intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
 		const request = context.switchToHttp().getRequest<Request>();
 		const user = request.user as UserDto;
@@ -54,10 +44,11 @@ export class ErrorsInterceptor implements NestInterceptor {
 						);
 
 						return new ForbiddenException();
-					} else if (err instanceof Prisma.NotFoundError) {
-						return new NotFoundException();
-					} else if (err instanceof Prisma.PrismaClientKnownRequestError) {
-						return new this.errorCodesStatusMapping[err.code]();
+					} else if (
+						err instanceof Prisma.NotFoundError ||
+						err instanceof Prisma.PrismaClientKnownRequestError
+					) {
+						return mapPrismaException(err);
 					} else {
 						// return new InternalServerErrorException(); // or InternalServerErrorException
 						return new BadGatewayException(); // or InternalServerErrorException
@@ -67,7 +58,3 @@ export class ErrorsInterceptor implements NestInterceptor {
 		);
 	}
 }
-
-export type ErrorCodesStatusMapping = {
-	[key: string]: new () => HttpException;
-};
---
+++ packages/backend/src/prisma/prisma-exception-mapper.ts
@@ -0,0 +1,66 @@
+import {
+	BadRequestException,
+	ConflictException,
+	HttpException,
+	InternalServerErrorException,
+	Logger,
+	NotFoundException,
+} from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+
+// TODO: review error messages
+export const mapPrismaException = (
+	exception: Prisma.PrismaClientKnownRequestError | Prisma.NotFoundError,
+): HttpException => {
+	let responseError: HttpException;
+
+	if (exception instanceof Prisma.NotFoundError) {
+		responseError = new NotFoundException();
+	} else if (exception.code === 'P2025') {
+		/**
+		 * P2025
+		 * An operation failed because it depends on one or more records that were required but not found. {cause}
+		 * https://www.prisma.io/docs/reference/api-reference/error-reference#p2025
+		 *
+		 */
+		// TODO: should be a 404 instead of a 400?
+		responseError = new BadRequestException(exception.meta?.cause);
+	} else if (exception.code === 'P2014') {
+		/**
+		 * P2014
+		 * The change you are trying to make would violate the required relation 'LeaseToTenant' between the `Lease` and `Tenant` models.
+		 * https://www.prisma.io/docs/reference/api-reference/error-reference#p2025
+		 *
+		 */
+		// TODO should we add a fallback error message?
+		const modela = exception.meta?.model_a_name;
+		const modelb = exception.meta?.model_b_name;
+		const message = `Sorry, but you can't delete a ${modelb} with an existing ${modela}. Please delete the ${modela} first.`;
+		responseError = new BadRequestException(message);
+	} else if (exception.code === 'P2000') {
+		/**
+		 * P2000
+		 * The provided value for the column is too long for the column's type. Column: {column_name}
+		 *
+		 * https://www.prisma.io/docs/reference/api-reference/error-reference#p2000
+		 */
+		responseError = new BadRequestException();
+	} else if (exception.code === 'P2002') {
+		/**
+		 * P2002
+		 * Unique constraint failed on the {constraint}
+		 *
+		 * https://www.prisma.io/docs/reference/api-reference/error-reference#p2002
+		 *
+		 */
+		responseError = new ConflictException();
+	} else {
+		// TODO return error message?
+		// TODO Test minimial error message (auto set in prod by Prisma)
+		Logger.warn('Potentially unhandled Prisma error:', 'PrismaExceptionMapper');
+		Logger.warn(exception, 'PrismaExceptionMapper');
+		responseError = new InternalServerErrorException();
+	}
+
+	return responseError;
+};
