cf47acb8ea2761e201a489c383f6385dc0ae104d
nbe <aleisa.naser@gmail.com>
2022-10-13 18:37:47 +0300

    hide file actions unless admin role

---
+++ .changeset/serious-books-flash.md
@@ -0,0 +1,5 @@
+---
+'@self/site': patch
+---
+
+conditionally show file actions dropdown
--- packages/site/src/lib/components/file/FileList.svelte.svelte
+++ packages/site/src/lib/components/file/FileList.svelte.svelte
@@ -37,6 +37,8 @@
 			addErrorToast('Failed to load file list.');
 		}
 	});
+
+	$: hideFileActions = $page.data.user?.role?.roleType !== 'ORGADMIN';
 </script>
 
 {#if files?.results.length}
@@ -70,13 +72,16 @@
 									window.open(url);
 								}}
 								class="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-8 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
+								class:rounded-md={hideFileActions}
 							>
 								View
 							</button>
 						</div>
 						<div slot="button">
 							<!-- Rename to HybridButtonChevron  -->
-							<HybridButton />
+							{#if !hideFileActions}
+								<HybridButton />
+							{/if}
 						</div>
 						<div slot="menu">
 							<DropdownMenu>
