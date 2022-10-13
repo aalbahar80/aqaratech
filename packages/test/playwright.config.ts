1bf647158af710fce6008c0b1fbc8656332a1da6
nbe <aleisa.naser@gmail.com>
2022-10-13 18:53:34 +0300

    change e2e test task to a root pipeline in tubo.json

--- packages/test/playwright.config.ts
+++ packages/test/playwright.config.ts
@@ -14,10 +14,8 @@ const API_FILES = '**/api/**/*.spec.ts';
 
 const config: PlaywrightTestConfig<TokenTestOptions> = {
 	globalSetup: require.resolve('./global-setup'),
-	reporter: [
-		['list'],
-		['html', { open: process.env.CI ? 'never' : 'on-failure' }],
-	],
+	// showing the reporter prevents turbo from caching the test results (on flakey tests)
+	reporter: [['list'], ['html', { open: process.env.CI ? 'never' : 'never' }]],
 	retries: 2,
 	timeout: process.env.CI ? 30 * 1000 : 5 * 1000,
 	maxFailures: 20,
