9bf4ce6987f75892e5df19cbf1501a7ec0060d50
nbe <aleisa.naser@gmail.com>
2022-10-13 12:33:15 +0300

    add notes to script, remove filter

--- scripts/vscode-task-mapper.sh
+++ scripts/vscode-task-mapper.sh
@@ -4,10 +4,19 @@
 # This scripts maps the file names to the monorepo root directory
 # so that VSCode can properly find the files in the `problems` tab
 
+# Helpful:
+# .vscode/tasks.json
+# https://regex101.com/r/b0olch/1
+# https://regex101.com/r/Ad250Y/1
+
+# TODO:
+# map /site which uses svelte-chech (use the machine readable output option)
+# map root directory 
+
 # Groups
 # 1: @self/
 # 2: package name
 # 3: :check:
 # 4: relative path
 # 5: rest of the line
-pnpm run check:all --filter !\\@self/site | sed -E 's/^(@self\/)(.*)(.*:check: )(.*)(\(.*)$/\1\2\3packages\/\2\/\4\5/g'
+pnpm run check:all | sed -E 's/^(@self\/)(.*)(.*:check: )(.*)(\(.*)$/\1\2\3packages\/\2\/\4\5/g'
