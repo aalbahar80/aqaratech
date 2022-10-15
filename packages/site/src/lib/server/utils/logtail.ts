aac23bb1a4a0edf6f764b8ce33ec651953402b76
nbe <aleisa.naser@gmail.com>
2022-10-15 09:07:24 +0300

    add env to logtail

--- packages/site/src/lib/server/utils/logtail.ts
+++ packages/site/src/lib/server/utils/logtail.ts
@@ -1,8 +1,25 @@
 import { environment } from '$aqenvironment';
 import { Logtail } from '@logtail/node';
+import type { Middleware } from '@logtail/types';
 
 export const logtail = new Logtail('qsYDfRpM2TXqyGeYhUNFReag');
 
+/*
+ * Middleware to add `environment` info to logtail logs.
+ */
+// eslint-disable-next-line @typescript-eslint/require-await
+export const enrichLogs: Middleware = async (log) => {
+	return {
+		...log,
+		environment: environment.PUBLIC_AQARATECH_ENV,
+	};
+};
+
+logtail.use(enrichLogs);
+
+/**
+ * Only enable logtail in production & staging.
+ */
 const shouldEnableLogtail = () => {
 	const liveEnvs = ['production', 'staging'];
 
