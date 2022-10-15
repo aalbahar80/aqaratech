fe2c5349ed895a8d5fe6b6a0cb6e98620b06f133
nbe <aleisa.naser@gmail.com>
2022-10-15 13:28:03 +0300

    remove pruned dirs before rebuilding compose

--- scripts/build-dockers.sh
+++ scripts/build-dockers.sh
@@ -1,3 +1,5 @@
+rm -rf ./packages/backend/pruned && \
+rm -rf ./packages/site/pruned && \
 pnpm build && \
 pnpm -F @self/backend --prod deploy ./packages/backend/pruned && \
 pnpm -F @self/site --prod deploy ./packages/site/pruned && \
