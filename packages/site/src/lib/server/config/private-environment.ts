5c057e0262b58d567d6ceb8da17948f9fb1bda97
nbe <aleisa.naser@gmail.com>
2022-10-15 12:39:59 +0300

    display path if env  var validation fails

--- packages/site/src/lib/server/config/private-environment.ts
+++ packages/site/src/lib/server/config/private-environment.ts
@@ -2,9 +2,9 @@ import { env } from '$env/dynamic/private';
 import { isProd } from '$lib/server/config/is-production';
 import { z } from 'zod';
 
-export const privateEnvironment = {
-	AUTH0_CLIENT_SECRET: z.string().parse(env.AUTH0_CLIENT_SECRET),
-	LOGTAIL_TOKEN: isProd
-		? z.string().parse(env.LOGTAIL_TOKEN)
-		: z.string().optional().parse(env.LOGTAIL_TOKEN),
-};
+const schema = z.object({
+	AUTH0_CLIENT_SECRET: z.string(),
+	LOGTAIL_TOKEN: isProd ? z.string() : z.string().optional(),
+});
+
+export const privateEnvironment = schema.parse(env);
