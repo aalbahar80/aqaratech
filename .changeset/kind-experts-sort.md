479be7f74074cc5b56e8cb736f79c66f1dbdcdb5
nbe <aleisa.naser@gmail.com>
2022-10-15 14:26:22 +0300

    enrich client-side sentry event

---
+++ .changeset/kind-experts-sort.md
@@ -0,0 +1,5 @@
+---
+'@self/site': patch
+---
+
+enrich client site sentry events
--- packages/site/src/hooks.client.ts
+++ packages/site/src/hooks.client.ts
@@ -2,17 +2,49 @@ import { ResponseError } from '$api/openapi';
 import * as Sentry from '@sentry/svelte';
 import type { HandleClientError } from '@sveltejs/kit';
 
-export const handleError: HandleClientError = ({ error }) => {
+export const handleError: HandleClientError = ({ error, event }) => {
 	console.log({ error });
 
-	Sentry.captureException(error);
-
 	if (error instanceof ResponseError) {
+		Sentry.captureEvent(
+			{
+				level: 'error',
+				message: error.message,
+				tags: {
+					status: error.response.status,
+					routeId: event.routeId,
+					pathname: event.url.pathname,
+				},
+				request: {
+					url: event.url.href,
+					query_string: event.url.search,
+				},
+			},
+			{
+				originalException: error,
+			},
+		);
+
 		return {
 			message: error.response.statusText || 'An error occurred',
 			status: error.response.status,
 		};
-	}
+	} else {
+		Sentry.captureException(error, {
+			tags: {
+				routeId: event.routeId,
+				pathname: event.url.pathname,
+			},
+			extra: {
+				href: event.url.href,
+				params: event.params,
+				query_string: event.url.search,
+			},
+			requestSession: {
+				status: 'errored',
+			},
+		});
 
-	return;
+		return;
+	}
 };
