809d75a0813073522a654c21134a970f177c7860
nbe <aleisa.naser@gmail.com>
2022-10-16 23:55:42 +0300

    rm unused app service methods

--- packages/backend/src/app.service.ts
+++ packages/backend/src/app.service.ts
@@ -1,14 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AppService {
-	constructor(private readonly prisma: PrismaService) {}
-
-	getHello(): string {
-		return 'Hello World!';
-	}
-
 	async getMetrics() {
 		// return this.prisma.$metrics.prometheus();
 	}
