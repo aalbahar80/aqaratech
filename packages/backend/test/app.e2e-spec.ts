16b897452687b4adf1dc7a352e65a7db2ffd9525
nbe <aleisa.naser@gmail.com>
2022-10-17 07:55:43 +0300

    fix errors in sample test files

--- packages/backend/test/app.e2e-spec.ts
+++ packages/backend/test/app.e2e-spec.ts
@@ -1,5 +1,5 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
@@ -23,6 +23,8 @@ describe('AppController (e2e)', () => {
 	// });
 
 	it('/ (GET)', () => {
+		// @ts-expect-error not using supertest
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
 		return request(app.getHttpServer()).get('/').expect(401);
 		// .expect('Hello World!');
 	});
