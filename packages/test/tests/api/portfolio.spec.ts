19948dbe6eccd58659a1db956ce39963bfa22603
nbe <aleisa.naser@gmail.com>
2022-10-22 05:06:41 +0300

    update portfolio tests

--- packages/test/tests/api/portfolio.spec.ts
+++ packages/test/tests/api/portfolio.spec.ts
@@ -4,35 +4,19 @@ import * as R from 'remeda';
 import type { PortfolioDto } from '../../types/api';
 import { test } from './api-fixtures';
 
-test(`can't be created without orgId`, async ({ request }) => {
-	const portfolio = R.pick(
-		portfolioFactory.build({
-			organizationId: '',
-		}),
-		['fullName'],
-	);
-
-	const res = await request.post(`/portfolios`, {
-		data: portfolio,
-	});
-
-	await expect(res).not.toBeOK();
-
-	expect(res.status()).toBe(400);
-});
-
 test(`can't be created without fullName`, async ({ request, org }) => {
 	const portfolio = R.pick(
 		portfolioFactory.build({
 			organizationId: org.organization.id,
 			label: '',
 		}),
-		['organizationId', 'label'],
+		['label'],
 	);
 
-	const res = await request.post(`/portfolios`, {
-		data: portfolio,
-	});
+	const res = await request.post(
+		`/organizations/${org.organization.id}/portfolios`,
+		{ data: portfolio },
+	);
 
 	await expect(res).not.toBeOK();
 
@@ -44,12 +28,13 @@ test(`can be created with minimal fields`, async ({ request, org }) => {
 		portfolioFactory.build({
 			organizationId: org.organization.id,
 		}),
-		['organizationId', 'fullName'],
+		['fullName'],
 	);
 
-	const res = await request.post(`/portfolios`, {
-		data: portfolio,
-	});
+	const res = await request.post(
+		`/organizations/${org.organization.id}/portfolios`,
+		{ data: portfolio },
+	);
 
 	await expect(res).toBeOK();
 
@@ -59,7 +44,6 @@ test(`can be created with minimal fields`, async ({ request, org }) => {
 test(`can update fullName only`, async ({ request, portfolio }) => {
 	const res = await request.patch(`/portfolios/${portfolio.id}`, {
 		data: {
-			organizationId: portfolio.organizationId,
 			fullName: 'new full name',
 		},
 	});
@@ -74,7 +58,6 @@ test(`can update fullName only`, async ({ request, portfolio }) => {
 test(`can update single field only`, async ({ request, portfolio }) => {
 	const res = await request.patch(`/portfolios/${portfolio.id}`, {
 		data: {
-			organizationId: portfolio.organizationId,
 			label: 'new label',
 		},
 	});
@@ -95,4 +78,29 @@ test(`returns title field`, async ({ request, portfolio }) => {
 	expect.soft(data).toHaveProperty('fullName');
 
 	expect(data).toHaveProperty('title');
+});
+
+test(`fullName is trimmed`, async ({ request, org }) => {
+	const portfolio = R.pick(
+		portfolioFactory.build({
+			organizationId: org.organization.id,
+		}),
+		['fullName'],
+	);
+
+	const res = await request.post(
+		`/organizations/${org.organization.id}/portfolios`,
+		{
+			data: {
+				...portfolio,
+				fullName: ` ${portfolio.fullName} `,
+			},
+		},
+	);
+
+	const data = (await res.json()) as PortfolioDto;
+
+	expect.soft(data).toHaveProperty('fullName');
+
+	expect(data.fullName).toBe(portfolio.fullName.trim());
 });
