53ecb857bdbc35d88a220b5b3cfb98426cb7d566
nbe <aleisa.naser@gmail.com>
2022-10-24 09:51:17 +0300

    rm unused transformers

--- packages/utils/src/schemas/utils/zod-transformers.ts
+++ packages/utils/src/schemas/utils/zod-transformers.ts
@@ -1,16 +1,3 @@
-import { isNumber } from 'remeda';
-
-export function trim<T>(v: T) {
-	if (typeof v === 'string') {
-		return v.trim();
-	}
-	return v;
-}
-
-export function falsyToNullExceptZero<T>(v: T) {
-	return isNumber(v) ? v : null;
-}
-
 export function digitsOnly(v: string) {
 	return v.match(/^[0-9]+$/) !== null;
 }
