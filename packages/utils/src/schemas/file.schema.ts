5aa502b459d62a9aff18ba31ee5cc722689e1002
nbe <aleisa.naser@gmail.com>
2022-10-24 04:56:57 +0300

    cast file to File type

--- packages/utils/src/schemas/file.schema.ts
+++ packages/utils/src/schemas/file.schema.ts
@@ -7,7 +7,7 @@ export const fileCreateSchema = z
 	.object({
 		organizationId: isID,
 		fileName: z.string().min(1, { message: 'Required' }).transform(trim),
-		file: z.record(z.any()),
+		file: z.record(z.any()).transform((value) => value as File),
 
 		relationKey: fileRelationKeySchema,
 		relationValue: isID,
