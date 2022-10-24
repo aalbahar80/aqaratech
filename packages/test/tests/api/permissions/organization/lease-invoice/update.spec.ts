c53eee5b3a91e76b58cda7c8e437925a49d6f8d1
nbe <aleisa.naser@gmail.com>
2022-10-24 16:10:55 +0300

    init invoice update tests

---
+++ packages/test/tests/api/permissions/organization/lease-invoice/update.spec.ts
@@ -0,0 +1,48 @@
+import { expect } from '@playwright/test';
+import { leaseInvoiceFactory, sample } from '@self/seed';
+import { randomUUID } from 'crypto';
+import * as R from 'remeda';
+import { test } from '../../../api-fixtures';
+
+const newLeaseInvoice = R.pick(
+	leaseInvoiceFactory.build({
+		organizationId: '',
+		portfolioId: '',
+		leaseId: '',
+	}),
+	['amount', 'postAt', 'memo'],
+);
+
+test('can update leaseInvoice in own org', async ({ request, invoice }) => {
+	const res = await request.patch(`/leaseInvoices/${invoice.id}`, {
+		data: newLeaseInvoice,
+	});
+
+	expect(res.status()).toBe(200);
+});
+
+test('cannot update leaseInvoice in another org', async ({
+	request,
+	org: _org,
+}) => {
+	const res = await request.patch(
+		`/leaseInvoices/${sample.leaseInvoices[0]!.id}`,
+		{
+			data: newLeaseInvoice,
+		},
+	);
+
+	await expect.soft(res).not.toBeOK();
+
+	expect(res.status()).toBe(400);
+});
+
+test('cannot update non-existing leaseInvoice', async ({ request }) => {
+	const res = await request.patch(`/leaseInvoices/${randomUUID()}`, {
+		data: newLeaseInvoice,
+	});
+
+	await expect.soft(res).not.toBeOK();
+
+	expect(res.status()).toBe(400);
+});
