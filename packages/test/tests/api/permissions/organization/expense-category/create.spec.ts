414f0934ea6727284c949d6e85378a138f276011
nbe <aleisa.naser@gmail.com>
2022-10-26 21:36:10 +0300

    test updating expense category

--- packages/test/tests/api/permissions/organization/expense-category/create.spec.ts
+++ packages/test/tests/api/permissions/organization/expense-category/create.spec.ts
@@ -82,6 +82,8 @@ test('cannot create expenseCategory with invalid isGroup', async ({
 	expect(res.status()).toBe(400);
 });
 
+// LabelEn
+
 const invalidLabelEn = [null, undefined, 1, true, false, [], {}, ''];
 
 for (const [index, labelEn] of invalidLabelEn.entries()) {
@@ -105,7 +107,30 @@ for (const [index, labelEn] of invalidLabelEn.entries()) {
 
 		expect(await res.json()).toHaveProperty('fieldErrors.labelEn');
 	});
+
+	test(`cannot update expenseCategory with invalid labelEn ${index}: ${labelEn}`, async ({
+		request,
+		org,
+		expenseCategory,
+	}) => {
+		const url = PostUrl(org.organization.id).expenseCategory;
+
+		const res = await request.patch(`${url}/${expenseCategory.id}`, {
+			data: {
+				...R.pick(expenseCategory, ['isGroup']),
+				labelEn,
+			},
+		});
+
+		await expect.soft(res).not.toBeOK();
+
+		expect(res.status()).toBe(400);
+
+		expect(await res.json()).toHaveProperty('fieldErrors.labelEn');
+	});
 }
+
+// LabelAr
 
 const invalidLabelAr = [1, true, false, [], {}];
 
@@ -130,7 +155,30 @@ for (const [index, labelAr] of invalidLabelAr.entries()) {
 
 		expect(await res.json()).toHaveProperty('fieldErrors.labelAr');
 	});
+
+	test(`cannot update expenseCategory with invalid labelAr ${index}: ${labelAr}`, async ({
+		request,
+		org,
+		expenseCategory,
+	}) => {
+		const url = PostUrl(org.organization.id).expenseCategory;
+
+		const res = await request.patch(`${url}/${expenseCategory.id}`, {
+			data: {
+				...R.pick(expenseCategory, ['labelEn', 'isGroup']),
+				labelAr,
+			},
+		});
+
+		await expect.soft(res).not.toBeOK();
+
+		expect(res.status()).toBe(400);
+
+		expect(await res.json()).toHaveProperty('fieldErrors.labelAr');
+	});
 }
+
+// isGroup
 
 const invalidIsGroup = [null, undefined, 1, [], {}, '', 'invalid'];
 
@@ -154,5 +202,33 @@ for (const [index, isGroup] of invalidIsGroup.entries()) {
 		expect(res.status()).toBe(400);
 
 		expect(await res.json()).toHaveProperty('fieldErrors.isGroup');
+	});
+}
+
+const invalidIsGroupUpdate = [null, 1, [], {}, '', 'invalid'];
+
+for (const [index, isGroup] of invalidIsGroupUpdate.entries()) {
+	// eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+	test(`cannot update expenseCategory.isGroup ${index}: ${isGroup}`, async ({
+		request,
+		org,
+		expenseCategory,
+	}) => {
+		const url = PostUrl(org.organization.id).expenseCategory;
+
+		const res = await request.patch(`${url}/${expenseCategory.id}`, {
+			data: {
+				...R.pick(expenseCategory, ['labelEn']),
+				isGroup,
+			},
+		});
+
+		await expect.soft(res).not.toBeOK();
+
+		expect(res.status()).toBe(400);
+
+		expect(await res.json()).toHaveProperty('formErrors', [
+			"Unrecognized key(s) in object: 'isGroup'",
+		]);
 	});
 }
