02c7ae7c7eace158c2301ee070f1dea7e58ef284
nbe <aleisa.naser@gmail.com>
2022-10-27 08:30:58 +0300

    test other POST role endpoints

--- packages/test/tests/api/permissions/organization/role/create.spec.ts
+++ packages/test/tests/api/permissions/organization/role/create.spec.ts
@@ -4,54 +4,102 @@ import { sample } from '@self/seed';
 import { randomUUID } from 'crypto';
 import { test } from '../../../api-fixtures';
 
-test('can create role in own org', async ({ request, org }) => {
-	const email = faker.internet.email();
-
-	const res = await request.post(
-		`/organizations/${org.organization.id}/roles`,
-		{ data: { email } },
-	);
-
-	expect(res.status()).toBe(201);
+const PostUrl = ({
+	organizationId,
+	portfolioId,
+	tenantId,
+}: {
+	organizationId: string;
+	portfolioId: string;
+	tenantId: string;
+}) => ({
+	orgadmin: `/organizations/${organizationId}/roles`,
+	portfolio: `/organizations/${organizationId}/portfolios/${portfolioId}/roles`,
+	tenant: `/organizations/${organizationId}/tenants/${tenantId}/roles`,
 });
 
-test('cannot create role in another org', async ({ request, org: _org }) => {
-	const email = faker.internet.email();
+const roleTypes = ['orgadmin', 'portfolio', 'tenant'] as const;
 
-	const res = await request.post(
-		`/organizations/${sample.organizations[0]!.id}/roles`,
-		{ data: { email } },
-	);
+for (const roleType of roleTypes) {
+	test(`can create role in own org: ${roleType}`, async ({
+		request,
+		org,
+		portfolio,
+		tenant,
+	}) => {
+		const email = faker.internet.email();
 
-	await expect.soft(res).not.toBeOK();
+		const url = PostUrl({
+			organizationId: org.organization.id,
+			portfolioId: portfolio.id,
+			tenantId: tenant.id,
+		})[roleType];
 
-	expect(res.status()).toBe(403);
-});
+		const res = await request.post(url, { data: { email } });
 
-test('cannot create role in non-existing org', async ({
-	request,
-	org: _org,
-}) => {
-	const email = faker.internet.email();
-
-	const res = await request.post(`/organizations/${randomUUID()}/roles`, {
-		data: { email },
+		expect(res.status()).toBe(201);
 	});
 
-	await expect.soft(res).not.toBeOK();
+	test(`cannot create role in another org ${roleType}`, async ({
+		request,
+		org: _org,
+		portfolio,
+		tenant,
+	}) => {
+		const email = faker.internet.email();
 
-	expect(res.status()).toBe(403);
-});
+		const url = PostUrl({
+			organizationId: sample.organizations[0]!.id,
+			portfolioId: portfolio.id,
+			tenantId: tenant.id,
+		})[roleType];
 
-test('cannot create role with invalid email', async ({ request, org }) => {
-	const res = await request.post(
-		`/organizations/${org.organization.id}/roles`,
-		{ data: { email: 'invalid-email' } },
-	);
+		const res = await request.post(url, { data: { email } });
 
-	await expect.soft(res).not.toBeOK();
+		await expect.soft(res).not.toBeOK();
 
-	expect(res.status()).toBe(400);
+		expect(res.status()).toBe(403);
+	});
 
-	expect(await res.json()).toHaveProperty('fieldErrors.email');
-});
+	test(`cannot create role in non-existing org ${roleType}`, async ({
+		request,
+		org: _org,
+		portfolio,
+		tenant,
+	}) => {
+		const email = faker.internet.email();
+
+		const url = PostUrl({
+			organizationId: randomUUID(),
+			portfolioId: portfolio.id,
+			tenantId: tenant.id,
+		})[roleType];
+
+		const res = await request.post(url, { data: { email } });
+
+		await expect.soft(res).not.toBeOK();
+
+		expect(res.status()).toBe(403);
+	});
+
+	test(`cannot create role with invalid email ${roleType}`, async ({
+		request,
+		org,
+		portfolio,
+		tenant,
+	}) => {
+		const url = PostUrl({
+			organizationId: org.organization.id,
+			portfolioId: portfolio.id,
+			tenantId: tenant.id,
+		})[roleType];
+
+		const res = await request.post(url, { data: { email: 'invalid-email' } });
+
+		await expect.soft(res).not.toBeOK();
+
+		expect(res.status()).toBe(400);
+
+		expect(await res.json()).toHaveProperty('fieldErrors.email');
+	});
+}
