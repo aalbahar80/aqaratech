b8a87b07e530f6ae7c5cab70effe1388fb882f2a
nbe <aleisa.naser@gmail.com>
2022-10-27 08:19:58 +0300

    use faker for emails

--- packages/test/tests/api/permissions/organization/role/create.spec.ts
+++ packages/test/tests/api/permissions/organization/role/create.spec.ts
@@ -1,36 +1,26 @@
+import { faker } from '@faker-js/faker';
 import { expect } from '@playwright/test';
-import { roleFactory, sample } from '@self/seed';
+import { sample } from '@self/seed';
 import { randomUUID } from 'crypto';
-import * as R from 'remeda';
 import { test } from '../../../api-fixtures';
 
 test('can create role in own org', async ({ request, org }) => {
-	const role = R.pick(
-		roleFactory.build({
-			organizationId: org.organization.id,
-		}),
-		['email'],
-	);
+	const email = faker.internet.email();
 
 	const res = await request.post(
 		`/organizations/${org.organization.id}/roles`,
-		{ data: role },
+		{ data: { email } },
 	);
 
 	expect(res.status()).toBe(201);
 });
 
-test('cannot create role in another org', async ({ request, org }) => {
-	const role = R.pick(
-		roleFactory.build({
-			organizationId: org.organization.id,
-		}),
-		['email'],
-	);
+test('cannot create role in another org', async ({ request, org: _org }) => {
+	const email = faker.internet.email();
 
 	const res = await request.post(
 		`/organizations/${sample.organizations[0]!.id}/roles`,
-		{ data: role },
+		{ data: { email } },
 	);
 
 	await expect.soft(res).not.toBeOK();
@@ -38,16 +28,14 @@ test('cannot create role in another org', async ({ request, org }) => {
 	expect(res.status()).toBe(403);
 });
 
-test('cannot create role in non-existing org', async ({ request, org }) => {
-	const role = R.pick(
-		roleFactory.build({
-			organizationId: org.organization.id,
-		}),
-		['email'],
-	);
+test('cannot create role in non-existing org', async ({
+	request,
+	org: _org,
+}) => {
+	const email = faker.internet.email();
 
 	const res = await request.post(`/organizations/${randomUUID()}/roles`, {
-		data: role,
+		data: { email },
 	});
 
 	await expect.soft(res).not.toBeOK();
@@ -56,17 +44,9 @@ test('cannot create role in non-existing org', async ({ request, org }) => {
 });
 
 test('cannot create role with invalid email', async ({ request, org }) => {
-	const role = R.pick(
-		roleFactory.build({
-			organizationId: org.organization.id,
-			email: 'invalid-email',
-		}),
-		['email'],
-	);
-
 	const res = await request.post(
 		`/organizations/${org.organization.id}/roles`,
-		{ data: role },
+		{ data: { email: 'invalid-email' } },
 	);
 
 	await expect.soft(res).not.toBeOK();
