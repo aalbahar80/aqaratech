007485dadde089bc5ca0050091826f7c812f1e0a
nbe <aleisa.naser@gmail.com>
2022-10-29 16:58:20 +0300

    decrease hover duration

--- packages/site/src/lib/components/sidebar/SidebarItem.svelte
+++ packages/site/src/lib/components/sidebar/SidebarItem.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-	class="mt-5 flex transform items-center rounded-md px-4 py-2 text-gray-600 transition-colors duration-300 hover:bg-gray-200 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-200"
+	class="mt-5 flex transform items-center rounded-md px-4 py-2 text-gray-600 transition-colors duration-100 hover:bg-gray-200 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-200"
 >
 	<slot />
 
