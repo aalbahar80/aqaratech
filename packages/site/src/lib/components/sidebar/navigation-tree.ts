bdc45fbc8571b344e69984c47e6fe1453e62740f
nbe <aleisa.naser@gmail.com>
2022-10-30 23:36:22 +0300

    add portfolioId to getNavigationTree

--- packages/site/src/lib/components/sidebar/navigation-tree.ts
+++ packages/site/src/lib/components/sidebar/navigation-tree.ts
@@ -13,49 +13,63 @@ import HeroiconsOutlineUser from '~icons/heroicons-outline/user';
 // Links
 import { LOGOUT } from '$lib/constants/routes';
 
-export const getNavigationTree = (user: User): NavigationItem[] => [
-	{
-		name: 'Financials',
-		href: '/tempsample/',
-		icon: HeroiconsOutlineDocumentReport,
-		children: [
-			{ name: 'Summary', href: '/financials/summary/' },
-			{
-				name: 'Income',
-				href: '/financials/income/',
-			},
-			{
-				name: 'Expenses',
-				href: '/financials/expenses/',
-			},
-			{ name: 'Payouts', href: '/financials/payouts/' },
-		],
-	},
-	{
-		name: 'Properties',
-		href: '/properties/',
-		icon: HeroiconsOutlineHome,
-	},
-	{
-		name: 'Leases',
-		href: '/leases/',
-		icon: HeroiconsOutlineDocumentText,
-	},
-	{
-		name: 'Account',
-		href: `/users/${user.id}/roles`,
-		icon: HeroiconsOutlineUser,
-		divided: true,
-	},
-	{
-		name: 'Settings',
-		href: '/settings/',
-		icon: HeroiconsOutlineCog8Tooth,
-		// path: '/settings/',
-	},
-	{
-		name: 'Logout',
-		href: LOGOUT,
-		icon: HeroiconsOutlineLogout,
-	},
-];
+export const getNavigationTree = (user: User): NavigationItem[] => {
+	if (!user.role?.portfolioId) {
+		throw new Error(
+			'Tried to get portfolio navigation tree without portfolioId',
+		);
+	}
+
+	return [
+		{
+			name: 'Financials',
+			href: '/tempsample/',
+			icon: HeroiconsOutlineDocumentReport,
+			children: [
+				{
+					name: 'Summary',
+					href: `portfolios/${user.role.portfolioId}/financials/summary/`,
+				},
+				{
+					name: 'Income',
+					href: `portfolios/${user.role.portfolioId}/financials/income/`,
+				},
+				{
+					name: 'Expenses',
+					href: `portfolios/${user.role.portfolioId}/financials/expenses/`,
+				},
+				{
+					name: 'Payouts',
+					href: `portfolios/${user.role.portfolioId}/financials/payouts/`,
+				},
+			],
+		},
+		{
+			name: 'Properties',
+			href: '/properties/',
+			icon: HeroiconsOutlineHome,
+		},
+		{
+			name: 'Leases',
+			href: '/leases/',
+			icon: HeroiconsOutlineDocumentText,
+		},
+		{
+			name: 'Account',
+			href: `/users/${user.id}/roles`,
+			icon: HeroiconsOutlineUser,
+			divided: true,
+		},
+		{
+			name: 'Settings',
+			href: '/settings/',
+			icon: HeroiconsOutlineCog8Tooth,
+			// path: '/settings/',
+		},
+		{
+			name: 'Logout',
+			href: LOGOUT,
+			icon: HeroiconsOutlineLogout,
+		},
+	];
+};
