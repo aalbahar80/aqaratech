f4d037f9e19c94be18f37336074cee32ff115024
nbe <aleisa.naser@gmail.com>
2022-10-31 02:03:06 +0300

    init financials/income/table route

---
+++ packages/site/src/routes/portfolios/[id]/financials/income/table/csv/+server.ts
@@ -0,0 +1,12 @@
+import { createApi } from '$api';
+import { respondWithCsv } from '$lib/utils/respond-with-csv';
+import type { RequestHandler } from './$types';
+
+export const GET: RequestHandler = async ({ params, fetch }) => {
+	const leaseInvoices = await createApi(fetch).leaseInvoices.findAll({
+		take: 999999999,
+		portfolioId: params.id,
+	});
+
+	return respondWithCsv(leaseInvoices.results, 'leaseInvoices');
+};
