2cfb1ec53800d6646f28b4d60165c002f60c553f
nbe <aleisa.naser@gmail.com>
2022-10-31 02:37:09 +0300

    init income pie chart

--- packages/site/src/routes/portfolios/[id]/financials/income/+page.svelte
+++ packages/site/src/routes/portfolios/[id]/financials/income/+page.svelte
@@ -1,15 +1,35 @@
 <script lang="ts">
+	import Chart from '$lib/components/charts/Chart.svelte';
+	import { revenuePie } from '$lib/components/charts/revenue-pie';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+
+	$: datasets = [
+		{
+			label: 'Payment Status',
+			data: [
+				data.income.paid.reduce((acc, i) => acc + i.amount, 0),
+				data.income.unpaid.reduce((acc, i) => acc + i.amount, 0),
+			],
+			backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)'],
+		},
+	];
 </script>
 
-<pre>{JSON.stringify(
-		{
-			income: data.income,
-		},
-		null,
-		2,
-	)}</pre>
-
 <a href="income/table">Table</a>
+
+<Chart let:height let:width>
+	{#if datasets?.[0].data[0] > 0 || datasets[0]?.data[1] > 0}
+		<canvas {height} {width} use:revenuePie={datasets} />
+	{:else}
+		<div
+			class="w-full overflow-hidden rounded-lg bg-white  shadow"
+			style="height: {height}px;"
+		>
+			<div class="px-4 py-5 sm:p-6">
+				<p>No data</p>
+			</div>
+		</div>
+	{/if}
+</Chart>
