99890c4f1608bc14f0b2e4d6350a12e82026d25f
nbe <aleisa.naser@gmail.com>
2022-10-31 02:53:42 +0300

    add bar chart

--- packages/site/src/routes/portfolios/[id]/financials/income/+page.svelte
+++ packages/site/src/routes/portfolios/[id]/financials/income/+page.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import Chart from '$lib/components/charts/Chart.svelte';
+	import { revenueChart } from '$lib/components/charts/revenue';
 	import { revenuePie } from '$lib/components/charts/revenue-pie';
+	import { getColor } from '$lib/utils/colors';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 
-	$: datasets = [
+	$: pieDatasets = [
 		{
 			label: 'Payment Status',
 			data: [
@@ -15,13 +17,38 @@
 			backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)'],
 		},
 	];
+
+	$: barDatasets = [
+		{
+			label: 'Paid',
+			borderColor: getColor(0, 2),
+			data: data.income.paid,
+			parsing: {
+				yAxisKey: 'amount',
+				xAxisKey: 'date',
+			},
+			backgroundColor: getColor(0, 2),
+			borderRadius: 10,
+		},
+		{
+			label: 'Unpaid',
+			borderColor: getColor(1, 2),
+			data: data.income.unpaid,
+			parsing: {
+				yAxisKey: 'amount',
+				xAxisKey: 'date',
+			},
+			backgroundColor: getColor(1, 2),
+			borderRadius: 10,
+		},
+	];
 </script>
 
 <a href="income/table">Table</a>
 
 <Chart let:height let:width>
-	{#if datasets?.[0].data[0] > 0 || datasets[0]?.data[1] > 0}
-		<canvas {height} {width} use:revenuePie={datasets} />
+	{#if pieDatasets[0]?.data[0] > 0 || pieDatasets[0]?.data[1] > 0}
+		<canvas {height} {width} use:revenuePie={{ datasesets: pieDatasets }} />
 	{:else}
 		<div
 			class="w-full overflow-hidden rounded-lg bg-white  shadow"
@@ -32,4 +59,8 @@
 			</div>
 		</div>
 	{/if}
+</Chart>
+
+<Chart let:height let:width>
+	<canvas {height} {width} use:revenueChart={{ datasets: barDatasets }} />
 </Chart>
