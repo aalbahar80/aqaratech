f79d2b13d4994a449e46432e99ca8931b710afe8
nbe <aleisa.naser@gmail.com>
2022-11-01 02:12:17 +0300

    fix modal-delete tests

--- packages/test/tests/components/modal-delete.spec.ts
+++ packages/test/tests/components/modal-delete.spec.ts
@@ -1,24 +1,29 @@
-import { expect, Page } from '@playwright/test';
+import { expect } from '@playwright/test';
 import { test as base } from '../api/api-fixtures';
 
-const test = base.extend<MyFixtures>({
+const test = base.extend({
 	page: async ({ page, org }, use) => {
 		await page.goto(
 			`/organizations/${org.organization.id}/settings/organization`,
 		);
+
 		await page.locator(`data-testid=more-actions-button`).click();
+
 		await page.locator('button:has-text("Delete")').click();
+
 		await use(page);
 	},
 });
 
 test('delete button is disabled by default', async ({ page }) => {
 	const btn = page.locator(`data-testid=confirm`);
+
 	await expect(btn).toBeDisabled();
 });
 
 test('delete button is not responsive', async ({ page }) => {
 	const btn = page.locator(`data-testid=confirm`);
+
 	await btn.click({ force: true });
 
 	// wait 2 seconds
@@ -30,20 +35,30 @@ test('delete button is not responsive', async ({ page }) => {
 
 test('filling prompt with incorrect text does not enable button', async ({
 	page,
+	org,
 }) => {
-	const input = page.locator(`input[placeholder="${org.fullName}"]`);
+	const input = page.locator(
+		`input[placeholder="${org.organization.fullName}"]`,
+	);
+
 	await input.fill('wrong');
+
 	const btn = page.locator(`data-testid=confirm`);
+
 	await expect(btn).toBeDisabled();
 });
 
-test('filling prompt with correct text enables button', async ({ page }) => {
-	const input = page.locator(`input[placeholder="${org.fullName}"]`);
-	await input.fill(org.fullName);
+test('filling prompt with correct text enables button', async ({
+	page,
+	org,
+}) => {
+	const input = page.locator(
+		`input[placeholder="${org.organization.fullName}"]`,
+	);
+
+	await input.fill(org.organization.fullName);
+
 	const btn = page.locator(`data-testid=confirm`);
+
 	await expect(btn).toBeEnabled();
 });
-
-type MyFixtures = {
-	page: Page;
-};
