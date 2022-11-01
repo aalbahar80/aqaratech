7cdfa4522d0cd021449fb019cf2040f532ef64ad
nbe <aleisa.naser@gmail.com>
2022-11-01 06:36:55 +0300

    add range store to summary load

--- packages/site/src/routes/portfolios/[id]/financials/summary/+page.ts
+++ packages/site/src/routes/portfolios/[id]/financials/summary/+page.ts
@@ -1,9 +1,14 @@
 import { createApi } from '$api';
+import { range } from '$lib/stores/range';
+import { get } from 'svelte/store';
 import type { PageLoad } from './$types';
 import { calculateNet } from './calculate-net';
 
-export const load: PageLoad = async ({ fetch, params }) => {
+export const load: PageLoad = async ({ fetch, params, depends }) => {
 	const api = createApi(fetch);
+
+	const { start, end } = get(range);
+	depends('range');
 
 	const [properties, income, expenses] = await Promise.all([
 		api.portfolios.findProperties({
@@ -12,10 +17,14 @@ export const load: PageLoad = async ({ fetch, params }) => {
 
 		api.portfolios.getIncomeByMonth({
 			portfolioId: params.id,
+			start,
+			end,
 		}),
 
 		api.portfolios.getExpensesByMonth({
 			portfolioId: params.id,
+			start,
+			end,
 		}),
 	]);
 
