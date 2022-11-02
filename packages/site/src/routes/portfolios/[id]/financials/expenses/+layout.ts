1366edc97a4db703bfe292236eb2cef6e994c50e
nbe <aleisa.naser@gmail.com>
2022-11-02 22:11:27 +0300

    add orgId to expense queries

--- packages/site/src/routes/portfolios/[id]/financials/expenses/+layout.ts
+++ packages/site/src/routes/portfolios/[id]/financials/expenses/+layout.ts
@@ -4,13 +4,16 @@ import { range } from '$lib/stores/filter/range';
 import { get } from 'svelte/store';
 import type { LayoutLoad } from './$types';
 
-export const load: LayoutLoad = async ({ fetch, params, depends }) => {
+export const load: LayoutLoad = async ({ fetch, params, depends, parent }) => {
 	const api = createApi(fetch);
 
 	const { start, end } = get(range);
 	depends(FilterEnum.Range);
 
+	const organizationId = (await parent()).user?.role?.organizationId;
+
 	const expenses = await api.portfolios.getExpensesByMonth({
+		organizationId,
 		portfolioId: params.id,
 		start,
 		end,
--- packages/site/src/routes/portfolios/[id]/financials/expenses/table/+page.ts
+++ packages/site/src/routes/portfolios/[id]/financials/expenses/table/+page.ts
@@ -9,13 +9,9 @@ export const load: PageLoad = async ({
 }) => {
 	const api = createApi(fetch);
 
-	const { page, take, sortOrder, orderBy } = parseParams(searchParams);
-	const expenses = await api.expenses.findAll({
-		page,
-		take,
-		sortOrder,
-		orderBy,
-		portfolioId: params.id,
+	const expenses = await api.portfolios.findAllExpenses({
+		id: params.id,
+		...parseParams(searchParams),
 	});
 
 	return { expenses };
