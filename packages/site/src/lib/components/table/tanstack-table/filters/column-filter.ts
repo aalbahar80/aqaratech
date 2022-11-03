566b9de5629fa7ed56613c69687e65bb561c1342
nbe <aleisa.naser@gmail.com>
2022-11-03 22:43:52 +0300

    add type

--- packages/site/src/lib/components/table/tanstack-table/filters/column-filter.ts
+++ packages/site/src/lib/components/table/tanstack-table/filters/column-filter.ts
@@ -1,22 +1,23 @@
+import type { Filter } from '$lib/models/interfaces/filter.interface';
 import type { Table } from '@tanstack/svelte-table';
 
-const getColumns = (table: Table<unknown>) =>
+const getColumns = (table: Table<unknown>): Filter['options'] =>
 	table.getAllLeafColumns().map((c) => ({
 		// eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
-		label: c.columnDef.header || c.id,
+		label: c.columnDef.header?.toString() || c.id,
 		value: c.id,
 		active: c.getIsVisible(),
 		action: c.getToggleVisibilityHandler(),
 	}));
 
-const getToggleAll = (table: Table<unknown>) => ({
+const getToggleAll = (table: Table<unknown>): Filter['options'][number] => ({
 	label: 'Toggle All',
 	value: 'toggle-all',
 	active: table.getIsAllColumnsVisible(),
 	action: (e: unknown) => table.getToggleAllColumnsVisibilityHandler()(e),
 });
 
-export const getColumnFilter = (table: Table<unknown>) => ({
+export const getColumnFilter = (table: Table<unknown>): Filter => ({
 	id: 'columns',
 	label: 'Columns',
 	options: [getToggleAll(table), ...getColumns(table)],
