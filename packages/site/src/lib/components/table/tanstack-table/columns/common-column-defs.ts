95d5383c5811df058a9baabad42e390b101ec9c6
nbe <aleisa.naser@gmail.com>
2022-11-04 00:00:28 +0300

    better location helper type

--- packages/site/src/lib/components/table/tanstack-table/columns/common-column-defs.ts
+++ packages/site/src/lib/components/table/tanstack-table/columns/common-column-defs.ts
@@ -1,27 +1,30 @@
 import type { BreadcrumbsDto } from '$api/openapi';
 import type { ColumnHelper } from '@tanstack/svelte-table';
 
-export const locationColumnDef = <
-	T extends { breadcrumbs: Partial<Pick<BreadcrumbsDto, 'property' | 'unit'>> },
->(
+export const locationColumnDef = <T extends Breadcrumbs>(
 	columnHelper: ColumnHelper<T>,
 ) =>
 	columnHelper.group({
 		header: 'Location',
 		footer: (props) => props.column.id,
 		columns: [
-			columnHelper.accessor('breadcrumbs', {
+			columnHelper.accessor((a) => a.breadcrumbs.property, {
 				id: 'property',
 				header: 'Property',
-				cell: (info) => info.getValue().property?.label || '',
+				cell: (info) =>
+					info.getValue<T['breadcrumbs']['property']>()?.label ?? '',
 				enableSorting: false,
 			}),
 
-			columnHelper.accessor('breadcrumbs', {
+			columnHelper.accessor((a) => a.breadcrumbs.unit, {
 				id: 'unit',
 				header: 'Unit',
-				cell: (info) => info.getValue().unit?.label || '',
+				cell: (info) => info.getValue<T['breadcrumbs']['unit']>()?.label ?? '',
 				enableSorting: false,
 			}),
 		],
 	});
+
+interface Breadcrumbs {
+	breadcrumbs: Partial<Pick<BreadcrumbsDto, 'property' | 'unit'>>;
+}
