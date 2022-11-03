a86ffe73c533f1516132adc5060f569d9d1b10e8
nbe <aleisa.naser@gmail.com>
2022-11-03 21:25:42 +0300

    show toggleAll option

--- packages/site/src/lib/components/table/tanstack-table/ColumnToggle.svelte
+++ packages/site/src/lib/components/table/tanstack-table/ColumnToggle.svelte
@@ -4,46 +4,28 @@
 	import type { Readable } from 'svelte/store';
 
 	export let table: Readable<Table<any>>;
-</script>
 
-<div class="inline-block rounded border border-black shadow">
-	<!-- <div class="border-b border-black px-1">
-		<label>
-			<input
-				checked={$table.getIsAllColumnsVisible()}
-				on:change={(e) => {
-					console.info($table.getToggleAllColumnsVisibilityHandler()(e));
-				}}
-				type="checkbox"
-			/>{' '}
-			Toggle All
-		</label>
-	</div> -->
-	{#each $table.getAllLeafColumns() as column}
-		<div class="px-1">
-			<label>
-				<input
-					checked={column.getIsVisible()}
-					on:change={column.getToggleVisibilityHandler()}
-					type="checkbox"
-				/>{' '}
-				{column.id}
-			</label>
-		</div>
-	{/each}
-</div>
+	$: options = $table.getAllLeafColumns().map((c) => ({
+		label: c.id,
+		value: c.id,
+		active: c.getIsVisible(),
+		action: c.getToggleVisibilityHandler(),
+	}));
+
+	$: toggleAll = {
+		label: 'Toggle All',
+		value: 'toggle-all',
+		active: $table.getIsAllColumnsVisible(),
+		action: (e: unknown) => $table.getToggleAllColumnsVisibilityHandler()(e),
+	};
+</script>
 
 <FilterBar
 	responsive={[
 		{
 			id: 'columns',
 			label: 'Columns',
-			options: $table.getAllLeafColumns().map((c) => ({
-				label: c.id,
-				value: c.id,
-				active: c.getIsVisible(),
-				action: c.getToggleVisibilityHandler(),
-			})),
+			options: [toggleAll, ...options],
 		},
 	]}
 />
