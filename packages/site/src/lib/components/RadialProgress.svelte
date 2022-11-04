18c20e3b677302f164126436fe8678a10e2266a4
nbe <aleisa.naser@gmail.com>
2022-11-05 01:34:41 +0300

    init radial progress component

---
+++ packages/site/src/lib/components/RadialProgress.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+	export let value: number;
+</script>
+
+<!-- https://daisyui.com/components/radial-progress/ -->
+<!-- https://github.com/saadeghi/daisyui/blob/master/src/components/unstyled/radial-progress.css -->
+<!-- https://github.com/saadeghi/daisyui/blob/master/src/components/styled/radial-progress.css -->
+
+<div class="radial-progress" style={`--value:${value};`}>{value}%</div>
+
+<style lang="postcss">
+	.radial-progress {
+		--value: 0;
+		--size: 5rem;
+		--thickness: calc(var(--size) / 10);
+	}
+	.radial-progress:after {
+		@apply bg-current;
+	}
+
+	.radial-progress {
+		@apply relative inline-grid h-[var(--size)] w-[var(--size)] place-content-center rounded-full bg-transparent;
+		vertical-align: middle;
+		box-sizing: content-box;
+	}
+	.radial-progress::-moz-progress-bar {
+		@apply appearance-none bg-transparent;
+	}
+	.radial-progress::-webkit-progress-value {
+		@apply appearance-none bg-transparent;
+	}
+	.radial-progress::-webkit-progress-bar {
+		@apply appearance-none bg-transparent;
+	}
+	.radial-progress:before,
+	.radial-progress:after {
+		@apply absolute rounded-full;
+		content: '';
+	}
+	.radial-progress:before {
+		@apply inset-0;
+		background: radial-gradient(farthest-side, currentColor 98%, #0000)
+				top/var(--thickness) var(--thickness) no-repeat,
+			conic-gradient(currentColor calc(var(--value) * 1%), #0000 0);
+		-webkit-mask: radial-gradient(
+			farthest-side,
+			#0000 calc(99% - var(--thickness)),
+			#000 calc(100% - var(--thickness))
+		);
+		mask: radial-gradient(
+			farthest-side,
+			#0000 calc(99% - var(--thickness)),
+			#000 calc(100% - var(--thickness))
+		);
+	}
+	.radial-progress:after {
+		inset: calc(50% - var(--thickness) / 2);
+		transform: rotate(calc(var(--value) * 3.6deg - 90deg))
+			translate(calc(var(--size) / 2 - 50%));
+	}
+</style>
