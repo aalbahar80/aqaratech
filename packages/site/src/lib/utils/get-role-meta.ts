fb4761efec71a37b6de77c94da4e4afbabd74069
nbe <aleisa.naser@gmail.com>
2022-11-04 22:00:41 +0300

    use getRoute for portfolio owners in roleMeta

--- packages/site/src/lib/utils/get-role-meta.ts
+++ packages/site/src/lib/utils/get-role-meta.ts
@@ -1,5 +1,7 @@
-import type { UserMeta } from '$lib/models/types/auth.type';
 import type { ValidatedRoleDto } from '$api/openapi';
+import type { UserMeta } from '$lib/models/types/auth.type';
+import { getRoute } from '$lib/utils/route-helpers/get-route';
+import { PageTypePortfolio } from '$lib/utils/route-helpers/route-helpers.type';
 import { entitiesMap } from '@self/utils';
 
 export const getRoleMeta = (role: ValidatedRoleDto): UserMeta => {
@@ -14,16 +16,25 @@ export const getRoleMeta = (role: ValidatedRoleDto): UserMeta => {
 				{ label: 'Tenants', href: '/tenants' },
 			],
 		};
-	} else if (role.roleType === 'PORTFOLIO') {
+	} else if (role.roleType === 'PORTFOLIO' && role.portfolioId) {
 		return {
 			roleLabel: entitiesMap.portfolio.singularCap,
-			home: `/portfolios/${role.portfolioId}/financials/summary`,
+			home: getRoute({
+				entity: 'portfolio',
+				pageType: PageTypePortfolio.Summary,
+				params: {
+					organizationId: role.organizationId,
+					portfolioId: role.portfolioId,
+				},
+				id: role.portfolioId,
+			}),
+
 			navLinks: [
 				{ label: 'Properties', href: '/properties' },
 				{ label: 'Leases', href: '/leases' },
 			],
 		};
-	} else if (role.roleType === 'TENANT') {
+	} else if (role.roleType === 'TENANT' && role.tenantId) {
 		return {
 			roleLabel: 'Tenant',
 			home: `/portal/tenant/${role.tenantId}`,
