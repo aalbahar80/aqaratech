ac119401911100593190882c142dee3c32fb5c5a
nbe <aleisa.naser@gmail.com>
2022-11-05 00:11:40 +0300

    move searchbutton outside of sidebar file

--- packages/site/src/lib/components/search/SearchButton.svelte
+++ packages/site/src/lib/components/search/SearchButton.svelte
@@ -1,54 +1,36 @@
 <script lang="ts">
 	import SearchPalette from '$lib/components/search/SearchPalette.svelte';
 	import { shortcut } from '$lib/utils/shortcut';
-	import { Search } from '@steeze-ui/heroicons';
-	import { Icon } from '@steeze-ui/svelte-icon';
 
 	let open = false;
 </script>
 
 <!-- https://daisyui.com/components/kbd/ -->
-<!-- use svelte:window to set event listener instead? -->
+
 <button
-	class="flex flex-auto justify-center lg:justify-end"
+	class="flex w-full items-center rounded-md border bg-white p-2 text-gray-700 hover:border-blue-400 hover:outline-none hover:ring hover:ring-blue-300 hover:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:hover:border-blue-300"
 	use:shortcut={{ control: true, code: 'KeyK' }}
 	on:click={() => {
 		open = true;
 	}}
 >
-	<SearchPalette bind:open />
-	<div
-		class="w-full max-w-lg lg:max-w-xs"
-		on:click={() => {
-			open = true;
-		}}
+	<svg
+		class="mr-3 h-5 w-5 flex-none text-gray-400"
+		viewBox="0 0 24 24"
+		fill="none"
 	>
-		<!-- <div
-						class="block w-full max-w-lg rounded-md border border-transparent bg-gray-700 py-2 pl-10 pr-3 leading-5 text-gray-300 placeholder-gray-400 focus:border-white focus:bg-white focus:text-gray-900 focus:outline-none focus:ring-white sm:text-sm lg:max-w-xs"
-					> -->
-		<div class="relative flex items-center text-gray-400">
-			<div
-				class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3"
-			>
-				<Icon
-					src={Search}
-					class="h-5 w-5 text-gray-400"
-					aria-hidden="true"
-					theme="solid"
-				/>
-			</div>
-			<span
-				id="search"
-				name="search"
-				class="block w-full rounded-md border border-transparent bg-gray-700 py-2 pl-10 pr-3 leading-5 text-gray-300 placeholder-gray-400 focus:border-white focus:bg-white focus:text-gray-900 focus:outline-none focus:ring-white sm:text-sm"
-			>
-				Search...
-			</span>
-			<kbd
-				class="absolute right-0 hidden pr-3 font-sans font-semibold sm:block"
-			>
-				<abbr title="Command" class="no-underline">⌘</abbr> K
-			</kbd>
-		</div>
-	</div>
+		<path
+			d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		/>
+	</svg>
+
+	Search
+	<kbd class="ml-auto flex-none pl-3 font-sans">
+		<abbr title="Command" class="no-underline">⌘</abbr> K
+	</kbd>
 </button>
+<SearchPalette bind:open />
