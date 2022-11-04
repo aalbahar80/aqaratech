df98c17a92e410770237e3c11d25c9abd10ef591
nbe <aleisa.naser@gmail.com>
2022-11-04 22:42:38 +0300

    rm old navbar files

--- packages/site/src/lib/components/navbar/NavbarIcon.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import logo from '../../../assets/placeholders/workflow-mark-teal-200-cyan-400.svg';
-</script>
-
-<a href="/">
-	<span class="sr-only">Workflow</span>
-	<img class="h-8 w-auto sm:h-10" src={logo} alt="logo" />
-</a>
