1679e7370993662c4d1867f48333672c04ac4269
nbe <aleisa.naser@gmail.com>
2022-11-05 00:09:35 +0300

    style search button

--- packages/site/src/lib/components/sidebar/Sidebar.svelte
+++ packages/site/src/lib/components/sidebar/Sidebar.svelte
@@ -17,14 +17,13 @@
 <script lang="ts">
 	import { afterNavigate } from '$app/navigation';
 	import { page } from '$app/stores';
-	import SearchButton from '$lib/components/search/SearchButton.svelte';
+	import SearchPalette from '$lib/components/search/SearchPalette.svelte';
 	import SidebarItem from '$lib/components/sidebar/SidebarItem.svelte';
 	import type { NavigationItem } from '$lib/components/sidebar/types';
 	import { clickOutside } from '$lib/utils/click-outside';
+	import { shortcut } from '$lib/utils/shortcut';
 	import clsx from 'clsx';
 	import { fly } from 'svelte/transition';
-	import SearchPalette from '$lib/components/search/SearchPalette.svelte';
-	import { shortcut } from '$lib/utils/shortcut';
 
 	export let navigationTree: NavigationItem[];
 
@@ -49,29 +48,30 @@
 	<!-- TODO extract to own component -->
 	{#if $page.data.user?.role?.roleType === 'ORGADMIN'}
 		<button
-			class="relative"
+			class="flex w-full items-center rounded-md border bg-white p-2 text-gray-700 hover:border-blue-400 hover:outline-none hover:ring hover:ring-blue-300 hover:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:hover:border-blue-300"
 			use:shortcut={{ control: true, code: 'KeyK' }}
 			on:click={() => {
 				open = true;
 			}}
 		>
-			<span class="absolute inset-y-0 left-0 flex items-center pl-3">
-				<svg class="h-5 w-5 text-gray-400" viewBox="0 0 24 24" fill="none">
-					<path
-						d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
-						stroke="currentColor"
-						stroke-width="2"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-					/>
-				</svg>
-			</span>
-
-			<span
-				class="block w-full rounded-md border bg-white py-2 pl-10 pr-4 text-gray-700 hover:border-blue-400 hover:outline-none hover:ring hover:ring-blue-300 hover:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:hover:border-blue-300"
+			<svg
+				class="mr-3 h-5 w-5 flex-none text-gray-400"
+				viewBox="0 0 24 24"
+				fill="none"
 			>
-				Search
-			</span>
+				<path
+					d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				/>
+			</svg>
+
+			Search
+			<kbd class="ml-auto flex-none pl-3 font-sans">
+				<abbr title="Command" class="no-underline">⌘</abbr> K
+			</kbd>
 		</button>
 		<SearchPalette bind:open />
 	{/if}
