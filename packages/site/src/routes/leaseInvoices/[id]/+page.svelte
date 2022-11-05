541ddd2ef4318ae4db4355574bea558e8aabeea8
nbe <aleisa.naser@gmail.com>
2022-11-05 07:00:02 +0300

    rm old leaseInvoiceRoute

--- packages/site/src/routes/leaseInvoices/[id]/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import LeaseInvoicePage from '$lib/components/leaseInvoice/LeaseInvoicePage.svelte';
-	import type { PageData } from './$types';
-
-	export let data: PageData;
-</script>
-
-<LeaseInvoicePage trx={data.leaseInvoice} />
---
+++ packages/site/src/routes/organizations/[organizationId]/portfolios/[portfolioId]/leaseInvoices/[leaseInvoiceId]/+layout.ts
@@ -0,0 +1,12 @@
+import { createApi } from '$api';
+import type { LayoutLoad } from './$types';
+
+export const load: LayoutLoad = async ({ params, fetch }) => {
+	const api = createApi(fetch);
+
+	const leaseInvoice = await api.leaseInvoices.findOne({
+		id: params.leaseInvoiceId,
+	});
+
+	return { leaseInvoice };
+};
