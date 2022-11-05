acd7fc3e7f4d0a306f691d1b947ba5abb1a032be
nbe <aleisa.naser@gmail.com>
2022-11-05 03:36:13 +0300

    add :propertyId/occupancy route

---
+++ packages/site/src/routes/organizations/[organizationId]/portfolios/[portfolioId]/properties/[propertyId]/occupancy/+page.ts
@@ -0,0 +1,30 @@
+import { createApi } from '$api';
+import { occupancyRange } from '$lib/components/charts/utils/occupancy-range';
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async ({ fetch, params }) => {
+	const { organizationId, portfolioId, propertyId } = params;
+
+	const api = createApi(fetch);
+
+	const [occupancy, futureOccupancy] = await Promise.all([
+		api.portfolios.getOccupancy({
+			organizationId,
+			portfolioId,
+			propertyId,
+			...occupancyRange.previous,
+		}),
+
+		api.portfolios.getOccupancy({
+			organizationId,
+			portfolioId,
+			propertyId,
+			...occupancyRange.future,
+		}),
+	]);
+
+	return {
+		occupancy,
+		futureOccupancy,
+	};
+};
