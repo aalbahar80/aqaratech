3631612eab371c642710b0d8b87e548b4baa9483
nbe <aleisa.naser@gmail.com>
2022-11-06 00:03:01 +0300

    add vitest config

---
+++ packages/utils/vitest.config.ts
@@ -0,0 +1,11 @@
+import path from 'node:path';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	test: {
+		alias: {
+			// Needed to let vitest know about the alias in tsconfig.json
+			src: path.resolve(__dirname, 'src'),
+		},
+	},
+});
