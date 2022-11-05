cc4464c3fcecb442e4a54091325686b30d89d10f
nbe <aleisa.naser@gmail.com>
2022-11-05 04:37:28 +0300

    rm filelist from detailspane

--- packages/site/src/lib/components/DetailsPane.svelte
+++ packages/site/src/lib/components/DetailsPane.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import DetailsPaneItem from '$lib/components/details-pane/DetailsPaneItem.svelte';
-	import FileList from '$lib/components/file/FileList.svelte.svelte';
 
 	export let details: [string, string | null][];
 </script>
@@ -10,6 +9,5 @@
 		{#each details as [key, value]}
 			<DetailsPaneItem {key} {value} />
 		{/each}
-		<FileList />
 	</dl>
 </div>
