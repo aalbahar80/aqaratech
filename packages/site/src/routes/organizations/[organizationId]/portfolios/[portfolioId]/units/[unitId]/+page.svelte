f434a8838281d08e3e41a1ce184afb2752a7b548
nbe <aleisa.naser@gmail.com>
2022-11-05 04:51:23 +0300

    rm unused load function

--- packages/site/src/routes/organizations/[organizationId]/portfolios/[portfolioId]/units/[unitId]/+page.svelte
+++ packages/site/src/routes/organizations/[organizationId]/portfolios/[portfolioId]/units/[unitId]/+page.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
 	import DetailsPane from '$lib/components/DetailsPane.svelte';
-	import LeaseList from '$lib/components/lease/LeaseList.svelte';
 	import { kwdFormat } from '$lib/utils/common';
-	import { create } from '$lib/utils/route-helpers';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
@@ -17,16 +15,3 @@
 </script>
 
 <DetailsPane {details} />
-<LeaseList
-	leases={data.leases}
-	formUrl={(function () {
-		const base = create({ entity: 'lease' });
-		const searchParams = new URLSearchParams({
-			portfolioId: data.unit.breadcrumbs.portfolio.id,
-			propertyId: data.unit.propertyId,
-			unitId: data.unit.id,
-		});
-		return `${base}?${searchParams.toString()}`;
-	})()}
-	showIndex
-/>
