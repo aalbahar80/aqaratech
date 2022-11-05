93fc8b7f932f752099398f3db31a5bdaef37b097
nbe <aleisa.naser@gmail.com>
2022-11-05 23:46:35 +0300

    add RoleGuard component

---
+++ packages/site/src/lib/utils/RoleGuard.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { RoleTypeEnum } from '$api/openapi';
+	import { page } from '$app/stores';
+
+	export let roles: RoleTypeEnum[];
+</script>
+
+{#if $page.data.user?.role && roles.includes($page.data.user.role.roleType)}
+	<slot />
+{/if}
