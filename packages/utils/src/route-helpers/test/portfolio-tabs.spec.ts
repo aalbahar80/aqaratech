6aafacd9574ef4e6d0428f2131d9df45ea1435c0
nbe <aleisa.naser@gmail.com>
2022-11-06 10:00:00 +0300

    dedupe portfolio tab tests

--- packages/utils/src/route-helpers/test/portfolio-tabs.spec.ts
+++ packages/utils/src/route-helpers/test/portfolio-tabs.spec.ts
@@ -1,5 +1,5 @@
 import { PageTypePortfolio } from 'src/route-helpers/enums/page-tab-portfolio.enum';
-import { PageTab } from 'src/route-helpers/enums/page-tab.enum';
+import type { PageTab } from 'src/route-helpers/enums/page-tab.enum';
 import { PageType } from 'src/route-helpers/enums/page-type.enum';
 import { getRoute } from 'src/route-helpers/get-route';
 import { expect, test } from 'vitest';
@@ -9,30 +9,6 @@ const baseInput = {
 	params: { organizationId: '1' },
 	id: '3',
 };
-
-const tabbed = [
-	[PageTab.Details, ''],
-	[PageTab.Files, 'files'],
-	[PageTab.Financials, 'financials'],
-	[PageTab.Occupancy, 'occupancy'],
-	[PageTab.Units, 'units'],
-	[PageTab.Leases, 'leases'],
-	[PageTab.Invoices, 'invoices'],
-];
-
-test.each(tabbed)('portfolio tab is prefixed: %s', (pageType, expected) => {
-	const prefix = '/details';
-
-	const url = `/organizations/1/portfolios/3${prefix}/${expected}`;
-
-	const input = {
-		...baseInput,
-		pageType,
-	};
-
-	// @ts-expect-error test
-	expect(getRoute(input)).toBe(url);
-});
 
 const untabbed: [PageTypePortfolio | PageTab | PageType, string][] = [
 	[PageType.Edit, 'edit'],
