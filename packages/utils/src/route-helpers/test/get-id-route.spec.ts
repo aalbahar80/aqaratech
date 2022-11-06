0378447673cd2101bfb3840fa5b477873242fe0c
nbe <aleisa.naser@gmail.com>
2022-11-06 09:56:19 +0300

    use describe.each to run recursive tests

--- packages/utils/src/route-helpers/test/get-id-route.spec.ts
+++ packages/utils/src/route-helpers/test/get-id-route.spec.ts
@@ -1,13 +1,7 @@
 import { PageTab } from 'src/route-helpers/enums/page-tab.enum';
 import { PageType } from 'src/route-helpers/enums/page-type.enum';
 import { getRoute } from 'src/route-helpers/get-route';
-import { expect, test, describe } from 'vitest';
-
-const baseInput = {
-	entity: 'property',
-	params: { organizationId: '1', portfolioId: '2' },
-	id: '3',
-} as const;
+import { describe, expect, test } from 'vitest';
 
 const pageTypes = [
 	[PageType.Edit, 'edit'],
@@ -20,26 +14,40 @@ const pageTypes = [
 	[PageTab.Invoices, 'invoices'],
 ] as const;
 
-test.each(pageTypes)('getRoute(%o) === %s', (pageType, expected) => {
-	const url = `/organizations/1/portfolios/2/properties/3/${expected}`;
+const entityTypes = [
+	{
+		entity: 'property',
+		url: (expected: string) =>
+			`/organizations/1/portfolios/2/properties/3/${expected}`,
+		baseInput: {
+			entity: 'property',
+			params: { organizationId: '1', portfolioId: '2' },
+			id: '3',
+		},
+	},
+	{
+		entity: 'tenant',
+		url: (expected: string) => `/organizations/1/tenants/2/${expected}`,
+		baseInput: {
+			entity: 'tenant',
+			params: { organizationId: '1' },
+			id: '2',
+		},
+	},
+] as const;
 
-	const input = {
-		...baseInput,
-		pageType,
-	};
+describe.each(entityTypes)('describe %o', (entityType) => {
+	test.each(pageTypes)(`%s route url`, (pageType, expected) => {
+		const url = entityType.url(expected);
 
-	// @ts-expect-error test
-	const result = getRoute(input);
+		const input = {
+			...entityType.baseInput,
+			pageType,
+		};
 
-	expect(result).toBe(url);
-});
+		// @ts-expect-error test
+		const result = getRoute(input);
 
-test('errors on invalid page type', () => {
-	const input = {
-		...baseInput,
-		pageType: 'invalid' as unknown,
-	};
-
-	// @ts-expect-error test
-	expect(() => getRoute(input)).toThrowError('Invalid page address');
+		expect(result).toBe(url);
+	});
 });
