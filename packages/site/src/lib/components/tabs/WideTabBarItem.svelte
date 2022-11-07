094d48d68e55b8bf62c6765bc912546b26a8331f
nbe <aleisa.naser@gmail.com>
2022-11-07 21:15:04 +0300

    widen tab items to fill space

--- packages/site/src/lib/components/tabs/WideTabBarItem.svelte
+++ packages/site/src/lib/components/tabs/WideTabBarItem.svelte
@@ -10,7 +10,7 @@
 <a
 	href={tab.href}
 	class={classes(
-		'w-1/4 border-b-2 py-4 px-1 text-center text-sm font-medium',
+		'w-full border-b-2 py-4 px-1 text-center text-sm font-medium',
 		active
 			? 'border-indigo-500 text-indigo-600'
 			: 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
