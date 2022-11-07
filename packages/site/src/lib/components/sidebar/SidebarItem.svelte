3dcfda5651d4d6699f5faa9404f53e2c0f678067
nbe <aleisa.naser@gmail.com>
2022-11-07 19:46:58 +0300

    style current sidebar items

--- packages/site/src/lib/components/sidebar/SidebarItem.svelte
+++ packages/site/src/lib/components/sidebar/SidebarItem.svelte
@@ -1,15 +1,21 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import ArrowIcon from '$lib/components/sidebar/ArrowIcon.svelte';
 	import type { NavigationItem } from '$lib/components/sidebar/types';
 	import clsx from 'clsx';
 
 	export let item: NavigationItem;
 	export let isChild = false;
+
+	$: isCurrent = $page.url.pathname === item.href;
 </script>
 
 <a
 	class={clsx(
-		'flex transform items-center rounded-md px-4 py-2 text-gray-600 transition-colors duration-100 hover:bg-gray-200 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-200',
+		'flex items-center rounded-md px-4 py-2',
+		isCurrent && isChild
+			? 'bg-gray-100 text-gray-700 dark:bg-gray-800 dark:text-gray-200'
+			: 'transform text-gray-600 transition-colors duration-100 hover:bg-gray-200 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-200',
 		isChild ? 'mt-2' : 'mt-5',
 	)}
 	href={item.href}
