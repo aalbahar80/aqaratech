c91eb66eb6b8a62faaa6e7c928690af25b6dbfab
nbe <aleisa.naser@gmail.com>
2022-11-08 05:44:36 +0300

    get scoped properties

--- packages/site/src/routes/organizations/[organizationId]/portfolios/[portfolioId]/(tabbed)/properties/+page.ts
+++ packages/site/src/routes/organizations/[organizationId]/portfolios/[portfolioId]/(tabbed)/properties/+page.ts
@@ -1,19 +1,14 @@
 import { createApi } from '$api';
-import { parseParams } from '$lib/utils/parse-params';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({
-	params,
-	url: { searchParams },
-	fetch,
-}) => {
+export const load: PageLoad = async ({ params, fetch }) => {
 	const api = createApi(fetch);
 
 	// TODO 5 use params.organizationId and params.portfolioId
 	const { organizationId, portfolioId } = params;
 
-	const properties = await api.properties.findAll({
-		...parseParams(searchParams),
+	const properties = await api.portfolios.findProperties({
+		id: portfolioId,
 	});
 
 	return { properties };
