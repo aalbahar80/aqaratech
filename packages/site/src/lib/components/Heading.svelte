bb9f8a8071bde260b288d3b30dfef7dd2241e080
nbe <aleisa.naser@gmail.com>
2022-11-09 05:27:21 +0300

    use getRoute for edit button links

--- packages/site/src/lib/components/Heading.svelte
+++ packages/site/src/lib/components/Heading.svelte
@@ -9,15 +9,15 @@
 	import { classes } from '$lib/utils/classes';
 	import { createFileHref, hasFileSupport } from '$lib/utils/file';
 	import { MenuItem } from '@rgossiaux/svelte-headlessui';
-	import { entitiesMap, type Entity } from '@self/utils';
+	import { getRoute, PageType, type Entity } from '@self/utils';
 	import Fa6SolidPaperclip from '~icons/fa6-solid/paperclip';
 	import Fa6SolidTrashCan from '~icons/fa6-solid/trash-can';
 
-	type IconTooltip = {
+	interface IconTooltip {
 		label: string | number | null | undefined;
 		icon: any;
 		tooltip: string;
-	};
+	}
 
 	export let title: string;
 	export let id: string;
@@ -59,7 +59,12 @@
 					<a
 						href={disallowEdit
 							? null
-							: `/${entitiesMap[entity].urlName}/${id}/edit`}
+							: getRoute({
+									entity,
+									pageType: PageType.Edit,
+									id,
+									params: $page.params,
+							  })}
 						class={classes(
 							'relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-8 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500',
 							disallowEdit ? 'cursor-not-allowed opacity-50' : '',
