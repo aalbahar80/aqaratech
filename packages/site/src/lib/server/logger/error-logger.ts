18b56c642292fd922bd9ec645d382df915754ea7
nbe <aleisa.naser@gmail.com>
2022-11-09 03:41:31 +0300

    simple errorLogger

--- packages/site/src/lib/server/logger/error-logger.ts
+++ packages/site/src/lib/server/logger/error-logger.ts
@@ -1,15 +1,5 @@
 import { logger } from '$lib/server/logger';
 
 export const errorLogger = (error: unknown) => {
-	if (error instanceof Error) {
-		logger.log({
-			level: 'error',
-			message: JSON.stringify({
-				name: error.name,
-				error: error.message,
-				stack: error.stack,
-				cause: error.cause,
-			}),
-		});
-	}
+	logger.error(error);
 };
