ac7cbdd5eb39a6f51cddb3ef075c27641e7b8a06
nbe <aleisa.naser@gmail.com>
2022-11-10 22:25:06 +0300

    rm details array from portfolio

--- packages/site/src/routes/organizations/[organizationId]/portfolios/[portfolioId]/(tabbed)/+page.svelte
+++ packages/site/src/routes/organizations/[organizationId]/portfolios/[portfolioId]/(tabbed)/+page.svelte
@@ -1,20 +1,8 @@
 <script lang="ts">
 	import AutoDetailsPane from '$lib/components/AutoDetailsPane.svelte';
-	import { toUTCFormat } from '$lib/utils/common';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
-
-	$: details = [
-		['Full Name', data.portfolio.fullName],
-		['Label', data.portfolio.label],
-		['Phone', data.portfolio.phone],
-		['Civil ID', data.portfolio.civilid],
-		[
-			'Date of birth',
-			data.portfolio.dob ? toUTCFormat(data.portfolio.dob) : null,
-		],
-	] as [string, string | null][];
 </script>
 
 <AutoDetailsPane
