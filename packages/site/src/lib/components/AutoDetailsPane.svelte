c16cc5d9956caec29b605328b4261c2333e5e2ee
nbe <aleisa.naser@gmail.com>
2022-11-10 22:12:36 +0300

    use ts toolbelt to Union.exclude

--- packages/site/src/lib/components/AutoDetailsPane.svelte
+++ packages/site/src/lib/components/AutoDetailsPane.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import DetailsPaneItem from '$lib/components/details-pane/DetailsPaneItem.svelte';
+	import { Union } from 'ts-toolbelt';
 
-	type Details = $$Generic<Record<unknown, unknown>>;
+	type Details = $$Generic;
 	type Keys = $$Generic<keyof Details>;
 
 	export let details: Details;
-	export let keys: Keys[];
+	export let keys: Union.Exclude<Keys, number | symbol>[];
 </script>
 
 <div id="detailsPane">
