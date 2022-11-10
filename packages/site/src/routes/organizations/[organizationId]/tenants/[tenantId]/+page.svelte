9fca2f0081ceea9d65c2b77dbb9532edea75a573
nbe <aleisa.naser@gmail.com>
2022-11-10 22:35:56 +0300

    refactor tenant details pane

--- packages/site/src/routes/organizations/[organizationId]/tenants/[tenantId]/+page.svelte
+++ packages/site/src/routes/organizations/[organizationId]/tenants/[tenantId]/+page.svelte
@@ -1,36 +1,21 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-	import DetailsPane from '$lib/components/DetailsPane.svelte';
-	import MemberList from '$lib/components/member/MemberList.svelte';
-	import { countries } from '$lib/constants/countries';
-	import { toUTCFormat } from '$lib/utils/common';
+	import AutoDetailsPane from '$lib/components/AutoDetailsPane.svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
-
-	$: details = [
-		['Full Name', data.tenant.fullName],
-		['Label', data.tenant.label],
-		['Phone', data.tenant.phone],
-		['Date of Birth', data.tenant.dob ? toUTCFormat(data.tenant.dob) : ''],
-		['Civil id', data.tenant.civilid],
-		[
-			'Nationality',
-			countries.find((c) => c.alpha3Code === data.tenant.nationality)?.name,
-		],
-		['Passport #', data.tenant.passportNum],
-		['Residency #', data.tenant.residencyNum],
-		[
-			'Residency Expiration',
-			data.tenant.residencyEnd ? toUTCFormat(data.tenant.residencyEnd) : '',
-		],
-	] as [string, string | null][];
 </script>
 
-<DetailsPane {details} />
-
-{#if $page.data.user?.role?.roleType === 'ORGADMIN'}
-	<MemberList roles={data.roles} />
-{/if}
-
-<!-- <LeaseInvoiceList leaseInvoices={data.invoices} /> -->
+<AutoDetailsPane
+	details={data.tenant}
+	keys={[
+		'fullName',
+		'label',
+		'phone',
+		'civilid',
+		'dob',
+		'nationality',
+		'passportNum',
+		'residencyNum',
+		'residencyEnd',
+	]}
+/>
