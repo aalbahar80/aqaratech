1edd73b94979613e02c38b21e3c841eeaa0b9790
nbe <aleisa.naser@gmail.com>
2022-11-10 19:38:55 +0300

    test minimal/all fields on form

--- packages/test/tests/forms/portfolio/edit.spec.ts
+++ packages/test/tests/forms/portfolio/edit.spec.ts
@@ -6,7 +6,38 @@ import { uuid } from '../../../utils/uuid';
 import { test } from '../../api/api-fixtures';
 import { FormPage } from '../form-page-model';
 
-test('can be edited', async ({ portfolio, page }) => {
+test('can be submitted with minimal fields', async ({ portfolio, page }) => {
+	const fields = R.pipe(
+		portfolioFactory.build({
+			organizationId: portfolio.id,
+		}),
+		R.pick(['fullName']),
+	);
+
+	const url = getRoute({
+		entity: 'portfolio',
+		pageType: PageType.Edit,
+		id: portfolio.id,
+		params: { organizationId: portfolio.organizationId },
+	});
+
+	await page.goto(url);
+
+	const formPage = new FormPage(page);
+	await formPage.fillForm(fields);
+	await formPage.save();
+
+	const urlAfterSubmit = getRoute({
+		entity: 'portfolio',
+		pageType: PageType.Id,
+		id: portfolio.id,
+		params: { organizationId: portfolio.organizationId },
+	});
+
+	await expect(page).toHaveURL(uuid(urlAfterSubmit));
+});
+
+test('can be submitted with all fields', async ({ portfolio, page }) => {
 	const fields = R.pipe(
 		portfolioFactory.build({
 			organizationId: portfolio.id,
--- packages/test/tests/forms/portfolio/new.spec.ts
+++ packages/test/tests/forms/portfolio/new.spec.ts
@@ -11,6 +11,36 @@ test('can be submitted with minimal fields', async ({ org, page }) => {
 		portfolioFactory.build({
 			organizationId: org.organization.id,
 		}),
+		R.pick(['fullName']),
+	);
+
+	const url = getRoute({
+		entity: 'portfolio',
+		pageType: PageType.New,
+		params: { organizationId: org.organization.id },
+	});
+
+	await page.goto(url);
+
+	const formPage = new FormPage(page);
+	await formPage.fillForm(portfolio);
+	await formPage.save();
+
+	const urlAfterSubmit = getRoute({
+		entity: 'portfolio',
+		pageType: PageType.Id,
+		id: ':uuid',
+		params: { organizationId: org.organization.id },
+	});
+
+	await expect(page).toHaveURL(uuid(urlAfterSubmit));
+});
+
+test('can be submitted with all fields', async ({ org, page }) => {
+	const portfolio = R.pipe(
+		portfolioFactory.build({
+			organizationId: org.organization.id,
+		}),
 		R.pick(['fullName', 'label', 'phone', 'dob', 'civilid']),
 	);
 
