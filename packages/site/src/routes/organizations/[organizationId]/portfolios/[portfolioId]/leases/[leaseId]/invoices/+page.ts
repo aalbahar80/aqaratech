d5e2d0cb3b8a76e5bea3911eae6475004cd07f1e
nbe <aleisa.naser@gmail.com>
2022-11-12 01:44:50 +0300

    implement invoice sort

--- packages/site/src/routes/organizations/[organizationId]/portfolios/[portfolioId]/leases/[leaseId]/invoices/+page.ts
+++ packages/site/src/routes/organizations/[organizationId]/portfolios/[portfolioId]/leases/[leaseId]/invoices/+page.ts
@@ -1,13 +1,19 @@
 import { createApi } from '$api';
+import { parseParams } from '$lib/utils/parse-params';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ params, fetch }) => {
+export const load: PageLoad = async ({
+	params,
+	fetch,
+	url: { searchParams },
+}) => {
 	const api = createApi(fetch);
 
 	const { leaseId } = params;
 
 	const invoices = await api.leases.findInvoices({
 		id: leaseId,
+		...parseParams(searchParams),
 	});
 
 	return { invoices };
