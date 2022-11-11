798f066f47702581d76391bb912e1985ddc8e149
nbe <aleisa.naser@gmail.com>
2022-11-11 20:51:22 +0300

    use as const for sample-data

--- packages/seed/src/sample-data.ts
+++ packages/seed/src/sample-data.ts
@@ -1,4 +1,3 @@
-import { Prisma } from '@prisma/client';
 import {
 	testOrgEmail,
 	testOrgId,
@@ -10,22 +9,34 @@ import {
 	testTenantId,
 	testTenantRoleId,
 } from './constant/ids.constant';
-import { organizationFactory, portfolioFactory, roleFactory } from './factory';
+import {
+	expenseFactory,
+	leaseFactory,
+	leaseInvoiceFactory,
+	organizationFactory,
+	payoutFactory,
+	portfolioFactory,
+	propertyFactory,
+	roleFactory,
+	tenantFactory,
+	unitFactory,
+} from './factory';
+import { userFactory } from './factory/user';
 
-const users = Prisma.validator<Prisma.UserCreateManyArgs['data']>()([
-	{
+const users = [
+	userFactory.build({
 		id: 'cb23d83d-869b-4b02-83e5-55997e3818b0',
 		email: testOrgEmail,
-	},
-	{
+	}),
+	userFactory.build({
 		id: '9794384b-dc9e-4917-81cf-ef261c6d11eb',
 		email: testPortfolioEmail,
-	},
-	{
+	}),
+	userFactory.build({
 		id: '89d7ff2a-b807-4041-ab7e-9afc7e27022f',
 		email: testTenantEmail,
-	},
-]);
+	}),
+] as const;
 
 const roles = [
 	roleFactory.build({
@@ -74,44 +85,44 @@ const portfolios = [
 	}),
 ] as const;
 
-const tenants = Prisma.validator<Prisma.TenantCreateManyArgs['data']>()([
-	{
+const tenants = [
+	tenantFactory.build({
 		id: testTenantId,
 		organizationId: testOrgId,
 		fullName: 'Tenant One',
 		label: 'Tenant 1',
-	},
-	{
+	}),
+	tenantFactory.build({
 		id: '723c124e-b566-408e-9aa4-6d5dd3d4929e',
 		organizationId: testOrgId,
 		fullName: 'Tenant updateable',
 		label: 'Tenant 2',
-	},
-]);
+	}),
+] as const;
 
-const properties = Prisma.validator<Prisma.PropertyCreateManyArgs['data']>()([
-	{
+const properties = [
+	propertyFactory.build({
 		id: '260bd3ea-28ac-4586-a30d-3d47171ef643',
 		organizationId: testOrgId,
 		portfolioId: testPortfolioId,
 		area: 'السالمية',
 		number: '99',
-	},
-]);
+	}),
+] as const;
 
-const units = Prisma.validator<Prisma.UnitCreateManyArgs['data']>()([
-	{
+const units = [
+	unitFactory.build({
 		id: '1df75d02-c437-4f20-a4e8-0a8754e84302',
 		organizationId: testOrgId,
 		portfolioId: testPortfolioId,
 		propertyId: properties[0]!.id,
 		unitNumber: '99',
 		type: 'فيلا',
-	},
-]);
+	}),
+] as const;
 
-const leases = Prisma.validator<Prisma.LeaseCreateManyArgs['data']>()([
-	{
+const leases = [
+	leaseFactory.build({
 		id: 'a7ce3151-2e4a-4eba-bbe0-5a600f23a829',
 		organizationId: testOrgId,
 		portfolioId: testPortfolioId,
@@ -120,13 +131,11 @@ const leases = Prisma.validator<Prisma.LeaseCreateManyArgs['data']>()([
 		unitId: units[0]!.id,
 		tenantId: tenants[0]!.id,
 		monthlyRent: 1000,
-	},
-]);
+	}),
+] as const;
 
-const leaseInvoices = Prisma.validator<
-	Prisma.LeaseInvoiceCreateManyArgs['data']
->()([
-	{
+const leaseInvoices = [
+	leaseInvoiceFactory.build({
 		id: '2b66cc94-1ec9-4aff-a6ab-d7a9cf2b57f6',
 		organizationId: testOrgId,
 		portfolioId: testPortfolioId,
@@ -135,8 +144,8 @@ const leaseInvoices = Prisma.validator<
 		postAt: new Date('2020-01-01'),
 		dueAt: new Date('2020-01-10'),
 		isPaid: false,
-	},
-	{
+	}),
+	leaseInvoiceFactory.build({
 		id: 'bdf188e7-1de4-46d8-a2fa-8a2158124f51',
 		organizationId: testOrgId,
 		portfolioId: testPortfolioId,
@@ -145,8 +154,8 @@ const leaseInvoices = Prisma.validator<
 		postAt: new Date('2020-01-01'),
 		dueAt: new Date('2020-01-10'),
 		isPaid: true,
-	},
-	{
+	}),
+	leaseInvoiceFactory.build({
 		id: '8190bf8d-9f18-439c-b087-31ea4950a13d',
 		organizationId: testOrgId,
 		portfolioId: testPortfolioId,
@@ -155,44 +164,44 @@ const leaseInvoices = Prisma.validator<
 		postAt: new Date('2020-01-01'),
 		dueAt: new Date('2020-01-10'),
 		isPaid: true,
-	},
-]);
+	}),
+] as const;
 
-const expenses = Prisma.validator<Prisma.ExpenseCreateManyArgs['data']>()([
-	{
+const expenses = [
+	expenseFactory.build({
 		id: 'c8a63068-8123-4a4a-84be-c4f61e12a47e',
 		organizationId: testOrgId,
 		portfolioId: testPortfolioId,
 		amount: 284,
 		postAt: new Date('2020-01-01'),
 		memo: 'Expense One',
-	},
-	{
+	}),
+	expenseFactory.build({
 		id: '5710b05b-bdaf-4a99-9aeb-0a8565986134',
 		organizationId: testOrgId,
 		portfolioId: testPortfolioId,
 		amount: 429,
 		postAt: new Date('2020-03-21'),
 		memo: 'Expense Two',
-	},
-]);
+	}),
+] as const;
 
-const payouts = Prisma.validator<Prisma.PayoutCreateManyArgs['data']>()([
-	{
+const payouts = [
+	payoutFactory.build({
 		id: '63f91b49-b257-4dc7-ab09-2f253b7aec3c',
 		organizationId: testOrgId,
 		portfolioId: testPortfolioId,
 		amount: 381,
 		postAt: new Date('2020-01-04'),
-	},
-	{
+	}),
+	payoutFactory.build({
 		id: '03388e4d-cfda-4854-ab5c-0a4bcb9d704a',
 		organizationId: testOrgId,
 		portfolioId: testPortfolioId,
 		amount: 22,
 		postAt: new Date('2020-04-02'),
-	},
-]);
+	}),
+] as const;
 
 export const sample = {
 	users,
