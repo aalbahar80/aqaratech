af1da6569e06d112be6aa5a60fb6a252a57e4bcf
nbe <aleisa.naser@gmail.com>
2022-11-12 14:02:47 +0300

    add role form test

---
+++ packages/test/tests/forms/role/create.spec.ts
@@ -0,0 +1,49 @@
+import { expect } from '@playwright/test';
+import { getRoute, PageTab } from '@self/utils';
+import { test } from '../../api/api-fixtures';
+import { FormPage } from '../form-page-model';
+
+// TODO: add organization to array, after making PageTab.Roles goes to organization roles page
+const email = 'test@aqtest.com';
+
+const entities = ['portfolio', 'tenant'] as const;
+
+for (const entity of entities) {
+	test(`can invite role to ${entity}`, async ({ page, tenant, portfolio }) => {
+		const instance = entity === 'tenant' ? tenant : portfolio;
+
+		const url = getRoute({
+			entity,
+			id: instance.id,
+			pageType: PageTab.Roles,
+			params: {
+				organizationId: instance.organizationId,
+			},
+		});
+
+		await page.goto(url);
+
+		await page.getByRole('link', { name: 'New' }).click();
+
+		const formPage = new FormPage(page);
+
+		await formPage.fillForm({ email });
+
+		await formPage.save();
+
+		const successUrl = getRoute({
+			entity,
+			pageType: PageTab.Roles,
+			id: instance.id,
+			params: {
+				organizationId: instance.organizationId,
+			},
+		});
+
+		await expect(page).toHaveURL(successUrl);
+
+		const cell = page.getByRole('cell', { name: email });
+
+		await expect(cell).toHaveText(email);
+	});
+}
