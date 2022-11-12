f33e5e9c153b236823f4890dff6cc6f550510e24
nbe <aleisa.naser@gmail.com>
2022-11-12 04:47:20 +0300

    add mark as paid button to table

--- packages/site/src/lib/components/leaseInvoice/LeaseInvoiceTable.svelte
+++ packages/site/src/lib/components/leaseInvoice/LeaseInvoiceTable.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
+	import { createApi } from '$api';
 	import type { LeaseInvoiceDto, PaginatedLeaseInvoiceDto } from '$api/openapi';
+	import { invalidateAll } from '$app/navigation';
 	import { page } from '$app/stores';
 	import Badge from '$lib/components/Badge.svelte';
 	import MenuItemChild from '$lib/components/buttons/MenuItemChild.svelte';
 	import MenuItemIcon from '$lib/components/buttons/MenuItemIcon.svelte';
 	import FilterBar from '$lib/components/filter/FilterBar.svelte';
 	import FilterRadio from '$lib/components/filter/FilterRadio.svelte';
+	import ActionButton from '$lib/components/table/tanstack-table/ActionButton.svelte';
 	import {
 		locationColumnDef,
 		viewColumnDef,
 	} from '$lib/components/table/tanstack-table/columns/common-column-defs';
 	import Table from '$lib/components/table/tanstack-table/Table.svelte';
+	import { addSuccessToast, handleApiError } from '$lib/stores/toast';
 	import { toUTCFormat } from '$lib/utils/common';
 	import { getInvoiceBadge } from '$lib/utils/get-badge';
 	import { MenuItem } from '@rgossiaux/svelte-headlessui';
@@ -68,6 +72,40 @@
 		}),
 
 		locationColumnDef(columnHelper),
+
+		columnHelper.display({
+			id: 'markAsPaid',
+			header: '',
+			cell: (props) => {
+				const invoice = props.row.original;
+
+				// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+				return renderComponent(ActionButton, {
+					options: {
+						label: 'Mark as Paid',
+						disabled: invoice.isPaid,
+						onClick: async () => {
+							try {
+								await createApi().leaseInvoices.update({
+									id: invoice.id,
+									updateLeaseInvoiceDto: {
+										isPaid: !invoice.isPaid,
+										paidAt: invoice.isPaid
+											? null
+											: new Date().toISOString().split('T')[0],
+									},
+								});
+								addSuccessToast('Invoice updated');
+								await invalidateAll();
+							} catch (e) {
+								console.error(e);
+								await handleApiError(e);
+							}
+						},
+					},
+				});
+			},
+		}),
 
 		viewColumnDef(columnHelper, 'leaseInvoice', $page.params),
 	];
