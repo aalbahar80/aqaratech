345c172466695fb5b971c771e5153d43b5103883
nbe <aleisa.naser@gmail.com>
2022-11-12 07:22:33 +0300

    generics in column filters

--- packages/site/src/lib/components/table/tanstack-table/filters/column-filter.ts
+++ packages/site/src/lib/components/table/tanstack-table/filters/column-filter.ts
@@ -1,7 +1,7 @@
 import type { Filter } from '$lib/models/interfaces/filter.interface';
 import type { Table } from '@tanstack/svelte-table';
 
-const getColumns = (table: Table<unknown>): Filter['options'] =>
+const getColumns = <T>(table: Table<T>): Filter['options'] =>
 	table.getAllLeafColumns().map((c) => ({
 		// eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
 		label: c.columnDef.header?.toString() || c.id,
@@ -10,14 +10,14 @@ const getColumns = (table: Table<unknown>): Filter['options'] =>
 		action: c.getToggleVisibilityHandler(),
 	}));
 
-const getToggleAll = (table: Table<unknown>): Filter['options'][number] => ({
+const getToggleAll = <T>(table: Table<T>): Filter['options'][number] => ({
 	label: 'Toggle All',
 	value: 'toggle-all',
 	active: table.getIsAllColumnsVisible(),
 	action: (e: unknown) => table.getToggleAllColumnsVisibilityHandler()(e),
 });
 
-export const getColumnFilter = (table: Table<unknown>): Filter => ({
+export const getColumnFilter = <T>(table: Table<T>): Filter => ({
 	id: 'columns',
 	label: 'Columns',
 	options: [getToggleAll(table), ...getColumns(table)],
