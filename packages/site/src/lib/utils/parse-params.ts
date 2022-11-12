c205bda0688fd520f0f70a81087e33129ccaa8df
nbe <aleisa.naser@gmail.com>
2022-11-12 09:19:00 +0300

    add string signature to parse params

--- packages/site/src/lib/utils/parse-params.ts
+++ packages/site/src/lib/utils/parse-params.ts
@@ -1,5 +1,5 @@
 export const parseParams = (searchParams: URLSearchParams) => {
-	const obj = {};
+	const obj: Record<string, string> = {};
 
 	for (const prop of searchParams.entries()) {
 		obj[prop[0]] = prop[1];
