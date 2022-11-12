fcf06f28ac0c91cf1f8fe3ec12dee552527ee9b4
nbe <aleisa.naser@gmail.com>
2022-11-12 16:01:10 +0300

    rm old roleform

--- packages/site/src/routes/roles/new/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import RoleForm from '$lib/components/role/RoleForm.svelte';
-	import type { PageData } from './$types';
-
-	export let data: PageData;
-</script>
-
-<RoleForm predefined={data.predefined} />
