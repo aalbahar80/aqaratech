150731971450ed2d695022a50f88782e66bdf85f
nbe <aleisa.naser@gmail.com>
2022-11-12 08:34:20 +0300

    rm ref to tenantlist

--- packages/site/src/routes/organizations/[organizationId]/tenants/+page.svelte
+++ packages/site/src/routes/organizations/[organizationId]/tenants/+page.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
-	import TenantList from '$lib/components/tenant/TenantList.svelte';
 	import TenantTable from '$lib/components/tenant/TenantTable.svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 </script>
-
-<TenantList tenants={data.tenants} />
 
 <TenantTable data={data.tenants} />
