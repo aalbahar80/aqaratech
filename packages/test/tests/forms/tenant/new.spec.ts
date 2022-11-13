eeee83a93ac091ffcded891e21cd175ba5287f7d
nbe <aleisa.naser@gmail.com>
2022-11-13 19:13:29 +0300

    add tenant form tests

--- packages/test/tests/forms/tenant/new.spec.ts
+++ packages/test/tests/forms/tenant/new.spec.ts
@@ -1,51 +1,51 @@
 import { expect } from '@playwright/test';
+import { tenantFactory } from '@self/seed';
+import { PageType } from '@self/utils';
+import * as R from 'remeda';
 import { test } from '../../api/api-fixtures';
+import { FormPage } from '../form-page-model';
 
-test('smoke', async ({ page }, info) => {
-	await page.goto('/tenants/new');
+const entity = 'tenant';
+const pageType = PageType.New;
 
-	await page.locator('input[name="fullName"]').fill('Example Tenant');
-	await page.locator('input[name="label"]').fill('ET');
-	await page.locator('input[name="phone"]').fill('91234567');
-	await page.locator('input[name="civilid"]').fill('123456789012');
-	await page.locator('input[name="dob"]').fill('2022-08-05');
-	await page.locator('input[name="passportNum"]').fill('1234567');
-	await page.locator('input[name="residencyNum"]').fill('12345');
-	await page.locator('input[name="residencyEnd"]').fill('2022-01-01');
-	await page.locator('#nationality').click();
-	await page.locator(`data-testid=EGY`).click();
-
-	await page.locator('text=Save').click();
-
-	const id = new RegExp(
-		`/tenants/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}`,
+test('can be submitted with minimal fields', async ({ org, page }) => {
+	const tenant = R.pipe(
+		tenantFactory.build({
+			organizationId: org.organization.id,
+		}),
+		R.pick(['fullName']),
 	);
-	await expect(page).toHaveURL(id);
-	const url = page.url();
 
-	// take screenshot
-	info.snapshotDir;
-
-	expect(page.locator('text=Tenant')).toBeTruthy();
-	await page.locator('#detailsPane').screenshot({
-		path:
-			info.snapshotDir +
-			`/tenant-${info.snapshotSuffix}-${info.project.name}.png`,
+	const formPage = new FormPage(page, {
+		entity,
+		pageType,
+		fixtures: { org },
 	});
 
-	await page.locator('text=Edit').click();
-	const edit = new RegExp(
-		`/tenants/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/edit`,
+	await formPage.goto();
+	await formPage.fillForm(tenant);
+	await formPage.save();
+
+	await expect(page).toHaveURL(formPage.getSuccessUrl());
+});
+
+test('can be submitted with all fields', async ({ org, page }) => {
+	const tenant = R.pipe(
+		tenantFactory.build({
+			organizationId: org.organization.id,
+		}),
+		R.pick(['fullName', 'label', 'phone', 'dob', 'civilid']),
 	);
-	await expect(page).toHaveURL(edit);
-	await page.locator('text=Save').click();
 
-	// ensure same entity
-	await expect(page).toHaveURL(url);
-
-	// match screenshot
-	expect(page.locator('text=Tenant')).toBeTruthy();
-	expect(await page.locator('#detailsPane').screenshot()).toMatchSnapshot({
-		name: 'tenant.png',
+	const formPage = new FormPage(page, {
+		entity,
+		pageType,
+		fixtures: { org },
 	});
+
+	await formPage.goto();
+	await formPage.fillForm(tenant);
+	await formPage.save();
+
+	await expect(page).toHaveURL(formPage.getSuccessUrl());
 });
