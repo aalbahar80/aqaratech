57e11ac4e534367f3874f2defde1f2a86eef8440
nbe <aleisa.naser@gmail.com>
2022-11-14 21:49:58 +0300

    init combobox model

--- packages/test/tests/forms/combobox-model.ts
+++ packages/test/tests/forms/combobox-model.ts
@@ -3,13 +3,36 @@ import { getLabel } from '@self/utils';
 
 const keyLabels = ['nationality', 'tenantId', 'type'];
 
+interface Option {
+	label: string;
+	value: string;
+}
+
 export class Combobox {
 	static keys = keyLabels.map((key) => getLabel(key));
 
 	readonly page: Page;
+	/**
+	 * The field label.
+	 */
+	readonly key: string;
+	readonly option: Option;
 
-	constructor(page: Page) {
+	constructor({
+		page,
+		key,
+		option,
+	}: {
+		page: Page;
+		option: Option;
+		key: string;
+	}) {
 		this.page = page;
+		this.key = getLabel(key);
+		this.option = {
+			label: option.label,
+			value: option.value,
+		};
 	}
 
 	async fill(key: string, value: string) {
