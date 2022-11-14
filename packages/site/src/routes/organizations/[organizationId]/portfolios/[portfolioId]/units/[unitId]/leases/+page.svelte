4f77d892090f8e870986231279418f4e93d2d2e0
nbe <aleisa.naser@gmail.com>
2022-11-15 02:09:18 +0300

    replace leaselist with table

--- packages/site/src/routes/organizations/[organizationId]/portfolios/[portfolioId]/units/[unitId]/leases/+page.svelte
+++ packages/site/src/routes/organizations/[organizationId]/portfolios/[portfolioId]/units/[unitId]/leases/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	import LeaseList from '$lib/components/lease/LeaseList.svelte';
+	import LeaseTable from '$lib/components/lease/LeaseTable.svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 </script>
 
-<LeaseList leases={data.leases} showIndex />
+<LeaseTable data={data.leases} />
--- packages/site/src/routes/organizations/[organizationId]/portfolios/[portfolioId]/units/[unitId]/leases/+page.ts
+++ packages/site/src/routes/organizations/[organizationId]/portfolios/[portfolioId]/units/[unitId]/leases/+page.ts
@@ -1,10 +1,18 @@
 import { createApi } from '$api';
+import { parseParams } from '$lib/utils/parse-params';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ fetch, params }) => {
+export const load: PageLoad = async ({
+	fetch,
+	params,
+	url: { searchParams },
+}) => {
 	const api = createApi(fetch);
 
-	const leases = await api.units.findLeases({ id: params.unitId, take: 100 });
+	const leases = await api.units.findLeases({
+		id: params.unitId,
+		...parseParams(searchParams),
+	});
 
 	return {
 		leases,
