a2bf507cef5f569563848901ac2ad385121108c6
nbe <aleisa.naser@gmail.com>
2022-11-15 02:14:28 +0300

    wrap filterbarbutton in roleguard

--- packages/site/src/lib/components/filter/FilterBarButtonForm.svelte
+++ packages/site/src/lib/components/filter/FilterBarButtonForm.svelte
@@ -1,17 +1,20 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import RoleGuard from '$lib/utils/RoleGuard.svelte';
 	import { getRoute, PageType, type GetFormRoute } from '@self/utils';
 
 	export let getRouteOptions: Omit<GetFormRoute, 'params' | 'pageType'>;
 </script>
 
-<a
-	href={getRoute({
-		pageType: PageType.New,
-		params: $page.params,
-		...getRouteOptions,
-	})}
-	class="inline-block text-sm font-medium text-gray-700 hover:text-gray-900"
->
-	+ New
-</a>
+<RoleGuard roles={['ORGADMIN']}>
+	<a
+		href={getRoute({
+			pageType: PageType.New,
+			params: $page.params,
+			...getRouteOptions,
+		})}
+		class="inline-block text-sm font-medium text-gray-700 hover:text-gray-900"
+	>
+		+ New
+	</a>
+</RoleGuard>
