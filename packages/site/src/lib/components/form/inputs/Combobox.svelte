f380df9d768bf467634d6212e4618b2e0a3581e5
nbe <aleisa.naser@gmail.com>
2022-11-15 01:19:08 +0300

    turn off autocomplete for combobox input

--- packages/site/src/lib/components/form/inputs/Combobox.svelte
+++ packages/site/src/lib/components/form/inputs/Combobox.svelte
@@ -195,6 +195,7 @@
 		class="w-full rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 disabled:cursor-not-allowed disabled:border-slate-200 disabled:bg-slate-50 disabled:text-slate-500 disabled:shadow-none sm:text-sm"
 		class:form-invalid={invalid}
 		placeholder="Search..."
+		autocomplete="off"
 		type="text"
 		bind:value={inputValue}
 		on:keydown={handleKeydown}
