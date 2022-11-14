efe1038d0ad79fc045799bdb59b57948b4ac6199
nbe <aleisa.naser@gmail.com>
2022-11-14 16:34:45 +0300

    test zodCheckbox

---
+++ packages/utils/src/schemas/utils/zod-checkbox.spec.ts
@@ -0,0 +1,20 @@
+import { zodCheckbox } from 'src/schemas/utils/zod-checkbox';
+import { expect, test } from 'vitest';
+
+const parsedToTrue = [true, 'on'];
+
+const parsedToFalse = [false, undefined];
+
+const invalid = ['off', '', null, 0, 1, '1', '0', 'true', 'false', 'abc'];
+
+test.each(parsedToTrue)('zodCheckbox.parse(%s) should return true', (arg) => {
+	expect(zodCheckbox.parse(arg)).toBe(true);
+});
+
+test.each(parsedToFalse)('zodCheckbox.parse(%s) should return false', (arg) => {
+	expect(zodCheckbox.parse(arg)).toBe(false);
+});
+
+test.each(invalid)('zodCheckbox.parse(%s) should throw', (arg) => {
+	expect(() => zodCheckbox.parse(arg)).toThrowError();
+});
