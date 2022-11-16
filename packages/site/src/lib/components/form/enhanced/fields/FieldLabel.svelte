b6d9231f624d0ea2eb4350f19c50f5ce08f2b2a1
nbe <aleisa.naser@gmail.com>
2022-11-16 08:47:53 +0300

    add field label asterisk

--- packages/site/src/lib/components/form/enhanced/fields/FieldLabel.svelte
+++ packages/site/src/lib/components/form/enhanced/fields/FieldLabel.svelte
@@ -7,9 +7,13 @@
 </script>
 
 <div class="flex justify-between">
-	<label for={formField.name} class="block text-sm font-medium text-gray-700"
-		>{formField.label}</label
-	>
+	<label for={formField.name} class="block text-sm font-medium text-gray-700">
+		{formField.label}
+
+		{#if formField.required}
+			<span class="text-red-600">*</span>
+		{/if}
+	</label>
 	<span class="text-sm text-gray-500" id={formField.hintId}
 		>{formField.hint}</span
 	>
