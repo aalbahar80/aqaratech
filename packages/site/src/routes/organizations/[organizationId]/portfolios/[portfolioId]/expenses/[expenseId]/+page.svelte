68a05956e6ed5af991d95461ed93bb54fc97aad6
nbe <aleisa.naser@gmail.com>
2022-11-16 03:53:48 +0300

    use autodetailspane for expense

--- packages/site/src/routes/organizations/[organizationId]/portfolios/[portfolioId]/expenses/[expenseId]/+page.svelte
+++ packages/site/src/routes/organizations/[organizationId]/portfolios/[portfolioId]/expenses/[expenseId]/+page.svelte
@@ -1,18 +1,21 @@
 <script lang="ts">
-	import DetailsPane from '$lib/components/DetailsPane.svelte';
+	import AutoDetailsPane from '$lib/components/AutoDetailsPane.svelte';
 	import ExpensePage from '$lib/components/expense/ExpensePage.svelte';
-	import { kwdFormat, toUTCFormat } from '$lib/utils/common';
+	import * as R from 'remeda';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
-
-	$: details = [
-		['Post Date', toUTCFormat(data.expense.postAt)],
-		['Amount', kwdFormat(data.expense.amount)],
-		['Category', data.expense.expenseType?.labelEn],
-		['Memo', data.expense.memo],
-	] as [string, string | null][];
 </script>
 
 <ExpensePage expense={data.expense} />
-<DetailsPane {details} />
+
+<AutoDetailsPane
+	details={R.mapValues(data.expense, (v, k) => {
+		if (k === 'expenseType' && v && typeof v === 'object' && 'labelEn' in v) {
+			return v.labelEn;
+		} else {
+			return v;
+		}
+	})}
+	keys={['postAt', 'amount', 'expenseType', 'memo']}
+/>
