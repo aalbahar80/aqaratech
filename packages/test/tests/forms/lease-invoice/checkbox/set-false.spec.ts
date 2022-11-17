a0dccbd6597289fff5a6af9fe6eb2570caac152e
nbe <aleisa.naser@gmail.com>
2022-11-18 01:36:29 +0300

    test invoice checkbox form

---
+++ packages/test/tests/forms/lease-invoice/checkbox/set-false.spec.ts
@@ -0,0 +1,37 @@
+import { expect } from '@playwright/test';
+import { PageType } from '@self/utils';
+import { test } from '../../../api/api-fixtures';
+import { FormPage } from '../../form-page-model';
+
+const entity = 'leaseInvoice';
+const pageType = PageType.Edit;
+
+test.use({
+	invoicesParams: [
+		{
+			isPaid: true,
+		},
+	],
+});
+
+test('can set to false', async ({ org, portfolio, invoice, page }) => {
+	const formPage = new FormPage(page, {
+		entity,
+		pageType,
+		id: invoice.id,
+		fixtures: { org, portfolio },
+	});
+
+	const fields = {
+		isPaid: false,
+		paidAt: null,
+	};
+
+	await formPage.goto();
+	await formPage.fillForm(fields);
+	await formPage.save();
+
+	await formPage.verifyDetails(fields);
+
+	await expect(page).toHaveURL(formPage.getSuccessUrl());
+});
---
+++ packages/test/tests/forms/lease-invoice/checkbox/set-true.spec.ts
@@ -0,0 +1,38 @@
+import { expect } from '@playwright/test';
+import { PageType } from '@self/utils';
+import { test } from '../../../api/api-fixtures';
+import { FormPage } from '../../form-page-model';
+
+const entity = 'leaseInvoice';
+const pageType = PageType.Edit;
+
+test.use({
+	invoicesParams: [
+		{
+			isPaid: false,
+			paidAt: null,
+		},
+	],
+});
+
+test('can set to true', async ({ org, portfolio, invoice, page }) => {
+	const formPage = new FormPage(page, {
+		entity,
+		pageType,
+		id: invoice.id,
+		fixtures: { org, portfolio },
+	});
+
+	const fields = {
+		isPaid: true,
+		paidAt: '2025-01-01',
+	};
+
+	await formPage.goto();
+	await formPage.fillForm(fields);
+	await formPage.save();
+
+	await formPage.verifyDetails(fields);
+
+	await expect(page).toHaveURL(formPage.getSuccessUrl());
+});
