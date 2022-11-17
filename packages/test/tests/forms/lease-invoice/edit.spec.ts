88c1013708018419065d2a7b4324def162433ccc
nbe <aleisa.naser@gmail.com>
2022-11-17 19:30:21 +0300

    add edit form test

---
+++ packages/test/tests/forms/lease-invoice/edit.spec.ts
@@ -0,0 +1,55 @@
+import { expect } from '@playwright/test';
+import { leaseInvoicePartialFactory } from '@self/seed';
+import { FIELDS, PageType } from '@self/utils';
+import * as R from 'remeda';
+import { test } from '../../api/api-fixtures';
+import { FormPage } from '../form-page-model';
+
+const entity = 'leaseInvoice';
+const pageType = PageType.Edit;
+
+test('can be submitted with minimal fields', async ({
+	org,
+	portfolio,
+	invoice,
+	page,
+}) => {
+	const fields = R.pick(
+		leaseInvoicePartialFactory(),
+		FIELDS.leaseInvoice.required,
+	);
+
+	const formPage = new FormPage(page, {
+		entity,
+		pageType,
+		id: invoice.id,
+		fixtures: { org, portfolio },
+	});
+
+	await formPage.goto();
+	await formPage.fillForm(fields);
+	await formPage.save();
+
+	await expect(page).toHaveURL(formPage.getSuccessUrl());
+});
+
+test('can be submitted with all fields', async ({
+	org,
+	portfolio,
+	invoice,
+	page,
+}) => {
+	const fields = R.pick(leaseInvoicePartialFactory(), FIELDS.leaseInvoice.all);
+
+	const formPage = new FormPage(page, {
+		entity,
+		pageType,
+		id: invoice.id,
+		fixtures: { org, portfolio },
+	});
+	await formPage.goto();
+	await formPage.fillForm(fields);
+	await formPage.save();
+
+	await expect(page).toHaveURL(formPage.getSuccessUrl());
+});
