6d9f0230efbc540e749308be5b18a60ec8e96dd6
nbe <aleisa.naser@gmail.com>
2022-11-17 17:51:48 +0300

    rm old form route and files

--- packages/site/src/lib/components/form/model/lease-invoice.ts
+++ packages/site/src/lib/components/form/model/lease-invoice.ts
@@ -27,16 +27,21 @@ export const leaseInvoiceFormModel = (pageType: FormTypeEnum) =>
 			postAt: createFormField('postAt', {
 				type: 'date',
 				required: true,
+				hint: "Note that a transaction cannot be paid before it's post date.\n\nتاريخ الاستحقاق",
 			}),
 
 			dueAt: createFormField('dueAt', {
 				type: 'date',
+				hint: 'If a due date is set, the transaction will be marked as "Past Due" after the due date. If a due date is not set, the transaction will only be marked as "Due" after it\'s post date.',
 			}),
 
 			paidAt: createFormField('paidAt', {
 				type: 'date',
+				hint: 'When was this transaction paid?\n\nتاريخ الدفع',
 			}),
 
-			memo: createFormField('memo'),
+			memo: createFormField('memo', {
+				hint: 'Enter a short description of the transaction. This will be visible to the tenant user.',
+			}),
 		},
 	});
--- packages/site/src/routes/leaseInvoices/[id]/edit/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import InvoiceForm from '$lib/components/leaseInvoice/InvoiceForm.svelte';
-	import type { PageData } from './$types';
-
-	export let data: PageData;
-</script>
-
-<InvoiceForm formType="update" data={data.invoice} />
