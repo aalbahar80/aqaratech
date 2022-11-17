248ff03635bbe1f187e45d52226ca1e21f216a99
nbe <aleisa.naser@gmail.com>
2022-11-17 18:52:18 +0300

    add new invoice form

---
+++ packages/site/src/routes/organizations/[organizationId]/portfolios/[portfolioId]/leaseInvoices/new/+page.server.ts
@@ -0,0 +1,25 @@
+import { handleForm } from '$lib/components/form/handle-form';
+import { leaseInvoiceCreateSchema } from '@self/utils';
+import type { Actions } from './$types';
+
+export const actions: Actions = {
+	default: async (event) => {
+		return handleForm({
+			entity: 'leaseInvoice',
+			schema: leaseInvoiceCreateSchema,
+			checkboxKeys: ['isPaid'],
+			event,
+			onSubmit: async (api, data, event) => {
+				const submitted = await api.organizations.createLeaseInvoice({
+					organizationId: event.params.organizationId,
+					createLeaseInvoiceDto: data,
+				});
+
+				return submitted.id;
+			},
+
+			fromParams: ['portfolioId'],
+			fromQuery: ['leaseId'],
+		});
+	},
+};
