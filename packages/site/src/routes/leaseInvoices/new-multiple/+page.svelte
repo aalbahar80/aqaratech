750f526314907456ea61d975e7ad502624099a78
nbe <aleisa.naser@gmail.com>
2022-11-17 17:52:11 +0300

    rm multiple invoice old route and files

--- packages/site/src/routes/leaseInvoices/new-multiple/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import Schedule from '$lib/components/lease/Schedule.svelte';
-	import type { PageData } from './$types';
-
-	export let data: PageData;
-</script>
-
-<Schedule lease={data.lease} />
