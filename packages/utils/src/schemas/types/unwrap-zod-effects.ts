9bebb11dcfe19a7ef123b52c3ca6a102bc832b96
nbe <aleisa.naser@gmail.com>
2022-11-18 12:46:13 +0300

    examples of zodtype infer generic

--- packages/utils/src/schemas/types/unwrap-zod-effects.ts
+++ packages/utils/src/schemas/types/unwrap-zod-effects.ts
@@ -34,4 +34,6 @@ type UnwrapTwice<T extends z.ZodTypeAny> = T extends z.ZodEffects<infer O>
 type L2 = UnwrapTwice<L>;
 // type UnwrapKeys<T> = K
 
-type E = UnwrappedInfer extends z.ZodEffects<z.ZodType<infer O>> ? O : never;
+type E0 = L extends z.ZodType<infer O> ? O : never;
+type E1 = L extends z.ZodEffects<z.ZodType<infer O>> ? O : never;
+type E2 = UnwrappedInfer extends z.ZodEffects<z.ZodType<infer O>> ? O : never;
