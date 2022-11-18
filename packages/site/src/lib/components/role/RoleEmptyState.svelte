8983a354f6dbac3c4b063a62fa3d03b591e554d0
nbe <aleisa.naser@gmail.com>
2022-11-18 07:17:48 +0300

    edit role empty state text

--- packages/site/src/lib/components/role/RoleEmptyState.svelte
+++ packages/site/src/lib/components/role/RoleEmptyState.svelte
@@ -1,41 +1,26 @@
+<script lang="ts">
+	import HeroiconsFolderPlus from '~icons/heroicons/folder-plus';
+	import HeroiconsPlusSmall from '~icons/heroicons/plus-small';
+</script>
+
 <div class="text-center">
-	<svg
+	<HeroiconsFolderPlus
 		class="mx-auto h-12 w-12 text-gray-400"
-		fill="none"
-		viewBox="0 0 24 24"
-		stroke="currentColor"
 		aria-hidden="true"
-	>
-		<path
-			vector-effect="non-scaling-stroke"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-			stroke-width="2"
-			d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z"
-		/>
-	</svg>
-	<h3 class="mt-2 text-sm font-medium text-gray-900">No projects</h3>
+	/>
+
+	<h3 class="mt-2 text-sm font-medium text-gray-900">No organizations</h3>
 	<p class="mt-1 text-sm text-gray-500">
-		Get started by creating a new project.
+		This account is not a part of any existing organizations.
 	</p>
 	<div class="mt-6">
 		<button
 			type="button"
 			class="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
 		>
-			<!-- Heroicon name: mini/plus -->
-			<svg
-				class="-ml-1 mr-2 h-5 w-5"
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 20 20"
-				fill="currentColor"
-				aria-hidden="true"
-			>
-				<path
-					d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z"
-				/>
-			</svg>
-			New Project
+			<HeroiconsPlusSmall class="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+
+			Create new organization
 		</button>
 	</div>
 </div>
