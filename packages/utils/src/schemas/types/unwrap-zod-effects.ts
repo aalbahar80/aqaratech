4c160ddb9e73b0e1d660bcaed761d27d86ef99cd
nbe <aleisa.naser@gmail.com>
2022-11-18 12:23:03 +0300

    double unwrap using innertype

--- packages/utils/src/schemas/types/unwrap-zod-effects.ts
+++ packages/utils/src/schemas/types/unwrap-zod-effects.ts
@@ -1,7 +1,23 @@
-import type { z } from 'zod';
+import type { leaseCreateSchema } from 'src/schemas/lease.schema';
+import type { z, ZodTypeAny } from 'zod';
 
+/**
+ * Use infer to get inner type of ZodEffects.
+ */
 export type UnwrapZodEffects<T extends z.ZodTypeAny> = T extends z.ZodEffects<
 	infer O
 >
 	? O
 	: never;
+
+/**
+ * Use innerType() to get inner type of ZodEffects.
+ */
+export type UnwrapZodEffects2<T extends z.ZodEffects<any>> =
+	T extends z.ZodEffects<ZodTypeAny> ? ReturnType<T['innerType']> : never;
+
+type L = typeof leaseCreateSchema;
+
+type UnwrappedInfer = UnwrapZodEffects<L>;
+
+type UnwrappedInfer2 = UnwrapZodEffects2<UnwrappedInfer>;
