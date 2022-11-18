f389b2d37931be70a9f5c255b920436fc21e4ee8
nbe <aleisa.naser@gmail.com>
2022-11-18 03:06:25 +0300

    rm old form and routes

--- packages/site/src/routes/organizations/[organizationId]/expenseCategories/[expenseCategoryId]/edit/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import ExpenseCategoryForm from '$lib/components/organization/ExpenseCategoryForm.svelte';
-	import type { PageData } from './$types';
-
-	export let data: PageData;
-</script>
-
-<ExpenseCategoryForm formType="update" data={data.expenseCategory} />
--- packages/site/src/routes/organizations/[organizationId]/expenseCategories/new/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	import ExpenseCategoryForm from '$lib/components/organization/ExpenseCategoryForm.svelte';
-</script>
-
-<ExpenseCategoryForm formType="create" />
