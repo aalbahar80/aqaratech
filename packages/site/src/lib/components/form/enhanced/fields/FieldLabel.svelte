b1619eadc12dbfa4da67f584fa2071ffa859af92
nbe <aleisa.naser@gmail.com>
2022-11-18 09:07:06 +0300

    use tooltip for form field hints

--- packages/site/src/lib/components/form/enhanced/fields/FieldLabel.svelte
+++ packages/site/src/lib/components/form/enhanced/fields/FieldLabel.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { FormField } from '$lib/components/form/model/form-field.interface';
+	import Tooltip from '$lib/components/Tooltip.svelte';
 
 	type Name = $$Generic;
 
@@ -7,14 +8,22 @@
 </script>
 
 <div class="flex justify-between">
-	<label for={formField.name} class="block text-sm font-medium text-gray-700">
-		{formField.label}
+	<div class="flex items-center gap-2">
+		<label for={formField.name} class="block text-sm font-medium text-gray-700">
+			{formField.label}
 
-		{#if formField.required}
-			<span class="text-red-600">*</span>
+			{#if formField.required}
+				<span class="text-red-600">*</span>
+			{/if}
+		</label>
+
+		{#if formField.hint}
+			<div class="mr-1.5">
+				<Tooltip text={formField.hint} />
+			</div>
 		{/if}
-	</label>
-	<span class="text-sm text-gray-500" id={formField.hintId}
-		>{formField.hint}</span
-	>
+	</div>
+	<!-- <span class="text-sm text-gray-500" id={formField.hintId}>
+			{formField.hint}
+		</span> -->
 </div>
