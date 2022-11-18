bee1c1898d4efbb49ba2d0605c73e977f7b55865
nbe <aleisa.naser@gmail.com>
2022-11-18 03:46:05 +0300

    add expense category id page

---
+++ packages/site/src/routes/organizations/[organizationId]/expense-categories/[expenseCategoryId]/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import AutoDetailsPane from '$lib/components/AutoDetailsPane.svelte';
+	import Heading from '$lib/components/Heading.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<Heading
+	title="Expense Category"
+	id={data.expenseCategory.id}
+	entity="expenseCategory"
+/>
+
+<AutoDetailsPane
+	details={data.expenseCategory}
+	keys={['labelEn', 'labelAr', 'isGroup']}
+/>
