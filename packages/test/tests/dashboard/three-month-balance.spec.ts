61ebbfdd764a052906d2f0072b3873c4e798ad97
nbe <aleisa.naser@gmail.com>
2022-11-18 15:56:55 +0300

    add dashboard placeholder tests

---
+++ packages/test/tests/dashboard/three-month-balance.spec.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { test } from '@playwright/test';
+
+test.fixme('should display correct balance', async ({}) => {});
+
+test.fixme(
+	'total expenses is sum of properties and unspecified',
+	async ({}) => {},
+);
