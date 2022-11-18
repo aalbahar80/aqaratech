cf9539b14e9b01b982c3781cf61a0cabb46d323f
nbe <aleisa.naser@gmail.com>
2022-11-18 06:17:25 +0300

    init role empty state

--- packages/site/src/lib/components/role/RoleList.svelte
+++ packages/site/src/lib/components/role/RoleList.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { ValidatedRoleDto } from '$api/openapi';
 	import RoleCard from '$lib/components/role/RoleCard.svelte';
+	import RoleEmptyState from '$lib/components/role/RoleEmptyState.svelte';
 	import StackedList from '$lib/components/StackedList.svelte';
 	import { formatDistance } from 'date-fns';
 	import Fa6SolidUserPlus from '~icons/fa6-solid/user-plus';
@@ -10,37 +11,41 @@
 	const formUrl = '/organizations/new';
 </script>
 
-<StackedList
-	entity="role"
-	count={roles.length}
-	message="No roles have been created yet."
-	formButtonProps={{
-		entity: 'role',
-		buttonText: 'Create new organization',
-		formUrl,
-	}}
-	hideActions={false}
->
-	{#each roles as role (role.id)}
-		{@const icons = [
-			{
-				label:
-					'Added ' +
-					formatDistance(new Date(role.createdAt), new Date(), {
-						addSuffix: true,
-					}),
-				icon: Fa6SolidUserPlus,
-				tooltip: 'createdAt',
-			},
-		]}
-		<li>
-			<RoleCard
-				{role}
-				{icons}
-				on:delete={(e) => {
-					roles = roles.filter((r) => r.id !== e.detail.id);
-				}}
-			/>
-		</li>
-	{/each}
-</StackedList>
+{#if roles.length > 0}
+	<StackedList
+		entity="role"
+		count={roles.length}
+		message="No roles have been created yet."
+		formButtonProps={{
+			entity: 'role',
+			buttonText: 'Create new organization',
+			formUrl,
+		}}
+		hideActions={false}
+	>
+		{#each roles as role (role.id)}
+			{@const icons = [
+				{
+					label:
+						'Added ' +
+						formatDistance(new Date(role.createdAt), new Date(), {
+							addSuffix: true,
+						}),
+					icon: Fa6SolidUserPlus,
+					tooltip: 'createdAt',
+				},
+			]}
+			<li>
+				<RoleCard
+					{role}
+					{icons}
+					on:delete={(e) => {
+						roles = roles.filter((r) => r.id !== e.detail.id);
+					}}
+				/>
+			</li>
+		{/each}
+	</StackedList>
+{:else}
+	<RoleEmptyState />
+{/if}
