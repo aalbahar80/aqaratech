520534cb9a7302600180fba976131cca2ad13af7
nbe <aleisa.naser@gmail.com>
2022-11-18 08:35:31 +0300

    re-login for new user test

--- packages/test/tests/onboarding/new-user.spec.ts
+++ packages/test/tests/onboarding/new-user.spec.ts
@@ -1,28 +1,37 @@
 import { expect, test } from '@playwright/test';
 import { testUsers } from '../api/fixtures/users/test-users';
 
+const email = 'dev.tester.1@mailthink.net';
+const password = 'cloud12';
+
+const user = { email, password };
+
 // Setting storageState should be enough to re-use auth state. However, this is
-// not working at the moment.
+// not working at the moment. So we manually login again for now.
 test.use({
 	storageState: testUsers.freshUser.storageStatePath,
-	// ignoreHTTPSErrors: true,
-	// bypassCSP: true,
-	// baseURL: process.env.PUBLIC_SITE_URL,
-	// page: async ({ browser }, use) => {
-	// 	// Create a new incognito browser context.
-	// 	const context = await browser.newContext({
-	// 		storageState: testUsers.freshUser.storageStatePath,
-	// 	});
 
-	// 	// Create a new page in a pristine context.
-	// 	const page = await context.newPage();
+	page: async ({ browser }, use) => {
+		// Create a new incognito browser context.
+		const context = await browser.newContext();
+		await context.clearCookies();
+		// Create a new page in a pristine context.
+		const page = await context.newPage();
 
-	// 	// Use fixture
-	// 	await use(page);
+		// Login
+		await page.goto('/');
+		await page.locator('text=Log In >> visible=true').click();
 
-	// 	// Gracefully close the context we created
-	// 	await context.close();
-	// },
+		await page.fill('input[name="username"]', user.email);
+		await page.fill('input[name="password"]', user.password);
+		await page.locator('button[name="action"]').click();
+
+		// Use fixture
+		await use(page);
+
+		// Gracefully close the context we created
+		await context.close();
+	},
 });
 
 test('new users are redirected to /welcome', async ({ page }) => {
