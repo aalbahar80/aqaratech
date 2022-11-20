0ce49988e8139f9fbef22b65bee3a845848eb45f
nbe <aleisa.naser@gmail.com>
2022-11-20 08:08:16 +0300

    test new category button

--- packages/test/tests/components/expense-tree/edit-button.spec.ts
+++ packages/test/tests/components/expense-tree/edit-button.spec.ts
@@ -1,4 +1,6 @@
 import { expect } from '@playwright/test';
+import { getLabel } from '@self/utils';
+import { resolveURL } from 'ufo';
 import { test } from './tree.fixture';
 
 test('expense tile links to edit form', async ({ page }) => {
@@ -14,4 +16,25 @@ test('expense tile links to edit form', async ({ page }) => {
 
 	const labelEn = page.getByLabel('Label En');
 	await expect(labelEn).toHaveValue(name);
+});
+
+test('new category button links to form', async ({ page, org }) => {
+	const btn = page.getByRole('link', { name: 'Create New category' });
+
+	const url = resolveURL(
+		'/organizations',
+		org.organization.id,
+		'expense-categories',
+		'new',
+	);
+
+	await expect.soft(btn).toHaveAttribute('href', url);
+
+	await btn.click();
+
+	await expect(page).toHaveURL(url);
+
+	const labelEn = page.getByLabel(getLabel('labelEn'));
+
+	await expect(labelEn).toBeEditable();
 });
