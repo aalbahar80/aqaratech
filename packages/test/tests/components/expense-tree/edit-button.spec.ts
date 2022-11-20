e61fdecc65410b4439effe421b3da68036844f3e
nbe <aleisa.naser@gmail.com>
2022-11-20 07:55:41 +0300

    test drag and drop tree

--- packages/test/tests/components/expense-tree/edit-button.spec.ts
+++ packages/test/tests/components/expense-tree/edit-button.spec.ts
@@ -31,3 +31,50 @@ test('expense tile links to edit form', async ({ page }) => {
 	const labelEn = page.getByLabel('Label En');
 	await expect(labelEn).toHaveValue(name);
 });
+
+test('can drag and drop expense category', async ({ page }) => {
+	const category = 'Fire Fighting Contract';
+	const group = 'Other CapEx';
+
+	const unchanged = page.getByRole('button', { name: 'No pending changes' });
+	await expect(unchanged).toBeDisabled();
+
+	const tile = page.getByText(category);
+	// const tile = page.getByText('Other CapEx');
+
+	const newCategory = page.getByText(group);
+
+	await page.waitForTimeout(1000);
+
+	// hover tile
+	await tile.hover();
+
+	await page.waitForTimeout(1000);
+
+	// hold left mouse button
+	await page.mouse.down();
+
+	await page.waitForTimeout(1000);
+
+	// move mouse to new category
+	// Hover twice on purpose: https://playwright.dev/docs/next/input#dragging-manually
+	await newCategory.hover();
+	await newCategory.hover();
+
+	await page.waitForTimeout(1000);
+
+	// release left mouse button
+	await page.mouse.up();
+
+	const save = page.getByRole('button', { name: 'Save changes' });
+	await expect(save).toBeEnabled();
+
+	const pending = page.getByText(`${group} → ${category}`);
+
+	await expect(pending).toBeVisible();
+
+	await save.click();
+
+	await expect(unchanged).toBeDisabled();
+	await expect(pending).not.toBeVisible();
+});
