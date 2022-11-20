7c2bb5f6949eb3fa08d121e51acf250a3dc2d505
nbe <aleisa.naser@gmail.com>
2022-11-20 12:55:32 +0300

    test role form buttons

---
+++ packages/test/tests/forms/role/form-link-buttons.spec.ts
@@ -0,0 +1,52 @@
+import { expect } from '@playwright/test';
+import { getRoute, PageTab } from '@self/utils';
+import { resolveURL } from 'ufo';
+import { test } from '../../api/api-fixtures';
+
+const inputs = ['tenant', 'portfolio', 'organization'] as const;
+
+for (const input of inputs) {
+	test(`create ${input} role button has predefined params`, async ({
+		page,
+		org,
+		portfolio,
+		tenant,
+	}) => {
+		const entities = {
+			organization: org.organization,
+			portfolio,
+			tenant,
+		};
+
+		const entity = entities[input];
+
+		const id = entity.id;
+
+		const url = getRoute({
+			entity: input,
+			id: id,
+			pageType: PageTab.Roles,
+			params: {
+				organizationId: org.organization.id,
+			},
+		});
+
+		const form = resolveURL(
+			'/organizations',
+			org.organization.id,
+			'roles',
+			'new',
+			`?relationKey=${input}&relationValue=${id}`,
+		);
+
+		await page.goto(url);
+
+		const btn = page.getByRole('link', { name: '+ New' });
+
+		await expect(btn).toHaveAttribute('href', form);
+
+		await btn.click();
+
+		await expect(page).toHaveURL(form);
+	});
+}
