1ca7fc4e515f1cc9226d92ea95e598b766119b27
nbe <aleisa.naser@gmail.com>
2022-11-20 08:31:01 +0300

    test badge update for invoice

--- packages/test/tests/forms/lease-invoice/checkbox/set-false.spec.ts
+++ packages/test/tests/forms/lease-invoice/checkbox/set-false.spec.ts
@@ -1,5 +1,5 @@
 import { expect } from '@playwright/test';
-import { getLabel, PageType } from '@self/utils';
+import { getLabel, getRoute, PageType } from '@self/utils';
 import { test } from '../../../api/api-fixtures';
 import { FormPage } from '../../form-page-model';
 
@@ -10,6 +10,7 @@ test.use({
 	invoicesParams: [
 		{
 			isPaid: true,
+			postAt: '2020-01-01',
 		},
 	],
 });
@@ -37,4 +38,41 @@ test('can set to false', async ({ org, portfolio, invoice, page }) => {
 	await formPage.verifyDetails(fields);
 
 	await expect(page).toHaveURL(formPage.getSuccessUrl());
+});
+
+test('badge updates with value', async ({ org, portfolio, invoice, page }) => {
+	const url = getRoute({
+		entity,
+		pageType: PageType.Id,
+		id: invoice.id,
+		params: {
+			organizationId: org.organization.id,
+			portfolioId: portfolio.id,
+		},
+	});
+
+	await page.goto(url);
+
+	const badge = page.getByTestId('badge');
+
+	await expect(badge).toHaveText('Paid');
+
+	await page.getByRole('link', { name: 'Edit' }).click();
+
+	// update invoice to be paid
+
+	const formPage = new FormPage(page);
+
+	const fields = {
+		isPaid: false,
+		paidAt: null,
+	};
+
+	await formPage.fillForm(fields);
+
+	await formPage.save();
+
+	// badge should update
+
+	await expect(badge).toHaveText('Past due');
 });
