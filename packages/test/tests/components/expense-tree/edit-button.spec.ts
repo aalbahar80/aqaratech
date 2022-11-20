759c5e4080a0fdc35d76713af8da0929f5220464
nbe <aleisa.naser@gmail.com>
2022-11-20 07:38:16 +0300

    create fixture for tree

--- packages/test/tests/components/expense-tree/edit-button.spec.ts
+++ packages/test/tests/components/expense-tree/edit-button.spec.ts
@@ -2,21 +2,26 @@ import { expect } from '@playwright/test';
 import { getRoute, PageTab } from '@self/utils';
 import { test } from '../../api/api-fixtures';
 
-test('expense tile links to edit form', async ({ page, org }) => {
-	const url = getRoute({
-		entity: 'organization',
-		id: org.organization.id,
-		pageType: PageTab.ExpenseCategories,
-		params: {},
-	});
+test.use({
+	page: async ({ page, org }, use) => {
+		const url = getRoute({
+			entity: 'organization',
+			id: org.organization.id,
+			pageType: PageTab.ExpenseCategories,
+			params: {},
+		});
 
-	await page.goto(url);
+		await page.goto(url);
 
+		await use(page);
+	},
+});
+
+test('expense tile links to edit form', async ({ page }) => {
 	const name = 'Fire Fighting Contract';
 
 	const tile = page.getByText(name);
 
-	// await tile.getByText('Edit').click();
 	await tile.getByRole('link', { name: 'Edit' }).click();
 
 	// expect page to have url ending with /edit
