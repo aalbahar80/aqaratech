9dcf665cf3f3ec17b2cce616697abaf529fc7fc7
nbe <aleisa.naser@gmail.com>
2022-11-21 13:52:27 +0300

    rm dupe range select

--- packages/site/src/routes/organizations/[organizationId]/portfolios/[portfolioId]/financials/expenses/+page.svelte
+++ packages/site/src/routes/organizations/[organizationId]/portfolios/[portfolioId]/financials/expenses/+page.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
 	import ExpenseTreemapCategory from '$lib/components/charts/treemap/ExpenseTreemapCategory.svelte';
 	import ExpenseTreemapProperty from '$lib/components/charts/treemap/ExpenseTreemapProperty.svelte';
-	import RangeSelect from '$lib/components/dashboard/RangeSelect.svelte';
 	import type { PageData } from './$types';
 	import BarChart from './BarChart.svelte';
 
 	export let data: PageData;
 </script>
-
-<RangeSelect />
 
 <BarChart expenses={data.expensesByMonth} />
 
