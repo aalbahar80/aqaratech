2c988dbccd97cabaa1414381c1c1e9f27ac6ecb1
nbe <aleisa.naser@gmail.com>
2022-11-22 00:42:53 +0300

    wire start/end filter in expenses table

--- packages/site/src/routes/organizations/[organizationId]/portfolios/[portfolioId]/financials/expenses/table/+page.ts
+++ packages/site/src/routes/organizations/[organizationId]/portfolios/[portfolioId]/financials/expenses/table/+page.ts
@@ -1,7 +1,8 @@
 import { createApi } from '$api';
 import { FilterEnum } from '$lib/stores/filter/Filter.enum';
-import { parseParams } from '$lib/utils/parse-params';
 import { property } from '$lib/stores/filter/property';
+import { range } from '$lib/stores/filter/range';
+import { parseParams } from '$lib/utils/parse-params';
 import { get } from 'svelte/store';
 import type { PageLoad } from './$types';
 
@@ -13,16 +14,29 @@ export const load: PageLoad = async ({
 }) => {
 	// Filter options
 	const propertyId = get(property);
-	depends(FilterEnum.Property);
-	// TODO: enable range here.
-	// const { start, end } = get(range);
-	// depends(FilterEnum.Range, FilterEnum.Property);
+	const { start, end } = get(range);
+	depends(FilterEnum.Range, FilterEnum.Property);
+
+	// If we use filter from the URL, we need to make an update here to avoid
+	// overriding the start/end when spreading the searchParams.
+	const queryFilter = searchParams.get('filter');
+
+	if (queryFilter) {
+		throw new Error(
+			'Update this code to merge the URL + start/end filter options',
+		);
+	}
+
+	const filter = {
+		postAt: { gte: new Date(start), lte: new Date(end) },
+	};
 
 	const api = createApi(fetch);
 
 	const expenses = await api.portfolios.findAllExpenses({
 		id: params.portfolioId,
 		propertyId,
+		filter,
 		...parseParams(searchParams),
 	});
 
