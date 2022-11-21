de1d0395a8e3cb16c68b09a142a410943f5ce233
nbe <aleisa.naser@gmail.com>
2022-11-21 14:12:46 +0300

    move RangeSelect up to financials layout

---
+++ packages/site/src/routes/organizations/[organizationId]/portfolios/[portfolioId]/financials/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import RangeSelect from '$lib/components/dashboard/RangeSelect.svelte';
+</script>
+
+<div class="inline-flex justify-end">
+	<div class="w-72">
+		<RangeSelect />
+	</div>
+</div>
+
+<slot />
--- packages/site/src/routes/organizations/[organizationId]/portfolios/[portfolioId]/financials/income/+layout.svelte
+++ packages/site/src/routes/organizations/[organizationId]/portfolios/[portfolioId]/financials/income/+layout.svelte
@@ -1,17 +1,10 @@
 <script lang="ts">
-	import RangeSelect from '$lib/components/dashboard/RangeSelect.svelte';
 	import DataTabs from '../DataTabs.svelte';
 	import type { LayoutData } from './$types';
 	import StatsIncome from './StatsIncome.svelte';
 
 	export let data: LayoutData;
 </script>
-
-<div class="inline-flex justify-end">
-	<div class="w-72">
-		<RangeSelect />
-	</div>
-</div>
 
 <StatsIncome {data} />
 
