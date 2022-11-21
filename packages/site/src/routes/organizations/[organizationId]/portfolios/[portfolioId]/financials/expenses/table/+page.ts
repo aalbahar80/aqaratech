838abda0077386c307e624eb29aadfb93111b8e7
nbe <aleisa.naser@gmail.com>
2022-11-21 16:07:08 +0300

    pass propertyId to expenses table

--- packages/site/src/routes/organizations/[organizationId]/portfolios/[portfolioId]/financials/expenses/table/+page.ts
+++ packages/site/src/routes/organizations/[organizationId]/portfolios/[portfolioId]/financials/expenses/table/+page.ts
@@ -1,16 +1,28 @@
 import { createApi } from '$api';
+import { FilterEnum } from '$lib/stores/filter/Filter.enum';
 import { parseParams } from '$lib/utils/parse-params';
+import { property } from '$lib/stores/filter/property';
+import { get } from 'svelte/store';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({
 	params,
 	url: { searchParams },
 	fetch,
+	depends,
 }) => {
+	// Filter options
+	const propertyId = get(property);
+	depends(FilterEnum.Property);
+	// TODO: enable range here.
+	// const { start, end } = get(range);
+	// depends(FilterEnum.Range, FilterEnum.Property);
+
 	const api = createApi(fetch);
 
 	const expenses = await api.portfolios.findAllExpenses({
 		id: params.portfolioId,
+		propertyId,
 		...parseParams(searchParams),
 	});
 
