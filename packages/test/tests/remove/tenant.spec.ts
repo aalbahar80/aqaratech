c5962e295d1353c9a17188af2acbe3e368eaa35c
nbe <aleisa.naser@gmail.com>
2022-11-22 19:06:57 +0300

    test tenant and unit delete

---
+++ packages/test/tests/remove/tenant.spec.ts
@@ -0,0 +1,26 @@
+import { getRoute, PageType } from '@self/utils';
+import * as R from 'remeda';
+import { test } from '../api/api-fixtures';
+import { RemoveModel } from './remove-model';
+
+test.use({
+	tenantsParams: R.times(3, () => ({})),
+});
+
+test('delete tenant', async ({ page, org, tenants }) => {
+	const url = getRoute({
+		entity: 'tenant',
+		pageType: PageType.List,
+		params: {
+			organizationId: org.organization.id,
+		},
+	});
+
+	const model = new RemoveModel({
+		page,
+		url,
+		items: tenants,
+	});
+
+	await model.deleteAndVerify();
+});
---
+++ packages/test/tests/remove/unit.spec.ts
@@ -0,0 +1,28 @@
+import { getRoute, PageTab } from '@self/utils';
+import * as R from 'remeda';
+import { test } from '../api/api-fixtures';
+import { RemoveModel } from './remove-model';
+
+test.use({
+	unitsParams: R.times(3, () => ({})),
+});
+
+test('delete unit', async ({ page, org, units }) => {
+	const url = getRoute({
+		entity: 'property',
+		pageType: PageTab.Units,
+		id: units[0].propertyId,
+		params: {
+			organizationId: org.organization.id,
+			portfolioId: units[0].portfolioId,
+		},
+	});
+
+	const model = new RemoveModel({
+		page,
+		url,
+		items: units,
+	});
+
+	await model.deleteAndVerify();
+});
