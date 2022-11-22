12dce2bbc7731b8daff8f49bb2caf5a135f4f05b
nbe <aleisa.naser@gmail.com>
2022-11-22 10:05:01 +0300

    convert onConfirm prop

--- packages/site/src/lib/components/toast/Modal.svelte
+++ packages/site/src/lib/components/toast/Modal.svelte
@@ -37,7 +37,6 @@
 	import HeroiconsExclamationTriangle from '~icons/heroicons/exclamation-triangle';
 
 	export let isLoading: boolean;
-	export let handleConfirm: () => Promise<void>;
 
 	let promptInput = '';
 </script>
@@ -143,7 +142,7 @@
 								class="order-first mt-3 inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
 								disabled={$isOpen.deletePrompt !== '' &&
 									promptInput !== $isOpen.deletePrompt}
-								on:click={handleConfirm}
+								on:click={$isOpen.onConfirm}
 							>
 								<Spinner loading={isLoading} />
 								Delete
