70b6fea50bc4d4f12ba44218dd0fb89c2a7b84b3
nbe <aleisa.naser@gmail.com>
2022-11-22 11:19:24 +0300

    init create modal delete factory

---
+++ packages/site/src/lib/components/toast/create-modal-delete.ts
@@ -0,0 +1,31 @@
+import { createApi, type Api } from '$api';
+import { handleApiError } from '$api/handle-api-error';
+import { goto } from '$app/navigation';
+import type { ModalContent } from '$lib/components/toast/modal-content';
+import { addSuccessToast } from '$lib/stores/toast';
+
+/**
+ * Factory function to create ModalContent for a delete modal.
+ */
+export const createModalDelete = ({
+	deletePrompt,
+	onDelete,
+}: {
+	deletePrompt?: string;
+	onDelete: (api: Api) => Promise<string>;
+}): ModalContent => {
+	return {
+		title: 'Delete',
+		description: 'Are you sure?',
+		deletePrompt: deletePrompt ?? '',
+		onConfirm: async () => {
+			try {
+				const url = await onDelete(createApi());
+				addSuccessToast();
+				void goto(url);
+			} catch (error) {
+				await handleApiError(error);
+			}
+		},
+	};
+};
