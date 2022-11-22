67bf0077ff6acece839b7422efcc0194fc14e07b
nbe <aleisa.naser@gmail.com>
2022-11-22 19:33:09 +0300

    add note about 404 redirecting

--- packages/site/src/routes/+layout.ts
+++ packages/site/src/routes/+layout.ts
@@ -6,6 +6,11 @@ import type { LayoutLoad } from './$types';
 
 // eslint-disable-next-line @typescript-eslint/require-await
 export const load: LayoutLoad = async ({ fetch, data, url: { pathname } }) => {
+	// Checking for data.user and redirecting here causes this function to catch
+	// all the random requests by bots and crawlers that are not logged in.
+	// If this becomes a problem, we can move this function one level deeper,
+	// letting all random requests fall through to the 404 page instead of
+	// redirecting them to the auth0 login page.
 	if (!isPublicRoute(pathname) && !data.user) {
 		throw redirect(302, LOGIN);
 	}
