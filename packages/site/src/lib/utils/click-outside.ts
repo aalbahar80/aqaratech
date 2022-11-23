85934009a6e917444ee5c4ecad36b595641f49c9
nbe <aleisa.naser@gmail.com>
2022-11-23 17:10:04 +0300

    replace click outside with typesafe version

--- packages/site/src/lib/utils/click-outside.ts
+++ packages/site/src/lib/utils/click-outside.ts
@@ -1,15 +1,24 @@
+/**
+ * Emits `clickoutside` event whenever pointer clicks outside of node
+ *
+ * Reference: https://cs.github.com/bluwy/pick-palette/blob/53847c3e5e6688e0b46854654e4784ad30e52561/src/actions/click-outside.ts?q=export+function+clickOutside%28node%3A+HTMLElement%29+%7B#L6
+ */
 export function clickOutside(node: HTMLElement) {
-	const handleClick = (event: any) => {
-		if (!node.contains(event.target)) {
-			node.dispatchEvent(new CustomEvent('outclick'));
+	function handleClick(e: MouseEvent) {
+		if (
+			e.target instanceof Node &&
+			!node.contains(e.target) &&
+			!e.defaultPrevented
+		) {
+			node.dispatchEvent(new MouseEvent('outclick', e));
 		}
-	};
+	}
 
-	document.addEventListener('click', handleClick, true);
+	document.addEventListener('click', handleClick);
 
 	return {
 		destroy() {
-			document.removeEventListener('click', handleClick, true);
+			document.removeEventListener('click', handleClick);
 		},
 	};
 }
