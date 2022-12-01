9ab92c611e3225020c2f8e0faf5590d57168dc84
nbe <aleisa.naser@gmail.com>
2022-12-02 00:06:09 +0300

    add note to selectedLabel helper

--- packages/test/utils/selected-label.ts
+++ packages/test/utils/selected-label.ts
@@ -1,4 +1,18 @@
 import type { Locator } from '@playwright/test';
 
+/**
+ * Get the selected label of a select element.
+ *
+ * This *does not* auto wait for the label. Therefore, it is recommended to use
+ * `toHaveValue` before calling this function.
+ *
+ * Example:
+ *
+ * ```typescript
+ * const el = page.getByRole('combobox', { name: 'Property' });
+ * await expect(el).toHaveValue('123');
+ * expect(await selectedLabel(el)).toBe('My Property');
+ * ```
+ */
 export const selectedLabel = async (loc: Locator) =>
 	loc.evaluate((e: HTMLSelectElement) => e.options[e.selectedIndex]?.innerText);
