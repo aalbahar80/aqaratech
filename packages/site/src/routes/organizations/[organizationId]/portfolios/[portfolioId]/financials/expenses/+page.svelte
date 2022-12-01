867526b9ed7100766869210fc81dc665182f1575
nbe <aleisa.naser@gmail.com>
2022-12-01 23:08:37 +0300

    add empty state for treemaps

--- packages/site/src/routes/organizations/[organizationId]/portfolios/[portfolioId]/financials/expenses/+page.svelte
+++ packages/site/src/routes/organizations/[organizationId]/portfolios/[portfolioId]/financials/expenses/+page.svelte
@@ -3,15 +3,20 @@
 	import ExpenseTreemapProperty from '$lib/components/charts/treemap/ExpenseTreemapProperty.svelte';
 	import type { PageData } from './$types';
 	import BarChart from './BarChart.svelte';
+	import ChartWrapper from '$lib/components/dashboard/cards/ChartWrapper.svelte';
 
 	export let data: PageData;
 </script>
 
 <BarChart expenses={data.expensesByMonth} />
 
-<ExpenseTreemapProperty expenses={data.expensesByLocation} />
+<ChartWrapper empty={data.expensesByLocation.length < 1}>
+	<ExpenseTreemapProperty expenses={data.expensesByLocation} />
+</ChartWrapper>
 
-<ExpenseTreemapCategory
-	expenses={data.expensesByCategory}
-	categories={data.categories}
-/>
+<ChartWrapper empty={data.expensesByCategory.length < 1}>
+	<ExpenseTreemapCategory
+		expenses={data.expensesByCategory}
+		categories={data.categories}
+	/>
+</ChartWrapper>
