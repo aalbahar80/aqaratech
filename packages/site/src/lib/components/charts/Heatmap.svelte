4376dbd1b9781fe3106ca305ba688a2621d3d4e1
nbe <aleisa.naser@gmail.com>
2022-12-02 07:53:05 +0300

    rm unused var

--- packages/site/src/lib/components/charts/Heatmap.svelte
+++ packages/site/src/lib/components/charts/Heatmap.svelte
@@ -3,7 +3,6 @@
 	import Chart from 'svelte-frappe-charts';
 
 	const colors = ['#ebedf0', '#c0ddf9', '#73b3f3', '#3886e1', '#17459e'];
-	const date1 = new Date().getTime() / 1000;
 	const data = {
 		dataPoints: {
 			1661065253.095: 20,
