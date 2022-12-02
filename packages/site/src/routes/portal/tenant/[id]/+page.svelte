d3b29f832e5b24160b05817c95bd5ad265823927
nbe <aleisa.naser@gmail.com>
2022-12-02 09:43:43 +0300

    rm old portal setup

--- packages/site/src/routes/portal/tenant/[id]/+page.svelte
+++ packages/site/src/routes/portal/tenant/[id]/+page.svelte
@@ -5,25 +5,4 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
-
-	const options = data.leases.results.map((lease, idx) => ({
-		value: lease.id,
-		label: `Lease #${data.leases.results.length - idx}: ${toUTCFormat(
-			lease.start,
-		)} - ${toUTCFormat(lease.end)}`,
-	}));
-
-	let selected = options[0]?.value;
-	$: filtered = data.invoices.results.filter(
-		(invoice) => invoice.leaseId === selected,
-	);
 </script>
-
-<!-- Use on:someEvent|once to display the payment successful badge? See docs abount `once` event modifier  -->
-<TrxList invoices={filtered}>
-	<Select
-		disabled={data.leases.results.length < 2}
-		{options}
-		bind:current={selected}
-	/>
-</TrxList>
--- packages/site/src/routes/portal/tenant/[id]/+page.ts
+++ packages/site/src/routes/portal/tenant/[id]/+page.ts
@@ -4,10 +4,9 @@ import type { PageLoad } from './$types';
 export const load: PageLoad = async ({ params, fetch }) => {
 	const api = createApi(fetch);
 
-	const [leases, invoices] = await Promise.all([
-		api.tenants.findLeases({ id: params.id, take: 50 }),
-		api.tenants.findInvoices({ id: params.id, take: 1000 }),
-	]);
+	const invoices = await api.tenants.findInvoices({ id: params.id });
 
-	return { leases, invoices };
+	return {
+		invoices,
+	};
 };
