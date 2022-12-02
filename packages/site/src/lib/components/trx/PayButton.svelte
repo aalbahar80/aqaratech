a69c087e643c13aa37c6a646dce13c727e755f2f
nbe <aleisa.naser@gmail.com>
2022-12-02 09:09:42 +0300

    disable pay button temporarily

--- packages/site/src/lib/components/trx/PayButton.svelte
+++ packages/site/src/lib/components/trx/PayButton.svelte
@@ -1,44 +1,16 @@
 <script lang="ts">
 	import type { LeaseInvoiceDto } from '$api/openapi';
-	import { goto } from '$app/navigation';
 	import Button from '$lib/components/buttons/Button.svelte';
-	import { addToast } from '$lib/stores/toast';
+	import { addErrorToast } from '$lib/stores/toast';
 	import HeroiconsOutlineCash from '~icons/heroicons-outline/cash';
 
 	export let invoice: LeaseInvoiceDto;
 
-	let loading = false;
-	const handlePayment = async () => {
-		// TODO: move this to use:action
-		// TODO: button spinner should also be use:action
-		loading = true;
-		try {
-			const res = await fetch(`/api/payments/getUrl?id=${invoice.id}`);
-			const data = await res.json();
-			if (res.ok) {
-				goto(data.paymentUrl).catch(console.error);
-			} else {
-				addToast({
-					props: {
-						kind: 'error',
-						title: 'Error',
-						subtitle: data.errorMsg,
-					},
-				});
-			}
-		} catch (err) {
-			console.error({ err }, 'PayButton.svelte ~ 24');
-			addToast({
-				props: {
-					kind: 'error',
-					title: 'Error',
-					subtitle: 'An error occured while grabbing payment url',
-				},
-			});
-		} finally {
-			loading = false;
-		}
+	const handlePayment = () => {
+		addErrorToast('Online payment not available yet');
 	};
+	// TODO: move this to use:action
+	// TODO: button spinner should also be use:action
 </script>
 
 <Button
@@ -46,7 +18,7 @@
 	--min-height="4rem"
 	text="Pay"
 	icon={HeroiconsOutlineCash}
-	disabled={new Date(invoice.postAt) > new Date()}
-	{loading}
+	disabled={true}
+	loading={false}
 	on:click={handlePayment}
 />
