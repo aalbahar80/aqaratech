8a103c0526cad7237a07053fcc3a93dbf3c6ddc7
nbe <aleisa.naser@gmail.com>
2022-12-04 04:35:59 +0300

    disable expense tree on mobile

--- packages/site/src/routes/organizations/[organizationId]/expense-categories/+page.svelte
+++ packages/site/src/routes/organizations/[organizationId]/expense-categories/+page.svelte
@@ -28,74 +28,86 @@
 
 	$: newList = fromHeirarchy({ root, original });
 	$: difference = diff(original, newList);
+
+	const isMobile =
+		typeof window !== 'undefined' &&
+		navigator.userAgent.match(
+			/Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i,
+		);
 </script>
 
-<a
-	href={getRoute({
-		entity: 'expenseCategory',
-		pageType: PageType.New,
-		params: $page.params,
-	})}
-	class="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
->
-	<Fa6SolidPlus class="-ml-1 mr-2 hidden h-5 w-5 sm:block" />
-	Create new category
-</a>
-
-<div class="flex flex-auto justify-between">
-	<div class="w-full">
-		<ExpenseTree node={root} bind:root />
-	</div>
-	<div
-		use:preventTabClose={!!difference.length}
-		class="sticky top-0 flex w-2/6 flex-initial flex-col self-start p-2"
+{#if isMobile}
+	This page is not supported on mobile devices. Please use a desktop browser.
+{:else}
+	<a
+		href={getRoute({
+			entity: 'expenseCategory',
+			pageType: PageType.New,
+			params: $page.params,
+		})}
+		class="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
 	>
-		<Button
-			icon={HeroiconsCheck}
-			text={difference.length ? `Save changes` : 'No pending changes'}
-			disabled={difference.length === 0}
-			as="button"
-			class="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-			on:click={() => {
-				console.debug(difference);
-				console.debug(newList);
+		<Fa6SolidPlus class="-ml-1 mr-2 hidden h-5 w-5 sm:block" />
+		Create new category
+	</a>
 
-				const organizationId = $page.params.organizationId;
-
-				// type workaround
-				if (!organizationId) {
-					throw new Error('Organization ID is required');
-				}
-
-				createApi()
-					.expenseCategories.updateAll({
-						updateExpenseCategoryTreeDto: newList.map((category) =>
-							R.omit(category, ['isGroup']),
-						),
-						organizationId,
-					})
-					.then((res) => {
-						addSuccessToast();
-						data.categories = res;
-					})
-					.catch(handleApiError);
-			}}
+	<div class="flex flex-auto justify-between">
+		<div class="w-full">
+			<ExpenseTree node={root} bind:root />
+		</div>
+		<div
+			use:preventTabClose={!!difference.length}
+			class="sticky top-0 flex w-2/6 flex-initial flex-col self-start p-2"
 		>
-			<Fa6SolidFloppyDisk />
-		</Button>
-		{#each difference as change}
-			{@const newParentNode = newList.find((node) => node.id === change.value)}
-			{@const changedNode =
-				typeof change.path[0] === 'number' ? newList[change.path[0]] : null}
-			<div class="flex items-center justify-between py-2 text-sm font-medium">
-				<span class="w-5/12">
-					{newParentNode?.labelEn || ''}
-				</span>
-				<span aria-hidden="true"> &rarr; </span>
-				<span class="w-5/12 text-indigo-600">
-					{changedNode?.labelEn}
-				</span>
-			</div>
-		{/each}
+			<Button
+				icon={HeroiconsCheck}
+				text={difference.length ? `Save changes` : 'No pending changes'}
+				disabled={difference.length === 0}
+				as="button"
+				class="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+				on:click={() => {
+					console.debug(difference);
+					console.debug(newList);
+
+					const organizationId = $page.params.organizationId;
+
+					// type workaround
+					if (!organizationId) {
+						throw new Error('Organization ID is required');
+					}
+
+					createApi()
+						.expenseCategories.updateAll({
+							updateExpenseCategoryTreeDto: newList.map((category) =>
+								R.omit(category, ['isGroup']),
+							),
+							organizationId,
+						})
+						.then((res) => {
+							addSuccessToast();
+							data.categories = res;
+						})
+						.catch(handleApiError);
+				}}
+			>
+				<Fa6SolidFloppyDisk />
+			</Button>
+			{#each difference as change}
+				{@const newParentNode = newList.find(
+					(node) => node.id === change.value,
+				)}
+				{@const changedNode =
+					typeof change.path[0] === 'number' ? newList[change.path[0]] : null}
+				<div class="flex items-center justify-between py-2 text-sm font-medium">
+					<span class="w-5/12">
+						{newParentNode?.labelEn || ''}
+					</span>
+					<span aria-hidden="true"> &rarr; </span>
+					<span class="w-5/12 text-indigo-600">
+						{changedNode?.labelEn}
+					</span>
+				</div>
+			{/each}
+		</div>
 	</div>
-</div>
+{/if}
