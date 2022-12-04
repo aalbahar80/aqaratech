769073575f3f510eeffee71c3c3ec6bee55a09ed
nbe <aleisa.naser@gmail.com>
2022-12-04 06:34:23 +0300

    don't create api in layout

--- packages/site/src/routes/+layout.ts
+++ packages/site/src/routes/+layout.ts
@@ -1,11 +1,10 @@
-import { createApi } from '$api';
 import { LOGIN } from '$lib/constants/routes';
 import { isPublicRoute } from '$lib/utils/is-public-route';
 import { redirect } from '@sveltejs/kit';
 import type { LayoutLoad } from './$types';
 
 // eslint-disable-next-line @typescript-eslint/require-await
-export const load: LayoutLoad = async ({ fetch, data, url: { pathname } }) => {
+export const load: LayoutLoad = async ({ data, url: { pathname } }) => {
 	// Checking for data.user and redirecting here causes this function to catch
 	// all the random requests by bots and crawlers that are not logged in.
 	// If this becomes a problem, we can move this function one level deeper,
@@ -15,10 +14,7 @@ export const load: LayoutLoad = async ({ fetch, data, url: { pathname } }) => {
 		throw redirect(302, LOGIN);
 	}
 
-	const apiClient = createApi(fetch);
-
 	return {
-		api: apiClient,
 		...data,
 	};
 };
