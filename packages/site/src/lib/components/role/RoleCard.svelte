fe4869890c133f1e3df73371a50fba29746f986b
nbe <aleisa.naser@gmail.com>
2022-12-04 05:01:03 +0300

    add spacing to role card

--- packages/site/src/lib/components/role/RoleCard.svelte
+++ packages/site/src/lib/components/role/RoleCard.svelte
@@ -15,43 +15,40 @@
 <a
 	href={`/auth/roles/${role.id}`}
 	data-sveltekit-reload
-	class="block hover:bg-gray-50"
+	class="flex flex-col gap-4 px-4 py-4 hover:bg-gray-50 sm:px-6"
 >
-	<div class="px-4 py-4 sm:px-6">
-		<div class="flex h-12 items-center justify-between">
-			<p class="select-all truncate text-sm font-medium text-indigo-600">
-				{role.organization.title}
-			</p>
-			<div class="flex place-items-center space-x-4">
-				{#if role.roleType === 'ORGADMIN'}
-					<Badge label={meta.roleLabel} badgeColor="indigo">
-						<BxsBusiness class="mr-2 inline" />
-					</Badge>
-				{:else if role.roleType === 'PORTFOLIO'}
-					<Badge label={meta.roleLabel} badgeColor="indigo">
-						<MdiAccountTie class="mr-2 inline" />
-					</Badge>
-				{:else if role.roleType === 'TENANT'}
-					<Badge label={meta.roleLabel} badgeColor="indigo">
-						<MdiAccount class="mr-2 inline" />
-					</Badge>
+	<div class="self-end">
+		{#if role.roleType === 'ORGADMIN'}
+			<Badge label={meta.roleLabel} badgeColor="indigo">
+				<BxsBusiness class="mr-2 inline" />
+			</Badge>
+		{:else if role.roleType === 'PORTFOLIO'}
+			<Badge label={meta.roleLabel} badgeColor="indigo">
+				<MdiAccountTie class="mr-2 inline" />
+			</Badge>
+		{:else if role.roleType === 'TENANT'}
+			<Badge label={meta.roleLabel} badgeColor="indigo">
+				<MdiAccount class="mr-2 inline" />
+			</Badge>
+		{/if}
+	</div>
+
+	<p class="select-all truncate text-sm font-medium text-indigo-600">
+		{role.organization.title}
+	</p>
+	<div class="mt-2 sm:flex sm:justify-between">
+		<div class="sm:flex sm:space-x-4">
+			{#each icons as { label, icon, tooltip } (tooltip)}
+				{#if label}
+					<p class="flex items-center text-sm text-gray-500">
+						<svelte:component
+							this={icon}
+							class="mr-1.5 h-4 w-4 flex-shrink-0 text-gray-400"
+						/>
+						{label}
+					</p>
 				{/if}
-			</div>
-		</div>
-		<div class="mt-2 sm:flex sm:justify-between">
-			<div class="sm:flex sm:space-x-4">
-				{#each icons as { label, icon, tooltip } (tooltip)}
-					{#if label}
-						<p class="flex items-center text-sm text-gray-500">
-							<svelte:component
-								this={icon}
-								class="mr-1.5 h-4 w-4 flex-shrink-0 text-gray-400"
-							/>
-							{label}
-						</p>
-					{/if}
-				{/each}
-			</div>
+			{/each}
 		</div>
 	</div>
 </a>
