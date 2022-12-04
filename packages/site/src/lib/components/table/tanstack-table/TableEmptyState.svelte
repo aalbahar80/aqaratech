cfa2fd9d6c22adfd1e420f6e0edecbc1a8f941a8
nbe <aleisa.naser@gmail.com>
2022-12-04 07:48:15 +0300

    increase empty state padding

--- packages/site/src/lib/components/table/tanstack-table/TableEmptyState.svelte
+++ packages/site/src/lib/components/table/tanstack-table/TableEmptyState.svelte
@@ -11,7 +11,7 @@
 		class="flex h-full flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 text-center"
 	>
 		<HeroiconsTableCells class="mx-auto h-20 w-20  text-gray-300" />
-		<span class="block pt-8 text-xl font-light text-gray-900">
+		<span class="block px-8 pt-8 text-xl font-light text-gray-900">
 			There are no items to display.
 		</span>
 	</div>
