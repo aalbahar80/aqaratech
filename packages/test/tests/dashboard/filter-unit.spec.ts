b99d0ff3556a1c560df910fb1d1fb21ed3d4d2f5
nbe <aleisa.naser@gmail.com>
2022-12-04 22:47:07 +0300

    loop through the properties

--- packages/test/tests/dashboard/filter-unit.spec.ts
+++ packages/test/tests/dashboard/filter-unit.spec.ts
@@ -31,22 +31,25 @@ test('all units are shown for each property', async ({
 
 	const filters = new Filters(page);
 
-	// Select a property
-	await filters.property.el.selectOption({ value: properties[0].id });
+	// Check that all units are shown for each property
+	for (const p of properties) {
+		// Select a property
+		await filters.property.el.selectOption({ value: p.id });
 
-	// Check that all units are shown
-	const el = filters.unit.el;
+		// Check that all units are shown
+		const el = filters.unit.el;
 
-	const options = await el.evaluate((el: HTMLSelectElement) => {
-		// return an array of unit id's + 'null' + 'undefined'
-		return Array.from(el.options).map((option) => option.value);
-	});
+		const options = await el.evaluate((el: HTMLSelectElement) => {
+			// return an array of unit id's + 'null' + 'undefined'
+			return Array.from(el.options).map((option) => option.value);
+		});
 
-	// check that all of properties[0].units are available to select from
-	const unitIds = units
-		.filter((unit) => unit.propertyId === properties[0].id)
-		.map((unit) => unit.id);
+		// check that all the properties' units are available to select from
+		const unitIds = units
+			.filter((unit) => unit.propertyId === p.id)
+			.map((unit) => unit.id);
 
-	expect.soft(options).toHaveLength(unitIds.length + 2); // +2 for 'null' and 'undefined'
-	expect(options).toEqual(expect.arrayContaining(unitIds));
+		expect.soft(options).toHaveLength(unitIds.length + 2); // +2 for 'null' and 'undefined'
+		expect.soft(options).toEqual(expect.arrayContaining(unitIds));
+	}
 });
