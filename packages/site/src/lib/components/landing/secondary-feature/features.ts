f29de164a24c855d2a97822acaef35a51e29bc54
nbe <aleisa.naser@gmail.com>
2022-12-07 04:05:33 +0300

    replace placeholder text in secondary features

--- packages/site/src/lib/components/landing/secondary-feature/features.ts
+++ packages/site/src/lib/components/landing/secondary-feature/features.ts
@@ -1,36 +1,37 @@
+// Screenshots
 import screenshotContacts from '../../../../assets/screenshots/contacts.png';
 import screenshotInventory from '../../../../assets/screenshots/inventory.png';
 import screenshotProfitLoss from '../../../../assets/screenshots/profit-loss.png';
-import HeroiconsArrowTrendingUpSolid from '~icons/heroicons/arrow-trending-up-solid';
-import BxsBusiness from '~icons/bxs/business';
-import MdiAccount from '~icons/mdi/account';
+
+// Icons
+import HeroiconsBuildingOffice2 from '~icons/heroicons/building-office-2';
+import HeroiconsUserGroup from '~icons/heroicons/user-group';
 import MdiAccountTie from '~icons/mdi/account-tie';
 
 export const secondaryFeatures = [
 	{
-		name: 'Reporting',
-		summary: 'Stay on top of things with always up-to-date reporting features.',
+		name: 'For property managers',
+		summary: 'Steamline your workflows',
 		description:
-			'We talked about reporting in the section above but we needed three items here, so mentioning it one more time for posterity.',
+			'Leverage our powerful tools to keep track of all your properties. Aqaratech helps you organize your business and work more efficiently.',
 		image: screenshotProfitLoss,
-		icon: HeroiconsArrowTrendingUpSolid,
+		icon: HeroiconsBuildingOffice2,
 	},
 	{
-		name: 'Inventory',
-		summary:
-			'Never lose track of what’s in stock with accurate inventory tracking.',
+		name: 'For landlords',
+		summary: 'Complete visibility',
 		description:
-			'We don’t offer this as part of our software but that statement is inarguably true. Accurate inventory tracking would help you for sure.',
+			'Use our dashboard to access and monitor all of your property information, granting you peace of mind helping you make informed decisions.',
 		image: screenshotInventory,
-		icon: BxsBusiness,
+		icon: MdiAccountTie,
 	},
 	{
-		name: 'Contacts',
-		summary:
-			'Organize all of your contacts, service providers, and invoices in one place.',
+		name: 'For tenants',
+		summary: 'Hassle-free rentals',
 		description:
-			'This also isn’t actually a feature, it’s just some friendly advice. We definitely recommend that you do this, you’ll feel really organized and professional.',
+			'Aqaratech simplifies the rental process by allowing tenants to pay their rent at any time, from any device.',
+
 		image: screenshotContacts,
-		icon: MdiAccount,
+		icon: HeroiconsUserGroup,
 	},
 ];
