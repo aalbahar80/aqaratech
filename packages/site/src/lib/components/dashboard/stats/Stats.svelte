e6bb9d2b35e5056ab7da7971220c0518de047b76
nbe <aleisa.naser@gmail.com>
2022-12-08 02:27:01 +0300

    move padding to title

--- packages/site/src/lib/components/dashboard/stats/Stats.svelte
+++ packages/site/src/lib/components/dashboard/stats/Stats.svelte
@@ -4,13 +4,13 @@
 
 <div>
 	{#if title}
-		<div class="flex justify-between">
+		<div class="flex justify-between pb-5">
 			<h3 class="text-2xl font-medium leading-6 text-gray-900">{title}</h3>
 			<slot name="details" />
 		</div>
 	{/if}
 	<dl
-		class="mt-5 grid grid-cols-1 divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow md:grid-cols-3 md:divide-y-0 md:divide-x"
+		class="grid grid-cols-1 divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow md:grid-cols-3 md:divide-y-0 md:divide-x"
 	>
 		<slot name="panes" />
 	</dl>
