cdb2d21df40fd5aa5b52359b0c7e77ef70523a35
nbe <aleisa.naser@gmail.com>
2022-12-08 02:30:08 +0300

    replace summary title

--- packages/site/src/routes/organizations/[organizationId]/portfolios/[portfolioId]/financials/summary/+page.svelte
+++ packages/site/src/routes/organizations/[organizationId]/portfolios/[portfolioId]/financials/summary/+page.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import SummaryStats from '$lib/components/dashboard/stats/SummaryStats.svelte';
+	import PopoverDivider from '$lib/components/popover/PopoverDivider.svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 </script>
 
-<h1 class="text-3xl font-bold text-gray-900">Summary</h1>
-
+<div class="pt-4">
+	<PopoverDivider />
+</div>
 <!-- TotalPanes -->
 <div class="flex flex-col gap-y-12">
 	<SummaryStats title="Net" data={data.net} />
