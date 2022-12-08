6352056bef08bc83d572bda95973f8836f8c9204
nbe <aleisa.naser@gmail.com>
2022-12-08 07:47:55 +0300

    grow anchor

--- packages/site/src/lib/components/sidebar/SidebarItem.svelte
+++ packages/site/src/lib/components/sidebar/SidebarItem.svelte
@@ -11,7 +11,7 @@
 	$: isCurrent = $page.url.pathname === item.href;
 </script>
 
-<a
+<div
 	class={clsx(
 		'flex items-center rounded-md px-4 py-2',
 		isCurrent && !item.children?.length
@@ -19,19 +19,24 @@
 			: 'transform text-gray-600 transition-colors duration-100 hover:bg-gray-200 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-200',
 		isChild ? 'mt-2' : 'mt-5',
 	)}
-	href={item.href}
-	on:click={() => {
-		expanded = true;
-	}}
 >
-	{#if item.icon}
-		<svelte:component this={item.icon} class="h-5 w-5" />
-	{:else}
-		<!-- 	whitespace to align text	 -->
-		<span class="h-5 w-5" />
-	{/if}
+	<a
+		class="inline-flex grow"
+		href={item.href}
+		on:click={() => {
+			// always expand when clicking on a link
+			expanded = true;
+		}}
+	>
+		{#if item.icon}
+			<svelte:component this={item.icon} class="h-5 w-5" />
+		{:else}
+			<!--	 whitespace to align text	 -->
+			<span class="h-5 w-5" />
+		{/if}
 
-	<span class="mx-4 font-medium">{item.name}</span>
+		<span class="mx-4 font-medium">{item.name}</span>
+	</a>
 
 	{#if item.children && item.children?.length > 0}
 		<button
