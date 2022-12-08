f290ea5a13711db263467cf8219939b5781aa15f
nbe <aleisa.naser@gmail.com>
2022-12-08 11:56:01 +0300

    extract dot pattern

---
+++ packages/site/src/lib/components/landing/features-2/DotPattern.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import clsx from 'clsx';
+
+	export let patternId: string;
+	export let cs: string;
+</script>
+
+<svg
+	class={clsx('absolute transform lg:block', cs)}
+	width={404}
+	height={784}
+	fill="none"
+	viewBox="0 0 404 784"
+	aria-hidden="true"
+>
+	<defs>
+		<pattern
+			id={patternId}
+			x={0}
+			y={0}
+			width={20}
+			height={20}
+			patternUnits="userSpaceOnUse"
+		>
+			<rect
+				x={0}
+				y={0}
+				width={4}
+				height={4}
+				class="text-gray-200"
+				fill="currentColor"
+			/>
+		</pattern>
+	</defs>
+	<rect width={404} height={784} fill={`url(#${patternId})`} />
+</svg>
---
+++ packages/site/src/lib/components/landing/features-2/DotPatternSm.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	export let patternId: string;
+</script>
+
+<svg
+	class="absolute left-1/2 -translate-x-1/2 translate-y-16 transform lg:hidden"
+	width={784}
+	height={404}
+	fill="none"
+	viewBox="0 0 784 404"
+	aria-hidden="true"
+>
+	<defs>
+		<pattern
+			id={patternId}
+			x={0}
+			y={0}
+			width={20}
+			height={20}
+			patternUnits="userSpaceOnUse"
+		>
+			<rect
+				x={0}
+				y={0}
+				width={4}
+				height={4}
+				class="text-gray-200"
+				fill="currentColor"
+			/>
+		</pattern>
+	</defs>
+	<rect width={784} height={404} fill={`url(#${patternId})`} />
+</svg>
