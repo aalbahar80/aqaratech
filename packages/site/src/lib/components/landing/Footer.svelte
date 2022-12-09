acd6c6028d37efb2f11bab29b6d4bbebab307fe9
nbe <aleisa.naser@gmail.com>
2022-12-09 17:33:18 +0300

    hide instagram icon

--- packages/site/src/lib/components/landing/Footer.svelte
+++ packages/site/src/lib/components/landing/Footer.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
 	import Container from '$lib/components/landing/secondary-feature/Container.svelte';
-	import TablerBrandInstagram from '~icons/tabler/brand-instagram';
+	// import TablerBrandInstagram from '~icons/tabler/brand-instagram';
 </script>
 
 <footer class="pt-16">
 	<Container>
 		<div
-			class="flex flex-col items-center border-t border-slate-400/10 py-10 sm:flex-row-reverse sm:justify-between"
+			class="flex flex-col items-center border-t border-slate-400/10 py-10 sm:flex-row-reverse sm:justify-around"
 		>
-			<div class="flex gap-x-6">
-				<a href="/" class="group" aria-label="Aqaratech on Instagram">
-					<TablerBrandInstagram
-						class="h-6 w-6 text-slate-500 group-hover:text-slate-700"
-						aria-hidden="true"
-					/>
-				</a>
-			</div>
+			<!-- <div class="flex gap-x-6"> -->
+			<!-- <a href="/" class="group" aria-label="Aqaratech on Instagram"> -->
+			<!-- 	<TablerBrandInstagram -->
+			<!-- 		class="h-6 w-6 text-slate-500 group-hover:text-slate-700" -->
+			<!-- 		aria-hidden="true" -->
+			<!-- 	/> -->
+			<!-- </a> -->
+			<!-- </div> -->
 			<p class="mt-6 text-sm text-slate-500 sm:mt-0">
 				Copyright &copy; {new Date().getFullYear()} Aqaratech. All rights reserved.
 			</p>
