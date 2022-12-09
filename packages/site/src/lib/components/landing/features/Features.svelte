a7fb093b551970f2a1d09f8c7099e3d992e475b4
nbe <aleisa.naser@gmail.com>
2022-12-09 12:01:32 +0300

    unify bg color

--- packages/site/src/lib/components/landing/features/Features.svelte
+++ packages/site/src/lib/components/landing/features/Features.svelte
@@ -3,7 +3,7 @@
 	import Feature from '$lib/components/landing/features/Feature.svelte';
 </script>
 
-<div class="overflow-hidden bg-gray-50 py-16 lg:py-24">
+<div class="overflow-hidden bg-gray-100 py-16 lg:py-24">
 	<div class="relative mx-auto max-w-xl px-6 lg:max-w-7xl lg:px-8">
 		{#each features as feature, n (feature.title)}
 			<Feature id={n + 1} {feature} />
