77f90484a34b3215488d2747481b304120b5f4ca
nbe <aleisa.naser@gmail.com>
2022-12-14 00:37:43 +0300

    init search test

---
+++ packages/test/tests/search/search.spec.ts
@@ -0,0 +1,41 @@
+import { expect } from '@playwright/test';
+
+import { getRoute, PageType } from '@self/utils';
+
+import { test } from '../api/api-fixtures';
+
+test.use({
+	portfoliosParams: [{ fullName: 'Alex Anderson' }],
+});
+
+test('search is enabled', async ({ page, org, portfolios: _portfolios }) => {
+	const url = getRoute({
+		entity: 'portfolio',
+		pageType: PageType.List,
+		params: {
+			organizationId: org.organization.id,
+		},
+	});
+
+	await page.goto(url);
+
+	const btn = page.getByRole('button', { name: 'Search' });
+	await btn.click();
+
+	// search
+	const input = page.getByPlaceholder('Search...');
+	await input.fill('Alex');
+
+	// check result
+	const result = page.getByRole('option', { name: 'Alex Anderson' });
+	await expect(result).toBeVisible();
+
+	// navigate to result
+	await result.click();
+	const name = page
+		.getByTestId('details-pane')
+		.getByTestId('fullName')
+		.getByText('Alex Anderson');
+
+	await expect(name).toBeVisible();
+});
