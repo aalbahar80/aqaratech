fa75cda843cb02d003a875a03f2f099a0fafb527
nbe <aleisa.naser@gmail.com>
2022-12-14 03:27:16 +0300

    don't persist melisearch volume in dev

--- packages/backend/seed/up-search.sh
+++ packages/backend/seed/up-search.sh
@@ -2,12 +2,12 @@
 
 isRunning=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:7700/health)
 
-if [ $isRunning -eq 200 ]; then
+if [ "$isRunning" -eq 200 ]; then
 	echo "MeiliSearch is already running"
 else
 	echo "MeiliSearch is not running"
 
 	# start it
 	echo "Starting MeiliSearch"
-	docker run --rm -d -p 7700:7700 -v "$(pwd)/meili_data:/meili_data" getmeili/meilisearch:v0.28.1
+	docker run --rm -d -p 7700:7700 getmeili/meilisearch:v0.28.1
 fi
