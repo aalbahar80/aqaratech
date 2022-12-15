fcf83c11df74f0550b79772d249119c183ad2726
nbe <aleisa.naser@gmail.com>
2022-12-15 06:40:44 +0300

    add mo screenshot smoke test

---
+++ packages/test/tests/forms/maintenance-order/screenshot.spec.ts
@@ -0,0 +1,43 @@
+import { expect } from '@playwright/test';
+
+import { getRoute, PageType } from '@self/utils';
+
+import { test } from '../../api/api-fixtures';
+
+test('screenshot smoke test', async ({
+	page,
+	org,
+	portfolio,
+	maintenanceOrder,
+}) => {
+	const url = getRoute({
+		entity: 'maintenanceOrder',
+		pageType: PageType.Id,
+		id: maintenanceOrder.id,
+		params: { organizationId: org.organization.id, portfolioId: portfolio.id },
+	});
+
+	await page.goto(url);
+
+	const original = await page.getByTestId('details-pane').screenshot();
+
+	await page.getByRole('link', { name: 'Edit' }).click();
+
+	const edit = getRoute({
+		entity: 'maintenanceOrder',
+		pageType: PageType.Edit,
+		id: maintenanceOrder.id,
+		params: { organizationId: org.organization.id, portfolioId: portfolio.id },
+	});
+
+	await expect(page).toHaveURL(edit);
+
+	await page.locator('text=Save').click();
+
+	// ensure same entity
+	await expect(page).toHaveURL(url);
+
+	const latest = await page.getByTestId('details-pane').screenshot();
+
+	expect(original.toString()).toEqual(latest.toString());
+});
