f6c98153e4308edd6d402e3dfc69ade291676ab6
nbe <aleisa.naser@gmail.com>
2022-12-16 05:45:17 +0300

    org imports

--- packages/site/src/routes/organizations/[organizationId]/portfolios/[portfolioId]/properties/[propertyId]/maintenance-orders/+page.ts
+++ packages/site/src/routes/organizations/[organizationId]/portfolios/[portfolioId]/properties/[propertyId]/maintenance-orders/+page.ts
@@ -1,8 +1,8 @@
 import { parseParams } from '$lib/utils/parse-params';
 
-import type { PageLoad } from './$types';
-
 import { createApi } from '$api';
+
+import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({
 	fetch,
