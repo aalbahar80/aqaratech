5f68230fd4967a2ffaf166174d6a6c75c6a57a0d
nbe <aleisa.naser@gmail.com>
2022-12-17 11:58:24 +0300

    use .catch for env debug level

--- packages/site/src/lib/environment/index.ts
+++ packages/site/src/lib/environment/index.ts
@@ -21,20 +21,9 @@ const envSchema = z.object({
 	PUBLIC_AQ_DEBUG_SITE: z.string().default('0'),
 	PUBLIC_COMMIT_SHA: z.string().optional(),
 
-	// validate debug level, if validation fails, set it to verbose
 	PUBLIC_AQ_DEBUG_LEVEL: z
 		.enum(['error', 'warn', 'info', 'verbose', 'debug', 'silly'])
-		.optional()
-		.transform((value) => {
-			if (value === undefined) {
-				console.log(
-					'PUBLIC_AQ_DEBUG_LEVEL is undefined or has failed validation, setting to verbose',
-				);
-				return 'verbose';
-			} else {
-				return value;
-			}
-		}),
+		.catch('debug'),
 });
 
 type EnvSchema = z.infer<typeof envSchema>;
