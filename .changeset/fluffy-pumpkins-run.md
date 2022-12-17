4154f784015c3aa8505a70ba2c6e22ed5e8dda51
nbe <aleisa.naser@gmail.com>
2022-12-17 10:14:25 +0300

    swapt 2nd feature title/subtitles

---
+++ .changeset/fluffy-pumpkins-run.md
@@ -0,0 +1,5 @@
+---
+'@self/site': patch
+---
+
+swap secondary feature title/subtitle
--- packages/site/src/lib/components/landing/secondary-feature/features.ts
+++ packages/site/src/lib/components/landing/secondary-feature/features.ts
@@ -11,24 +11,24 @@ import MdiAccountTie from '~icons/mdi/account-tie';
 
 export const secondaryFeatures = [
 	{
-		name: 'For property managers',
-		summary: 'Steamline your workflows',
+		summary: 'For property managers',
+		name: 'Steamline your workflows',
 		description:
 			'Leverage our powerful tools to keep track of all your properties. Aqaratech helps you organize your business and work more efficiently.',
 		image: screenshotProfitLoss,
 		icon: HeroiconsBuildingOffice2,
 	},
 	{
-		name: 'For owners',
-		summary: 'Complete visibility',
+		summary: 'For owners',
+		name: 'Complete visibility',
 		description:
 			'Use our dashboard to access and monitor all of your property information, granting you peace of mind helping you make informed decisions.',
 		image: screenshotDashboard,
 		icon: MdiAccountTie,
 	},
 	{
-		name: 'For tenants',
-		summary: 'Hassle-free rentals',
+		summary: 'For tenants',
+		name: 'Hassle-free rentals',
 		description:
 			'Aqaratech simplifies the rental process by allowing tenants to track their payments and leases.',
 		// 'Aqaratech simplifies the rental process by allowing tenants to pay their rent at any time, from any device.',
