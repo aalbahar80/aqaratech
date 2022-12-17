dbf6a39c32f8e5349c31d94705467f93442358d0
nbe <aleisa.naser@gmail.com>
2022-12-17 18:02:14 +0300

    better unwrap schema generics

--- packages/utils/src/schemas/types/keys-of-schema.ts
+++ packages/utils/src/schemas/types/keys-of-schema.ts
@@ -5,14 +5,13 @@ import type { z } from 'zod';
 /**
  * Extracts keys from a Zod schema.
  */
-// @ts-expect-error fix
-export type KeyOfSchema<T> = UnwrapSchema<InnerSchema<T>>;
+export type KeyOfSchema<T extends z.ZodTypeAny> = UnwrapSchema<InnerSchema<T>>;
 
 export type UnwrapSchema<T extends z.ZodTypeAny> = T extends z.ZodType<infer O>
 	? KeysOfUnion<O>
 	: never;
 
-export type InnerSchema<T> = T extends z.ZodEffects<any>
+export type InnerSchema<T> = T extends z.ZodEffects<z.ZodEffects<z.ZodTypeAny>>
 	? ReturnType<ReturnType<T['innerType']>['innerType']> // leaseCreateSchema needs to be unwrapped twice
 	: T;
 
