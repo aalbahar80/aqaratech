1216bbfc3244d39c97097e0cd5b8ddd7c9e6e118
nbe <aleisa.naser@gmail.com>
2022-12-23 23:58:50 +0300

    split eslint config import

---
+++ packages/eslint-config-custom/import.js
@@ -0,0 +1,80 @@
+module.exports = {
+	settings: {
+		// https://github.com/import-js/eslint-plugin-import#typescript
+		'import/resolver': {
+			node: true,
+			typescript: true,
+			// typescript: {
+			// 	alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+			// 	project: [
+			// 		// For sibling packages: Helps each .eslintrc.js file find its own tsconfig.json file
+			// 		'../*/tsconfig.json',
+			// 		// For root: Helps root .eslintrc.js file find its own tsconfig.json file
+			// 		'../../tsconfig.json',
+			// 	],
+			// },
+		},
+		'import/parsers': {
+			// https://github.com/ota-meshi/eslint-plugin-svelte/issues/312#issuecomment-1333345847
+			'@typescript-eslint/parser': ['.ts', '.tsx', '.cts', '.mts'],
+			// Avoids linting some js files with svelte-eslint-parser (when importing from *.svelte?)
+			'@babel/parser': ['.js', 'jsx', '.cjs', '.mjs'],
+		},
+		'import/internal-regex': '^@self',
+	},
+	rules: {
+		// Detect circular dependencies.
+		// Be conservative with maxDepth, as it can be expensive.
+		'import/no-cycle': ['error', { maxDepth: 2 }],
+		'import/no-self-import': 'error',
+
+		'import/no-unresolved': 'off', // superseded by typescript
+
+		// sort imports
+		'import/order': [
+			'error',
+			{
+				// Docs: https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/order.md#importorder
+				'newlines-between': 'always',
+				warnOnUnassignedImports: true,
+				alphabetize: {
+					order: 'asc',
+					caseInsensitive: true, // in import paths
+				},
+				pathGroupsExcludedImportTypes: ['svelte'], // allows pathGroups to work with svelte imports
+				// distinctGroup: false, // not released yet
+				groups: [
+					'builtin',
+					'external',
+					'internal',
+					'parent',
+					'sibling',
+					'index',
+					'object',
+					'type',
+				],
+				pathGroups: [
+					// cheatsheet: https://globster.xyz/
+					// Order in array matters.
+					{
+						// matches svelte, svelte/transition, etc
+						pattern: '+(svelte|\\$app){,*/**}',
+						position: 'before',
+						group: 'internal',
+					},
+					{
+						pattern: '+(@self){,*/**}',
+						position: 'before',
+						group: 'internal',
+					},
+					{
+						pattern: '+(src|$lib){,*/**}',
+						position: 'before',
+						group: 'internal',
+					},
+				],
+			},
+		],
+		// 'sort-imports': ['error'],
+	},
+};
--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,3 +1,5 @@
+const importConfig = require('./import');
+
 /**
  * @type {import("eslint").Linter.Config}
  */
@@ -39,27 +41,7 @@ module.exports = {
 	},
 	root: true,
 	settings: {
-		// https://github.com/import-js/eslint-plugin-import#typescript
-		'import/resolver': {
-			node: true,
-			typescript: true,
-			// typescript: {
-			// 	alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
-			// 	project: [
-			// 		// For sibling packages: Helps each .eslintrc.js file find its own tsconfig.json file
-			// 		'../*/tsconfig.json',
-			// 		// For root: Helps root .eslintrc.js file find its own tsconfig.json file
-			// 		'../../tsconfig.json',
-			// 	],
-			// },
-		},
-		'import/parsers': {
-			// https://github.com/ota-meshi/eslint-plugin-svelte/issues/312#issuecomment-1333345847
-			'@typescript-eslint/parser': ['.ts', '.tsx', '.cts', '.mts'],
-			// Avoids linting some js files with svelte-eslint-parser (when importing from *.svelte?)
-			'@babel/parser': ['.js', 'jsx', '.cjs', '.mjs'],
-		},
-		'import/internal-regex': '^@self',
+		...importConfig.settings,
 	},
 	rules: {
 		// https://github.com/vercel/turborepo/blob/main/packages/eslint-plugin-turbo/docs/rules/no-undeclared-env-vars.md
@@ -88,60 +70,6 @@ module.exports = {
 				],
 			},
 		],
-
-		// Detect circular dependencies.
-		// Be conservative with maxDepth, as it can be expensive.
-		'import/no-cycle': ['error', { maxDepth: 2 }],
-		'import/no-self-import': 'error',
-
-		'import/no-unresolved': 'off', // superseded by typescript
-
-		// sort imports
-		'import/order': [
-			'error',
-			{
-				// Docs: https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/order.md#importorder
-				'newlines-between': 'always',
-				warnOnUnassignedImports: true,
-				alphabetize: {
-					order: 'asc',
-					caseInsensitive: true, // in import paths
-				},
-				pathGroupsExcludedImportTypes: ['svelte'], // allows pathGroups to work with svelte imports
-				// distinctGroup: false, // not released yet
-				groups: [
-					'builtin',
-					'external',
-					'internal',
-					'parent',
-					'sibling',
-					'index',
-					'object',
-					'type',
-				],
-				pathGroups: [
-					// cheatsheet: https://globster.xyz/
-					// Order in array matters.
-					{
-						// matches svelte, svelte/transition, etc
-						pattern: '+(svelte|\\$app){,*/**}',
-						position: 'before',
-						group: 'internal',
-					},
-					{
-						pattern: '+(@self){,*/**}',
-						position: 'before',
-						group: 'internal',
-					},
-					{
-						pattern: '+(src|$lib){,*/**}',
-						position: 'before',
-						group: 'internal',
-					},
-				],
-			},
-		],
-		// 'sort-imports': ['error'],
 
 		'no-unused-vars': 'off',
 		'@typescript-eslint/no-unused-vars': [
@@ -190,6 +118,8 @@ module.exports = {
 		// Overkill:
 		// '@typescript-eslint/strict-boolean-expressions': 'warn',
 		// '@typescript-eslint/prefer-readonly-parameter-types': 'warn',
+
+		...importConfig.rules,
 	},
 	ignorePatterns: ['.eslintrc.*', '*.sh'],
 };
