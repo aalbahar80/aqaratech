05b174b6d6af6e1d3b7213154477a97a30995fe2
nbe <aleisa.naser@gmail.com>
2022-12-27 19:46:53 +0300

    init minio setup

---
+++ packages/backend/seed/up-minio.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+isRunning=$(docker inspect -f '{{.State.Running}}' minio)
+
+if [ "$isRunning" = "true" ]; then
+	echo "Minio is already running"
+
+	# Re-use
+	exit 0
+
+	# Force restart - might cause issues with testing (backend likes to start before meilisearch)
+	# echo "Stopping MeiliSearch container"
+	# docker stop meili
+else
+	echo "Minio is not running"
+fi
+
+echo "Starting Minio container"
+docker run --rm -d --name=minio -p 9000:9000 -p 9001:9001 \
+	quay.io/minio/minio server /data --console-address ":9001"
