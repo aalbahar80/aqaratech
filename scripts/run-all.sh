3fdd2d14179095444b70c4fc689e9f0cd85128ad
nbe <aleisa.naser@gmail.com>
2022-12-27 14:28:01 +0300

    Squashed commit of the following:
    commit d7de5c4ea5a47cea1304b7179876285b5e59f268
    Author: nbe <aleisa.naser@gmail.com>
    Date:   Tue Dec 27 14:22:11 2022 +0300
    
        add health check before test
    
    commit 86d68e177bcbd7b346255cb278c1628634baaab8
    Author: nbe <aleisa.naser@gmail.com>
    Date:   Tue Dec 27 14:18:34 2022 +0300
    
        rm redundanct close
    
    commit aa51557533907b41a69e05865bd52a64ad6ff062
    Author: nbe <aleisa.naser@gmail.com>
    Date:   Tue Dec 27 14:16:03 2022 +0300
    
        rm dummy script names
    
    commit e7e9507887263764407bc11c10bb0677cbb10cf4
    Author: nbe <aleisa.naser@gmail.com>
    Date:   Tue Dec 27 14:13:40 2022 +0300
    
        add trap cleanup code
    
    commit a4bba369031bddb1857d094b2a6651b4da80c448
    Author: nbe <aleisa.naser@gmail.com>
    Date:   Tue Dec 27 14:06:15 2022 +0300
    
        exit old running preview process
    
    commit fa20566971ad5017b21f625723af7164e424fca7
    Author: nbe <aleisa.naser@gmail.com>
    Date:   Tue Dec 27 14:01:11 2022 +0300
    
        handle all errors
    
    commit 0d3ad600f4bbb1140a72790e0393394742321ef4
    Author: nbe <aleisa.naser@gmail.com>
    Date:   Tue Dec 27 13:59:57 2022 +0300
    
        handle errors
    
    commit f58fa51943fb1520a56626019c590548631c50b2
    Author: nbe <aleisa.naser@gmail.com>
    Date:   Tue Dec 27 13:14:51 2022 +0300
    
        use send
    
    commit 08f5ea521a4c802fbead656f6cb449e8e14881c7
    Author: nbe <aleisa.naser@gmail.com>
    Date:   Tue Dec 27 13:13:41 2022 +0300
    
        use all vars
    
    commit 790361fc062339faaa92b87e227ba68bc832d1e6
    Author: nbe <aleisa.naser@gmail.com>
    Date:   Tue Dec 27 13:11:02 2022 +0300
    
        use variables
    
    commit aba3dbe1c79cb907c0e876721c69bae130e00c7a
    Author: nbe <aleisa.naser@gmail.com>
    Date:   Tue Dec 27 13:02:06 2022 +0300
    
        chuck setup
    
    commit 5e70f2d67505fc759485438e7eb09d3a35f66771
    Author: nbe <aleisa.naser@gmail.com>
    Date:   Tue Dec 27 12:51:05 2022 +0300
    
        pane orchestration ok
    
    commit f65d2da373e992526438ab6bab82fac59b823942
    Author: nbe <aleisa.naser@gmail.com>
    Date:   Tue Dec 27 12:34:43 2022 +0300
    
        async poc
    
    commit ab2544192e317e7d5f5a995274e98c82c14146c0
    Author: nbe <aleisa.naser@gmail.com>
    Date:   Tue Dec 27 11:18:46 2022 +0300
    
        init script

---
+++ scripts/run-all.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+set -x
+set -e
+
+session_name="mysession"
+window_name="mywindow"
+
+# Get the process ID of the current script
+pid=$$
+
+# Hook to exit running processes in tmux panes before our script is killed.
+# Triggered by any of the kill commands below, or by the user pressing Ctrl+C.
+trap "tmux send -t "${session_name}:${window_name}.1" 'C-c'; tmux send -t "${session_name}:${window_name}.2" 'C-c'; echo 'Cleanup complete'" EXIT
+
+# Check if the tmux session already exists
+if tmux has-session -t "${session_name}:${window_name}.1"; then
+	# If the first pane exists, re-use it and clear its contents
+	tmux clear-history -t "${session_name}:${window_name}.1"
+	echo "Re-using pane 1"
+else
+	# If the session does not exist, create it and the panes
+	tmux new-session -d -s "${session_name}" -n "${window_name}"
+	echo "Creating session and panes"
+fi
+
+if tmux has-session -t "${session_name}:${window_name}.2"; then
+	# If the second pane exists, re-use it and clear its contents
+	# Exit any running process in the pane
+	# tmux send-keys -t "${session_name}:${window_name}.2" 'C-c'
+	tmux clear-history -t "${session_name}:${window_name}.2"
+	echo "Re-using pane 2"
+else
+	# If the second pane does not exist, create it
+	tmux split-window -v -t "${session_name}:${window_name}.1"
+	echo "Creating pane 2"
+fi
+
+# Run pnpm run check in the first pane and wait for it to finish
+tmux send -t "${session_name}:${window_name}.1" 'pnpm run check && tmux wait -S check || kill '"$pid"'' Enter
+tmux wait check
+
+# Run pnpm run lint in the first pane and wait for it to finish
+tmux send -t "${session_name}:${window_name}.1" 'pnpm run lint && tmux wait -S lint || kill '"$pid"'' Enter
+tmux wait lint
+
+# Run pnpm run build in the first pane and wait for it to finish
+tmux send -t "${session_name}:${window_name}.1" 'pnpm run build && tmux wait -S build || kill '"$pid"'' Enter
+tmux wait build
+
+# Run pnpm run preview in the second pane
+tmux send -t "${session_name}:${window_name}.2" 'pnpm run preview || kill '"$pid"'' Enter
+
+# Wait for servers to start using ./scripts/health-probe.sh
+./scripts/health-probe.sh http://localhost:3002/health 10
+./scripts/health-probe.sh http://localhost:3000/health 5
+
+# Run pnpm run test in the first pane and wait for it to finish
+tmux send -t "${session_name}:${window_name}.1" 'pnpm run test && tmux wait -S test || kill '"$pid"'' Enter
+tmux wait test
