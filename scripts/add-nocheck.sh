56faf5e5d51090296eff9fe8a75d642f317406b8
nbe <aleisa.naser@gmail.com>
2023-01-01 20:34:18 +0300

    rm eslint-disable from generated files

--- scripts/add-nocheck.sh
+++ scripts/add-nocheck.sh
@@ -7,3 +7,8 @@ sed -i '1 s/^/\/\/ @ts-nocheck\
 # // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 sed -i '1 s/^/\/\/ eslint-disable-next-line @typescript-eslint\/ban-ts-comment\
 /' packages/site/src/api/openapi/runtime.ts
+
+# Remove unused eslint-disable directives
+sed -i '/\/\* eslint-disable \*\//d' packages/site/src/api/openapi/index.ts
+
+sed -i '/\/\* eslint-disable \*\//d' packages/site/src/api/openapi/apis/index.ts
