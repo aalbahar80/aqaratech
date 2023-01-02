c7ff7ed2434984de8fc3792234033652816403c6
nbe <aleisa.naser@gmail.com>
2023-01-02 04:09:20 +0300

    save locale in local storage

--- packages/site/src/lib/i18n/LocaleSwitcher.svelte
+++ packages/site/src/lib/i18n/LocaleSwitcher.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+
 	import { setLocale, locale } from '$i18n/i18n-svelte';
 	import { locales } from '$i18n/i18n-util';
 	import { loadLocaleAsync } from '$i18n/i18n-util.async';
@@ -6,6 +8,8 @@
 	import { LOCALE_LABELS } from './locale-labels';
 
 	import type { Locales } from '$i18n/i18n-types';
+
+	const key = 'preferred-locale';
 
 	const switchLocale = async (newLocale: Locales) => {
 		if (!newLocale || $locale === newLocale) return;
@@ -23,7 +27,19 @@
 		document
 			.querySelector('html')
 			?.setAttribute('dir', newLocale === 'ar' ? 'rtl' : 'ltr');
+
+		// NOTE: Remove localStorage usage if implementing lang in URL
+		localStorage.setItem(key, newLocale);
 	};
+
+	onMount(async () => {
+		console.log('onMount');
+		const preferredLocale = localStorage.getItem(key) as Locales;
+
+		if (preferredLocale) {
+			await switchLocale(preferredLocale);
+		}
+	});
 
 	$: unselectedLocale = locales.find((l) => l !== $locale);
 </script>
