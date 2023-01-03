e4da57678de803d02b325dd9a3055eb71f6a7832
nbe <aleisa.naser@gmail.com>
2023-01-03 09:52:43 +0300

    rm unused i18n example load page

--- packages/site/src/routes/[lang=lang]/+page.server.ts
+++
@@ -1,6 +0,0 @@
-import type { PageServerLoad } from './$types';
-
-export const load: PageServerLoad = ({ locals: { LL } }) => {
-	// TEST: rm
-	console.info(LL.log({ fileName: '+page.server.ts' }));
-};
