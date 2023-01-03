5795b57474778bb6904eaf43a98c563ebebfe972
nbe <aleisa.naser@gmail.com>
2023-01-03 04:06:27 +0300

    ignore unused i18n formatter

--- packages/site/src/i18n/formatters.ts
+++ packages/site/src/i18n/formatters.ts
@@ -2,6 +2,8 @@ import type { Locales, Formatters } from './i18n-types';
 import type { FormattersInitializer } from 'typesafe-i18n';
 
 export const initFormatters: FormattersInitializer<Locales, Formatters> = (
+	// @ts-expect-error - unused
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	locale: Locales,
 ) => {
 	const formatters: Formatters = {
