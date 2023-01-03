b3661c51c64066310e66027a5cd6d5222d770c2d
nbe <aleisa.naser@gmail.com>
2023-01-03 09:21:28 +0300

    destructure data obj

--- packages/site/src/routes/+layout.ts
+++ packages/site/src/routes/+layout.ts
@@ -7,28 +7,25 @@ import { LOGIN } from '$lib/constants/routes';
 import { isPublicRoute } from '$lib/utils/is-public-route';
 
 // TODO: destructure `data` to be more verbose
-export const load: LayoutLoad = async ({ data, url: { pathname } }) => {
+export const load: LayoutLoad = async ({
+	data: { user, locale },
+	url: { pathname },
+}) => {
+	// load dictionary into memory
+	await loadLocaleAsync(locale);
+
 	// Checking for data.user and redirecting here causes this function to catch
 	// all the random requests by bots and crawlers that are not logged in.
 	// If this becomes a problem, we can move this function one level deeper,
 	// letting all random requests fall through to the 404 page instead of
 	// redirecting them to the auth0 login page.
-	if (!isPublicRoute(pathname) && !data.user) {
+	if (!isPublicRoute(pathname) && !user) {
 		throw redirect(302, LOGIN);
 	}
 
-	console.log({ dl: data.locale }); // WARN: Remove
-
-	// TODO: Move to top?
-	// load dictionary into memory
-	await loadLocaleAsync(data.locale);
-	// await loadLocaleAsync('en');
-
-	// const locale: Locales = 'en'; // WARN: hard-coded for now
-
 	return {
-		...data,
+		user,
 		// pass locale to the "rendering context"
-		locale: data.locale,
+		locale,
 	};
 };
