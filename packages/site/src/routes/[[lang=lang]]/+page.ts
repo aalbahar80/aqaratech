4a6e5276ae032a6b0ee478aca19a2f1083e36074
nbe <aleisa.naser@gmail.com>
2023-01-03 06:05:53 +0300

    rm ll.title call

--- packages/site/src/routes/[[lang=lang]]/+page.ts
+++ packages/site/src/routes/[[lang=lang]]/+page.ts
@@ -13,8 +13,10 @@ export const load: PageLoad = async ({ parent }) => {
 	setLocale(locale);
 	// get the translation functions value from the store
 	const $LL = get(LL);
+	// DEBUG: rm
 	console.info($LL.log({ fileName: '+page.ts' }));
 	return {
-		title: $LL.title({ year: 2021 }),
+		// DEBUG: rm
+		title: $LL.buttons.search(),
 	};
 };
