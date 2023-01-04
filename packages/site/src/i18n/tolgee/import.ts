9fa5887a3868ab02fa26083f9c160cc4ac6b5180
nbe <aleisa.naser@gmail.com>
2023-01-05 01:48:44 +0300

    use en as fallback for missing translations

--- packages/site/src/i18n/tolgee/import.ts
+++ packages/site/src/i18n/tolgee/import.ts
@@ -3,10 +3,13 @@ import { readFileSync } from 'node:fs';
 import { dirname, join } from 'node:path';
 import { inspect } from 'util';
 
+import { defu } from 'defu';
 import {
 	storeTranslationToDisk,
 	type ImportLocaleMapping,
 } from 'typesafe-i18n/importer';
+
+import en from '../en/index';
 
 import type { Locales } from '../../i18n/i18n-types';
 import type { BaseTranslation } from 'typesafe-i18n';
@@ -31,11 +34,13 @@ const getDataFromAPI = (): BaseTranslation => {
 	const arTranslations = join(moduleDir, './generated/ar.json');
 	const ar = JSON.parse(
 		readFileSync(arTranslations, { encoding: 'utf-8' }),
-	) as BaseTranslation;
+	) as Record<string, unknown>;
 
 	console.log(inspect(ar, false, 999, true));
 
-	return ar;
+	const merged = addMissingTranslations(ar);
+
+	return merged;
 };
 
 const importTranslationsForLocale = async (locale: Locales) => {
@@ -51,5 +56,24 @@ const importTranslationsForLocale = async (locale: Locales) => {
 
 	console.log(`translations imported for locale '${result ?? ''}'`);
 };
+
+/** Use english as fallback for missing translations */
+const addMissingTranslations = (ar: Record<string, unknown>) => {
+	// count number of missing translations
+	// @ts-expect-error - ignore
+	const missing = recurseKeys(en) - recurseKeys(ar);
+
+	console.log(`\n\n--- MISSING TRANSLATIONS COUNT: ${missing} ---\n\n`);
+
+	return defu(ar, en);
+};
+
+/** Recursively count number of keys in object */
+const recurseKeys = (obj: Record<string, unknown>): number =>
+	Object.keys(obj).reduce((acc, curr) => {
+		if (typeof obj[curr] === 'object')
+			return ++acc + recurseKeys(obj[curr] as Record<string, unknown>);
+		else return ++acc;
+	}, 0);
 
 await importTranslationsForLocale('ar');
