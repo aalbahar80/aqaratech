a75664a7f91bbe7f99ccb960630c1a6acbbd798e
nbe <aleisa.naser@gmail.com>
2023-01-04 20:50:00 +0300

    improve readability

--- packages/utils/src/schemas/utils/zod-number.ts
+++ packages/utils/src/schemas/utils/zod-number.ts
@@ -1,17 +1,15 @@
 import { z } from 'zod';
 
 const tryParseToNumber = (val: unknown) => {
-	if (typeof val === 'string') {
-		if (val.trim().length === 0) {
-			// transform empty strings to null before passing them to Number(''), which returns 0
-			return null;
-		} else {
-			const num = Number(val);
-
-			return isNaN(num) ? null : num;
-		}
-	} else {
+	if (typeof val !== 'string') {
 		return val;
+	} else if (val.trim().length === 0) {
+		// transform empty strings to null before passing them to Number(''), which returns 0
+		return null;
+	} else {
+		const num = Number(val);
+
+		return isNaN(num) ? null : num;
 	}
 };
 
