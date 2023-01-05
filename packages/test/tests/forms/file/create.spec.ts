95e89431e230c0439cc3e58212254c6c3e3c8e91
nbe <aleisa.naser@gmail.com>
2023-01-05 20:11:45 +0300

    extract verify uploaded file

--- packages/test/tests/forms/file/create.spec.ts
+++ packages/test/tests/forms/file/create.spec.ts
@@ -1,10 +1,8 @@
-import { promises } from 'node:fs';
-
 import { expect } from '@playwright/test';
 
 import { getRoute, PageTab } from '@self/utils';
 
-import { getPresignedUrl } from '../../../utils/get-presigned-url';
+import { verifyUploadedFile } from '../../../utils/get-presigned-url';
 import { test } from '../../api/api-fixtures';
 import { FileFormPage } from '../file-form-model';
 
@@ -35,23 +33,11 @@ test('file can be uploaded', async ({ page, request, portfolio }) => {
 
 	await expect(page).toHaveURL(url);
 
-	// grab uploaded file
-
 	const key = `portfolio/${portfolio.id}/${fileName}`;
 
-	const presignedUrl = await getPresignedUrl({ request, key });
-
-	const res = await request.get(presignedUrl);
-
-	const uploadedFile = await res.body();
-
-	// file buffer matches
-
-	const localFile = await promises.readFile(localFilePath);
-
-	expect(uploadedFile.toString()).toEqual(localFile.toString());
-
-	// file size matches
-
-	expect(uploadedFile).toHaveLength(localFile.length);
+	await verifyUploadedFile({
+		key,
+		localFilePath,
+		request,
+	});
 });
--- packages/test/utils/get-presigned-url.ts
+++ packages/test/utils/get-presigned-url.ts
@@ -1,3 +1,5 @@
+import { promises } from 'node:fs';
+
 import { expect, type APIRequestContext } from '@playwright/test';
 import { withQuery } from 'ufo';
 
@@ -21,4 +23,30 @@ export const getPresignedUrl = async ({
 	const presignedUrl = await response.text();
 
 	return presignedUrl;
+};
+
+export const verifyUploadedFile = async ({
+	request,
+	key,
+	localFilePath,
+}: {
+	request: APIRequestContext;
+	key: string;
+	localFilePath: string;
+}) => {
+	const presignedUrl = await getPresignedUrl({ request, key });
+
+	const res = await request.get(presignedUrl);
+
+	const uploadedFile = await res.body();
+
+	// file buffer matches
+
+	const localFile = await promises.readFile(localFilePath);
+
+	expect(uploadedFile.toString()).toEqual(localFile.toString());
+
+	// file size matches
+
+	expect(uploadedFile).toHaveLength(localFile.length);
 };
