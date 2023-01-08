c76ed6b3b7c4151a4699799cf39354e1d3fd17ff
nbe <aleisa.naser@gmail.com>
2023-01-08 21:46:42 +0300

    create test for pdf upload

--- packages/test/tests/forms/file/create.spec.ts
+++ packages/test/tests/forms/file/create.spec.ts
@@ -6,38 +6,43 @@ import { verifyUploadedFile } from '../../../utils/get-presigned-url';
 import { test } from '../../api/api-fixtures';
 import { FileFormPage } from '../file-form-model';
 
-const localFilePath = './tests/forms/file/upload-test.png';
+const FILES = {
+	png: './tests/forms/file/upload-test.png',
+	pdf: './tests/components/pdf/invoice-paid-sample.pdf',
+};
 
-test('file can be uploaded', async ({ page, request, portfolio }) => {
-	const fileName = 'test-file-upload';
-	// TODO add random characters to file name for cleaner testing
+for (const [name, filePath] of Object.entries(FILES)) {
+	test(`file can be uploaded ${name}`, async ({ page, request, portfolio }) => {
+		const fileName = `test-file-upload-${name}`;
+		// TODO add random characters to file name for cleaner testing
 
-	// upload file
-	const url = getRoute({
-		entity: 'portfolio',
-		pageType: PageTab.Files,
-		id: portfolio.id,
-		params: {
-			organizationId: portfolio.organizationId,
-			portfolioId: portfolio.id,
-		},
+		// upload file
+		const url = getRoute({
+			entity: 'portfolio',
+			pageType: PageTab.Files,
+			id: portfolio.id,
+			params: {
+				organizationId: portfolio.organizationId,
+				portfolioId: portfolio.id,
+			},
+		});
+
+		await page.goto(url);
+
+		await page.getByRole('link', { name: 'New', exact: true }).click();
+
+		const form = new FileFormPage(page);
+		await form.setFile(fileName, filePath);
+		await form.save();
+
+		await expect(page).toHaveURL(url);
+
+		const key = `portfolio/${portfolio.id}/${fileName}`;
+
+		await verifyUploadedFile({
+			key,
+			localFilePath: filePath,
+			request,
+		});
 	});
-
-	await page.goto(url);
-
-	await page.getByRole('link', { name: 'New', exact: true }).click();
-
-	const form = new FileFormPage(page);
-	await form.setFile(fileName, localFilePath);
-	await form.save();
-
-	await expect(page).toHaveURL(url);
-
-	const key = `portfolio/${portfolio.id}/${fileName}`;
-
-	await verifyUploadedFile({
-		key,
-		localFilePath,
-		request,
-	});
-});
+}
