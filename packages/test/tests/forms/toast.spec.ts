b85d894864dbc8c39fd079ce2775a04d63dec165
nbe <aleisa.naser@gmail.com>
2023-01-09 22:38:22 +0300

    test form toasts

---
+++ packages/test/tests/forms/toast.spec.ts
@@ -0,0 +1,60 @@
+import { expect } from '@playwright/test';
+
+import { PageType } from '@self/utils';
+
+import { test } from '../api/api-fixtures';
+
+import { FormPage } from './form-page-model';
+
+const entity = 'portfolio';
+const pageType = PageType.Edit;
+
+test.describe('Form toasts', () => {
+	test('success', async ({ org, portfolio, page }) => {
+		const formPage = new FormPage(page, {
+			entity,
+			pageType,
+			id: portfolio.id,
+			fixtures: { org, portfolio },
+		});
+
+		await formPage.goto();
+		await formPage.save();
+
+		const toast = page.getByRole('status');
+
+		const success = toast.getByText('Success');
+		const loading = toast.getByText('Loading');
+
+		await expect(loading).toBeVisible();
+		await expect(loading).toBeHidden();
+		await expect(success).toBeVisible();
+		await expect(success).toBeHidden();
+	});
+
+	test('invalid', async ({ org, portfolio, page }) => {
+		const formPage = new FormPage(page, {
+			entity,
+			pageType,
+			id: portfolio.id,
+			fixtures: { org, portfolio },
+		});
+
+		await formPage.goto();
+		// Enter invalid data
+		await formPage.fillForm({ fullName: '' });
+		await formPage.save();
+
+		const toast = page.getByRole('status');
+
+		const loading = toast.getByText('Loading');
+		const invalid = toast.getByText('Invalid form');
+
+		await expect(loading).toBeVisible();
+		await expect(loading).toBeHidden();
+		await expect(invalid).toBeVisible();
+		await expect(invalid).toBeHidden({
+			timeout: 7000, // invalid toasts stay for a bit longer
+		});
+	});
+});
