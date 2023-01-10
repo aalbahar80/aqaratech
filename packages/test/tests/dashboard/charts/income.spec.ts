b3ca2346f20951aea16248ddad6d6ed0078e540c
nbe <aleisa.naser@gmail.com>
2023-01-10 08:43:35 +0300

    update income screenshots

--- packages/test/tests/dashboard/charts/income.spec.ts
+++ packages/test/tests/dashboard/charts/income.spec.ts
@@ -2,6 +2,9 @@ import { expect } from '@playwright/test';
 
 import { PageTypePortfolio } from '@self/utils';
 
+import { Filters } from '../filter-model';
+
+import { chartTestPresets } from './filter-presets';
 import { test } from './fixture';
 
 // PERF: Perform all tests in the same context/instance
@@ -11,29 +14,52 @@ import { test } from './fixture';
 
 test.use({ tab: PageTypePortfolio.Income });
 
-test.describe('income page', () => {
-	test('looks the same', async ({ page }) => {
-		await expect(page).toHaveScreenshot({
-			fullPage: true,
-			maxDiffPixelRatio: 0.01,
+const incomeChartTestPresets = chartTestPresets.filter(
+	// Unspecified Property doesn't exist in income page
+	(n) => n.name !== 'Unspecified Property',
+);
+
+for (const preset of incomeChartTestPresets) {
+	test.describe(`income page - filter - ${preset.name}`, () => {
+		// apply filter preset
+		test.beforeEach(async ({ page }) => {
+			const filters = new Filters(page);
+
+			// Select a property
+			await filters.property.el.selectOption({ label: preset.filter.property });
+
+			// Select a unit
+			if ('unit' in preset.filter) {
+				await filters.unit.el.selectOption({ label: preset.filter.unit });
+			}
+
+			// wait for fresh data
+			await page.waitForLoadState('networkidle');
+		});
+
+		test('looks the same', async ({ page }) => {
+			await expect(page).toHaveScreenshot({
+				fullPage: true,
+				// maxDiffPixelRatio: 0.01,
+			});
+		});
+
+		test('income pie chart', async ({ page }) => {
+			const chart = page
+				.getByTestId('chart-card')
+				.filter({ hasText: 'Income: by Payment Status' });
+
+			await expect(chart).toHaveScreenshot();
+		});
+
+		test('income bar chart', async ({ page }) => {
+			const chart = page
+				.getByTestId('chart-card')
+				.filter({ hasText: 'Income: by Month' });
+
+			await expect(chart).toHaveScreenshot({
+				maxDiffPixelRatio: 0.01, // firefox fails without this
+			});
 		});
 	});
-
-	test('income pie chart', async ({ page }) => {
-		const chart = page
-			.getByTestId('chart-card')
-			.filter({ hasText: 'Income: by Payment Status' });
-
-		await expect(chart).toHaveScreenshot();
-	});
-
-	test('income bar chart', async ({ page }) => {
-		const chart = page
-			.getByTestId('chart-card')
-			.filter({ hasText: 'Income: by Month' });
-
-		await expect(chart).toHaveScreenshot({
-			maxDiffPixelRatio: 0.01, // firefox fails without this
-		});
-	});
-});
+}
