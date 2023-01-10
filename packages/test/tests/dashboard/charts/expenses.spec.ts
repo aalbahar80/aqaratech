d332f92c0c62ab745d45b9fcfa996e0b4084ec9f
nbe <aleisa.naser@gmail.com>
2023-01-10 06:34:37 +0300

    apply filter preset to expense chart tests

--- packages/test/tests/dashboard/charts/expenses.spec.ts
+++ packages/test/tests/dashboard/charts/expenses.spec.ts
@@ -2,6 +2,9 @@ import { expect } from '@playwright/test';
 
 import { PageTypePortfolio } from '@self/utils';
 
+import { Filters } from '../filter-model';
+
+import { chartTestPresets } from './filter-presets';
 import { test } from './fixture';
 
 // PERF: Perform all tests in the same context/instance
@@ -11,31 +14,47 @@ import { test } from './fixture';
 
 test.use({ tab: PageTypePortfolio.Expenses });
 
-test.describe('expense page', () => {
-	test('looks the same', async ({ page }) => {
-		await expect(page).toHaveScreenshot({
-			fullPage: true,
-			maxDiffPixelRatio: 0.01,
+// for (const [n, filterPreset] of filterPresets.entries()) {
+for (const preset of chartTestPresets) {
+	test.describe(`expense page - filter - ${preset.name}`, () => {
+		// apply filter preset
+		test.beforeEach(async ({ page }) => {
+			const filters = new Filters(page);
+
+			// Select a property
+			await filters.property.el.selectOption({ label: preset.filter.property });
+			await page.waitForLoadState('networkidle');
+
+			// Select a unit
+			// await filters.unit.el.selectOption({ label: filterPreset.unit });
+			// await page.waitForLoadState('networkidle');
 		});
-	});
 
-	test('expense bar chart', async ({ page }) => {
-		const chart = page
-			.getByTestId('chart-card')
-			.filter({ hasText: 'Expenses: by Month' });
-
-		await expect(chart).toHaveScreenshot({
-			maxDiffPixelRatio: 0.01, // firefox fails without this
+		test('looks the same', async ({ page }) => {
+			await expect(page).toHaveScreenshot({
+				fullPage: true,
+				maxDiffPixelRatio: 0.01,
+			});
 		});
+
+		test('expense bar chart', async ({ page }) => {
+			const chart = page
+				.getByTestId('chart-card')
+				.filter({ hasText: 'Expenses: by Month' });
+
+			await expect(chart).toHaveScreenshot({
+				maxDiffPixelRatio: 0.01, // firefox fails without this
+			});
+		});
+
+		test('expense treemap - location', async ({ page }) => {
+			const chart = page
+				.getByTestId('chart-card')
+				.filter({ hasText: 'Expenses: by Location' });
+
+			await expect(chart).toHaveScreenshot();
+		});
+
+		// TEST: add treemap - category
 	});
-
-	test('expense treemap - location', async ({ page }) => {
-		const chart = page
-			.getByTestId('chart-card')
-			.filter({ hasText: 'Expenses: by Location' });
-
-		await expect(chart).toHaveScreenshot();
-	});
-
-	// TEST: add treemap - category
-});
+}
---
+++ packages/test/tests/dashboard/charts/filter-presets.ts
@@ -0,0 +1,38 @@
+interface filterPreset {
+	range: string;
+	property: string;
+	unit: string;
+}
+
+interface ChartPresetTest {
+	/** An identifiable name for the test */
+	name: string;
+	filter: filterPreset;
+}
+
+const base = {
+	range: 'Last 12 Months',
+	property: 'All properties',
+	unit: 'All units',
+} satisfies filterPreset;
+
+export const chartTestPresets = [
+	{
+		name: 'Base',
+		filter: base,
+	},
+	{
+		name: 'Unspecified Property',
+		filter: {
+			...base,
+			property: 'Unspecified Property',
+		},
+	},
+	{
+		name: 'Specific Property',
+		filter: {
+			...base,
+			property: 'بيان ق 1 م 44',
+		},
+	},
+] satisfies ChartPresetTest[];
