5ef6f4af52bab9869adb867f4b21e2d6400529c7
nbe <aleisa.naser@gmail.com>
2023-01-11 22:42:56 +0300

    use new context in logout test

--- packages/test/tests/auth/logout.spec.ts
+++ packages/test/tests/auth/logout.spec.ts
@@ -2,17 +2,35 @@ import { expect, test } from '@playwright/test';
 
 import { Cookie } from '@self/utils';
 
-test('cookies are cleared', async ({ page }) => {
+test('cookies are cleared', async ({ browser }) => {
+	// create new context avoid interfering with interfering with other tests
+	const context = await browser.newContext();
+	const page = await context.newPage();
+
+	const cookieNames = [Cookie.idToken, Cookie.accessToken];
+
+	// assert cookies exist at start
+	const initialCookies = await page.context().cookies();
+
+	for (const cookieName of cookieNames) {
+		const cookie = initialCookies.find((cookie) => cookie.name === cookieName);
+
+		expect(cookie, {
+			message: 'Cookies should be defined at start of test',
+		}).toBeDefined();
+	}
+
+	// logout
 	await page.goto('/auth/logout');
 
 	// expect idToken and accessToken to be cleared
 	const cookies = await page.context().cookies();
-
-	const cookieNames = [Cookie.idToken, Cookie.accessToken];
 
 	for (const cookieName of cookieNames) {
 		const cookie = cookies.find((cookie) => cookie.name === cookieName);
 
 		expect(cookie).toBe(undefined);
 	}
+
+	await context.close();
 });
