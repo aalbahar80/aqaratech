921bb0f89430809c0ebd16a8161aa3eb25f0a800
nbe <aleisa.naser@gmail.com>
2023-01-13 13:04:23 +0300

    define get-intl-label

---
+++ packages/site/src/lib/i18n/get-intl-label.ts
@@ -0,0 +1,19 @@
+import { get } from 'svelte/store';
+
+import { getLabel } from '@self/utils';
+
+import L, { locale } from '$i18n/i18n-svelte';
+
+/** Attempt to get label based on locale, falling back to English. */
+export const getIntlLabel = (key: string) => {
+	const currentLocale = get(locale);
+	const LL = get(L);
+
+	const enLabel = getLabel(key);
+
+	if (currentLocale === 'ar') {
+		return LL.fields[key] ?? enLabel;
+	} else {
+		return enLabel;
+	}
+};
