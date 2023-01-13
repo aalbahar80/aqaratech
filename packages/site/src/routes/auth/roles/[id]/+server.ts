74820cbf17fa826d1fa28a2cd4cb20ad93f35359
nbe <aleisa.naser@gmail.com>
2023-01-13 11:46:39 +0300

    set default locale in role endpoint

--- packages/site/src/routes/auth/roles/[id]/+server.ts
+++ packages/site/src/routes/auth/roles/[id]/+server.ts
@@ -2,6 +2,7 @@ import type { RequestHandler } from './$types';
 
 import { Cookie } from '@self/utils';
 
+import { baseLocale } from '$i18n/i18n-util';
 import { MAX_AGE, PREF_LOCALE, REDIRECT_TO } from '$lib/constants/misc';
 
 export const GET: RequestHandler = async ({
@@ -17,7 +18,8 @@ export const GET: RequestHandler = async ({
 	});
 
 	const location =
-		url.searchParams.get(REDIRECT_TO) ?? `/concierge?${PREF_LOCALE}=${locale}`;
+		url.searchParams.get(REDIRECT_TO) ??
+		`/concierge?${PREF_LOCALE}=${locale ?? baseLocale}`;
 
 	return new Response(undefined, {
 		status: 302,
