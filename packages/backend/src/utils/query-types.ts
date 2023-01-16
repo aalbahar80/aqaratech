2aa00b9a62e6e0782e7922ae4fb259627fb6703b
nbe <aleisa.naser@gmail.com>
2023-01-16 11:41:22 +0300

    custom query-types implementation

---
+++ packages/backend/src/utils/query-types.ts
@@ -0,0 +1,49 @@
+// @ts-nocheck
+// Source: https://github.com/xpepermint/query-types/blob/master/index.js
+
+const isObject = (val: unknown): val is object => val.constructor === Object;
+
+const isNumber = (val: unknown): val is number =>
+	!isNaN(parseFloat(val)) && isFinite(val);
+
+const isBoolean = (val: unknown): val is boolean =>
+	val === 'false' || val === 'true';
+
+const isArray = Array.isArray;
+
+const parseValue = (val: unknown): unknown => {
+	if (typeof val == 'undefined' || val == '') {
+		return null;
+	} else if (isBoolean(val)) {
+		return parseBoolean(val);
+	} else if (isArray(val)) {
+		return parseArray(val);
+	} else if (isObject(val)) {
+		return parseObject(val);
+	} else if (isNumber(val)) {
+		return parseNumber(val);
+	} else {
+		return val;
+	}
+};
+
+const parseObject = (obj: Record<string, unknown>) => {
+	const result = {};
+	for (const key in obj) {
+		const val = parseValue(obj[key]);
+		if (val !== null) result[key] = val;
+	}
+	return result;
+};
+
+const parseArray = (arr: unknown[]) => {
+	return arr.map(parseValue);
+};
+
+const parseNumber = (val: unknown) => {
+	return Number(val);
+};
+
+const parseBoolean = (val: unknown) => {
+	return val === 'true';
+};
