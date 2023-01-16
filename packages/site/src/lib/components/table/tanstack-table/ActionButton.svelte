990dc4d90bcb557781cd741a455eebce78b4cb55
nbe <aleisa.naser@gmail.com>
2023-01-16 16:30:23 +0300

    fix table button flicker by deduping ts element

--- packages/site/src/lib/components/table/tanstack-table/ActionButton.svelte
+++ packages/site/src/lib/components/table/tanstack-table/ActionButton.svelte
@@ -8,7 +8,7 @@
 	export let options: ActionButtonProps;
 </script>
 
-<td class="relative text-right text-sm font-medium">
+<div class="relative text-right text-sm font-medium">
 	<button
 		type="button"
 		class="inline-flex items-center rounded-md border border-gray-300 bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-30"
@@ -17,4 +17,4 @@
 	>
 		{options.label}
 	</button>
-</td>
+</div>
