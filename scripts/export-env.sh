b9703b3672dd49b1ffaba607988fa447c4294348
nbe <aleisa.naser@gmail.com>
2023-01-18 02:27:16 +0300

    print debug info export-env

--- scripts/export-env.sh
+++ scripts/export-env.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # A script to set & export environment variables from a .env file
 # Usage: ./export-env.sh .env
 
@@ -11,11 +13,29 @@
 # . ./scripts/export-env.sh .env
 #
 # Sveltekit preview command:
-# "preview": ". ../../scripts/export-env.sh ../../.env && PORT=3000 ORIGIN=$PUBLIC_SITE_URL node build/index.js",
+# "preview": ". ../../scripts/export-env.sh && PORT=3000 ORIGIN=$PUBLIC_SITE_URL node build/index.js",
 
 # Start
 
-ENV_FILE=$1
+# path of this file
+THIS_FILE=$(realpath ${BASH_SOURCE[0]})
+echo "THIS_FILE: $THIS_FILE"
+
+THIS_DIR=$(dirname $THIS_FILE)
+echo "THIS_DIR: $THIS_DIR"
+
+ENV_FILE=$(realpath $THIS_DIR/../.env)
+echo "ENV_FILE: $ENV_FILE"
+
+# Debug info
+echo "dollar0: $0"
+echo "BASH_SOURCE: ${BASH_SOURCE}"
+echo "BASH_SOURCE[0]: ${BASH_SOURCE[0]}"
+echo "BASH_SOURCE[1]: ${BASH_SOURCE[1]}"
+echo "realpath bash_source: $(realpath ${BASH_SOURCE[0]})"
+echo "pwd: $(pwd)"
+echo "dirname d0: $(dirname "$0")"
+echo "realpath d0: $(realpath "$0")"
 
 # warn if running without source
 if [ "$BASH_SOURCE" = "$0" ]; then
