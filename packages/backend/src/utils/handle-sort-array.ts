3409ef75e000c44fca9b9d3aa7a6eb4b1015ddf5
nbe <aleisa.naser@gmail.com>
2023-01-19 11:12:54 +0300

    move obj to loop

--- packages/backend/src/utils/handle-sort-array.ts
+++ packages/backend/src/utils/handle-sort-array.ts
@@ -18,11 +18,10 @@ export const handleSortArray = (sort: ParsedQueryModel['sort']) => {
 
 	// Handle dot notation in sort array
 	sort.forEach((n) => {
-		const obj = {};
-
 		// Handle dot notation for the requested key
 		R.forEachObj.indexed(n, (val, key) => {
 			if (typeof key === 'string') {
+				const obj = {};
 				set(obj, key, val);
 				final.push(obj);
 			}
@@ -31,12 +30,11 @@ export const handleSortArray = (sort: ParsedQueryModel['sort']) => {
 		// Add an id tiebreaker, where id is a sibling of the requested key
 		// e.g. [{ 'lease.tenantId': 'desc' }] => [{ 'lease': { 'tenantId': 'desc' } }, { 'lease': { 'id': 'desc' } }]
 
-		const tiebreaker = {};
-
 		R.forEachObj.indexed(n, (val, key) => {
 			if (typeof key === 'string') {
 				const withoutLast = key.split('.').slice(0, -1).join('.');
 				if (withoutLast.length > 0) {
+					const tiebreaker = {};
 					set(tiebreaker, withoutLast, { id: val });
 					final.push(tiebreaker);
 				}
