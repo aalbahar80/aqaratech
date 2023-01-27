7c9082280dc8c6ffe0d7268c1ba5ee4188bcada5
nbe <aleisa.naser@gmail.com>
2023-01-27 20:26:46 +0300

    make test for redirect to login page less flaky

--- packages/test/tests/auth/token/expired-jwt.spec.ts
+++ packages/test/tests/auth/token/expired-jwt.spec.ts
@@ -7,12 +7,45 @@ import { expect, test } from '../auth-fixtures';
 // test.use({ token: { name: Cookie.idToken, value: '123' } });
 
 test('redirect to login form', async ({ page }) => {
-	await page.goto(siteURL);
 	// expect to be redirected to login page
-	const emailInput = page.locator('input[name="username"]');
-	const passwordInput = page.locator('input[name="password"]');
-	await expect(emailInput).toBeVisible();
-	await expect(passwordInput).toBeVisible();
+	// Checking for username/password input is flaky because it's on an external site,
+	// if we still want to do so, at lease use test.slow()
+	// await page.goto(siteURL);
+	// const emailInput = page.locator('input[name="username"]');
+	// const passwordInput = page.locator('input[name="password"]');
+	// await expect(emailInput).toBeVisible();
+	// await expect(passwordInput).toBeVisible();
+
+	// Instead, test for the redirect to /auth/login
+
+	// catch the request when it happens
+	const requestPromise = page.waitForRequest((res) =>
+		res.url().includes('/auth/login'),
+	);
+
+	await page.goto(siteURL);
+
+	const request = await requestPromise;
+
+	const response = await request.response();
+	expect(response).toBeTruthy();
+
+	if (!response) throw new Error('No response'); // type purposes only
+	expect.soft(response.status()).toBe(302);
+
+	const locationHeader = response.headers()['location'];
+	expect(locationHeader).toBeTruthy();
+
+	// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+	const location = new URL(locationHeader!);
+	expect.soft(location.host).toContain('auth0.com');
+	expect.soft(location.pathname).toBe('/authorize');
+
+	// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+	const redirectParam = new URL(location.searchParams.get('redirect_uri')!);
+	const redirect = new URL(redirectParam);
+
+	expect.soft(redirect.pathname).toBe('/auth/callback');
 });
 
 test('cookies are cleared', async ({ page }) => {
