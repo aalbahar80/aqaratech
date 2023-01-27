f9b214d5c2e99934187f169c1744f82017c5ac5b
nbe <aleisa.naser@gmail.com>
2023-01-27 22:48:37 +0300

    better seperation of seed commands

--- packages/backend/seed/create-test-db.sh
+++ packages/backend/seed/create-test-db.sh
@@ -27,4 +27,4 @@ echo "Postgres is ready @ $DATABASE_URL. Note the port is $(echo $DATABASE_URL |
 
 cd ../backend
 
-DATABASE_URL=$DATABASE_URL pnpm prisma migrate reset --force
+DATABASE_URL=$DATABASE_URL pnpm prisma migrate reset --force --skip-seed
