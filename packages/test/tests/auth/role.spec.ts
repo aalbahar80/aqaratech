2db9b0a05138f893da0e9ac86e0d7b2490a74976
nbe <aleisa.naser@gmail.com>
2023-01-29 05:29:54 +0300

    use fixture

--- packages/test/tests/auth/role.spec.ts
+++ packages/test/tests/auth/role.spec.ts
@@ -4,7 +4,41 @@ import { testOrgEmail } from '@self/seed';
 import { Cookie, PageType, getRoute } from '@self/utils';
 
 import { getCookie } from '../../utils/get-cookie';
-import { test } from '../api/api-fixtures';
+import { test as base } from '../api/api-fixtures';
+
+const test = base.extend({
+	page: async ({ browser }, use) => {
+		// - user has multiple roles, both in same org and different orgs
+		// - user has no role cookie set (simulating new login)
+		// - check that the role is resolved to one with same orgId
+		// - check that the role is resolved to one with higher priority
+
+		// create new context avoid interfering with interfering with other tests
+		const context = await browser.newContext();
+
+		const page = await context.newPage();
+
+		// clear the role cookie only
+		const cookies = await page.context().cookies();
+
+		await page.context().clearCookies();
+		await page
+			.context()
+			.addCookies(cookies.filter((cookie) => cookie.name !== Cookie.role));
+
+		// assert that the role cookie is cleared
+		expect(
+			await getCookie({
+				context: page.context(),
+				cookieName: Cookie.role,
+			}),
+		).toBeUndefined();
+
+		await use(page);
+
+		await context.close();
+	},
+});
 
 test.use({
 	rolesParams: [
@@ -20,69 +54,37 @@ test.use({
 	],
 });
 
-test('role is resolved correctly', async ({
-	page: originalPage,
-	invoice,
-	org,
-}) => {
-	// - user has multiple roles, both in same org and different orgs
-	// - user has no role cookie set (simulating new login)
-	// - check that the role is resolved to one with same orgId
-	// - check that the role is resolved to one with higher priority
+test.describe('role is resolved correctly', () => {
+	test('role is resolved correctly', async ({ page, invoice, org }) => {
+		const url = getRoute({
+			entity: 'leaseInvoice',
+			pageType: PageType.Id,
+			id: invoice.id,
+			params: {
+				organizationId: invoice.organizationId,
+				portfolioId: invoice.portfolioId,
+			},
+		});
 
-	// create new context avoid interfering with interfering with other tests
-	// const context = await browser.newContext();
-	const context = await originalPage.context().browser()?.newContext();
-	if (!context) throw new Error('Context is undefined');
+		// prepare to catch response
+		const responsePromise = page.waitForResponse(url);
 
-	const page = await context.newPage();
+		await page.goto(url);
 
-	// clear the role cookie only
-	const cookies = await page.context().cookies();
+		// check that response is ok
+		const response = await responsePromise;
+		expect.soft(response.status()).toBe(200);
 
-	const withoutRoleCookie = cookies.filter(
-		(cookie) => cookie.name !== Cookie.role,
-	);
-
-	await page.context().clearCookies();
-	await page.context().addCookies(withoutRoleCookie);
-
-	// assert that the role cookie is cleared
-	expect(
-		await getCookie({
+		// check that the role is resolved to one with same orgId
+		const roleCookie = await getCookie({
 			context: page.context(),
 			cookieName: Cookie.role,
-		}),
-	).toBeUndefined();
+		});
 
-	const url = getRoute({
-		entity: 'leaseInvoice',
-		pageType: PageType.Id,
-		id: invoice.id,
-		params: {
-			organizationId: invoice.organizationId,
-			portfolioId: invoice.portfolioId,
-		},
+		expect.soft(roleCookie).toMatchObject({
+			value: org.roleId,
+		});
 	});
 
-	// prepare to catch response
-	const responsePromise = page.waitForResponse(url);
-
-	await page.goto(url);
-
-	// check that response is ok
-	const response = await responsePromise;
-	expect.soft(response.status()).toBe(200);
-
-	// check that the role is resolved to one with same orgId
-	const roleCookie = await getCookie({
-		context: page.context(),
-		cookieName: Cookie.role,
-	});
-
-	expect.soft(roleCookie).toMatchObject({
-		value: org.roleId,
-	});
-
-	await context.close();
+	test.fixme('existing role cookie is not overwritten', async () => {});
 });
