f91885d9a2829d8cd5437028c5aee68078d226d9
nbe <aleisa.naser@gmail.com>
2023-02-01 02:54:27 +0300

    fix lint in test

--- packages/test/tests/api/fixtures/site-url.ts
+++ packages/test/tests/api/fixtures/site-url.ts
@@ -1,2 +1,5 @@
-// use zod to validate if needed
-export const siteURL = process.env.PUBLIC_SITE_URL;
+import type { AqaratechEnv } from '@self/utils';
+
+// eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
+export const siteURL = process.env
+	.PUBLIC_SITE_URL as AqaratechEnv['PUBLIC_SITE_URL'];
