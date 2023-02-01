8d91107b67bc9769a9d1033e611400032a4189a1
nbe <aleisa.naser@gmail.com>
2023-02-01 16:01:32 +0300

    update fmtCell

--- packages/site/src/lib/components/table/tanstack-table/columns/as-date.ts
+++ packages/site/src/lib/components/table/tanstack-table/columns/as-date.ts
@@ -1,17 +1,16 @@
+import { get } from 'svelte/store';
+import { fmt } from '@self/utils';
+
 import type { CellContext } from '@tanstack/svelte-table';
 
-import { fmtCurrency, fmtDate, fmtNumber } from '$lib/i18n/format';
+import { locale } from '$i18n/i18n-svelte';
 
 type CellDateType = 'currency' | 'date' | 'number';
 
-const formatters = {
-	currency: fmtCurrency,
-	date: fmtDate,
-	number: fmtNumber,
-};
-
 export const fmtCell =
 	(type: CellDateType) => (info: CellContext<unknown, unknown | undefined>) => {
-		const val = info.getValue();
-		return val ? formatters[type](val) : '';
+		const CL = get(locale);
+
+		const value = info.getValue();
+		return value ? fmt({ type, value, locale: CL }) : '';
 	};
