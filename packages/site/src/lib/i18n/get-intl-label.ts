3a604413d60ff31265124e404ed18a6122b53137
nbe <aleisa.naser@gmail.com>
2023-02-02 21:30:14 +0300

    get locale dictionary only for ar locale, otherwise no need to get it

--- packages/site/src/lib/i18n/get-intl-label.ts
+++ packages/site/src/lib/i18n/get-intl-label.ts
@@ -6,11 +6,12 @@ import L, { locale } from '$i18n/i18n-svelte';
 /** Attempt to get label based on locale, falling back to English. */
 export const getIntlLabel = (key: string) => {
 	const currentLocale = get(locale);
-	const LL = get(L);
 
 	const enLabel = getLabel(key);
 
 	if (currentLocale === 'ar') {
+		const LL = get(L);
+
 		// @ts-expect-error wip
 		// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 		return (LL.fields[key]() as string) || enLabel;
