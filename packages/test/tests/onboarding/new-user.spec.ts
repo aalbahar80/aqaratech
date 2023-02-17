c4e4ec7c95063e0ed5bf8cfa18e8da0b349b521c
nbe <aleisa.naser@gmail.com>
2023-02-17 19:53:55 +0300

    add new portfolio user test

--- packages/test/tests/onboarding/new-user.spec.ts
+++ packages/test/tests/onboarding/new-user.spec.ts
@@ -1,18 +1,30 @@
 import path from 'path';
 
-import { expect, test } from '@playwright/test';
+import { expect } from '@playwright/test';
 
+import { PageTypePortfolio, getRoute } from '@self/utils';
+
+import { prisma } from '../../prisma';
 import { globalStoragePath } from '../../utils/global-storage-path';
+import { test } from '../api/api-fixtures';
+import { siteURL } from '../api/fixtures/site-url';
 import { testUsers } from '../api/fixtures/users/test-users';
 
 // "New user" is a user that is logging in for the first time.
 
+const email = testUsers.freshUser.email;
 const file = testUsers.freshUser.storageStateFilename;
 const storagePath = path.join(globalStoragePath, file);
 
 test.use({ storageState: storagePath });
 
 test.describe('new user', () => {
+	test.beforeEach(async () => {
+		// Clear the user from the database before each test.
+		await prisma.role.deleteMany({ where: { user: { email } } });
+		await prisma.user.deleteMany({ where: { email } });
+	});
+
 	test('is redirected to /welcome if no role', async ({ page }) => {
 		await page.goto('/');
 
@@ -35,7 +47,34 @@ test.describe('new user', () => {
 		await expect(page).toHaveURL('/en/welcome');
 	});
 
-	test.fixme('is redirected to portfolio portal if portolio', async () => {});
+	test('is redirected to portfolio portal if portolio', async ({
+		page,
+		portfolio,
+	}) => {
+		// simulate that the user has been invited to a portfolio
+		await prisma.role.create({
+			data: {
+				user: { create: { email } },
+				portfolio: { connect: { id: portfolio.id } },
+				roleType: 'PORTFOLIO',
+				organization: { connect: { id: portfolio.organizationId } },
+			},
+		});
+
+		await page.goto('/');
+		await page.getByRole('link', { name: 'Log in' }).first().click();
+
+		const url = getRoute({
+			entity: 'portfolio',
+			id: portfolio.id,
+			pageType: PageTypePortfolio.Summary,
+			params: {
+				organizationId: portfolio.organizationId,
+			},
+		});
+
+		await expect(page).toHaveURL(`${siteURL}${url}`);
+	});
 
 	test.fixme('is redirected to tenant portal if tenant', async () => {});
 });
