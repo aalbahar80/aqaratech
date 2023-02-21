2b600f2fa3bd207edc549fb275c42ad29c4b9053
nbe <aleisa.naser@gmail.com>
2023-02-21 13:27:20 +0300

    test invoice reminder email

---
+++ packages/test/tests/pay/email.spec.ts
@@ -0,0 +1,70 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { expect } from '@playwright/test';
+
+import { getRoute, PageType } from '@self/utils';
+
+import { prisma } from '../../prisma';
+import { test } from '../api/api-fixtures';
+
+test.use({ invoicesParams: [{ isPaid: false }] });
+
+const email1 = 'test-1@aqtest.com';
+const email2 = 'test-2@aqtest.com';
+const emails = [email1, email2];
+
+test.beforeAll(() => {
+	// create users
+	emails.forEach((email) => {
+		prisma.user
+			.create({
+				data: { email, fullName: 'test' },
+			})
+			.catch(() => {});
+	});
+});
+
+test('invoice due email', async ({ page, tenant, invoice }) => {
+	// ensure tenant has an email
+	emails.forEach((email) => {
+		prisma.role
+			.create({
+				data: {
+					roleType: 'TENANT',
+					organization: { connect: { id: invoice.organizationId } },
+					tenant: { connect: { id: tenant.id } },
+					user: { connect: { email } },
+				},
+			})
+			.catch(() => {});
+	});
+
+	const url = getRoute({
+		entity: 'leaseInvoice',
+		id: invoice.id,
+		pageType: PageType.Id,
+		params: {
+			organizationId: invoice.organizationId,
+			portfolioId: invoice.portfolioId,
+		},
+	});
+
+	await page.goto(url);
+
+	const send = page.getByRole('button', { name: 'Send reminder' });
+
+	const responsePromise = page.waitForResponse((res) =>
+		res.url().includes('send-email'),
+	);
+
+	await send.click();
+
+	const response = await responsePromise;
+
+	expect(response.status()).toBe(201);
+
+	const body = (await response.json()) as string[];
+
+	// check email was sent
+	expect(body).toEqual(emails);
+	expect(body).toHaveLength(2);
+});
