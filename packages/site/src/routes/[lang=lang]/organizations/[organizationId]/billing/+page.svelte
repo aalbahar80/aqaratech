c82668e7e1718d140730b8debec066b1719da966
nbe <aleisa.naser@gmail.com>
2023-02-23 07:58:35 +0300

    fix cancel sub endpoint, don't use custom enhance

--- packages/site/src/routes/[lang=lang]/organizations/[organizationId]/billing/+page.svelte
+++ packages/site/src/routes/[lang=lang]/organizations/[organizationId]/billing/+page.svelte
@@ -18,9 +18,10 @@
 		<div slot="button">
 			<form
 				method="POST"
+				action="?/unsubscribe"
 				data-sveltekit-reload
 				rel="external"
-				use:enhance={externalRedirect}
+				use:enhance
 			>
 				<button
 					class="inline-flex items-center justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 font-medium text-red-700 hover:bg-red-200 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:text-sm"
