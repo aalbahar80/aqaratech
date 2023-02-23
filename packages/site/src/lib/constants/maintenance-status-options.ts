54e534810e5784c332236bbcd1f8b983cd3035dc
nbe <aleisa.naser@gmail.com>
2023-02-23 19:38:22 +0300

    use i18n labels in MO status form

--- packages/site/src/lib/constants/maintenance-status-options.ts
+++ packages/site/src/lib/constants/maintenance-status-options.ts
@@ -1,11 +1,30 @@
+import { get } from 'svelte/store';
 import { MAINTENANCE_ORDER_STATUS } from '@self/utils';
+
+import L from '$i18n/i18n-svelte';
+
+type Writeable<T> = { -readonly [P in keyof T]: T[P] };
 
 // use empty string to represent null,
 // otherwise a string 'null' may be sent to the server
 // https://stackoverflow.com/a/62303327/9689661
 export const maintenanceStatusOptions = MAINTENANCE_ORDER_STATUS.map(
-	(status) => ({
-		label: status,
-		value: status,
-	}),
+	(status) => {
+		const LL = get(L);
+
+		const maintenanceStatusLabels = {
+			PENDING: LL.badge.inProgress(),
+			COMPLETED: LL.badge.completed(),
+			CANCELLED: LL.badge.cancelled(),
+			'': '',
+		} satisfies Record<
+			Writeable<(typeof MAINTENANCE_ORDER_STATUS)[number]>,
+			string
+		>;
+
+		return {
+			label: maintenanceStatusLabels[status],
+			value: status,
+		};
+	},
 );
