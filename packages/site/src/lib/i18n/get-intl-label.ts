fd2e51938af19cad3df392ddd98b25ec56e8601d
nbe <aleisa.naser@gmail.com>
2023-02-26 15:27:42 +0300

    display arabic category label in expense pane

--- packages/site/src/lib/i18n/get-intl-label.ts
+++ packages/site/src/lib/i18n/get-intl-label.ts
@@ -3,6 +3,14 @@ import { getLabel } from '@self/utils';
 
 import L, { locale } from '$i18n/i18n-svelte';
 
+/** For i18n.
+ * Key: any string value.
+ * Value: the key in the LL object.
+ */
+const synonyms: Record<string, string> = {
+	category: 'categoryId',
+};
+
 /** Attempt to get label based on locale, falling back to English. */
 export const getIntlLabel = (key: string) => {
 	const currentLocale = get(locale);
@@ -14,7 +22,7 @@ export const getIntlLabel = (key: string) => {
 
 		// @ts-expect-error wip
 		// eslint-disable-next-line @typescript-eslint/no-unsafe-call
-		return (LL.fields[key]() as string) || enLabel;
+		return (LL.fields[synonyms[key] ?? key]() as string) || enLabel;
 	} else {
 		return enLabel;
 	}
