4890731c670967d3c6132cec29ed9bebc44f1acc
nbe <aleisa.naser@gmail.com>
2023-02-27 11:45:57 +0300

    rm unused data

--- packages/site/src/routes/[lang=lang]/organizations/[organizationId]/expense-categories/+page.svelte
+++ packages/site/src/routes/[lang=lang]/organizations/[organizationId]/expense-categories/+page.svelte
@@ -1,11 +1,7 @@
 <script lang="ts">
-	import type { PageData } from './$types';
-
 	import { categories } from '$lib/stores/expense-categories';
 	import Tree from '$lib/tree/Tree.svelte';
 	import { toHeirarchy } from '$lib/utils/expense-type-options';
-
-	export let data: PageData;
 </script>
 
 <Tree root={toHeirarchy($categories)} />
