702190a58001851a365148f016bc4e29e99c00a8
nbe <aleisa.naser@gmail.com>
2023-02-27 12:44:44 +0300

    link to tree from category page

--- packages/site/src/routes/[lang=lang]/organizations/[organizationId]/expense-categories/[expenseCategoryId]/+page.svelte
+++ packages/site/src/routes/[lang=lang]/organizations/[organizationId]/expense-categories/[expenseCategoryId]/+page.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import type { PageData } from './$types';
+	import { getRoute, PageType } from '@self/utils';
 
 	import L from '$i18n/i18n-svelte';
+	import Arrow from '$lib/components/Arrow.svelte';
 	import AutoDetailsPane from '$lib/components/AutoDetailsPane.svelte';
+	import TextButton from '$lib/components/buttons/TextButton.svelte';
 	import Heading from '$lib/components/Heading.svelte';
 
 	export let data: PageData;
@@ -13,6 +17,20 @@
 	id={data.expenseCategory.id}
 	entity="expenseCategory"
 />
+
+<TextButton>
+	<a
+		href={getRoute({
+			entity: 'expenseCategory',
+			pageType: PageType.List,
+			params: $page.params,
+		})}
+	>
+		{$L.buttons.view()}
+		{$L.general.all()}
+		<Arrow />
+	</a>
+</TextButton>
 
 <AutoDetailsPane
 	details={data.expenseCategory}
