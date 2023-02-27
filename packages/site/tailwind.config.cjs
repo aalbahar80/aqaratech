736692367c17efd2435c42e49c7c3eb74b919a12
nbe <aleisa.naser@gmail.com>
2023-02-27 11:07:52 +0300

    add tailwind plugin for logical direction

--- packages/site/tailwind.config.cjs
+++ packages/site/tailwind.config.cjs
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
@@ -18,6 +20,34 @@ module.exports = {
 		require('@tailwindcss/typography'),
 		require('@tailwindcss/forms'),
 		require('@tailwindcss/aspect-ratio'),
+
+		// TODO: remove once feature is released
+		// https://github.com/tailwindlabs/tailwindcss/pull/10166#issuecomment-1432382055
+		plugin(function ({ matchUtilities, theme }) {
+			matchUtilities(
+				{
+					ms: (value) => ({
+						'margin-inline-start': value,
+					}),
+					me: (value) => ({
+						'margin-inline-end': value,
+					}),
+					ps: (value) => ({
+						'padding-inline-start': value,
+					}),
+					pe: (value) => ({
+						'padding-inline-end': value,
+					}),
+					start: (value) => ({
+						'inset-inline-start': value,
+					}),
+					end: (value) => ({
+						'inset-inline-end': value,
+					}),
+				},
+				{ values: theme('spacing') },
+			);
+		}),
 	],
 
 	darkMode: 'class', // disable auto dark mode
