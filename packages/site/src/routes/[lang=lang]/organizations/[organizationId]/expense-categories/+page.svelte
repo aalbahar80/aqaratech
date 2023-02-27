365f7bf409957aa83be3d9491587d8608539aeb4
nbe <aleisa.naser@gmail.com>
2023-02-27 12:36:03 +0300

    add title to tree page

--- packages/site/src/routes/[lang=lang]/organizations/[organizationId]/expense-categories/+page.svelte
+++ packages/site/src/routes/[lang=lang]/organizations/[organizationId]/expense-categories/+page.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
+	import L from '$i18n/i18n-svelte';
 	import { categories } from '$lib/stores/expense-categories';
 	import Tree from '$lib/tree/Tree.svelte';
 	import { toHeirarchy } from '$lib/utils/expense-type-options';
 </script>
+
+<!-- Title -->
+<div class="flex items-center pb-4">
+	<h2
+		class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl"
+	>
+		{$L.entity.expenseCategory.plural()}
+	</h2>
+</div>
 
 <Tree root={toHeirarchy($categories)} />
