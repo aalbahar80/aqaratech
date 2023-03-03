5348996c527aa8b3b566df59a15cae4bc86dd0c3
nbe <aleisa.naser@gmail.com>
2023-03-03 07:35:29 +0300

    explicitly specify hero text breakpoint

--- packages/site/src/lib/components/landing/Hero.svelte
+++ packages/site/src/lib/components/landing/Hero.svelte
@@ -9,9 +9,10 @@
 <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8" data-testid="hero">
 	<div class="flex flex-col gap-8 pt-20 pb-16 text-center lg:pt-32">
 		<h1
-			class="font-display mx-auto max-w-4xl text-3xl font-medium tracking-tight text-slate-900 min-[400px]:text-4xl min-[500px]:text-5xl sm:text-7xl"
+			class="font-display mx-auto max-w-4xl text-5xl font-medium tracking-tight text-slate-900 max-[300px]:text-4xl sm:text-7xl"
 		>
 			{$L.landing.hero.titlePrefix()}
+			<br />
 			<span class="relative whitespace-nowrap text-blue-600">
 				<UnderlineDecoration />
 				<span class="relative"> {$L.landing.hero.titleSuffix()}</span>
