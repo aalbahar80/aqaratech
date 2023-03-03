96a7b3d10e1e6cd812346df5710cb0b9a59a5998
nbe <aleisa.naser@gmail.com>
2023-03-03 13:41:43 +0300

    decrease table header height on mobile

--- packages/site/src/lib/components/filter/FilterHero.svelte
+++ packages/site/src/lib/components/filter/FilterHero.svelte
@@ -3,8 +3,8 @@
 	export let subtitle = '';
 </script>
 
-<div class="border-b border-gray-200 pb-10 pt-20">
-	<h1 class="text-4xl font-extrabold tracking-tight text-gray-900">
+<div class="border-b border-gray-200 pb-5 pt-10 md:pb-10 md:pt-20">
+	<h1 class="text-3xl font-extrabold tracking-tight text-gray-900 md:text-4xl">
 		<slot name="title">{title}</slot>
 	</h1>
 	<p class="mx-auto mt-4 max-w-3xl text-base text-gray-500">
