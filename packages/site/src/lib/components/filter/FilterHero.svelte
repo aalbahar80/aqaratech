0bc2b5fe900eeaec44a49bed8b024f87d59be66e
nbe <aleisa.naser@gmail.com>
2023-03-04 22:35:15 +0300

    rm table header subtitle

--- packages/site/src/lib/components/filter/FilterHero.svelte
+++ packages/site/src/lib/components/filter/FilterHero.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
 	export let title: string;
-	export let subtitle = '';
 </script>
 
-<div class="border-b border-gray-200 pb-5 pt-10">
+<div class="border-b border-gray-200 pb-2.5 pt-7 md:pb-5 md:pt-10">
 	<h1 class="text-3xl font-extrabold tracking-tight text-gray-900 md:text-4xl">
 		<slot name="title">{title}</slot>
 	</h1>
-	<p class="mx-auto mt-4 max-w-3xl text-base text-gray-500">
-		<slot name="subtitle">{subtitle}</slot>
-	</p>
+	<!-- <p class="mx-auto mt-4 max-w-3xl text-base text-gray-500"> -->
+	<!-- 	<slot name="subtitle">{subtitle}</slot> -->
+	<!-- </p> -->
 </div>
