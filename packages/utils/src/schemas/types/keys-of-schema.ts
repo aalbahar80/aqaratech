32ca03b7509ce45d6d2fc4200073ffd85aa6ed55
nbe <aleisa.naser@gmail.com>
2023-03-07 23:36:38 +0300

    rm unused

--- packages/utils/src/schemas/types/keys-of-schema.ts
+++ packages/utils/src/schemas/types/keys-of-schema.ts
@@ -13,22 +13,3 @@ export type UnwrapSchema<T extends z.ZodTypeAny> = T extends z.ZodType<infer O>
 export type InnerSchema<T> = T extends z.ZodEffects<z.ZodEffects<z.ZodTypeAny>>
 	? ReturnType<ReturnType<T['innerType']>['innerType']> // leaseCreateSchema needs to be unwrapped twice
 	: T;
-
-/**
- * Extracts keys from a Zod schema.
- */
-export type KeyOfSchemaIntersection<T extends z.ZodTypeAny> =
-	T extends z.ZodType<infer O> ? keyof O : never;
-
-/**
- * Extracts and combines keys from multiple Zod schemas into a union.
- */
-export type KeyOfMultipleSchemas<T extends z.ZodTypeAny[]> = T extends [
-	...z.ZodType<infer U>[],
-]
-	? KeysOfUnion<U>
-	: never;
-
-export const keysOfSchema = <T extends z.AnyZodObject>(
-	schema: T,
-): KeyOfSchema<T> => schema.keyof().options;
