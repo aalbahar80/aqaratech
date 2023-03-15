dc0feba4df8146e27b354bba7233cf6928cdbe23
nbe <aleisa.naser@gmail.com>
2023-03-16 02:17:45 +0300

    display invoice sum

--- packages/site/src/routes/[lang=lang]/organizations/[organizationId]/leaseInvoices/+page.svelte
+++ packages/site/src/routes/[lang=lang]/organizations/[organizationId]/leaseInvoices/+page.svelte
@@ -7,11 +7,14 @@
 
 	import L from '$i18n/i18n-svelte';
 	import DateFilter from '$lib/components/dashboard/filter/DateFilter.svelte';
+	import StatisticsPane from '$lib/components/dashboard/stats/StatisticsPane.svelte';
+	import Stats from '$lib/components/dashboard/stats/Stats.svelte';
 	import LeaseInvoiceTable from '$lib/components/leaseInvoice/LeaseInvoiceTable.svelte';
 	import { portfolioColumnDef } from '$lib/components/table/tanstack-table/columns/portfolio';
 	import { isPaidFilter } from '$lib/components/table/tanstack-table/filters/is-paid';
 	import { isPaidLateFilter } from '$lib/components/table/tanstack-table/filters/is-paid-late';
 	import { isPaidOnlineFilter } from '$lib/components/table/tanstack-table/filters/is-paid-online';
+	import { fmtCurrency } from '$lib/i18n/format';
 
 	export let data: PageData;
 
@@ -25,6 +28,16 @@
 		<DateFilter />
 	</div>
 </div>
+
+<Stats>
+	<svelte:fragment slot="panes">
+		<StatisticsPane
+			primaryText={$L.general.total() + ' ' + $L.entity.leaseInvoice.plural()}
+			secondaryText={$L.general.forPeriod()}
+			primaryValue={fmtCurrency(data.invoices.sum)}
+		/>
+	</svelte:fragment>
+</Stats>
 
 <LeaseInvoiceTable
 	data={data.invoices}
