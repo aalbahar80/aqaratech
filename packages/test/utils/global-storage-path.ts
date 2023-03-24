dbcb623fef1dbf466242acb5cc9a59d0ff0eac8a
nbe <aleisa.naser@gmail.com>
2023-03-24 21:57:52 +0300

    store login state in cache instead of tmp. Persists across nix-shells

--- packages/test/utils/global-storage-path.ts
+++ packages/test/utils/global-storage-path.ts
@@ -4,7 +4,8 @@ import path from 'node:path';
 /** Use an absolute path for storing cookies. A good place that is
  * cross-platform is the OS's temp directory. Works well with git worktrees. */
 export const globalStoragePath = path.join(
-	os.tmpdir(),
+	os.homedir(),
+	'.cache',
 	'playwright',
 	'storage-state',
 );
