dd9d30bc014ba9fef18209644967b02579998dc0
nbe <aleisa.naser@gmail.com>
2023-04-13 20:56:11 +0300

    update deps - turbo to 1.9.1

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   description = "A prisma test project";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs-turbo.url = "github:thenbe/nixpkgs/turbo-1.9.0";
+  inputs.nixpkgs-turbo.url = "github:thenbe/nixpkgs/turbo-1.9.1";
 
   outputs = {
     self,
