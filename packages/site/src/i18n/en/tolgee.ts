4b011b69ac67a94ac0d0794ef6b8f3fbf5eda9a3
nbe <aleisa.naser@gmail.com>
2023-04-15 22:50:31 +0300

    tolgee update 2023-04-15, backport pricing/faqs translations

--- packages/site/src/i18n/en/tolgee.ts
+++ packages/site/src/i18n/en/tolgee.ts
@@ -14,6 +14,47 @@ export const tolgee = {
 				'Start managing your properties with just a few clicks! Sign up now and see the difference.',
 			title: 'Get started today',
 		},
+		faqs: {
+			'1': {
+				a: 'Aqaratech is a property management platform designed to streamline property management for owners and tenants. It offers dedicated portals for owners and tenants, enabling easy communication, online rent payments, maintenance requests, and expense tracking.',
+				q: 'What is Aqaratech?',
+			},
+			'2': {
+				a: 'The owner’s portal provides property owners with a dashboard to manage their properties. Includes features like tenant management, rent collection, expense tracking, maintenance requests, and document storage, all within an easy-to-use interface.',
+				q: 'How does the owner’s portal work?',
+			},
+			'3': {
+				a: 'The tenant’s portal allows tenants to view their lease details, submit maintenance requests, communicate with property managers, and pay rent online securely and conveniently.',
+				q: 'What features are available in the tenant’s portal?',
+			},
+			'4': {
+				a: 'Aqaratech offers a secure online payment system, allowing tenants to pay rent using various payment methods. Property managers can track payments, generate receipts, and set up automatic reminders for upcoming rent due dates.',
+				q: 'How do online rent payments work in Aqaratech?',
+			},
+			'5': {
+				a: 'Tenants can submit maintenance requests directly through their portal by providing details about the issue and attaching any relevant photos. Property managers can then track, prioritize, and resolve these requests within the owner’s portal.',
+				q: 'How do tenants submit maintenance requests through Aqaratech?',
+			},
+			'6': {
+				a: 'The expense tracking feature in Aqaratech enables property owners and managers to monitor income and expenses related to their properties. They can categorize expenses, track invoices, and generate customizable financial reports.',
+				q: 'How does the expense tracking feature work for property owners?',
+			},
+			'7': {
+				a: 'Yes, Aqaratech offers a mobile friendly interface for both Android and iOS devices, allowing property owners and tenants to access their portals and manage their properties on-the-go.',
+				q: 'Is Aqaratech available on mobile devices?',
+			},
+			'8': {
+				a: 'Aqaratech adheres to industry-standard security protocols and data protection regulations to ensure the confidentiality and security of your sensitive information.',
+				q: 'How secure is my data with Aqaratech?',
+			},
+			'9': {
+				a: 'Yes, we offer a free trial version for interested users to try the platform features before committing to a subscription. Please contact our sales team for more information on setting up a trial.',
+				q: 'Is there a free trial or demo available for Aqaratech?',
+			},
+			subtitle:
+				'If you have any other questions, please feel free to contact us.',
+			title: 'Frequently Asked Questions',
+		},
 		features: {
 			data: {
 				bullets: {
@@ -86,6 +127,22 @@ export const tolgee = {
 		},
 		location: 'Location',
 		map: 'Map',
+		pricing: {
+			features: {
+				'1': 'Online collection',
+				'2': 'File storage',
+				'3': 'SMS and email notifications',
+				'4': 'Auto-reminders',
+				'5': 'Multi-beneficiary profiles',
+				'6': 'Real-time payment tracking',
+				'7': 'Flexible payments for tenants',
+				'8': 'Auto settlements & reconciliation',
+			},
+			price: '$5',
+			priceSubtitle: 'Per unit',
+			subtitle: 'Subscription Details.',
+			title: 'Pricing',
+		},
 		secondaryFeatures: {
 			managers: {
 				description:
