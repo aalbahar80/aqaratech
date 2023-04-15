4f0a093fbce2f4e0a3959237e386c387218265ec
nbe <aleisa.naser@gmail.com>
2023-04-15 22:05:50 +0300

    reorder english keys

--- packages/site/src/i18n/en/tolgee.ts
+++ packages/site/src/i18n/en/tolgee.ts
@@ -1,56 +1,51 @@
 export const tolgee = {
 	aqaratech: {
-		fullName: 'Aqaratech, LLC.',
-		shortName: 'Aqaratech',
 		address: {
 			line1: '25 Alshuhada St, Sulaiman Tower',
 			line2: 'Sharq, 15305, Kuwait',
 		},
+		fullName: 'Aqaratech, LLC.',
+		shortName: 'Aqaratech',
 	},
 	landing: {
-		hero: {
-			titlePrefix: 'Manage your properties',
-			titleSuffix: 'with ease.',
-			subtitle: 'Property management simplified.',
+		callToAction: {
+			button: 'Create Account',
 			description:
-				'Aqaratech provides a comprehensive, easy-to-use platform where you can manage multiple properties, keep track of important tasks and deadlines, and quickly access important information – all in one place.',
+				'Try Aqaratech and experience the ease and convenience of managing your properties with just a few clicks! Sign up now and see the difference it can make for your business.',
+			title: 'Get started today',
 		},
 		features: {
 			data: {
-				title: 'Data and Analytics',
 				bullets: {
+					monitor:
+						"Monitor your properties' occupancy rates and vacancy periods using heatmaps.",
 					track:
 						'Comprehensive tracking of property-related data, including units, leases, tenants, and more.',
 					visualize:
 						"Visualize your properties' financial data to identify trends and patterns over time. Use the interactive dashboards to view income, expenses, and profit/loss for each portfolio, property, and unit.",
-					monitor:
-						"Monitor your properties' occupancy rates and vacancy periods using heatmaps.",
 				},
+				title: 'Data and Analytics',
 			},
-
-			rental: {
-				title: 'Rental Payment Tracking',
+			document: {
 				bullets: {
-					track:
-						'Easy tracking and management of rental payments and lease agreements.',
-					reminders: 'Automated payment reminders.',
+					access: 'Access your documents from anywhere, using any device.',
+					upload:
+						'Store and manage important documents related to your properties and tenants, including lease agreements, rental payment receipts, and maintenance records.',
 				},
+				title: 'Document Management',
 			},
-
 			expense: {
-				title: 'Expense Tracking and Visualization',
 				bullets: {
-					track:
-						'Comprehensive expense tracking provides you with valuable granularity for expense analysis. Choose to attribute expenses to a portfolio, property, or even a single unit.',
 					categorize:
 						'Categorize expenses using a hierarchical system to better understand your expense sources.',
+					track:
+						'Comprehensive expense tracking provides you with valuable granularity for expense analysis. Choose to attribute expenses to a portfolio, property, or even a single unit.',
 					visualize:
 						'Visualize expenses using interactive treemaps, which allow you to quickly identify your cost patterns.',
 				},
+				title: 'Expense Tracking and Visualization',
 			},
-
 			filter: {
-				title: 'Advanced Filtering',
 				bullets: {
 					customizable:
 						'Customizable filters, so you can focus on the information that is most relevant to you.',
@@ -58,57 +53,57 @@ export const tolgee = {
 						'Filter your properties and tenant data by location, including specific properties or units within properties.',
 					time: 'Filter by time, including specific date ranges or time periods.',
 				},
+				title: 'Advanced Filtering',
 			},
-
-			document: {
-				title: 'Document Management',
+			rental: {
 				bullets: {
-					upload:
-						'Store and manage important documents related to your properties and tenants, including lease agreements, rental payment receipts, and maintenance records.',
-					access: 'Access your documents from anywhere, using any device.',
+					reminders: 'Automated payment reminders.',
+					track:
+						'Easy tracking and management of rental payments and lease agreements.',
 				},
+				title: 'Rental Payment Tracking',
 			},
-
 			search: {
-				title: 'Powerful Search',
 				bullets: {
 					all: 'Search through all of your data at the same time.',
 					spell:
 						"Find what you're looking for without knowing the exact spelling.",
 				},
+				title: 'Powerful Search',
 			},
-		},
-		secondaryFeatures: {
-			managers: {
-				summary: 'For property managers',
-				name: 'Streamline your workflows',
-				description:
-					'Leverage our powerful tools to keep track of all your properties. Aqaratech helps you organize your business and work more efficiently.',
-			},
-			owners: {
-				summary: 'For owners',
-				name: 'Complete visibility',
-				description:
-					'Use our dashboard to access and monitor all of your property information, granting you peace of mind helping you make informed decisions.',
-			},
-			tenants: {
-				summary: 'For tenants',
-				name: 'Hassle-free rentals',
-				description:
-					'Aqaratech simplifies the rental process by allowing tenants to track their payments and leases.',
-				// 'Aqaratech simplifies the rental process by allowing tenants to pay their rent at any time, from any device.',
-			},
-		},
-		callToAction: {
-			title: 'Get started today',
-			description:
-				'Try Aqaratech and experience the ease and convenience of managing your properties with just a few clicks! Sign up now and see the difference it can make for your business.',
-			button: 'Create Account',
 		},
 		footer: {
 			legal: 'Aqaratech, WLL. All rights reserved.',
 		},
+		hero: {
+			description:
+				'Aqaratech provides a comprehensive, easy-to-use platform where you can manage multiple properties, keep track of important tasks and deadlines, and quickly access important information – all in one place.',
+			subtitle: 'Property management simplified.',
+			titlePrefix: 'Manage your properties',
+			titleSuffix: 'with ease.',
+		},
 		location: 'Location',
 		map: 'Map',
+		secondaryFeatures: {
+			managers: {
+				description:
+					'Leverage our powerful tools to keep track of all your properties. Aqaratech helps you organize your business and work more efficiently.',
+				name: 'Streamline your workflows',
+				summary: 'For property managers',
+			},
+			owners: {
+				description:
+					'Use our dashboard to access and monitor all of your property information, granting you peace of mind helping you make informed decisions.',
+				name: 'Complete visibility',
+				summary: 'For owners',
+			},
+			tenants: {
+				description:
+					'Aqaratech simplifies the rental process by allowing tenants to track their payments and leases.',
+				// 'Aqaratech simplifies the rental process by allowing tenants to pay their rent at any time, from any device.',
+				name: 'Hassle-free rentals',
+				summary: 'For tenants',
+			},
+		},
 	},
 };
