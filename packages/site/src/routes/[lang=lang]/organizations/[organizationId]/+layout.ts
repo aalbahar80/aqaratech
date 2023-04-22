7a0a78e201465daf9b52c2a986cb7ab037668df4
nbe <aleisa.naser@gmail.com>
2023-04-22 04:42:43 +0300

    fix: only getporfolios for admins

--- packages/site/src/routes/[lang=lang]/organizations/[organizationId]/+layout.ts
+++ packages/site/src/routes/[lang=lang]/organizations/[organizationId]/+layout.ts
@@ -1,13 +1,19 @@
 import type { LayoutLoad } from './$types';
 
+import { assertRole } from '$lib/assertions/user-role';
 import { getPortfolios } from '$lib/components/portfolio/get-portfolios';
 
 export const load: LayoutLoad = async ({ fetch, parent }) => {
-	const { queryClient } = await parent();
+	const { queryClient, user } = await parent();
 
-	// Used in nav-tree
-	await queryClient.prefetchQuery({
-		queryKey: ['portfolios'],
-		queryFn: async () => await getPortfolios({ fetch }),
-	});
+	assertRole(user);
+
+	if (user.role.roleType === 'ORGADMIN') {
+		// Used in nav-tree
+		await queryClient.prefetchQuery({
+			queryKey: ['portfolios'],
+			queryFn: async () => await getPortfolios({ fetch }),
+			// only for admins
+		});
+	}
 };
