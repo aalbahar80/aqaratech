4429c11f51afbc8fe778ed8386fa1e00480c8c66
nbe <aleisa.naser@gmail.com>
2023-05-05 06:15:13 +0300

    add sendSMS invoice unit test

---
+++ packages/backend/src/lease-invoices/lease-invoices.spec.ts
@@ -0,0 +1,94 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable security/detect-possible-timing-attacks */
+import { Test } from '@nestjs/testing';
+
+import { EnvModule } from 'src/env/env.module';
+import { NovuService } from 'src/novu/novu.service';
+
+import { LeaseInvoicesService } from './lease-invoices.service';
+
+const invoice = {
+	id: '1',
+	amount: 2,
+	postAt: new Date(),
+	portfolioId: '3',
+	organizationId: '4',
+	lease: {
+		tenant: {
+			id: '5',
+			phone: '1234567890',
+			roles: [{ user: { email: 'user@example.com' } }],
+		},
+	},
+};
+
+describe('LeaseInvoicesService', () => {
+	let service: LeaseInvoicesService;
+	let novuService: NovuService;
+
+	beforeEach(async () => {
+		const moduleRef = await Test.createTestingModule({
+			providers: [LeaseInvoicesService],
+			imports: [EnvModule],
+		})
+			.useMocker((token) => {
+				// mock NovuService to avoid sending SMS
+				if (token === NovuService) {
+					return {
+						sendSMS: vi.fn().mockResolvedValue(undefined),
+					};
+				}
+
+				if (typeof token === 'function') {
+					const mock = vi.fn(token);
+					return mock;
+				}
+
+				if (typeof token === 'symbol') {
+					return token;
+				}
+
+				return;
+			})
+			.compile();
+
+		service = moduleRef.get(LeaseInvoicesService);
+		novuService = moduleRef.get(NovuService);
+	});
+
+	it('should be defined', () => {
+		expect(service).toBeDefined();
+	});
+
+	it('should call sendSMS once', async () => {
+		const spy = vi.spyOn(novuService, 'sendSMS');
+
+		await service.notify({
+			method: 'SMS',
+			invoice,
+		});
+
+		expect(spy).toHaveBeenCalledOnce();
+	});
+
+	it('should call sendSMS with correct arguments', async () => {
+		const spy = vi.spyOn(novuService, 'sendSMS');
+
+		await service.notify({
+			method: 'SMS',
+			invoice,
+		});
+
+		const template = {
+			to: {
+				subscriberId: '5',
+				phone: '1234567890',
+			},
+			payload: {
+				content: `Use the link to view and pay your rent online: ${process.env.PUBLIC_API_URL}/leaseInvoices/1/pay`,
+			},
+		};
+
+		expect(spy).toHaveBeenCalledWith(template);
+	});
+});
