c2b96e8fda1f0a5e35a0a40a1a3540f07ff9b3ae
nbe <aleisa.naser@gmail.com>
2023-05-05 04:35:50 +0300

    store openapi-generator-cli in cache dir

--- packages/backend/get-openapi.sh
+++ packages/backend/get-openapi.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
+set -e
+
 VERSION="6.4.0"
 JAR_FILE="openapi-generator-cli-$VERSION.jar"
+CACHE_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/openapi-generator"
+mkdir -p "$CACHE_DIR"
 
-# check if openapi-generator-cli.jar exists in the current directory
-if [ ! -f "$JAR_FILE" ]; then
+# check if openapi-generator-cli.jar exists in the cache directory
+if [ ! -f "$CACHE_DIR/$JAR_FILE" ]; then
 	echo "Downloading $JAR_FILE..."
 	# download openapi-generator-cli.jar
 	# wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/6.1.0/openapi-generator-cli-6.1.0.jar -O ./openapi-generator-cli-6.1.0.jar
-	wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/$VERSION/$JAR_FILE -O ./$JAR_FILE
+	wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/$VERSION/$JAR_FILE -O "$CACHE_DIR/$JAR_FILE"
 else
 	echo "$JAR_FILE already exists. Skipping download..."
 fi
@@ -17,4 +21,4 @@ fi
 # openapitools.json will always refer to the BASE_NAME, so make sure it points to the specific version.
 BASE_NAME="openapi-generator-cli.jar"
 
-ln -sf $JAR_FILE $BASE_NAME
+ln -sf "$CACHE_DIR/$JAR_FILE" $BASE_NAME
