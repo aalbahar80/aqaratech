dd6da255eee48bc264cbb26cb64b9f0a1d0e638b
nbe <aleisa.naser@gmail.com>
2023-05-07 14:17:15 +0300

    fixup! add backend vitest db setup

--- packages/backend/test/util/run-integration.sh
+++ packages/backend/test/util/run-integration.sh
@@ -7,6 +7,6 @@ DATABASE_URL="postgresql://postgres:postgres@localhost:5435/aq-db"
 export DATABASE_URL
 docker compose -f docker-compose-test.yml up -d
 echo '🟡 - Waiting for database to be ready...'
-"$DIR/wait-for-it.sh" -t 3 "${DATABASE_URL}" -- echo '🟢 - Database is ready!'
+"$DIR/wait-for-it.sh" -t 1 "${DATABASE_URL}" -- echo '🟢 - Database is ready!'
 pnpm prisma migrate reset --force --skip-seed
 pnpm vitest --run
