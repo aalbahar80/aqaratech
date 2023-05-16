d5878ad29911069a5f2190bfc716b4bb76df01eb
nbe <aleisa.naser@gmail.com>
2023-05-16 14:52:47 +0300

    rm custom tailwind logical properties plugin

--- packages/site/tailwind.config.cjs
+++ packages/site/tailwind.config.cjs
@@ -1,5 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const plugin = require('tailwindcss/plugin');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -25,36 +23,6 @@ module.exports = {
 		require('@tailwindcss/forms'),
 		require('@tailwindcss/aspect-ratio'),
 		require('@tailwindcss/container-queries'),
-
-		// TODO: remove once feature is released
-		// https://github.com/tailwindlabs/tailwindcss/pull/10166#issuecomment-1432382055
-		plugin(function ({ matchUtilities, theme }) {
-			matchUtilities(
-				{
-					/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-					ms: (value) => ({
-						'margin-inline-start': value,
-					}),
-					me: (value) => ({
-						'margin-inline-end': value,
-					}),
-					ps: (value) => ({
-						'padding-inline-start': value,
-					}),
-					pe: (value) => ({
-						'padding-inline-end': value,
-					}),
-					start: (value) => ({
-						'inset-inline-start': value,
-					}),
-					end: (value) => ({
-						'inset-inline-end': value,
-					}),
-					/* eslint-enable @typescript-eslint/no-unsafe-assignment */
-				},
-				{ values: theme('spacing') },
-			);
-		}),
 	],
 
 	darkMode: 'class', // disable auto dark mode
