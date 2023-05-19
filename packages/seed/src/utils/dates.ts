5d3eb65aaadfd9dca92f60c47daf6c4729a363e0
nbe <aleisa.naser@gmail.com>
2023-05-19 23:29:55 +0300

    set seed reference date one year in future

--- packages/seed/src/utils/dates.ts
+++ packages/seed/src/utils/dates.ts
@@ -1,5 +1,9 @@
 import { faker } from '@faker-js/faker';
 
 export const timespan = 2;
-export const createdAt = () => faker.date.past({ years: timespan });
-export const updatedAt = () => faker.date.past({ years: timespan });
+
+// one year from now
+const refDate = new Date(new Date().setFullYear(new Date().getFullYear() + 1));
+
+export const createdAt = () => faker.date.past({ years: timespan, refDate });
+export const updatedAt = () => faker.date.past({ years: timespan, refDate });
