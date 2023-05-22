aaffd7390355c87cc79ede3d81d5b27108109727
nbe <aleisa.naser@gmail.com>
2023-05-22 03:16:41 +0300

    convert vite config export to object

--- packages/site/vite.config.js
+++ packages/site/vite.config.js
@@ -8,59 +8,54 @@ import { defineConfig } from 'vite';
 
 import { version } from './package.json';
 
-const ANALYZE_BUNDLE = process.env['ANALYZE_BUNDLE'] === '1';
+console.log({
+	SENTRY_RELEASE_ENABLE: process.env.SENTRY_RELEASE_ENABLE,
+	SENTRY_RELEASE_VERSION: process.env.SENTRY_RELEASE_VERSION,
+	SENTRY_RELEASE_ENVIRONMENT: process.env.SENTRY_RELEASE_ENVIRONMENT,
+	ANALYZE_BUNDLE: process.env.ANALYZE_BUNDLE,
+});
 
-export default defineConfig(() => {
-	console.log({
-		SENTRY_RELEASE_ENABLE: process.env.SENTRY_RELEASE_ENABLE,
-		SENTRY_RELEASE_VERSION: process.env.SENTRY_RELEASE_VERSION,
-		SENTRY_RELEASE_ENVIRONMENT: process.env.SENTRY_RELEASE_ENVIRONMENT,
-	});
-
-	/** @type {import('vite').UserConfig} */
-	const config = {
-		define: {
-			__AQARATECH_APP_VERSION__: JSON.stringify(version),
-			__SENTRY_DEBUG__: false,
-		},
-		plugins: [
-			sentrySvelteKit({
-				debug: !!process.env.SENTRY_RELEASE_ENABLE,
-				autoUploadSourceMaps: !!process.env.SENTRY_RELEASE_ENABLE,
-				// Other sentry config in `sentry.properties`
-				sourceMapsUploadOptions: {
-					debug: true,
-					release: 'site-' + (process.env.SENTRY_RELEASE_VERSION ?? 'unknown'),
-					setCommits: {
-						auto: true,
-					},
-					deploy: {
-						env:
-							process.env.SENTRY_RELEASE_ENVIRONMENT === 'prod'
-								? 'production'
-								: 'staging',
-					},
-					// cleanArtifacts: true, // enable?
+/** @type {import('vite').UserConfigExport} */
+export default defineConfig({
+	define: {
+		__AQARATECH_APP_VERSION__: JSON.stringify(version),
+		__SENTRY_DEBUG__: false,
+	},
+	plugins: [
+		sentrySvelteKit({
+			debug: !!process.env.SENTRY_RELEASE_ENABLE,
+			autoUploadSourceMaps: !!process.env.SENTRY_RELEASE_ENABLE,
+			// Other sentry config in `sentry.properties`
+			sourceMapsUploadOptions: {
+				debug: true,
+				release: 'site-' + (process.env.SENTRY_RELEASE_VERSION ?? 'unknown'),
+				setCommits: {
+					auto: true,
 				},
-			}),
-			sveltekit(),
-			icons({ compiler: 'svelte' }),
-			// See: https://github.com/btd/rollup-plugin-visualizer#usage
-			...(ANALYZE_BUNDLE
-				? [
-						visualizer({
-							emitFile: true,
-							filename: 'stats.html',
-						}),
-				  ]
-				: []),
-		],
-		clearScreen: false,
-		ssr: {
-			// set chart.js && papaparse as `noExternal` to avoid issues in SSR (when running `vite preview`)
-			noExternal: ['typesafe-i18n'],
-		},
-	};
-
-	return config;
+				deploy: {
+					env:
+						process.env.SENTRY_RELEASE_ENVIRONMENT === 'prod'
+							? 'production'
+							: 'staging',
+				},
+				// cleanArtifacts: true, // enable?
+			},
+		}),
+		sveltekit(),
+		icons({ compiler: 'svelte' }),
+		// See: https://github.com/btd/rollup-plugin-visualizer#usage
+		...(process.env.ANALYZE_BUNDLE === '1'
+			? [
+					visualizer({
+						emitFile: true,
+						filename: 'stats.html',
+					}),
+			  ]
+			: []),
+	],
+	clearScreen: false,
+	ssr: {
+		// set chart.js && papaparse as `noExternal` to avoid issues in SSR (when running `vite preview`)
+		noExternal: ['typesafe-i18n'],
+	},
 });
