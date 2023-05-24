4fe99d8b0729daf3e44a5e6c0c05e8e6c54b341b
nbe <aleisa.naser@gmail.com>
2023-05-24 20:46:10 +0300

    side-by-side hero text EXE-71

--- packages/site/src/lib/components/landing/Hero.svelte
+++ packages/site/src/lib/components/landing/Hero.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import HeroSubtitle from './HeroSubtitle.svelte';
 	import UnderlineDecoration from './UnderlineDecoration.svelte';
 
 	import L from '$i18n/i18n-svelte';
@@ -11,22 +12,30 @@
 	data-testid="hero"
 >
 	<div class="flex flex-col gap-8 pb-16 pt-20 text-center lg:pt-32">
-		<h1
-			class="font-display mx-auto max-w-4xl text-5xl font-medium tracking-tight text-slate-900 max-[300px]:text-4xl sm:text-7xl"
-		>
-			{$L.landing.hero.titlePrefix()}
-			<br />
-			<span class="relative whitespace-nowrap text-blue-600">
-				<UnderlineDecoration />
-				<span class="relative"> {$L.landing.hero.titleHighlight()}</span>
-			</span>
-			{$L.landing.hero.titleSuffix()}
-		</h1>
+		<div class="flex flex-col pb-12 lg:flex-row">
+			<h1
+				class="font-display mx-auto max-w-4xl text-5xl font-medium tracking-tight text-slate-900 max-[300px]:text-4xl sm:text-7xl"
+			>
+				{$L.landing.hero.titlePrefix()}
+				<br />
+				<span class="relative whitespace-nowrap text-blue-600">
+					<UnderlineDecoration />
+					<span class="relative"> {$L.landing.hero.titleHighlight()}</span>
+				</span>
+				{$L.landing.hero.titleSuffix()}
+			</h1>
 
-		<p class="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">
-			{$L.landing.hero.subtitle()}
-			{$L.landing.hero.description()}
-		</p>
+			<p
+				class="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700 lg:hidden"
+			>
+				{$L.landing.hero.subtitle()}
+				{$L.landing.hero.description()}
+			</p>
+			<div class="hidden lg:block">
+				<HeroSubtitle />
+			</div>
+		</div>
+
 		<div class="flex justify-center gap-8">
 			<a
 				href="https://ojab1kn3vda.typeform.com/to/eet5fojG"
