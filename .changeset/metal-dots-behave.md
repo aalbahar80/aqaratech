ebb6240140d6d6a91b7a3f1b6099de2fa17aa879
nbe <aleisa.naser@gmail.com>
2023-05-24 23:24:44 +0300

    hide cta and secondary features EXE-76

---
+++ .changeset/metal-dots-behave.md
@@ -0,0 +1,5 @@
+---
+'@self/site': patch
+---
+
+hide cta and secondary feature EXE-76
--- packages/site/src/routes/[lang=lang]/+page.svelte
+++ packages/site/src/routes/[lang=lang]/+page.svelte
@@ -1,19 +1,17 @@
 <script lang="ts">
-	import CallToAction from '$lib/components/landing/CallToAction.svelte';
 	import Contact from '$lib/components/landing/Contact.svelte';
 	import Faqs from '$lib/components/landing/Faqs.svelte';
 	import Features from '$lib/components/landing/features/Features.svelte';
 	import Footer from '$lib/components/landing/Footer.svelte';
 	import Hero from '$lib/components/landing/Hero.svelte';
 	import Pricing from '$lib/components/landing/pricing/Pricing.svelte';
-	import SecondaryFeatures from '$lib/components/landing/secondary-feature/SecondaryFeatures.svelte';
 	import PopoverDivider from '$lib/components/popover/PopoverDivider.svelte';
 </script>
 
 <Hero />
 <PopoverDivider />
-<SecondaryFeatures />
-<CallToAction />
+<!-- <SecondaryFeatures /> -->
+<!-- <CallToAction /> -->
 <Features />
 <Pricing />
 <Faqs />
