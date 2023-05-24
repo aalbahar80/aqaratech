5063842dd59faa56575143276d7726de55b41602
nbe <aleisa.naser@gmail.com>
2023-05-24 18:49:12 +0300

    extract HeroSubtitle component

---
+++ packages/site/src/lib/components/landing/HeroSubtitle.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import L from '$i18n/i18n-svelte';
+</script>
+
+<div class="mx-auto max-w-2xl md:text-center">
+	<h2 class="text-3xl tracking-tight text-slate-900 sm:text-4xl">
+		{$L.landing.hero.subtitle()}
+	</h2>
+	<p class="mt-4 text-lg tracking-tight text-slate-700">
+		{$L.landing.hero.description()}
+	</p>
+</div>
--- packages/site/src/lib/components/landing/secondary-feature/SecondaryFeatures.svelte
+++ packages/site/src/lib/components/landing/secondary-feature/SecondaryFeatures.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import L from '$i18n/i18n-svelte';
+	import HeroSubtitle from '$lib/components/landing/HeroSubtitle.svelte';
 	import Container from '$lib/components/landing/secondary-feature/Container.svelte';
 	import { getSecondaryFeatures } from '$lib/components/landing/secondary-feature/features';
 	import FeaturesDesktop from '$lib/components/landing/secondary-feature/FeaturesDesktop.svelte';
@@ -14,14 +15,7 @@
 	class="pb-14 pt-20 sm:pb-20 sm:pt-32 lg:pb-32"
 >
 	<Container>
-		<div class="mx-auto max-w-2xl md:text-center">
-			<h2 class="text-3xl tracking-tight text-slate-900 sm:text-4xl">
-				{$L.landing.hero.subtitle()}
-			</h2>
-			<p class="mt-4 text-lg tracking-tight text-slate-700">
-				{$L.landing.hero.description()}
-			</p>
-		</div>
+		<HeroSubtitle />
 		<FeaturesMobile {secondaryFeatures} />
 		<FeaturesDesktop {secondaryFeatures} />
 	</Container>
