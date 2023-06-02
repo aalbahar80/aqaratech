f334cd8e8f219d460831f7d9cba3b41e8d54fc03
nbe <aleisa.naser@gmail.com>
2023-06-02 09:26:16 +0300

    fix: print layout quirks on chrome for details pane

--- packages/site/src/lib/components/details-pane/DetailsPaneItem.svelte
+++ packages/site/src/lib/components/details-pane/DetailsPaneItem.svelte
@@ -12,15 +12,17 @@
 	export let label: string | undefined = undefined;
 </script>
 
+<!-- HACK: print: and sm: are duplicated because chrome refuses to apply the sm: to the print layout -->
 <div
-	class="px-4 py-5 first:rounded-t-md last:rounded-b-md odd:bg-gray-50 even:bg-white print:py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
+	class="px-4 py-5 first:rounded-t-md last:rounded-b-md odd:bg-gray-50 even:bg-white print:grid print:grid-cols-3 print:py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
 	data-testid={key}
 >
 	<dt class="text-sm font-medium text-gray-500">
 		{label ?? getIntlLabel(key)}
 	</dt>
+	<!-- HACK: don't specify a breakpoint selector on col-span-2, else chrome won't apply it on the print layout -->
 	<dd
-		class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0"
+		class="col-span-2 mt-1 text-sm text-gray-900 sm:mt-0"
 		data-testid={value ?? ''}
 	>
 		{#if key === 'mfPaymentId' && value && typeof value === 'string'}
