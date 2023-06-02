670685a54bd5e374d78cc54ea031e21b1ca2b435
nbe <aleisa.naser@gmail.com>
2023-06-02 19:34:21 +0300

    replace deprecated svelte types

--- packages/site/src/lib/models/types/icon.type.ts
+++ packages/site/src/lib/models/types/icon.type.ts
@@ -1,5 +1,4 @@
 import type { SvelteComponentTyped } from 'svelte';
+import type { SvelteHTMLElements } from 'svelte/elements';
 
-export type Icon = typeof SvelteComponentTyped<
-	svelte.JSX.IntrinsicElements['svg']
->;
+export type Icon = typeof SvelteComponentTyped<SvelteHTMLElements['svg']>;
