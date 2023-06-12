d4d1740b8e18cf2a4f85977d531c171255eadac2
nbe <aleisa.naser@gmail.com>
2023-06-12 06:44:51 +0300

    drop all requests to source maps

--- packages/site/src/hooks/misc.ts
+++ packages/site/src/hooks/misc.ts
@@ -1,12 +1,22 @@
+import { isLiveEnv } from '@self/utils';
+
 import type { Handle } from '@sveltejs/kit';
 
 import { environment } from '$aqenvironment';
 
-// TODO: drop all js.map requests early?
-
 export const handleMisc = (async ({ event, resolve }) => {
 	// if (event.url.pathname.startsWith('/api/')) {
 	// 	return fetch(event.request);
+
+	// drop all requests to source maps
+	if (
+		event.url.pathname.endsWith('js.map') &&
+		isLiveEnv(environment.PUBLIC_AQARATECH_ENV)
+	) {
+		return new Response(null, {
+			status: 404,
+		});
+	}
 
 	if (environment.PUBLIC_AQARATECH_ENV !== 'production') {
 		event.setHeaders({
