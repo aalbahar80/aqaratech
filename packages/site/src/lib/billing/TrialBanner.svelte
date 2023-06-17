9ce47cb9770766a65114b4bb0e084287f7454408
nbe <aleisa.naser@gmail.com>
2023-06-17 20:23:06 +0300

    (site) remove unused files

--- packages/site/src/lib/billing/TrialBanner.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import Banner from '$lib/components/navbar/Banner.svelte';
-</script>
-
-<!-- FIX: refine props -->
-<Banner
-	message={`Your free trial will expire in ${20} days.`}
-	buttonText="Manage Subscription"
-/>
--- packages/site/src/lib/components/leaseInvoice/pay-url.ts
+++
@@ -1,4 +0,0 @@
-import { environment } from '$lib/environment';
-
-export const getPayURL = (id: string) =>
-	`${environment.PUBLIC_API_URL}/leaseInvoices/${id}/pay`;
