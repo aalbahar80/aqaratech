7bb3f65a9fd65a82d94b59fd9764705a6bab4f5f
Naser Aleisa <aleisa.naser@gmail.com>
2024-05-10 00:30:26 +0300

    chore: update flake inputs

--- flake.nix
+++ flake.nix
@@ -1,25 +1,24 @@
-# Example: https://github.com/pimeys/nix-prisma-example/tree/main
-# Might need to run `nix flake init`, then replace contents of generated file
 {
   description = "Aqaratech dev environment";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs-prisma.url = "github:thenbe/nixpkgs/prisma-4.15.0";
+  inputs.nixpkgs-prisma.url = "github:thenbe/nixpkgs/prisma-4.15.0"; # prisma v4.15.0
 
-  outputs = {
-    self,
-    nixpkgs,
-    nixpkgs-prisma,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , nixpkgs-prisma
+    }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
       pkgs = nixpkgs.legacyPackages.${system};
       # Define some packages here to easily switch between versions
       inherit (nixpkgs-prisma.legacyPackages.${system}) prisma-engines;
       inherit (nixpkgs-prisma.legacyPackages.${system}.nodePackages) prisma;
 
       # Tier binary package
-      tier = {pkgs, ...}:
+      tier = { pkgs, ... }:
         pkgs.stdenv.mkDerivation rec {
           pname = "tier";
           version = "0.11.1";
@@ -47,23 +46,24 @@
             homepage = "https://github.com/tierrun/tier";
           };
         };
-    in {
+    in
+    {
       devShell = pkgs.mkShell {
-        nativeBuildInputs = [pkgs.bashInteractive];
+        nativeBuildInputs = [ pkgs.bashInteractive ];
         buildInputs = with pkgs; [
           prisma # npm binary doesn't work on nixOS
           openssl # otherwise prisma will complain about missing openssl
           go-task # version installed by pnpm is nowhere to be found on nixOS
           zulu # java for openapi-generator-cli
           # openapi-generator-cli # npm binary works on nixOS
-          (tier {inherit pkgs;})
+          (tier { inherit pkgs; })
         ];
         shellHook = ''
-          export PRISMA_MIGRATION_ENGINE_BINARY="${prisma-engines}/bin/migration-engine"
-          export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
-          export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
-          export PRISMA_INTROSPECTION_ENGINE_BINARY="${prisma-engines}/bin/introspection-engine"
-          export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
+          export PRISMA_MIGRATION_ENGINE_BINARY=${prisma-engines}/bin/migration-engine
+          export PRISMA_QUERY_ENGINE_BINARY=${prisma-engines}/bin/query-engine
+          export PRISMA_QUERY_ENGINE_LIBRARY=${prisma-engines}/lib/libquery_engine.node
+          export PRISMA_INTROSPECTION_ENGINE_BINARY=${prisma-engines}/bin/introspection-engine
+          export PRISMA_FMT_BINARY=${prisma-engines}/bin/prisma-fmt
 
           # Delete the binary in node_modules/.bin to make sure we always use the one from nix
           # NOTE: might need to reload nix develop whenever we run `pnpm install`
